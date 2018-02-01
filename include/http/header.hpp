fa54a6692d972c132ce112350bab030f5fca84e8
Troels F. Roennow <troels@wonop.com>
2018-02-01 09:39:00 +0000

    Adding mime types

--- include/http/header.hpp
+++ include/http/header.hpp
@@ -1,11 +1,31 @@
 #ifndef HTTP_HEADER_HPP
 #define HTTP_HEADER_HPP
+#include<vector>
 namespace fetch {
 namespace http {
 
-class Header 
+struct HeaderEntry
 {
+  std::string name;
+  std::string value;
+};
 
+  
+class Header : private std::vector< HeaderEntry >
+{
+public:
+  typedef typename std::vector< HeaderEntry >::iterator iterator;
+  typedef typename std::vector< HeaderEntry >::const_iterator const_iterator;
+  void Add(std::string const& name, std::string const &value) {
+    push_back({name, value });
+  }
+  
+  iterator begin() noexcept { return std::vector< HeaderEntry >::begin(); }
+  iterator end() noexcept { return std::vector< HeaderEntry >::end(); }
+  const_iterator begin() const noexcept { return std::vector< HeaderEntry >::begin(); }
+  const_iterator end() const noexcept { return std::vector< HeaderEntry >::end(); }
+  const_iterator cbegin() const noexcept { return std::vector< HeaderEntry >::cbegin(); }
+  const_iterator cend() const noexcept { return std::vector< HeaderEntry >::cend(); }      
 };
 
 };
---
+++ include/http/request.hpp
@@ -0,0 +1,15 @@
+#ifndef HTTP_REQUEST_HPP
+#define HTTP_REQUEST_HPP
+#include"http/header.hpp"
+#include"http/status.hpp"
+
+namespace fetch
+{
+namespace http
+{
+
+
+};
+};
+
+$endif
