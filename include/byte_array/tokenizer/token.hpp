c93e1b1e8f65982b713560dafe10320add7296cf
Troels F. Roennow <troels.roennow@fetch.ai>
2018-04-09 14:46:59 +0100

    Minor tweaks to the tokenizer

--- include/byte_array/tokenizer/token.hpp
+++ include/byte_array/tokenizer/token.hpp
@@ -14,6 +14,7 @@ class Token : public ConstByteArray {
   Token(std::string const &str) : ConstByteArray(str.c_str()) {}
 
   Token(ConstByteArray const &other) : ConstByteArray(other) {}
+  Token(ConstByteArray &&other) : ConstByteArray(other) {}  
 
   Token(ConstByteArray const &other, std::size_t const &start,
         std::size_t const &length)
@@ -22,18 +23,16 @@ class Token : public ConstByteArray {
   void SetType(std::size_t const &t) { type_ = t; }
   void SetLine(std::size_t const &l) { line_ = l; }
   void SetChar(std::size_t const &c) { char_ = c; }
-  void SetFilename(ConstByteArray filename) { filename_ = filename; }
 
   std::size_t type() const { return type_; }
   std::size_t line() const { return line_; }
   std::size_t character() const { return char_; }
-  ConstByteArray filename() const { return filename_; }
+
 
  private:
   std::size_t type_ = -1;
   std::size_t line_ = 0;
   std::size_t char_ = 0;
-  ConstByteArray filename_;
 };
 };
 };
--- tests/byte_array/tokenizer/tokenizer.cpp
+++ tests/byte_array/tokenizer/tokenizer.cpp
@@ -1,7 +1,190 @@
 #include<iostream>
 #include<byte_array/tokenizer/tokenizer.hpp>
 #include<byte_array/consumers.hpp>
+#include <string>
+#include <fstream>
+#include <streambuf>
+
 using namespace fetch::byte_array;
+
+int main(int argc, char **argv) 
+{
+  Tokenizer test;
+  enum {
+    E_TOKEN = 0,    
+    E_BYTE_ARRAY = 1,
+    E_OPERATOR = 2,
+    E_WHITESPACE = 3,
+    E_INTEGER = 4,
+    E_FLOATING_POINT = 5,
+    E_KEYWORD = 6,        
+    E_CATCH_ALL = 7
+  };
+
+/*
+[](ConstByteArray const &str, uint64_t &pos) {
+      uint64_t oldpos = pos;
+      uint64_t N = pos + 1;
+      if ((N < str.size()) && (str[pos] == '-') && ('0' <= str[N]) &&
+        (str[N] <= '9'))
+        pos += 2;
+      
+      while ((pos < str.size()) && ('0' <= str[pos]) && (str[pos] <= '9')) ++pos;
+      if(pos != oldpos) {
+        int ret = int(E_INTEGER); 
+
+        if((pos < str.size()) && (str[pos] == '.')) {
+          ++pos;
+          ret = int(E_FLOATING_POINT);
+          while ((pos < str.size()) && ('0' <= str[pos]) && (str[pos] <= '9')) ++pos;          
+        }
+        
+        if((pos < str.size()) && ((str[pos] == 'e') || (str[pos] == 'f') || (str[pos] == 'E') || (str[pos] == 'F') ) ) {
+          uint64_t rev = 1;          
+          ++pos;
+
+          if((pos < str.size()) && ((str[pos] == '-') || (str[pos] == '+' ) ) ) {
+            ++pos;
+            ++rev;
+          }
+
+          oldpos = pos;
+          while ((pos < str.size()) && ('0' <= str[pos]) && (str[pos] <= '9')) ++pos;
+          if( oldpos == pos ) {
+            pos -= rev;
+          }       
+          ret = int(E_FLOATING_POINT);          
+        }
+        
+        return ret;
+      }
+            
+      return -1; 
+      }*/  
+
+   /*
+   test.AddConsumer([](ConstByteArray const &str, uint64_t &pos) -> int {
+       uint64_t oldpos = pos;
+       
+       while ((pos < str.size()) && ('0' <= str[pos]) && (str[pos] <= '9')) ++pos;
+       
+       if(pos != oldpos) {
+         int ret =E_INTEGER; 
+         
+         if((pos < str.size()) && (str[pos] == '.')) {
+          ++pos;
+          ret = int(E_FLOATING_POINT);
+          while ((pos < str.size()) && ('0' <= str[pos]) && (str[pos] <= '9')) ++pos;          
+         }        
+         return ret;
+       }
+       
+       return -1; 
+     });
+   */
+   test.AddConsumer([](ConstByteArray const &str, uint64_t &pos) -> int {
+       switch( str[pos] ) {
+       case '*':
+       case '/':         
+       case '-':
+       case '+':
+       case ':':
+       case '=':         
+         ++pos;
+         return E_OPERATOR;
+       }
+       
+                    
+       return -1; 
+     });
+   
+   
+
+   
+
+   test.AddConsumer([](ConstByteArray const &str, uint64_t &pos) -> int{
+       static std::vector< ConstByteArray > keywords = {
+         "if", "then", "begin", "end", "procedure", "function", "program", 
+       };
+
+       for(auto const &k: keywords) {
+         if(str.Match(k, pos)) {
+           pos += k.size();           
+           return int(E_KEYWORD);
+         }
+       }
+       
+       return -1; 
+     });   
+
+
+   test.AddConsumer([](ConstByteArray const &str, uint64_t &pos) -> int{
+       uint64_t oldpos = pos;       
+       while ((('a' <= str[pos]) && (str[pos] <= 'z')) ||
+         (('A' <= str[pos]) && (str[pos] <= 'Z')) || (str[pos] == '\''))
+         ++pos;
+       if(oldpos == pos) return -1;
+       return E_TOKEN;
+     });
+
+  test.AddConsumer([](ConstByteArray const &str, uint64_t &pos) {
+      uint64_t oldpos = pos;
+      while ((pos < str.size()) && ((str[pos] == ' ') || (str[pos] == '\n') ||
+          (str[pos] == '\r') || (str[pos] == '\t')))
+        ++pos;
+      
+      if(pos == oldpos) return -1;           
+      return int(E_WHITESPACE);      
+    });
+
+  /*
+  test.AddConsumer([](ConstByteArray const &str, uint64_t &pos) {
+      ++pos;      
+      return int(E_CATCH_ALL);      
+    });
+  */
+
+  
+//  test.Parse("test.file", R"(232    332e- 32.15e 32.3e-2.)" );
+  FILE * pFile;
+  long lSize;
+  size_t result;
+
+  for(std::size_t i = 0; i <  1; ++i) {
+    test.clear();
+
+    ByteArray data;
+    pFile = fopen ( argv[1] , "r" );    
+    if (pFile==NULL) {fputs ("File error",stderr); exit (1);}
+    
+    // obtain file size:
+    fseek (pFile , 0 , SEEK_END);
+    lSize = ftell (pFile);
+    rewind (pFile);    
+    data.Resize(lSize);
+    result = fread (data.pointer(),1,lSize,pFile);
+    fclose(pFile);
+    
+    
+    
+    test.Parse(data);
+  }
+  
+  std::cout << argv[1] << std::endl;
+  
+  std::cout << test.size() << std::endl;
+  
+  /*
+  for(auto &t : test) {
+    std::cout << t.filename() << " line " << t.line() << ", char " << t.character() << std::endl;
+    std::cout << t.type() << " " << t.size() << " " << t << std::endl;
+  } 
+  */ 
+  return 0;
+  
+}
+
+/*
 void test1() {
   Tokenizer test;
   enum {
@@ -23,7 +206,7 @@ void test1() {
   test.Parse("test.file", R"({
   "x": function helloworld() { var x = 2 * 2; x+=1; }
 }
-)" );
+)" ); 
 
   for(auto &t : test) {
     std::cout << t.filename() << " line " << t.line() << ", char " << t.character() << std::endl;
@@ -50,12 +233,7 @@ int main() {
     B_CATCH_ALL = 16    
   };
 
-  /*
-  test.AddConsumer(B_CONTEXT_START, consumers::Keyword("{"));
-  test.AddConsumer(B_CONTEXT_START, consumers::Keyword("{"), "function");
-  test.AddConsumer(B_CONTEXT_END, consumers::Keyword("}"), "function");    
-  test.CreateTokenSpace("function", B_CONTEXT_START, B_CONTEXT_START, B_CONTEXT_END);
-  */
+
   test.CreateSubspace("function", B_CONTEXT_START, "{", B_CONTEXT_END, "}");
   
   test.AddConsumer(A_TOKEN, consumers::AlphaNumericLetterFirst);
@@ -84,3 +262,4 @@ Noun+ Verb { a + {b} } Noun + Verb
   
   return 0;
 }
+*/
