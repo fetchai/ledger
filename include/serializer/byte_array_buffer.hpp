7504bc17a4593f3715e70cf31cede4e75633423a
nhutton <nathan.cameron.hutton@gmail.com>
2018-05-02 14:21:16 +0100

    Do not put original byte array buffer fix in

--- include/serializer/byte_array_buffer.hpp
+++ include/serializer/byte_array_buffer.hpp
@@ -38,7 +38,7 @@ class ByteArrayBuffer {
   void SkipBytes(std::size_t const &size) { pos_ += size; }
 
   template <typename T>
-  ByteArrayBuffer &operator<<(T val) {
+  ByteArrayBuffer &operator<<(T const &val) {
     Serialize(*this, val);
     return *this;
   }
@@ -50,7 +50,7 @@ class ByteArrayBuffer {
   }
 
   template <typename T>
-  ByteArrayBuffer &Pack(T val) {
+  ByteArrayBuffer &Pack(T const &val) {
     return this->operator<<(val);
   }
 
