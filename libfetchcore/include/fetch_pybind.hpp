8b218632834bf99981c345c4fc8474f61f075fa0
Troels F. Roennow <troels@wonop.com>
2018-05-03 21:37:53 +0100

    Removing pybind warning

--- libfetchcore/include/fetch_pybind.hpp
+++ libfetchcore/include/fetch_pybind.hpp
@@ -2,12 +2,20 @@
 #pragma GCC diagnostic ignored "-Wconversion"
 #pragma GCC diagnostic ignored "-Wpedantic"
 #pragma GCC diagnostic ignored "-Wall"
+#pragma GCC diagnostic ignored "-Wextra"
+#pragma GCC diagnostic ignored "-Wmany-braces-around-scalar-init"
+
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wconversion"
 #pragma clang diagnostic ignored "-Wpedantic"
+#pragma clang diagnostic ignored "-pedantic"
 #pragma clang diagnostic ignored "-Wall"
+#pragma clang diagnostic ignored "-Wextra"
+#pragma clang diagnostic ignored "-Wmany-braces-around-scalar-init"
 
-#pragma warning(push, 0)  
+
+#pragma warning(push, 0)
+
 #include <pybind11/pybind11.h>
 #include <pybind11/operators.h>
 #include <pybind11/numpy.h>
