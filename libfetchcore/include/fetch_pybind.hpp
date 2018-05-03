5d8519a506642162f23cb20b14f4c7718cb693d7
Troels F. Roennow <troels@wonop.com>
2018-05-03 20:58:16 +0100

    Disabling Wall for pybind

--- libfetchcore/include/fetch_pybind.hpp
+++ libfetchcore/include/fetch_pybind.hpp
@@ -1,11 +1,11 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wconversion"
 #pragma GCC diagnostic ignored "-Wpedantic"
-#pragma GCC diagnostic ignored "-Werror"
+#pragma GCC diagnostic ignored "-Wall"
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wconversion"
 #pragma clang diagnostic ignored "-Wpedantic"
-#pragma clang diagnostic ignored "-Werror"
+#pragma clang diagnostic ignored "-Wall"
 
 
 #include <pybind11/pybind11.h>
