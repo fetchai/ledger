26afbf13f2768b45b4d7183ad776d8eecc746d40
Peter Bukva <peter.bukva@fetch.ai>
2018-07-16 18:08:13 +0100

    Simplifying ECDSAPrivateKey class (fix for clang CRASH)

--- libs/crypto/include/crypto/openssl_ecdsa_private_key.hpp
+++ libs/crypto/include/crypto/openssl_ecdsa_private_key.hpp
@@ -9,17 +9,11 @@ namespace fetch {
 namespace crypto {
 namespace openssl {
 
-template<const int P_ECDSA_Curve_NID = NID_secp256k1
-       , template<typename T, const memory::eDeleteStrategy P_DeleteStrategy = memory::eDeleteStrategy::canonical, typename T_Deleter=memory::detail::OpenSSLDeleter<T, P_DeleteStrategy>> class T_OSSLSharedPtr = memory::ossl_shared_ptr>
+template<const int P_ECDSA_Curve_NID = NID_secp256k1>
 class ECDSAPrivateKey
 {
-public:
-    template<typename T, const memory::eDeleteStrategy P_DeleteStrategy = memory::eDeleteStrategy::canonical> 
-    using OSSLSharedPtr = T_OSSLSharedPtr<T, P_DeleteStrategy>;
-
-private:
-    const byte_array::ConstByteArray _key_data;
-    OSSLSharedPtr<EC_KEY> _key;
+    //const byte_array::ConstByteArray _key_data;
+    memory::ossl_shared_ptr<EC_KEY> _key;
 
 public:
     using ECDSACurveType = ECDSACurve<P_ECDSA_Curve_NID>;
@@ -27,17 +21,17 @@ public:
     ECDSAPrivateKey() = delete;
 
     ECDSAPrivateKey(const byte_array::ConstByteArray& key_data)
-        : _key_data(key_data)
-        , _key(EC_KEY_new_by_curve_name(NID_secp256k1)) {
+        :/* _key_data(key_data)
+        ,*/ _key(EC_KEY_new_by_curve_name(NID_secp256k1)) {
 
         assert(ECDSACurveType::privateKeySize == _key_data.size());
 
-        OSSLSharedPtr<BIGNUM> private_key_as_BN(BN_new());
-        BN_bin2bn(_key_data.pointer(), int(ECDSACurveType::privateKeySize), private_key_as_BN.get());
+        memory::ossl_shared_ptr<BIGNUM, memory::eDeleteStrategy::clearing> private_key_as_BN(BN_new());
+        BN_bin2bn(key_data.pointer(), int(ECDSACurveType::privateKeySize), private_key_as_BN.get());
         EC_KEY_set_private_key(_key.get(), private_key_as_BN.get());
     }
 
-    OSSLSharedPtr<const EC_KEY> get() const {
+    memory::ossl_shared_ptr<const EC_KEY> get() const {
         return _key;
     }
 
