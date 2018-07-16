0633560e549551121ad7f6810ed4d3f691f895cd
void* <2702771+pbukva@users.noreply.github.com>
2018-07-16 12:12:34 +0100

    OpenSSL resource management (memory & context) (#117)
    This change implement safe OpenSSL resource management for C++ environment.
    
    Specifically it implements general solution for memory and (openssl)context resorces exploiting C++ constructor/destructor concept, using standard C++ components designed for this purposes (std::unique_ptr<...> and std::shard_ptr<...>).
    
    NOTE: This change is precursor for further C++ code depending on the OpenSSL C library, located mostly in the fetch::crypto namepsace.

---
+++ libs/crypto/include/crypto/openssl_memory.hpp
@@ -0,0 +1,50 @@
+#ifndef CRYPTO_OPENSSL_MEMORY_HPP
+#define CRYPTO_OPENSSL_MEMORY_HPP
+
+#include "crypto/openssl_memory_detail.hpp"
+#include <memory>
+
+namespace fetch {
+namespace crypto {
+namespace openssl {
+namespace memory {
+
+    template <typename T
+            , const eDeleteStrategy P_DeleteStrategy = eDeleteStrategy::canonical
+            , typename T_Deleter = detail::OpenSSLDeleter<T, P_DeleteStrategy>>
+    using ossl_unique_ptr = std::unique_ptr<T, T_Deleter>;
+
+    template <typename T
+            , const eDeleteStrategy P_DeleteStrategy = eDeleteStrategy::canonical
+            , typename T_Deleter = detail::OpenSSLDeleter<T, P_DeleteStrategy>>
+    class ossl_shared_ptr : public std::shared_ptr<T>
+    {
+    public:
+        using Base = std::shared_ptr<T>;
+        using Deleter = T_Deleter;
+        static constexpr eDeleteStrategy deleteStrategy = P_DeleteStrategy;
+
+        using Base::Base;
+        using Base::reset;
+
+        template< class Y >
+        explicit ossl_shared_ptr( Y* ptr = nullptr) : Base(ptr, Deleter()) {
+        }
+
+        //void reset() noexcept {
+        //    Base::reset(nullptr, Deleter());
+        //}
+
+        template< class Y >
+        void reset( Y* ptr ) {
+            Base::reset(ptr, Deleter());
+        }
+    };
+
+} //* memory namespace
+} //* openssl namespace
+} //* crypto namespace
+} //* fetch namespace
+
+#endif //CRYPTO_OPENSSL_MEMORY_HPP
+
---
+++ libs/crypto/tests/gtests/ecdsa_test.cpp
@@ -0,0 +1,20 @@
+//#include "crypto/ecdsa.hpp"
+//#include "core/byte_array/encoders.hpp"
+
+#include "gtest/gtest.h"
+//#include "gmock/gmock.h"
+
+namespace fetch {
+namespace crypto {
+namespace openssl {
+namespace memory {
+
+namespace {
+
+} // namespace anonymous
+
+} // namespace memory
+} // namespace openssl
+} // namespace crypto
+} // namespace fetch
+
