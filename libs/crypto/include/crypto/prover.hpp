21ba54337ced4d220157a1ec395bbe75291cca01
nhutton <nathan.cameron.hutton@gmail.com>
2018-07-17 13:09:43 +0100

    remove accidental include

--- libs/crypto/include/crypto/prover.hpp
+++ libs/crypto/include/crypto/prover.hpp
@@ -1,18 +1,28 @@
 #ifndef CRYPTO_PROVER_HPP
 #define CRYPTO_PROVER_HPP
+
 #include "core/byte_array/byte_array.hpp"
+
 namespace fetch {
 namespace crypto {
+
 class Prover {
  public:
-  typedef byte_array::ByteArray byte_array_type;
+  using byte_array_type = byte_array::ByteArray;
 
   virtual void Load(byte_array_type const &) = 0;
   virtual bool Sign(byte_array_type const &text) = 0;
   virtual byte_array_type document_hash() = 0;
   virtual byte_array_type signature() = 0;
+  //virtual bool Sign(
+  //        byte_array_type const &private_key,
+  //        byte_array_type const &data_to_sign,
+  //        byte_array_type &signature,
+  //        byte_array_type &data_hash) = 0;
 };
+
 }
 }
 
 #endif
+
