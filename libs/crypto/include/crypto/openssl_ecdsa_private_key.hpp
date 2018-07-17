1844499de83037c17eaf45c49848aa8a8805066b
Peter Bukva <peter.bukva@fetch.ai>
2018-07-17 10:52:32 +0100

    WIP: Draft of setting pub. to priv. key to enable dependent openssl functionality

--- libs/crypto/include/crypto/openssl_ecdsa_private_key.hpp
+++ libs/crypto/include/crypto/openssl_ecdsa_private_key.hpp
@@ -4,6 +4,7 @@
 #include "core/byte_array/const_byte_array.hpp"
 #include "crypto/openssl_common.hpp"
 #include "crypto/openssl_memory.hpp"
+#include "crypto/openssl_context_session.hpp"
 
 namespace fetch {
 namespace crypto {
@@ -12,26 +13,68 @@ namespace openssl {
 template<const int P_ECDSA_Curve_NID = NID_secp256k1>
 class ECDSAPrivateKey
 {
-    //const byte_array::ConstByteArray _key_data;
-    memory::ossl_shared_ptr<EC_KEY> _key;
-
 public:
+    using eDelStrat = memory::eDeleteStrategy;
+    template <typename T
+            , const eDelStrat P_DeleteStrategy = eDelStrat::canonical>
+    using ShrdPtr = memory::ossl_shared_ptr<T, P_DeleteStrategy>;
     using ECDSACurveType = ECDSACurve<P_ECDSA_Curve_NID>;
-    
-    ECDSAPrivateKey() = delete;
 
-    ECDSAPrivateKey(const byte_array::ConstByteArray& key_data)
+private:
+    //const byte_array::ConstByteArray _key_data;
+    ShrdPtr<EC_KEY> _key;
+
+    static ShrdPtr<BIGNUM, eDelStrat::clearing> keyAsBN(const byte_array::ConstByteArray& key_data) {
+        if (ECDSACurveType::privateKeySize != key_data.size()) {
+            throw std::runtime_error("Lenght of provided byte array does not correspond to expected lenght for selected elliptic curve");
+        }
+
+        ShrdPtr<BIGNUM, eDelStrat::clearing> private_key_as_BN(BN_new());
+        BN_bin2bn(key_data.pointer(), int(ECDSACurveType::privateKeySize), private_key_as_BN.get());
+        return private_key_as_BN;
+    }
+
+    static ShrdPtr<BIGNUM, eDelStrat::clearing> keyAsBN(const std::string& hex_string_key_data) {
+        assert(ECDSACurveType::privateKeySize*2 == hex_string_key_data.size());
+        BIGNUM *private_key_as_BN = nullptr;
+        BN_hex2bn(&private_key_as_BN, hex_string_key_data.c_str());
+        return ShrdPtr<BIGNUM, eDelStrat::clearing>(private_key_as_BN);
+    }
+
+    ECDSAPrivateKey(ShrdPtr<BIGNUM, eDelStrat::clearing> private_key_as_BN)
         :/* _key_data(key_data)
         ,*/ _key(EC_KEY_new_by_curve_name(NID_secp256k1)) {
 
-        assert(ECDSACurveType::privateKeySize == _key_data.size());
+        const int res = EC_KEY_set_private_key(_key.get(), private_key_as_BN.get());
+        if (!res) {
+            throw std::runtime_error("EC_KEY_set_private_key(...) failed.");
+        }
 
-        memory::ossl_shared_ptr<BIGNUM, memory::eDeleteStrategy::clearing> private_key_as_BN(BN_new());
-        BN_bin2bn(key_data.pointer(), int(ECDSACurveType::privateKeySize), private_key_as_BN.get());
-        EC_KEY_set_private_key(_key.get(), private_key_as_BN.get());
+        const EC_GROUP *group = EC_KEY_get0_group(_key.get());
+        memory::ossl_unique_ptr<EC_POINT> public_key(EC_POINT_new(group));
+        context::Session<BN_CTX> session{ShrdPtr<BN_CTX>(BN_CTX_new())};
+        
+        if (!EC_POINT_mul(group, public_key.get(), private_key_as_BN.get(), NULL, NULL, session.context().get())) {
+            throw std::runtime_error("EC_POINT_mul(...) failed.");
+        }
+        
+        if (!EC_KEY_set_public_key(_key.get(), public_key.get())) {
+            throw std::runtime_error("EC_KEY_set_public_key(...) failed.");
+        }
     }
 
-    memory::ossl_shared_ptr<const EC_KEY> get() const {
+public:
+    ECDSAPrivateKey() = delete;
+
+    ECDSAPrivateKey(const byte_array::ConstByteArray& key_data)
+        : ECDSAPrivateKey(ECDSAPrivateKey::keyAsBN(key_data)) {
+    }
+
+    ECDSAPrivateKey(const std::string& hex_string_key_data)
+        : ECDSAPrivateKey(ECDSAPrivateKey::keyAsBN(hex_string_key_data)) {
+    }
+
+    ShrdPtr<EC_KEY> key() const {
         return _key;
     }
 
--- libs/crypto/tests/gtests/openssl_private_key_test.cpp
+++ libs/crypto/tests/gtests/openssl_private_key_test.cpp
@@ -1,5 +1,7 @@
 #include "crypto/openssl_ecdsa_private_key.hpp"
 
+#include "openssl/ec.h"
+
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
@@ -26,10 +28,41 @@ TEST_F(ECDCSAPrivateKeyTest, test_creation_of_private_key) {
         0x16, 0x26, 0x07, 0x83, 0xe4, 0x0b, 0x16, 0x73, 0x16, 0x73, 0x62,
         0x2a, 0xc8, 0xa5, 0xb0, 0x45, 0xfc, 0x3e, 0xa4, 0xaf, 0x70, 0xf7,
         0x27, 0xf3, 0xf9, 0xe9, 0x2b, 0xdd, 0x3a, 0x1d, 0xdc, 0x42};
+    std::string priv_key_hex_str = "16260783e40b16731673622ac8a5b045fc3ea4af70f727f3f9e92bdd3a1ddc42";
 
     //* Production code
     ECDSAPrivateKey<> x(priv_key_data);
-    EXPECT_TRUE(x.get());
+    EXPECT_TRUE(x.key());
+
+    ECDSAPrivateKey<> y(priv_key_hex_str);
+    EXPECT_TRUE(y.key());
+
+    std::cerr << "x.key().get() = " << x.key().get() << std::endl;
+    std::cerr << "x.key() len = " << i2d_ECPrivateKey(x.key().get(), NULL) << std::endl;
+    std::vector<unsigned char> x_out((std::size_t)i2d_ECPrivateKey(x.key().get(), NULL), 0);
+    std::cerr << "x_out = ";
+    for (auto itm : x_out) {
+        std::cerr << itm << ", ";
+    }
+    std::cerr << std::endl;
+
+    unsigned char *_x_out = &x_out.front();
+    if(!i2d_ECPrivateKey(x.key().get(), &_x_out)) {
+    //    throw std::runtime_error("Error: i2d_ECPrivateKey(x, ..) fails.");
+    }
+    std::cerr << "x_out(after) = ";
+    for (auto itm : x_out) {
+        std::cerr << itm << ", ";
+    }
+    std::cerr << std::endl;
+
+    std::vector<unsigned char> y_out((std::size_t)i2d_ECPrivateKey(y.key().get(), NULL), 0);
+    unsigned char *_y_out = &y_out.front();
+    if(!i2d_ECPrivateKey(y.key().get(), &_y_out)) {
+    //    throw std::runtime_error("Error: i2d_ECPrivateKey(y, ..) fails.");
+    }
+
+    EXPECT_EQ(x_out, y_out);
 }
 
 //TEST_F(ECDCSAPrivateKeyTest, test_Deleter_called_after_construction) {
