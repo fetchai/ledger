3a63129db1f2fed58f2897976f093700f2387db7
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2018-07-25 15:35:44 +0100

    Fix thread saftey issue

--- libs/ledger/tests/chaincode/fake_storage_unit.hpp
+++ libs/ledger/tests/chaincode/fake_storage_unit.hpp
@@ -5,6 +5,7 @@
 #include "crypto/sha256.hpp"
 #include "ledger/storage_unit/storage_unit_interface.hpp"
 
+#include <mutex>
 #include <vector>
 #include <algorithm>
 #include <unordered_set>
@@ -16,8 +17,11 @@ public:
   using state_store_type = std::unordered_map<fetch::byte_array::ConstByteArray, fetch::byte_array::ConstByteArray, fetch::crypto::CallableFNV>;
   using state_archive_type = std::unordered_map<bookmark_type, state_store_type>;
   using lock_store_type = std::unordered_set<fetch::byte_array::ConstByteArray, fetch::crypto::CallableFNV>;
+  using mutex_type = std::mutex;
+  using lock_guard_type = std::lock_guard<mutex_type>;
 
   document_type GetOrCreate(fetch::byte_array::ConstByteArray const &key) override {
+    lock_guard_type lock(mutex_);
     document_type doc;
 
     auto it = state_.find(key);
@@ -30,8 +34,8 @@ public:
     return doc;
   }
 
-
   document_type Get(fetch::byte_array::ConstByteArray const &key) override {
+    lock_guard_type lock(mutex_);
     document_type doc;
 
     auto it = state_.find(key);
@@ -45,10 +49,12 @@ public:
   }
 
   void Set(fetch::byte_array::ConstByteArray const &key, fetch::byte_array::ConstByteArray const &value) override {
+    lock_guard_type lock(mutex_);
     state_[key] = value;
   }
 
   bool Lock(fetch::byte_array::ConstByteArray const &key) override {
+    lock_guard_type lock(mutex_);
     bool success = false;
 
     bool const already_locked = locks_.find(key) != locks_.end();
@@ -61,6 +67,7 @@ public:
   }
 
   bool Unlock(fetch::byte_array::ConstByteArray const &key) override {
+    lock_guard_type lock(mutex_);
     bool success = false;
 
     bool const already_locked = locks_.find(key) != locks_.end();
@@ -73,10 +80,12 @@ public:
   }
 
   void AddTransaction(fetch::chain::Transaction const &tx) override {
+    lock_guard_type lock(mutex_);
     transactions_[tx.digest()] = tx;
   }
 
   bool GetTransaction(fetch::byte_array::ConstByteArray const &digest, fetch::chain::Transaction &tx) override {
+    lock_guard_type lock(mutex_);
     bool success = false;
 
     auto it = transactions_.find(digest);
@@ -89,6 +98,7 @@ public:
   }
 
   hash_type Hash() override {
+    lock_guard_type lock(mutex_);
     fetch::crypto::SHA256 hasher{};
 
     std::vector<fetch::byte_array::ConstByteArray> keys;
@@ -109,10 +119,12 @@ public:
   }
 
   void Commit(bookmark_type const &bookmark) override {
+    lock_guard_type lock(mutex_);
     state_archive_[bookmark] = state_;
   }
 
   void Revert(bookmark_type const &bookmark) override {
+    lock_guard_type lock(mutex_);
     auto it = state_archive_.find(bookmark);
     if (it != state_archive_.end()) {
       state_ = it->second;
@@ -125,6 +137,7 @@ public:
 
 private:
 
+  mutex_type mutex_;
   transaction_store_type transactions_;
   state_store_type state_;
   lock_store_type locks_;
