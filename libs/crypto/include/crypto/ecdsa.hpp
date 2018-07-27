671f9b160971029bedd70a9f972b1b3ceebb3f64
Peter Bukva <peter.bukva@fetch.ai>
2018-07-27 15:30:34 +0100

    Plugging crypto openssl layer in to Prover & Verifier components

--- libs/crypto/include/crypto/ecdsa.hpp
+++ libs/crypto/include/crypto/ecdsa.hpp
@@ -1,13 +1,9 @@
 #pragma once
-#include "crypto/hash.hpp"
-#include "crypto/prover.hpp"
-#include "crypto/sha256.hpp"
-#include "crypto/verifier.hpp"
 
-#include <openssl/bn.h>
-#include <openssl/ec.h>
-#include <openssl/ecdsa.h>
-#include <openssl/obj_mac.h>
+#include "crypto/prover.hpp"
+#include "crypto/verifier.hpp"
+#include "crypto/ecdsa_signature.hpp"
+
 
 #include <core/assert.hpp>
 
@@ -16,214 +12,72 @@ namespace crypto {
 
 class ECDSAVerifier : public Verifier
 {
+  using PublicKey = openssl::ECDSAPublicKey<>;
+  using Signature = openssl::ECDSASignature<>;
+
 public:
-  ECDSAVerifier(Identity const &ident) : identity_(ident)
+  ECDSAVerifier(Identity const &ident)
+    : identity_{ident}
+    , public_key_{identity_.identifier()}
   {
-    uint8_t const *pub_bytes_copy;
-    key_           = EC_KEY_new_by_curve_name(NID_secp256k1);
-    pub_bytes_copy = identity_.identifier().pointer();
-
-    if (!o2i_ECPublicKey(&key_, &pub_bytes_copy, long(identity_.identifier().size())))
-    {
-      std::cout << identity_.identifier().size() << std::endl;
-
-      TODO_FAIL("Failed to set the public key.");
-    }
   }
 
   bool Verify(byte_array_type const &data, byte_array_type const &signature) override
   {
-    byte_array_type const hash = Hash<SHA256>(data);
-
-    // TODO: Verify sizes
-    int verify_status = ECDSA_verify(0, hash.pointer(), int(hash.size()), signature.pointer(),
-                                     int(signature.size()), key_);
-
-    return (1 == verify_status);
+    Signature sig {signature};
+    return sig.Verify(public_key_, data);
   }
 
   Identity identity() override { return identity_; }
 
 private:
   Identity identity_;
-  EC_KEY * key_ = nullptr;
+  PublicKey public_key_;
 };
 
 class ECDSASigner : public Prover
 {
-  void FreeMemory()
-  {
-    if (key_ != nullptr) EC_KEY_free(key_);
-    if (group_ != nullptr) EC_GROUP_clear_free(group_);
-    key_   = nullptr;
-    group_ = nullptr;
-  }
+  using PrivateKey = openssl::ECDSAPrivateKey<>;
+  using Signature = openssl::ECDSASignature<>;
 
+  using PrivateKeyPtr = std::unique_ptr<PrivateKey>; 
+  using SignaturePtr = std::unique_ptr<Signature>; 
 public:
-  enum
+  void Load(byte_array_type const &private_key) override
   {
-    PRIVATE_KEY_SIZE = 32
-  };
-
-  void Load(byte_array_type const &private_key) override { SetPrivateKey(private_key); }
+    SetPrivateKey(private_key);
+  }
 
   void SetPrivateKey(byte_array_type const &private_key)
   {
-    FreeMemory();
-
-    // Setting the private key
-    private_key_ = private_key;
-    key_         = EC_KEY_new_by_curve_name(NID_secp256k1);
-
-    BIGNUM *private_as_bn = BN_new();
-    assert(private_key.size() == 32);
-
-    BN_bin2bn(private_key.pointer(), int(private_key.size()), private_as_bn);
-    EC_KEY_set_private_key(key_, private_as_bn);
-
-    // Deriving the public key
-    BN_CTX *ctx = BN_CTX_new();
-    BN_CTX_start(ctx);
-
-    // Should be freed
-    EC_GROUP const *group      = EC_KEY_get0_group(key_);
-    EC_POINT *      public_key = EC_POINT_new(group);
-    EC_POINT_mul(group, public_key, private_as_bn, NULL, NULL, ctx);
-    EC_KEY_set_public_key(key_, public_key);
-
-    // Getting public key
-    //   EC_KEY_set_conv_form(key_, POINT_CONVERSION_COMPRESSED);
-    public_key_.Resize(std::size_t(i2o_ECPublicKey(key_, NULL)));
-
-    //    assert( public_key_.size() == 64 );
-    uint8_t *pub_copy = public_key_.pointer();
-    if (std::size_t(i2o_ECPublicKey(key_, &pub_copy)) != public_key_.size())
-    {
-      TODO_FAIL("Unable to decode public key");
-    }
-
-    EC_POINT_free(public_key);
-
-    BN_CTX_end(ctx);
-    BN_CTX_free(ctx);
-
-    BN_clear_free(private_as_bn);
+     private_key_ = std::make_unique<PrivateKey>(private_key); 
   }
 
   void GenerateKeys()
   {
-    FreeMemory();
-
-    const int set_group_success = 1;
-    const int gen_success       = 1;
-
-    key_ = EC_KEY_new();
-    if (key_ == nullptr)
-    {
-      TODO_FAIL("Failed to create new EC Key\n");
-    }
-
-    group_ = EC_GROUP_new_by_curve_name(NID_secp192k1);
-    if (group_ == nullptr)
-    {
-      TODO_FAIL("Failed to create new EC Group\n");
-    }
-
-    int set_group_status = EC_KEY_set_group(key_, group_);
-
-    if (set_group_success != set_group_status)
-    {
-      TODO_FAIL("Failed to set group for EC Key\n");
-    }
-
-    int gen_status = EC_KEY_generate_key(key_);
-    if (gen_success != gen_status)
-    {
-      TODO_FAIL("Failed to generate EC Key\n");
-    }
-
-    public_key_.Resize(std::size_t(i2o_ECPublicKey(key_, NULL)));
-    uint8_t *pub_copy = public_key_.pointer();
-    if (std::size_t(i2o_ECPublicKey(key_, &pub_copy)) != public_key_.size())
-    {
-      TODO_FAIL("Unable to decode public key");
-    }
+     private_key_ = std::make_unique<PrivateKey>();
   }
 
   bool Sign(byte_array_type const &text) final override
   {
-    hash_ = Hash<SHA256>(text);
-    signature_.Resize(std::size_t(ECDSA_size(key_)));
-    uint32_t len = uint32_t(signature_.size());
-    int ret = ECDSA_sign(0, hash_.pointer(), int(hash_.size()), signature_.pointer(), &len, key_);
-    return (ret != 1);
+   signature_ = std::make_unique<Signature>(Signature::Sign(*private_key_, text));
+   return true;
   }
 
-  Identity identity() final override { return Identity("ECDSA_NID_secp192k1", public_key_); }
+  Identity identity() final override
+  { 
+    return Identity(PrivateKey::ecdsa_curve_type::sn, public_key());
+  }
 
-  byte_array_type document_hash() final override { return hash_; }
-
-  byte_array_type signature() final override { return signature_; }
-  byte_array_type public_key() { return public_key_; }
-  byte_array_type private_key() { return private_key_; }
+  byte_array_type document_hash() final override { return signature_->hash(); }
+  byte_array_type signature() final override { return signature_->signature(); }
+  byte_array_type public_key() { return private_key_->publicKey().keyAsBin(); }
+  byte_array_type private_key() { return private_key_->KeyAsBin();}
 
 private:
-  byte_array_type hash_;
-  byte_array_type private_key_;
-  byte_array_type public_key_;
-  EC_KEY *        key_ = nullptr;
-  byte_array_type signature_;
-  EC_GROUP *      group_ = nullptr;
-
-  EC_KEY *bbp_ec_new_keypair(const uint8_t *priv_bytes)
-  {
-    EC_KEY *        key;
-    BIGNUM *        priv;
-    BN_CTX *        ctx;
-    const EC_GROUP *group;
-    EC_POINT *      pub;
-
-    /* init empty OpenSSL EC keypair */
-
-    key = EC_KEY_new_by_curve_name(NID_secp256k1);
-
-    /* set private key through BIGNUM */
-
-    priv = BN_new();
-    BN_bin2bn(priv_bytes, 32, priv);
-    EC_KEY_set_private_key(key, priv);
-
-    /* derive public key from private key and group */
-
-    ctx = BN_CTX_new();
-    BN_CTX_start(ctx);
-
-    group = EC_KEY_get0_group(key);
-    pub   = EC_POINT_new(group);
-    EC_POINT_mul(group, pub, priv, NULL, NULL, ctx);
-    EC_KEY_set_public_key(key, pub);
-
-    /* release resources */
-
-    EC_POINT_free(pub);
-    BN_CTX_end(ctx);
-    BN_CTX_free(ctx);
-    BN_clear_free(priv);
-
-    return key;
-  }
-
-  EC_KEY *bbp_ec_new_pubkey(const uint8_t *pub_bytes, int pub_len)
-  {
-    EC_KEY *       key;
-    const uint8_t *pub_bytes_copy;
-
-    key            = EC_KEY_new_by_curve_name(NID_secp256k1);
-    pub_bytes_copy = pub_bytes;
-    o2i_ECPublicKey(&key, &pub_bytes_copy, pub_len);
-
-    return key;
-  }
+  PrivateKeyPtr private_key_;
+  SignaturePtr signature_;
 };
+
 }  // namespace crypto
 }  // namespace fetch
