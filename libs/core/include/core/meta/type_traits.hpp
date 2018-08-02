bae0ed1dd772f06225ce28c1834a078286d69054
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2018-08-02 16:08:11 +0100

    Fix style updates

--- libs/core/include/core/meta/type_traits.hpp
+++ libs/core/include/core/meta/type_traits.hpp
@@ -5,22 +5,22 @@
 namespace fetch {
 namespace meta {
 
-template<bool C, typename R = void>
+template <bool C, typename R = void>
 using EnableIf = typename std::enable_if<C, R>::type;
 
-template<typename T, typename R = T>
+template <typename T, typename R = T>
 using IfIsIntegerLike = EnableIf<(!std::is_same<T, bool>::value) && std::is_integral<T>::value, R>;
 
-template<typename T, typename R = T>
+template <typename T, typename R = T>
 using IfIsFloatLike = EnableIf<std::is_floating_point<T>::value, R>;
 
-template<typename T, typename R = T>
+template <typename T, typename R = T>
 using IfIsBooleanLike = EnableIf<std::is_same<T, bool>::value, R>;
 
-template<typename T, typename R = T>
+template <typename T, typename R = T>
 using IfIsByteArrayLike = EnableIf<std::is_same<T, byte_array::ByteArray>::value, R>;
 
-template<typename T, typename R = T>
+template <typename T, typename R = T>
 using IfIsStdStringLike = EnableIf<std::is_same<T, std::string>::value, R>;
 
 }  // namespace meta
