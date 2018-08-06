72960a5f26093f1662a1096d17f38242436214df
baykaner <khan.baykaner@fetch.ai>
2018-08-06 14:00:40 +0100

    ongoing work on sign kernel

--- libs/math/tests/math/kernels/sign/sign.cpp
+++ libs/math/tests/math/kernels/sign/sign.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 #include <gtest/gtest.h>
-#include "math/kernels/relu.hpp"
+#include "math/kernels/sign.hpp"
 #include "math/shape_less_array.hpp"
 
 
@@ -32,22 +32,30 @@ ShapeLessArray<data_type, container_type> RandomArrayPositive(std::size_t n, std
   return a1;
 }
 
+ShapeLessArray<data_type, container_type> RandomArrayZero(std::size_t n, std::size_t m)
+{
+  static fetch::random::LinearCongruentialGenerator gen;
+  ShapeLessArray<data_type, container_type> a1(n);
+  for (std::size_t i = 0; i < n; ++i)
+  {
+    a1.At(i) = data_type(0);
+  }
+  return a1;
+}
 
 TEST(ndarray, zeros_out) {
 
   std::size_t n = 1000;
-  ShapeLessArray<data_type, container_type> test_array = RandomArrayNegative(n, n);
+  ShapeLessArray<data_type, container_type> test_array = RandomArrayZero(n, n);
   ShapeLessArray<data_type, container_type> test_array_2 = RandomArrayNegative(n, n);
 
-  // sanity check that all values less than 0
+  // sanity check that all values equal 0
   for (std::size_t i = 0; i < n; ++i) {
-    ASSERT_TRUE(test_array[i] < 0);
+    ASSERT_TRUE(test_array[i] == 0);
   }
 
-  //
-  test_array_2.Relu(test_array);
-
-  // sanity check that all values less than 0
+  // check that sign(0) = 0
+  test_array_2.Sign(test_array);
   for (std::size_t i = 0; i < n; ++i) {
     ASSERT_TRUE(test_array_2[i] == 0);
   }
@@ -56,29 +64,27 @@ TEST(ndarray, zeros_out) {
 TEST(ndarray, negative_ones) {
 
   std::size_t n = 1000;
-  ShapeLessArray<data_type, container_type> test_array = RandomArrayPositive(n, n);
+  ShapeLessArray<data_type, container_type> test_array = RandomArrayNegative(n, n);
   ShapeLessArray<data_type, container_type> test_array_2 = RandomArrayPositive(n, n);
 
   // sanity check that all values less than 0
   for (std::size_t i = 0; i < n; ++i) {
-    ASSERT_TRUE(test_array[i] >= 0);
+    ASSERT_TRUE(test_array[i] <= 0);
   }
 
-  //
+  // check that sign(-) = -1
   test_array_2.Relu(test_array);
-
-  // sanity check that all values less than 0
   for (std::size_t i = 0; i < n; ++i) {
-    ASSERT_TRUE(test_array_2[i] == test_array[i]);
+    ASSERT_TRUE(test_array_2[i] == -1);
   }
 
 TEST(ndarray, positive_ones) {
 
   std::size_t n = 1000;
   ShapeLessArray<data_type, container_type> test_array = RandomArrayPositive(n, n);
-  ShapeLessArray<data_type, container_type> test_array_2 = RandomArrayPositive(n, n);
+  ShapeLessArray<data_type, container_type> test_array_2 = RandomArrayNegative(n, n);
 
-  // sanity check that all values less than 0
+  // sanity check that all values gt than 0
   for (std::size_t i = 0; i < n; ++i) {
     ASSERT_TRUE(test_array[i] >= 0);
   }
@@ -88,6 +94,6 @@ TEST(ndarray, positive_ones) {
 
   // sanity check that all values less than 0
   for (std::size_t i = 0; i < n; ++i) {
-    ASSERT_TRUE(test_array_2[i] == test_array[i]);
+    ASSERT_TRUE(test_array_2[i] == 1);
   }
 }
