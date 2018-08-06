fbe958b3ea008587f5487db05fe1a7d3d33381f1
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2018-08-06 16:52:53 +0100

    Minor improvements to the debug mutex

--- libs/core/include/core/macros.hpp
+++ libs/core/include/core/macros.hpp
@@ -7,3 +7,15 @@
 #else
 #define FETCH_MAYBE_UNUSED
 #endif
+
+#if defined(__clang__)
+#define FETCH_THREAD_ANNOTATION_ATTRIBUTE__(x)   __attribute__((x))
+#else
+#define FETCH_THREAD_ANNOTATION_ATTRIBUTE__(x)   // no-op
+#endif
+
+#define FETCH_GUARDED_BY(x) \
+  FETCH_THREAD_ANNOTATION_ATTRIBUTE__(guarded_by(x))
+
+#define FETCH_PTR_GUARDED_BY(x) \
+  FETCH_THREAD_ANNOTATION_ATTRIBUTE__(pt_guarded_by(x))
