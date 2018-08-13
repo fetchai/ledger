904a7be2e3a24862392ecb3d7be07415e5bcd0d3
baykaner <khan.baykaner@fetch.ai>
2018-08-13 10:57:08 +0100

    fixed and implemented new tests for ndarray view. refactored Compute Index methods

--- libs/math/tests/math/ndarray_view/ndarray_view.cpp
+++ libs/math/tests/math/ndarray_view/ndarray_view.cpp
@@ -1,4 +1,3 @@
-//#include <cmath>
 #include <iomanip>
 #include <iostream>
 
@@ -28,7 +27,39 @@ using _S = fetch::memory::SharedArray<D>;
 template <typename D>
 using _A = NDArray<D, _S<D>>;
 
-TEST(ndarray, get_small_view)
+TEST(ndarray, 2d_view_full)
+{
+  // set up a 4d array
+  std::vector<std::size_t> shape = {5, 5};
+  _A<double> test_array = _A<double>(shape);
+  for (std::size_t i = 0; i < (5*5); ++i)
+  {
+    test_array[i] = i;
+  }
+
+  // set up a valid view shape
+  std::vector<std::vector<std::size_t>> view_shape = {{0,5,1},{0,5,1}};
+  NDArrayView array_view = NDArrayView();
+  for (auto item : view_shape)
+  {
+    array_view.from.push_back(item[0]);
+    array_view.to.push_back(item[1]);
+    array_view.step.push_back(item[2]);
+  }
+
+  // extract the view shape into a new ndarray
+  std::vector<std::size_t> new_shape = {5, 5};
+  _A<double> new_array = _A<double>(new_shape);
+  new_array = test_array.GetRange(array_view);
+
+  for (std::size_t i = 0; i < new_array.data().size(); ++i)
+  {
+    ASSERT_TRUE(test_array[i] == new_array[i]);
+  }
+
+}
+
+TEST(ndarray, 3d_view_full)
 {
   // set up a 4d array
   std::vector<std::size_t> shape = {5, 5, 5};
@@ -60,18 +91,81 @@ TEST(ndarray, get_small_view)
 
 }
 
-TEST(ndarray, get_big_irregular_view)
+TEST(ndarray, 4d_view_full)
 {
   // set up a 4d array
-  std::vector<std::size_t> shape = {2, 7, 4, 3};
+  std::vector<std::size_t> shape = {5, 5, 5, 5};
   _A<double> test_array = _A<double>(shape);
-  for (std::size_t i = 0; i < (2*7*4*3); ++i)
+  for (std::size_t i = 0; i < (5*5*5*5); ++i)
   {
     test_array[i] = i;
   }
 
   // set up a valid view shape
-  std::vector<std::vector<std::size_t>> view_shape = {{0,2,1},{0,7,1},{0,4,1},{0,3,1}};
+  std::vector<std::vector<std::size_t>> view_shape = {{0,5,1},{0,5,1},{0,5,1},{0,5,1}};
+  NDArrayView array_view = NDArrayView();
+  for (auto item : view_shape)
+  {
+    array_view.from.push_back(item[0]);
+    array_view.to.push_back(item[1]);
+    array_view.step.push_back(item[2]);
+  }
+
+  // extract the view shape into a new ndarray
+  std::vector<std::size_t> new_shape = {5, 5, 5, 5};
+  _A<double> new_array = _A<double>(new_shape);
+  new_array = test_array.GetRange(array_view);
+
+  for (std::size_t i = 0; i < new_array.data().size(); ++i)
+  {
+    ASSERT_TRUE(test_array[i] == new_array[i]);
+  }
+}
+
+TEST(ndarray, 6d_view_full)
+{
+  // set up a 4d array
+  std::vector<std::size_t> shape = {5, 5, 5, 5, 5, 5};
+  _A<double> test_array = _A<double>(shape);
+  for (std::size_t i = 0; i < (5*5*5*5*5*5); ++i)
+  {
+    test_array[i] = i;
+  }
+
+  // set up a valid view shape
+  std::vector<std::vector<std::size_t>> view_shape = {{0,5,1},{0,5,1},{0,5,1},{0,5,1},{0,5,1},{0,5,1}};
+  NDArrayView array_view = NDArrayView();
+  for (auto item : view_shape)
+  {
+    array_view.from.push_back(item[0]);
+    array_view.to.push_back(item[1]);
+    array_view.step.push_back(item[2]);
+  }
+
+  // extract the view shape into a new ndarray
+  std::vector<std::size_t> new_shape = {5, 5, 5, 5, 5, 5};
+  _A<double> new_array = _A<double>(new_shape);
+  new_array = test_array.GetRange(array_view);
+
+  for (std::size_t i = 0; i < new_array.data().size(); ++i)
+  {
+    ASSERT_TRUE(test_array[i] == new_array[i]);
+  }
+
+}
+
+TEST(ndarray, 2d_irregular_view)
+{
+  // set up a 4d array
+  std::vector<std::size_t> shape = {5, 10};
+  _A<double> test_array = _A<double>(shape);
+  for (std::size_t i = 0; i < (5 * 10); ++i)
+  {
+    test_array[i] = i;
+  }
+
+  // set up a valid view shape
+  std::vector<std::vector<std::size_t>> view_shape = {{0, 5, 1},{0, 10, 1}};
   NDArrayView array_view = NDArrayView();
   for (auto item : view_shape)
   {
@@ -83,7 +177,80 @@ TEST(ndarray, get_big_irregular_view)
   std::cout << "array view set up" << std::endl;
 
   // extract the view shape into a new ndarray
-  std::vector<std::size_t> new_shape = {2, 7, 4, 3};
+  std::vector<std::size_t> new_shape = {5, 10};
+  _A<double> new_array = _A<double>(new_shape);
+  new_array = test_array.GetRange(array_view);
+
+  std::cout << "getrange complete " << std::endl;
+
+  for (std::size_t i = 0; i < new_array.data().size(); ++i)
+  {
+    ASSERT_TRUE(test_array[i] == new_array[i]);
+  }
+
+}
+
+
+TEST(ndarray, 3d_irregular_view)
+{
+  // set up a 4d array
+  std::vector<std::size_t> shape = {5, 10, 10};
+  _A<double> test_array = _A<double>(shape);
+  for (std::size_t i = 0; i < (5 * 10 * 10); ++i)
+  {
+    test_array[i] = i;
+  }
+
+  // set up a valid view shape
+  std::vector<std::vector<std::size_t>> view_shape = {{0, 5, 1}, {0, 10, 1}, {0, 10, 1}};
+  NDArrayView array_view = NDArrayView();
+  for (auto item : view_shape)
+  {
+    array_view.from.push_back(item[0]);
+    array_view.to.push_back(item[1]);
+    array_view.step.push_back(item[2]);
+  }
+
+  std::cout << "array view set up" << std::endl;
+
+  // extract the view shape into a new ndarray
+  std::vector<std::size_t> new_shape = {5, 10, 10};
+  _A<double> new_array = _A<double>(new_shape);
+  new_array = test_array.GetRange(array_view);
+
+  std::cout << "getrange complete " << std::endl;
+
+  for (std::size_t i = 0; i < new_array.data().size(); ++i)
+  {
+    ASSERT_TRUE(test_array[i] == new_array[i]);
+  }
+
+}
+
+TEST(ndarray, 6d_irregular_view)
+{
+  // set up a 4d array
+  std::vector<std::size_t> shape = {1, 2, 3, 4, 5, 6};
+  _A<double> test_array = _A<double>(shape);
+  for (std::size_t i = 0; i < (1 * 2 * 3 * 4 * 5 * 6); ++i)
+  {
+    test_array[i] = i;
+  }
+
+  // set up a valid view shape
+  std::vector<std::vector<std::size_t>> view_shape = {{0,1,1},{0,2,1},{0,3,1},{0,4,1},{0,5,1},{0,6,1}};
+  NDArrayView array_view = NDArrayView();
+  for (auto item : view_shape)
+  {
+    array_view.from.push_back(item[0]);
+    array_view.to.push_back(item[1]);
+    array_view.step.push_back(item[2]);
+  }
+
+  std::cout << "array view set up" << std::endl;
+
+  // extract the view shape into a new ndarray
+  std::vector<std::size_t> new_shape = {0, 1, 2, 3, 4, 5, 6};
   _A<double> new_array = _A<double>(new_shape);
   new_array = test_array.GetRange(array_view);
 
@@ -96,21 +263,65 @@ TEST(ndarray, get_big_irregular_view)
     // std::cout << std::endl;
     ASSERT_TRUE(test_array[i] == new_array[i]);
   }
-
 }
+
+
+
+TEST(ndarray, 2d_big_step)
+{
+  // set up a 4d array
+  std::vector<std::size_t> shape = {8, 8};
+  _A<double> test_array = _A<double>(shape);
+  for (std::size_t i = 0; i < (8 * 8); ++i)
+  {
+    test_array[i] = i;
+  }
+
+  // set up a valid view shape
+  std::vector<std::vector<std::size_t>> view_shape = {{0, 8, 4},{0, 8, 4}};
+  NDArrayView array_view = NDArrayView();
+  for (auto item : view_shape)
+  {
+    array_view.from.push_back(item[0]);
+    array_view.to.push_back(item[1]);
+    array_view.step.push_back(item[2]);
+  }
+
+  std::cout << "array view set up" << std::endl;
+
+  // extract the view shape into a new ndarray
+  std::vector<std::size_t> new_shape = {8, 8};
+  _A<double> new_array = _A<double>(new_shape);
+  new_array = test_array.GetRange(array_view);
+
+  std::cout << "getrange complete " << std::endl;
+
+  for (std::size_t i = 0; i < new_array.data().size(); ++i)
+  {
+    // std::cout << test_array[i] << std::endl;
+    // std::cout << new_array[i] << std::endl;
+    // std::cout << std::endl;
+    ASSERT_TRUE(test_array[i] == new_array[i]);
+  }
+}
+
+
+
+
 //
-//TEST(ndarray, big_step_view)
+//
+//TEST(ndarray, regular_subset_view)
 //{
 //  // set up a 4d array
-//  std::vector<std::size_t> shape = {8, 8};
+//  std::vector<std::size_t> shape = {6, 6, 6};
 //  _A<double> test_array = _A<double>(shape);
-//  for (std::size_t i = 0; i < (5*7*4); ++i)
+//  for (std::size_t i = 0; i < (6*6*6); ++i)
 //  {
 //    test_array[i] = i;
 //  }
 //
 //  // set up a valid view shape
-//  std::vector<std::vector<std::size_t>> view_shape = {{0,8,4},{0,8,4}};
+//  std::vector<std::vector<std::size_t>> view_shape = {{0,3,1},{0,3,1},{0,3,1}};
 //  NDArrayView array_view = NDArrayView();
 //  for (auto item : view_shape)
 //  {
@@ -122,7 +333,7 @@ TEST(ndarray, get_big_irregular_view)
 //  std::cout << "array view set up" << std::endl;
 //
 //  // extract the view shape into a new ndarray
-//  std::vector<std::size_t> new_shape = {8, 8};
+//  std::vector<std::size_t> new_shape = {3, 3, 3};
 //  _A<double> new_array = _A<double>(new_shape);
 //  new_array = test_array.GetRange(array_view);
 //
@@ -136,48 +347,9 @@ TEST(ndarray, get_big_irregular_view)
 //    ASSERT_TRUE(test_array[i] == new_array[i]);
 //  }
 //}
-
-
-TEST(ndarray, view_smaller_than_array)
-{
-  // set up a 4d array
-  std::vector<std::size_t> shape = {6, 6, 6};
-  _A<double> test_array = _A<double>(shape);
-  for (std::size_t i = 0; i < (6*6*6); ++i)
-  {
-    test_array[i] = i;
-  }
-
-  // set up a valid view shape
-  std::vector<std::vector<std::size_t>> view_shape = {{0,3,1},{0,3,1},{0,3,1}};
-  NDArrayView array_view = NDArrayView();
-  for (auto item : view_shape)
-  {
-    array_view.from.push_back(item[0]);
-    array_view.to.push_back(item[1]);
-    array_view.step.push_back(item[2]);
-  }
-
-  std::cout << "array view set up" << std::endl;
-
-  // extract the view shape into a new ndarray
-  std::vector<std::size_t> new_shape = {3, 3, 3};
-  _A<double> new_array = _A<double>(new_shape);
-  new_array = test_array.GetRange(array_view);
-
-  std::cout << "getrange complete " << std::endl;
-
-  for (std::size_t i = 0; i < new_array.data().size(); ++i)
-  {
-    // std::cout << test_array[i] << std::endl;
-    // std::cout << new_array[i] << std::endl;
-    // std::cout << std::endl;
-    ASSERT_TRUE(test_array[i] == new_array[i]);
-  }
-}
-
-
-
-
+//
+//
+//
+//
 
 
