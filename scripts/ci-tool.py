c578686425e41a0943ffbae531a4423d4bf5c9d7
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2018-08-20 16:41:55 +0100

    Update CI tool to generate junit test results

--- scripts/ci-tool.py
+++ scripts/ci-tool.py
@@ -7,11 +7,97 @@
 #
 
 import os
+import re
+import sys
 import argparse
 import subprocess
+import xml.etree.ElementTree as ET
 
 
 BUILD_TYPES = ('Debug', 'Release', 'RelWithDebInfo', 'MinSizeRel')
+
+
+def output(*args):
+    text = ' '.join(map(str, args))
+    if text != '':
+        sys.stdout.write(text)
+        sys.stdout.write('\n')
+        sys.stdout.flush()
+
+
+def parse_ctest_format(path):
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+    # loop through all of the tests
+    data = {}
+    for test in root.findall('./Testing/Test'):
+
+        # extract all the information required
+        test_name = test.find('./Name').text
+        test_path = test.find('./Path').text
+        execution_time = float(test.find("./Results/NamedMeasurement[@name='Execution Time']/Value").text)
+        status = test.attrib['Status']
+        if status != 'passed':
+            status = test.find("./Results/NamedMeasurement[@name='Exit Code']/Value").text.lower()
+
+        output = test.find("./Results/Measurement/Value").text
+
+        # extract the test library
+        match  = re.match(r'./libs/(\w+)/tests', test_path)
+        test_library = 'misc'
+        if match is not None:
+            test_library = match.group(1)
+
+        # store the data
+        testcases = data.get(test_library, [])
+        testcases.append((test_name, test_path, test_library, execution_time, status, output))
+        data[test_library] = testcases
+
+    return data
+
+
+def create_junit_format(output_path, data):
+    testsuite_names = data.keys()
+
+    testsuites = ET.Element("testsuites")
+
+    all_tests = 0
+    all_time = 0
+    all_failures = 0
+
+    for group in sorted(testsuite_names):
+        group_data = data[group]
+
+        group_size = len(group_data)
+        group_time = sum(map(lambda x: x[3], group_data))
+        group_failures = sum(map(lambda x: 0 if x[4] == 'passed' else 1, group_data))
+
+        all_tests += group_size
+        all_time += group_time
+        all_failures ++ group_failures
+
+        # get all the test data for this suite
+        testsuite = ET.SubElement(testsuites, "testsuite", id='0', name=group, tests=str(group_size), failures=str(group_failures), time=str(group_time))
+
+        # iterate through test cases
+        for name, path, _, time, status, output in group_data:
+            testcase = ET.SubElement(testsuite, "testcase", id='0', name=name, time=str(time))
+            if status != 'passed':
+
+                # extract more status
+                failure = ET.SubElement(testcase, 'failure', type=status, message="Test {}".format(status))
+                stdout = ET.SubElement(testcase, 'system-out').text = output
+
+    # update the final aggregations
+    testsuites.set('tests', str(all_tests))
+    testsuites.set('failures', str(all_failures))
+    testsuites.set('time', str(all_time))
+
+    # generate the output file
+    with open(output_path, 'w') as output_file:
+        output_file.write('<?xml version="1.0" encoding="UTF-8" ?>')
+        output_file.write(ET.tostring(testsuites, encoding="utf-8").decode())
 
 
 def build_type(text):
@@ -26,16 +112,17 @@ def parse_commandline():
     parser.add_argument('-p', '--build-path-prefix', default='build-', help='The prefix to be used for the naming of the build folder')
     parser.add_argument('-B', '--build', action='store_true', help='Build the project')
     parser.add_argument('-T', '--test', action='store_true', help='Test the project')
+    parser.add_argument('--force-build-folder', help='Specify the folder directly that should be used for the build / test')
     return parser.parse_args()
 
 
 def build_project(project_root, build_root, options):
-    print('Source.:', project_root)
-    print('Build..:', build_root)
-    print('Options:')
+    output('Source.:', project_root)
+    output('Build..:', build_root)
+    output('Options:')
     for key, value in options.items():
-        print(' - {} = {}'.format(key, value))
-    print('\n')
+        output(' - {} = {}'.format(key, value))
+    output('\n')
 
     # ensure the build directory exists
     os.makedirs(build_root, exist_ok=True)
@@ -44,17 +131,51 @@ def build_project(project_root, build_root, options):
     cmd = ['cmake']
     cmd += ['-D{}={}'.format(k, v) for k, v in options.items()]
     cmd += [project_root]
-    subprocess.check_call(cmd, cwd=build_root)
+    exit_code = subprocess.call(cmd, cwd=build_root)
+    if exit_code != 0:
+        output('Failed to configure cmake project')
+        sys.exit(exit_code)
 
     # make the project
-    subprocess.check_call(['make', '-j'], cwd=build_root)
+    #exit_code = subprocess.call(['make', '-j'], cwd=build_root)
+    exit_code = subprocess.call(['ninja'], cwd=build_root)
+    if exit_code != 0:
+        output('Failed to make the project')
+        sys.exit(exit_code)
 
 
 def test_project(build_root):
+    TEST_NAME = 'Test'
+
     if not os.path.isdir(build_root):
         raise RuntimeError('Build Root doesn\'t exist, unable to test project')
 
-    subprocess.check_call(['ctest', '--no-compress-output', '-T', 'Test'], cwd=build_root)
+    exit_code = subprocess.call(['ctest', '--no-compress-output', '-T', TEST_NAME], cwd=build_root)
+
+    # load the test format
+    test_tag_path = os.path.join(build_root, 'Testing', 'TAG')
+    if not os.path.isfile(test_tag_path):
+        output('Unable to detect ctest output path:', test_tag_path)
+        sys.exit(1)
+
+    # load the tag
+    tag_folder = open(test_tag_path, 'r').read().splitlines()[0]
+    tag_folder_path = os.path.join(build_root, 'Testing', tag_folder, '{}.xml'.format(TEST_NAME))
+
+    if not os.path.isfile(tag_folder_path):
+        output('Unable to locate CTest summary XML:', tag_folder_path)
+        sys.exit(1)
+
+    # parse the incoming ctest xml format
+    test_data = parse_ctest_format(tag_folder_path)
+
+    # convert the data to the Jenkins Junit
+    test_results = os.path.join(build_root, 'TestResults.xml')
+    create_junit_format(test_results, test_data)
+
+    if exit_code != 0:
+        output('Test unsuccessful')
+        sys.exit(exit_code)
 
 
 def main():
@@ -65,6 +186,8 @@ def main():
     # define all the build roots
     project_root = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
     build_root = os.path.join(project_root, '{}{}'.format(args.build_path_prefix, args.build_type.lower()))
+    if args.force_build_folder:
+        build_root = os.path.abspath(args.force_build_folder)
 
     options = {
         'CMAKE_BUILD_TYPE': args.build_type
