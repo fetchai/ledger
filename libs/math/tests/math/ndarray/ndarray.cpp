933f18a4d937dbdb54c8eab4bc65032f37e51c79
baykaner <khan.baykaner@fetch.ai>
2018-08-28 17:32:52 +0100

    updated licence

--- libs/math/tests/math/ndarray/ndarray.cpp
+++ libs/math/tests/math/ndarray/ndarray.cpp
@@ -15,6 +15,7 @@
 //   limitations under the License.
 //
 //------------------------------------------------------------------------------
+
 // #include <algorithm>
 #include <iomanip>
 #include <iostream>
