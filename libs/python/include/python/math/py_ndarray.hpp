1892d4bc35846ce6adac2c2ce89f5c063c77c3fa
baykaner <khan.baykaner@fetch.ai>
2018-09-03 13:41:15 +0100

    bugfix of compile error on mac

--- libs/python/include/python/math/py_ndarray.hpp
+++ libs/python/include/python/math/py_ndarray.hpp
@@ -530,7 +530,7 @@ void BuildNDArray(std::string const &custom_name, pybind11::module &module)
 
       .def("scatter",
            [](NDArray<T> &input_array, std::vector<T> &updates,
-              std::vector<std::uint64_t> &indices) {
+              std::vector<std::size_t> &indices) {
              fetch::math::Scatter(input_array, updates, indices);
            })
       .def("gather",
