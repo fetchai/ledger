9478aeeaf4646cb3b1a929aeba8f0bd8a8b69f70
baykaner <khan.baykaner@fetch.ai>
2018-09-03 14:08:34 +0100

    applied style

--- libs/python/include/python/math/py_ndarray.hpp
+++ libs/python/include/python/math/py_ndarray.hpp
@@ -529,8 +529,7 @@ void BuildNDArray(std::string const &custom_name, pybind11::module &module)
       .def("sign", [](NDArray<T> &a) { fetch::math::Sign(a); })
 
       .def("scatter",
-           [](NDArray<T> &input_array, std::vector<T> &updates,
-              std::vector<std::size_t> &indices) {
+           [](NDArray<T> &input_array, std::vector<T> &updates, std::vector<std::size_t> &indices) {
              fetch::math::Scatter(input_array, updates, indices);
            })
       .def("gather",
