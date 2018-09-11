4c56dde84ee60d8ba0a0703bd081d2cfab951e1e
baykaner <khan.baykaner@fetch.ai>
2018-09-11 12:34:08 +0100

    AST now handles basic arithmetic, decimal points, parenthesis and spaces

--- libs/math/include/math/computation_graph/computation_graph.hpp
+++ libs/math/include/math/computation_graph/computation_graph.hpp
@@ -18,13 +18,17 @@
 //------------------------------------------------------------------------------
 
 #include <iostream>       // std::cout
+#include <string>         // std::string
 #include <stack>          // std::stack
 #include <deque>          // std::deque
-#include <string>         // std::string
-#include <memory>
+#include <vector>          // std::vector
 
-//#include<stdio.h>
-//#include<stdlib.h>
+#include <algorithm>       // std::find_if
+#include <sstream>        // std::istringstream
+#include <iterator>       // std::istream_iterator
+#include <memory>
+#include <cassert>
+
 
 namespace fetch {
 namespace math {
@@ -43,8 +47,10 @@ static bool IsOperator(char &c)
       return true;
     case '/':
       return true;
-    case '%':
-      return true;
+//    case '%':
+//      return true;
+//    case '^':
+//      return true;
     default:
       return false;
   }
@@ -54,10 +60,12 @@ static bool IsOperator(char &c)
 enum OPERATOR_PRECEDENCE
 {
 //  PARENTHESIS = 10,
-  DIVIDE = 9,
-  MULTIPLY = 8,
-  ADD = 7,
-  SUBTRACT = 6,
+  MODULO = 10,
+  POWER = 9,
+  DIVIDE = 8,
+  MULTIPLY = 7,
+  ADD = 6,
+  SUBTRACT = 5,
   DEFAULT = -1
 };
 
@@ -73,6 +81,10 @@ static OPERATOR_PRECEDENCE GetPrecedence(char &c)
       return OPERATOR_PRECEDENCE::MULTIPLY;
     case '/':
       return OPERATOR_PRECEDENCE::DIVIDE;
+    case '%':
+      return OPERATOR_PRECEDENCE::MODULO;
+    case '^':
+      return OPERATOR_PRECEDENCE::POWER;
 //    case '(':
 //      return OPERATOR_PRECEDENCE::PARENTHESIS;
 //    case ')':
@@ -83,21 +95,21 @@ static OPERATOR_PRECEDENCE GetPrecedence(char &c)
 }
 }
 
-
-
- class ExpressionNode : public std::enable_shared_from_this<ExpressionNode>
+class ExpressionNode : public std::enable_shared_from_this<ExpressionNode>
 {
  public:
 
   std::string name;
 
-  // if this is a leaf node VAL will be a number, otherwise it will be the operation to apply on the child nodes
-  // when we compute the graph VAL will again be replaced by the result of the operation
+  // VAL is the value of this node on the graph
+  // During parsing val is assigned as either a leaf node (in which case VAL will be a number), otherwise it will be
+  // an operation to apply on child nodes where it will be stored as a char
+  // During Run the graph will be computed and VAL will be replaced by the result of the operations (as a number)
   union VAL
-   {
+  {
     char c;             // occupies 1 byte
     double val;         // occupies 8 bytes
-   };                      // the whole union occupies 4 bytes
+  };                      // the whole union occupies 4 bytes
 
   std::shared_ptr<ExpressionNode> left_node_ptr = nullptr;
   std::shared_ptr<ExpressionNode> right_node_ptr = nullptr;
@@ -133,18 +145,124 @@ static OPERATOR_PRECEDENCE GetPrecedence(char &c)
 //template <typename T>
 class ComputationGraph
 {
+ private:
+  enum TOKEN_TYPE
+  {
+    OPEN_PAREN,
+    CLOSE_PAREN,
+    OPERATOR,
+    NUMERIC,
+    ALPHA,
+    IGNORE_TYPE,
+    NONE
+  };
+
+  /**
+   * checks whether the new token type is different from the previous token type and thus should count as a new token
+   * @return
+   */
+  bool NewTokenType(TOKEN_TYPE &prev_type, TOKEN_TYPE &cur_type)
+  {
+    bool change_type = false;
+    if (prev_type != TOKEN_TYPE::IGNORE_TYPE)
+    {
+      if ((cur_type != prev_type) ||
+      ((prev_type == TOKEN_TYPE::OPERATOR) && (cur_type == TOKEN_TYPE::OPERATOR)) ||
+      ((prev_type == TOKEN_TYPE::OPEN_PAREN) && (cur_type == TOKEN_TYPE::OPEN_PAREN)) ||
+      ((prev_type == TOKEN_TYPE::CLOSE_PAREN) && (cur_type == TOKEN_TYPE::CLOSE_PAREN)))
+      {
+        change_type = true;
+      }
+    }
+    return change_type;
+  }
 
  public:
-  using expression_node_type = typename fetch::math::computation_graph::ExpressionNode;
-  using expression_node_graph = std::deque<std::shared_ptr<expression_node_type>>;
+  using EXPRESSION_NODE_TYPE = typename fetch::math::computation_graph::ExpressionNode;
+  using EXPRESSION_NODE_GRAPH = std::deque<std::shared_ptr<EXPRESSION_NODE_TYPE>>;
+  using OPERATOR_STACK = std::stack<char, std::deque<char>>;
 //  using EXPRESSION_STACK = std::stack<std::shared_ptr<expression_node_type>, expression_container_type>;
 
-  expression_node_graph expression_graph;
-  std::stack<char, std::deque<char>> operator_stack;
+  EXPRESSION_NODE_GRAPH expression_graph;
+  OPERATOR_STACK operator_stack;
 
   ComputationGraph()
   {
   }
+
+  void Reset()
+  {
+    EXPRESSION_NODE_GRAPH empty_graph;
+    std::swap(expression_graph, empty_graph);
+
+    OPERATOR_STACK empty_operator_stack;
+    std::swap(operator_stack, empty_operator_stack);
+  }
+
+  void Tokenize(std::string &input, std::vector<std::string> &ret, std::vector<TOKEN_TYPE> &token_types)
+  {
+    TOKEN_TYPE prev_type = TOKEN_TYPE::NONE;
+    TOKEN_TYPE cur_type = TOKEN_TYPE::NONE;
+    std::string cur_token;
+    for (std::size_t j = 0; j < input.size(); ++j)
+    {
+
+      // figure out what type this character is
+      if (isalpha(input[j])) { cur_type = TOKEN_TYPE::ALPHA; }
+      else if (isdigit(input[j])) { cur_type = TOKEN_TYPE::NUMERIC; }
+      else if (input[j] == '(') { cur_type = TOKEN_TYPE::OPEN_PAREN; }
+      else if (input[j] == ')') { cur_type = TOKEN_TYPE::CLOSE_PAREN; }
+      else if (input[j] == '.') { cur_type = TOKEN_TYPE::NUMERIC; }
+      else if (ispunct(input[j]))
+      {
+        if (helper_funcs::IsOperator(input[j]))
+        {
+          cur_type = TOKEN_TYPE::OPERATOR;
+        }
+        else
+        {
+          cur_type = TOKEN_TYPE::IGNORE_TYPE;
+        }
+      }
+      else if (isspace(input[j])) { cur_type = TOKEN_TYPE::IGNORE_TYPE; }
+      else
+      {
+        cur_type = TOKEN_TYPE::NONE;
+        std::cout << "unhandled errror!!!" << std::endl;
+      }
+
+      // final character so push current token
+      if (j == (input.size() - 1))
+      {
+        if (cur_type != TOKEN_TYPE::IGNORE_TYPE)
+        {
+          token_types.push_back(cur_type);
+
+          cur_token.push_back(input[j]);
+          ret.push_back(cur_token);
+        }
+      }
+      else
+      {
+        // if theres a type change from the previous character
+        if ((j > 0) && (NewTokenType(prev_type, cur_type)))
+        {
+          ret.push_back(cur_token);
+          token_types.push_back(prev_type);
+
+          cur_token = {};
+        }
+        if (cur_type != TOKEN_TYPE::IGNORE_TYPE)
+        {
+          cur_token.push_back(input[j]);
+        }
+
+      }
+
+      prev_type = cur_type;
+    }
+  }
+
 
   /**
    * converts a string into a graph of expressions to compute
@@ -153,85 +271,109 @@ class ComputationGraph
    */
   void ParseExpression(std::string input)
   {
+    std::vector<std::string> tokens{};
+    std::vector<TOKEN_TYPE> token_types{};
+    Tokenize(input, tokens, token_types);
 
-    // TODO: split input string on spaces
-    // iterate through characters
+//    for (std::size_t i = 0; i < input.size(); ++i)
+    std::string open_paren = ("(");
+    std::string close_paren = (")");
 
-    char c;
-
-    for (std::size_t i = 0; i < input.size(); ++i)
+    for (std::size_t i = 0; i < tokens.size(); ++i)
     {
-      c = input[i];
 
-      if (c == ' ')
-      {
-        // ignore white space
-      }
-      else if (c == '(')
-      {
-        operator_stack.push(c);
-      }
-      else if (isdigit(c))
-      {
+      // spaces should have been removed
+      assert(token_types[i] != TOKEN_TYPE::IGNORE_TYPE);
 
-        expression_graph.emplace_back(std::make_unique<ExpressionNode>(double(c - '0')));
-      }
-      else if (helper_funcs::IsOperator(c))
+      switch (token_types[i])
       {
+        case TOKEN_TYPE::OPEN_PAREN:
+          operator_stack.push('(');
+          break;
+        case TOKEN_TYPE::ALPHA:
+          break;
+//        expression_graph.emplace_back(std::make_unique<ExpressionNode>(double(cur_token - '0')));
+//          expression_graph.emplace_back(std::make_unique<ExpressionNode>(std::stod(tokens[i])));
+        case TOKEN_TYPE::NUMERIC:
+//        expression_graph.emplace_back(std::make_unique<ExpressionNode>(double(cur_token - '0')));
+          std::cout << "numeric: " << std::stod(tokens[i]) << std::endl;
+          expression_graph.emplace_back(std::make_unique<ExpressionNode>(std::stod(tokens[i])));
+          break;
+        case TOKEN_TYPE::OPERATOR:
+          std::cout << "op: " << tokens[i][0] << std::endl;
 
-        while (!(operator_stack.empty()) && (helper_funcs::GetPrecedence(operator_stack.top()) >= helper_funcs::GetPrecedence(c)))
-        {
-          std::shared_ptr<ExpressionNode> e2 = std::move(expression_graph.back());
-          expression_graph.pop_back();
-          std::shared_ptr<ExpressionNode> e1 = std::move(expression_graph.back());
-          expression_graph.pop_back();
+          assert (helper_funcs::IsOperator(tokens[i][0]));
 
-          expression_graph.emplace_back(std::make_unique<ExpressionNode>(operator_stack.top(), std::move(e1), std::move(e2)));
+          while (!(operator_stack.empty()) && (helper_funcs::GetPrecedence(operator_stack.top()) >= helper_funcs::GetPrecedence(tokens[i][0])))
+          {
+            std::shared_ptr<ExpressionNode> e2 = std::move(expression_graph.back());
+            expression_graph.pop_back();
+            std::shared_ptr<ExpressionNode> e1 = std::move(expression_graph.back());
+            expression_graph.pop_back();
+
+            expression_graph.emplace_back(std::make_unique<ExpressionNode>(operator_stack.top(), std::move(e1), std::move(e2)));
+            operator_stack.pop();
+
+            // instruct the child nodes about their parentage for later traversal
+            expression_graph.back()->SetChildNodesParent();
+          }
+
+          operator_stack.push(tokens[i][0]);
+          break;
+
+        case TOKEN_TYPE::CLOSE_PAREN:
+
+          while(operator_stack.top() != '(')
+          {
+            std::shared_ptr<ExpressionNode> e2 = std::move(expression_graph.back());
+            expression_graph.pop_back();
+            std::shared_ptr<ExpressionNode> e1 = std::move(expression_graph.back());
+            expression_graph.pop_back();
+
+            expression_graph.emplace_back(std::make_unique<ExpressionNode>(operator_stack.top(), std::move(e1), std::move(e2)));
+            operator_stack.pop();
+
+            // instruct the child nodes about their parentage for later traversal
+            expression_graph.back()->SetChildNodesParent();
+          }
+
+          // pop the ( off the operator stack
           operator_stack.pop();
+          break;
+        case TOKEN_TYPE::NONE:
+          std::cout << "NONE token: " << tokens[i] << std::endl;
+          break;
+        case TOKEN_TYPE::IGNORE_TYPE:
+          std::cout << "IGNORE_TYPE token: " << tokens[i] << std::endl;
+          break;
 
-          // instruct the child nodes about their parentage for later traversal
-          expression_graph.back()->SetChildNodesParent();
-        }
-
-        operator_stack.push(c);
-      }
-      else if (c == ')')
-      {
-        while(operator_stack.top() != '(')
-        {
-          std::shared_ptr<ExpressionNode> e2 = std::move(expression_graph.back());
-          expression_graph.pop_back();
-          std::shared_ptr<ExpressionNode> e1 = std::move(expression_graph.back());
-          expression_graph.pop_back();
-
-          expression_graph.emplace_back(std::make_unique<ExpressionNode>(operator_stack.top(), std::move(e1), std::move(e2)));
-          operator_stack.pop();
-
-          // instruct the child nodes about their parentage for later traversal
-          expression_graph.back()->SetChildNodesParent();
-        }
-
-        // pop the ( off the operator stack
-        operator_stack.pop();
-      }
-      else
-      {
-        std::cout << " unhandled error. character type: " << c << std::endl;
       }
     }
 
     while (!(operator_stack.empty()))
     {
-      std::shared_ptr<ExpressionNode> e2 = std::move(expression_graph.back());
-      expression_graph.pop_back();
-      std::shared_ptr<ExpressionNode> e1 = std::move(expression_graph.back());
-      expression_graph.pop_back();
+      if(operator_stack.top() != '(')
+      {
+        std::shared_ptr<ExpressionNode> e2 = std::move(expression_graph.back());
+        expression_graph.pop_back();
+        std::shared_ptr<ExpressionNode> e1 = std::move(expression_graph.back());
+        expression_graph.pop_back();
 
-      expression_graph.emplace_back(std::make_unique<ExpressionNode>(operator_stack.top(), std::move(e1), std::move(e2)));
-      operator_stack.pop();
+        expression_graph.emplace_back(std::make_unique<ExpressionNode>(operator_stack.top(), std::move(e1), std::move(e2)));
+        operator_stack.pop();
 
-      // instruct the child nodes about their parentage for later traversal
-      expression_graph.back()->SetChildNodesParent();
+        // instruct the child nodes about their parentage for later traversal
+        expression_graph.back()->SetChildNodesParent();
+      }
+
+      // pop the ( off the operator stack
+      if(!(operator_stack.empty()))
+      {
+        if(operator_stack.top() == '(')
+        {
+          operator_stack.pop();
+        }
+      }
     }
 
     return;
@@ -257,6 +399,10 @@ class ComputationGraph
         return l * r;
       case '/':
         return l / r;
+//      case '%':
+//        return (l % r);
+//      case '^':
+//        return (l ^ r);
       default:
         std::cout << "should never occur error!!" << std::endl;
         return 0;
@@ -285,6 +431,11 @@ class ComputationGraph
       else if (cur_node_ptr->left_node_ptr->evaluated && cur_node_ptr->right_node_ptr->evaluated) // compute and ascend - both evaluated
       {
         // compute the op
+        std::cout << "compute: " << std::endl;
+        std::cout << "left: " << cur_node_ptr->left_node_ptr->val.val << std::endl;
+        std::cout << "op: " << cur_node_ptr->val.c << std::endl;
+        std::cout << "right: " << cur_node_ptr->right_node_ptr->val.val << std::endl;
+
         cur_node_ptr->val.val = compute_op(cur_node_ptr->left_node_ptr->val.val, cur_node_ptr->right_node_ptr->val.val, cur_node_ptr->val.c);
         cur_node_ptr->evaluated = true;
 
--- libs/math/tests/math/computation_graph/computation_graph.cpp
+++ libs/math/tests/math/computation_graph/computation_graph.cpp
@@ -22,45 +22,85 @@
 
 #include "math/computation_graph/computation_graph.hpp"
 
-//using namespace fetch::math;
-
 using namespace fetch::math::computation_graph;
-TEST(computation_graph, arithmetic_parse)
-{
 
-//  ComputationGraph<double> computation_graph;
-  ComputationGraph computation_graph;
-  computation_graph.ParseExpression("2 + 1 - (3 * 4)");
+TEST(computation_graph, simple_arithmetic)
+{
   double result;
+  ComputationGraph computation_graph;
+
+  computation_graph.ParseExpression("1 + 2");
   computation_graph.Run(result);
   std::cout << "result: " << result << std::endl;
+  ASSERT_TRUE(result == double(3));
 
-  ASSERT_TRUE(result == double(-9));
-}
-
-TEST(computation_graph, arithmetic_parse_2)
-{
-
-//  ComputationGraph<double> computation_graph;
-  ComputationGraph computation_graph;
-  computation_graph.ParseExpression("1 + 1 + 1 + 1");
-  double result;
+  computation_graph.Reset();
+  computation_graph.ParseExpression("1 - 2");
   computation_graph.Run(result);
   std::cout << "result: " << result << std::endl;
+  ASSERT_TRUE(result == double(-1));
 
-  ASSERT_TRUE(result == double(4));
-}
-
-TEST(computation_graph, arithmetic_parse_3)
-{
-
-//  ComputationGraph<double> computation_graph;
-  ComputationGraph computation_graph;
-  computation_graph.ParseExpression("1 + 2  - ((3 * 20) / 4)");
-  double result;
+  computation_graph.Reset();
+  computation_graph.ParseExpression("1 * 2");
   computation_graph.Run(result);
   std::cout << "result: " << result << std::endl;
+  ASSERT_TRUE(result == double(2));
 
-  ASSERT_TRUE(result == double(1.5));
+  computation_graph.Reset();
+  computation_graph.ParseExpression("1 / 2");
+  computation_graph.Run(result);
+  std::cout << "result: " << result << std::endl;
+  ASSERT_TRUE(result == double(0.5));
+
 }
 
+TEST(computation_graph, multi_parenthesis_test)
+{
+  double result;
+  ComputationGraph computation_graph;
+
+  computation_graph.ParseExpression("(((1 + 2)))");
+  computation_graph.Run(result);
+  std::cout << "result: " << result << std::endl;
+  ASSERT_TRUE(result == double(3));
+
+  computation_graph.Reset();
+  computation_graph.ParseExpression("((1 - 2) * (3 / 4))");
+  computation_graph.Run(result);
+  std::cout << "result: " << result << std::endl;
+  ASSERT_TRUE(result == double(-0.75));
+
+}
+
+TEST(computation_graph, odd_num_nodes)
+{
+  double result;
+  ComputationGraph computation_graph;
+
+  computation_graph.ParseExpression("4 * 6 / 3");
+  computation_graph.Run(result);
+  std::cout << "result: " << result << std::endl;
+  ASSERT_TRUE(result == double(8));
+}
+
+TEST(computation_graph, multi_digit_nums)
+{
+  double result;
+  ComputationGraph computation_graph;
+
+  computation_graph.ParseExpression("100 * 62 / 31");
+  computation_graph.Run(result);
+  std::cout << "result: " << result << std::endl;
+  ASSERT_TRUE(result == double(200));
+}
+
+TEST(computation_graph, decimal_place_nums)
+{
+  double result;
+  ComputationGraph computation_graph;
+
+  computation_graph.ParseExpression("10.0 * 62.5 / 31.25");
+  computation_graph.Run(result);
+  std::cout << "result: " << result << std::endl;
+  ASSERT_TRUE(result == double(20));
+}
