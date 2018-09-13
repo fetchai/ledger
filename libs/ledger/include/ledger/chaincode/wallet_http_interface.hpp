a50d99f9e73c1c4ed595a2d9deb5cc80a672081a
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2018-09-13 15:09:37 +0100

    Automatic style checking

--- libs/ledger/include/ledger/chaincode/wallet_http_interface.hpp
+++ libs/ledger/include/ledger/chaincode/wallet_http_interface.hpp
@@ -51,21 +51,25 @@ public:
   {
 
     // register all the routes
-    Post("/api/wallet/register", [this](http::ViewParameters const &, http::HTTPRequest const &request) {
-      return OnRegister(request);
-    });
+    Post("/api/wallet/register",
+         [this](http::ViewParameters const &, http::HTTPRequest const &request) {
+           return OnRegister(request);
+         });
 
-    Post("/api/wallet/balance", [this](http::ViewParameters const &, http::HTTPRequest const &request) {
-      return OnBalance(request);
-    });
+    Post("/api/wallet/balance",
+         [this](http::ViewParameters const &, http::HTTPRequest const &request) {
+           return OnBalance(request);
+         });
 
-    Post("/api/wallet/transfer", [this](http::ViewParameters const &, http::HTTPRequest const &request) {
-      return OnTransfer(request);
-    });
+    Post("/api/wallet/transfer",
+         [this](http::ViewParameters const &, http::HTTPRequest const &request) {
+           return OnTransfer(request);
+         });
 
-    Post("/api/wallet/transactions", [this](http::ViewParameters const &, http::HTTPRequest const &request) {
-      return OnTransactions(request);
-    });
+    Post("/api/wallet/transactions",
+         [this](http::ViewParameters const &, http::HTTPRequest const &request) {
+           return OnTransactions(request);
+         });
   }
 
 private:
