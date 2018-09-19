3d0ec51817e498a60265f419817c9cb5423435e8
nhutton <nathan.cameron.hutton@gmail.com>
2018-09-19 10:44:11 +0100

    add all coverage generation for Troels

--- scripts/generate_coverage.py
+++ scripts/generate_coverage.py
@@ -31,13 +31,78 @@ def create_empty_dir(dirname):
     except:
         pass
 
+def get_whitelist(targets : map):
+
+    whitelist = ['tokenizer_test',
+                 'encode_decode_test',
+                 'referenced_byte_array_test',
+                 'lfg_test',
+                 'json_document_test',
+                 'variant_test',
+                 'sha256_test',
+                 'fetch-crypto-gtests',
+                 'proof_of_work_test',
+                 'ledger_identifier_test',
+                 'chaincode_test',
+                 #'executors_test',
+                 #'main_chain_test',
+                 'transaction_types_test',
+                 'blas',
+                 'exp_test',
+                 'spline_test',
+                 'rectangular_load_save_test',
+                 'shared_rectangular_array_test',
+                 'matrix_test',
+                 'manhattan_test',
+                 'correlation_test',
+                 'ndarray_test',
+                 'ndarray_view_test',
+                 'ndarray_iterator_test',
+                 'ndarray_broadcast_test',
+                 'ndarray_squeeze_test',
+                 'relu_test',
+                 'sign_test',
+                 'l2loss_test',
+                 #'tcp_client_stress_test',
+                 #'thread_manager_stress_test',
+                 'dummy_test',
+                 #'thread_pool_stress_test',
+                 'callable_test',
+                 'peer_test',
+                 'file_object_test',
+                 'random_access_stack_test',
+                 'versioned_random_access_stack_test',
+                 'key_value_index_test',
+                 #'document_store_test',
+                 'variant_stack_test',
+                 'versioned_file_object_test',
+                 'versioned_kvi_test',
+                 #'object_store_test',
+                 #'object_sync_test',
+                 'storage',
+                 'vec_exp',
+                 'parallel_dispatcher_test',
+                 'log2_test',
+                 'native_test',
+                 'sse_test']
+
+    remove = [i for i in targets if i not in whitelist]
+    for i in remove: del targets[i]
+
 def get_coverage_reports(targets : map):
     for key, val in targets.items():
         os.system("rm -f default.profraw")
         print("Executing: " + key)
         print(val)
-        if not (os.system(val) == 0):
-            print("Failed when executing: "+val)
+
+        popen = subprocess.Popen(val, stdout=subprocess.PIPE, cwd=os.getcwd())
+
+        output = popen.communicate()[0]
+        output = output.decode("utf-8")
+
+        if popen.returncode != 0:
+            print("Failed to run")
+            sys.exit(1)
 
         raw_file_name     = key +".profraw"
         indexed_file_name = key +".profdata"
@@ -50,13 +115,20 @@ def get_coverage_reports(targets : map):
         os.system("llvm-profdata merge -sparse  "+raw_file_name+" -o " + indexed_file_name)
         os.system("llvm-cov show -Xdemangler c++filt "+val+" -instr-profile="+indexed_file_name + " -format=html -o " +directory_name)
 
-# This isn't enabled yet since it will take too long
 def get_all_coverage_reports(targets : map):
     all_profs = ""
+    all_binaries = ""
     for key, val in targets.items():
         raw_file_name     = key +".profraw"
         indexed_file_name = key +".profdata"
         all_profs += indexed_file_name + " "
+        all_binaries += "-object " + val + " "
+
+    command_to_execute = "llvm-profdata merge -sparse  "+all_profs+" -o final.profdata"
+    os.system(command_to_execute)
+
+    command_to_execute = "llvm-cov show -Xdemangler c++filt "+all_binaries+" -instr-profile=final.profdata -format=html -o coverage/all_coverage"
+    os.system(command_to_execute)
 
 def main():
 
@@ -115,8 +187,13 @@ def main():
         targets[test_name] = temp
         pp.pprint(targets)
 
+    print("Targets: ")
+    pp.pprint(list(targets.keys()))
+
     # create empty coverage dir
     create_empty_dir("coverage")
+
+    get_whitelist(targets)
 
     get_coverage_reports(targets)
     get_all_coverage_reports(targets)
