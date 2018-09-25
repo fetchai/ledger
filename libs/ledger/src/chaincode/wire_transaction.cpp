4a4fa2a036b6ec628d3d73f0f565ef9d440df082
Peter Bukva <peter.bukva@fetch.ai>
2018-09-25 16:54:53 +0100

    WIP: New format version for wire transaction

--- libs/ledger/src/chaincode/wire_transaction.cpp
+++ libs/ledger/src/chaincode/wire_transaction.cpp
@@ -46,33 +46,33 @@ byte_array::ByteArray ToWireTransaction(MutableTransaction const &tx, bool const
     resources.CopyFrom(tx.resources().begin(), tx.resources().end());
 
     tx_debug_data["resources"] = resources;
+
+    if (tx.signatures().size() > 0)
+    {
+      script::VariantArray signatures{ tx.signatures().size() };
+      std::size_t i = 0;
+      std::size_t identity_serialised_size = 0;
+      auto eval_identity_size = serializers::LazyEvalArgumentFactory([&identity_serialised_size](auto& stream){
+        identity_serialised_size = stream.size();
+      });
+
+      serializers::ByteArrayBuffer stream;
+      for( auto const& sig: tx.signatures())
+      {
+        auto &sig_v = signatures[i++];
+        sig_v.MakeObject();
+        stream.Resize(0, serializers::eResizeParadigm::absolute);
+        stream.Append(sig.first, eval_identity_size, sig.second);
+        sig_v[byte_array::ToBase64(stream.data().SubArray(0, identity_serialised_size))] = byte_array::ToBase64(stream.data().SubArray(identity_serialised_size, stream.data().size()-identity_serialised_size));
+      }
+
+      tx_debug_data["signatures"] = signatures;
+    }
     tx_v["dbg"] = tx_debug_data;
   }
 
   auto txdfs{TxDataForSigningCFactory(tx)};
-  tx_v["data"] = byte_array::ToBase64(txdfs.DataForSigning());
-
-  script::VariantArray signatures{ tx.signatures().size() };
-  std::size_t i = 0;
-  std::size_t identity_serialised_size = 0;
-  auto eval_identity_size = serializers::LazyEvalArgumentFactory([&identity_serialised_size](auto& stream){
-    identity_serialised_size = stream.size();
-  });
-
-  serializers::ByteArrayBuffer stream;
-  for( auto const& sig: tx.signatures())
-  {
-    auto &sig_v = signatures[i++];
-    sig_v.MakeObject();
-    stream.Resize(0, serializers::eResizeParadigm::absolute);
-    stream.Append(sig.first, eval_identity_size, sig.second);
-    sig_v[byte_array::ToBase64(stream.data().SubArray(0, identity_serialised_size))] = byte_array::ToBase64(stream.data().SubArray(identity_serialised_size, stream.data().size()-identity_serialised_size));
-  }
-
-  if (signatures.size() > 0 )
-  {
-    tx_v["signatures"] = signatures;
-  }
+  tx_v["data"] = byte_array::ToBase64(txdfs.DataForSigning(tx.signatures()));
 
   std::stringstream str_stream;
   str_stream << tx_v;
@@ -88,28 +88,8 @@ MutableTransaction FromWireTransaction(byte_array::ConstByteArray const &transac
   
   serializers::ByteArrayBuffer stream{ byte_array::FromBase64(tx_v["data"].As<byte_array::ByteArray>()) };
   auto txdata {TxDataForSigningCFactory(tx)};
-  stream >> txdata;
-
-  auto const &signatures = tx_v["signatures"];
-
   MutableTransaction::signatures_type mtx_signatures;
-
-  signatures.ForEach([&mtx_signatures](script::Variant const &sig_pair_v) -> bool{
-    sig_pair_v.ForEach([&mtx_signatures](script::Variant const &identity_v, script::Variant const &signature_v) -> bool{
-      auto identity_bin = byte_array::FromBase64(identity_v.As<byte_array::ByteArray>());
-      auto signature_bin = byte_array::FromBase64(signature_v.As<byte_array::ByteArray>());
-      serializers::ByteArrayBuffer i_stream{ identity_bin };
-      crypto::Identity identity;
-      i_stream >> identity;
-
-      serializers::ByteArrayBuffer s_stream{ signature_bin };
-      Signature signature;
-      s_stream >> signature;
-      mtx_signatures[identity] = signature;
-      return true;
-    });
-    return true;
-  });
+  stream >> txdata >> mtx_signatures;
 
   tx.set_signatures(mtx_signatures);
 
