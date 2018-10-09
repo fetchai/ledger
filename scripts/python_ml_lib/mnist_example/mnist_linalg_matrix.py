fixed sigmoid op and derivative

scripts/python_ml_lib/mnist_example/mnist_linalg_matrix.py
@@ -312,4 +312,23 @@ def run_mnist():
 # import cProfile
 # cProfile.run('run_mnist()')
 
-run_mnist()
+# run_mnist()
+
+
+import numpy as np
+
+X = np.array([[1], [2], [3]])
+y = np.array([[1], [2.5], [3.5]])
+
+get_theta = lambda theta: np.array([[0, theta]])
+
+thetas = list(map(get_theta, [0.5, 1.0, 1.5]))
+
+X = np.hstack([np.ones([3, 1]), X])
+
+def cost(X, y, theta):
+    inner = np.power(((X @ theta.T) - y), 2)
+    return np.sum(inner) / (2 * len(X))
+
+for i in range(len(thetas)):
+    print(cost(X, y, thetas[i]))