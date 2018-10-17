e9d1554ede6a849d40d5edee446acc5f7eebca2d
baykaner <khan.baykaner@fetch.ai>
2018-10-17 15:10:00 +0100

    removed includes of redundant files

--- libs/ml/tests/ml/layers/layers.cpp
+++ libs/ml/tests/ml/layers/layers.cpp
@@ -21,7 +21,6 @@
 #include <iostream>
 
 #include "math/linalg/matrix.hpp"
-#include "ml/computation_graph/computation_graph.hpp"
 #include "ml/layers/layers.hpp"
 #include "ml/ops/ops.hpp"
 #include "ml/session.hpp"
