3bf938325bc58533c8390dcbb3af3c9409796cd4
Saira <saira.bibi@emumba.com>
2018-11-09 15:46:37 +0500

    bennchmarking of multiple stacks

---
+++ libs/storage/benchmarks/stack_benchmarks/variant_stack_bench.cpp
@@ -0,0 +1,50 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include <benchmark/benchmark.h>
+#include <gtest/gtest.h>
+#include <stack>
+
+#include "core/random/lfg.hpp"
+#include "storage/variant_stack.hpp"
+
+using fetch::storage::VariantStack;
+
+class VariantStackBench : public ::benchmark::Fixture
+{
+protected:
+  void SetUp(const ::benchmark::State &st) override
+  {
+    stack_.New("Variant_bench.db");
+  }
+
+  void TearDown(const ::benchmark::State &) override
+  {}
+
+  VariantStack                              stack_;
+  fetch::random::LaggedFibonacciGenerator<> lfg_;
+};
+
+BENCHMARK_F(VariantStackBench, WritingIntToStack)(benchmark::State &st)
+{
+  for (auto _ : st)
+  {
+    uint64_t random = lfg_();
+    stack_.Push(random) ;
+  }
+}
