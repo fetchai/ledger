eeafb97a9c2047a78e29d2cd5c20fc9c3d58e2f3
baykaner <khan.baykaner@fetch.ai>
2018-11-12 19:24:07 +0000

    updated test to 4 cluster case

--- libs/math/tests/math/clustering/kmeans.cpp
+++ libs/math/tests/math/clustering/kmeans.cpp
@@ -29,22 +29,32 @@ using data_type      = double;
 using container_type = fetch::memory::SharedArray<data_type>;
 using matrix_type    = Matrix<data_type, container_type>;
 
-TEST(clustering_test, kmeans_test)
+TEST(clustering_test, kmeans_test_2d_4k)
 {
 
   matrix_type A{100, 2};
   matrix_type ret{100, 1};
   std::size_t K = 2;
 
-  for (std::size_t i = 0; i < 50; ++i)
+  for (std::size_t i = 0; i < 25; ++i)
   {
     A.Set(i, 0, static_cast<data_type>(-i - 50));
     A.Set(i, 1, static_cast<data_type>(-i - 50));
   }
-  for (std::size_t i = 50; i < 100; ++i)
+  for (std::size_t i = 25; i < 50; ++i)
   {
-    A.Set(i, 0, static_cast<data_type>(i));
-    A.Set(i, 1, static_cast<data_type>(i));
+    A.Set(i, 0, static_cast<data_type>(-i - 50));
+    A.Set(i, 1, static_cast<data_type>(i + 50));
+  }
+  for (std::size_t i = 50; i < 75; ++i)
+  {
+    A.Set(i, 0, static_cast<data_type>(i + 50));
+    A.Set(i, 1, static_cast<data_type>(-i - 50));
+  }
+  for (std::size_t i = 75; i < 100; ++i)
+  {
+    A.Set(i, 0, static_cast<data_type>(i + 50));
+    A.Set(i, 1, static_cast<data_type>(i + 50));
   }
 
   auto now    = std::chrono::system_clock::now();
@@ -55,13 +65,23 @@ TEST(clustering_test, kmeans_test)
   matrix_type clusters    = fetch::math::clustering::KMeans(A, K, random_seed);
 
   std::size_t group_0 = static_cast<std::size_t>(clusters[0]);
-  for (std::size_t j = 0; j < clusters.size() / 2; ++j)
+  for (std::size_t j = 0; j < 25; ++j)
   {
     ASSERT_TRUE(group_0 == static_cast<std::size_t>(clusters[j]));
   }
-  std::size_t group_1 = static_cast<std::size_t>(clusters[clusters.size() / 2]);
-  for (std::size_t j = clusters.size() / 2; j < clusters.size(); ++j)
+  std::size_t group_1 = static_cast<std::size_t>(clusters[25]);
+  for (std::size_t j = 25; j < 50; ++j)
   {
     ASSERT_TRUE(group_1 == static_cast<std::size_t>(clusters[j]));
+  }
+  std::size_t group_2 = static_cast<std::size_t>(clusters[50]);
+  for (std::size_t j = 50; j < 75; ++j)
+  {
+    ASSERT_TRUE(group_2 == static_cast<std::size_t>(clusters[j]));
+  }
+  std::size_t group_3 = static_cast<std::size_t>(clusters[75]);
+  for (std::size_t j = 75; j < 100; ++j)
+  {
+    ASSERT_TRUE(group_3 == static_cast<std::size_t>(clusters[j]));
   }
 }
