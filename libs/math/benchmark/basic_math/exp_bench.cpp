5f63b3ab894bc16f3c067554c4a1e58981dba6a7
Talal Irfan <talal.irfan@emumba.com>
2018-11-15 11:43:39 +0500

    Moved and converted math benchmarks

---
+++ libs/math/benchmark/basic_math/exp_bench.cpp
@@ -0,0 +1,69 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "benchmark/benchmark.h"
+#include <chrono>
+#include <cmath>
+#include <iostream>
+
+#include "core/random/lcg.hpp"
+#include "math/approx_exp.hpp"
+
+template <uint8_t N, uint64_t C>
+static void BM_ApproxExpImplementation(benchmark::State &state)
+{
+
+  fetch::math::ApproxExpImplementation<N, C> fexp;
+  double                                     x = state.range(0);
+  for (auto _ : state)
+  {
+    // Running loop to get greater benchmarks.
+    for (int i = 0; i < 1000; i++)
+    {
+      x = fexp(x);
+    }
+  }
+}
+BENCHMARK_TEMPLATE(BM_ApproxExpImplementation, 0, 0)
+    ->RangeMultiplier(10)
+    ->Range(1, 1000000)
+    ->Complexity();
+BENCHMARK_TEMPLATE(BM_ApproxExpImplementation, 8, 60801)
+    ->RangeMultiplier(10)
+    ->Range(1, 1000000)
+    ->Complexity();
+BENCHMARK_TEMPLATE(BM_ApproxExpImplementation, 12, 60801)
+    ->RangeMultiplier(10)
+    ->Range(1, 1000000)
+    ->Complexity();
+
+static void BM_exp(benchmark::State &state)
+{
+  double x = state.range(0);
+  for (auto _ : state)
+  {
+    // Running loop to get greater benchmarks.
+    for (int i = 0; i < 1000; i++)
+    {
+      x = exp(x);
+    }
+  }
+}
+BENCHMARK(BM_exp)->RangeMultiplier(10)->Range(1, 1000000)->Complexity();
+
+BENCHMARK_MAIN();
--- libs/math/tests/test_spline.cpp
+++ libs/math/tests/test_spline.cpp
@@ -62,76 +62,8 @@ void test1(F &f, double from, double to, double max)
   }
 }
 
-template <std::size_t N, typename F, std::size_t MAX = 100000000>
-double test_timing(F &f, double x_value, double from, double to)
-{
-  fetch::math::spline::Spline<> spline;
-  spline.SetFunction(f, from, to, N);
-
-  volatile double x;
-
-  auto t1a = std::chrono::high_resolution_clock::now();
-  for (std::size_t i = 0; i < MAX; ++i)
-  {
-    x = x_value;
-  }
-  auto t2a = std::chrono::high_resolution_clock::now();
-
-  double us1 = double(std::chrono::duration_cast<std::chrono::milliseconds>(t2a - t1a).count());
-
-  auto            t1b = std::chrono::high_resolution_clock::now();
-  volatile double y;
-  for (std::size_t i = 0; i < MAX; ++i)
-  {
-    x = x_value;
-    y = spline(x);
-  }
-  auto t2b   = std::chrono::high_resolution_clock::now();
-  x          = y;
-  double us2 = double(std::chrono::duration_cast<std::chrono::milliseconds>(t2b - t1b).count());
-
-  auto t1c = std::chrono::high_resolution_clock::now();
-  for (std::size_t i = 0; i < MAX; ++i)
-  {
-    x = x_value;
-    y = f(x);
-  }
-  auto t2c = std::chrono::high_resolution_clock::now();
-
-  double us3 = double(std::chrono::duration_cast<std::chrono::milliseconds>(t2c - t1c).count());
-
-  return (us3 - us1) / (us2 - us1);
-  //  auto t3 = std::chrono::high_resolution_clock::now();
-}
-
-void benchmark()
-{
-  static fetch::random::LinearCongruentialGenerator gen;
-  std::cout << "Benchmarking Sin ... " << std::endl;
-  std::cout << test_timing<8>(dsin, gen.AsDouble() * 100, 0, 100) << std::endl;
-  std::cout << test_timing<16>(dsin, gen.AsDouble() * 100, 0, 100) << std::endl;
-  std::cout << test_timing<20>(dsin, gen.AsDouble() * 100, 0, 100) << std::endl;
-
-  std::cout << "Benchmarking Cos ... " << std::endl;
-  std::cout << test_timing<8>(dcos, gen.AsDouble() * 100, 0, 100) << std::endl;
-  std::cout << test_timing<16>(dcos, gen.AsDouble() * 100, 0, 100) << std::endl;
-  std::cout << test_timing<20>(dcos, gen.AsDouble() * 100, 0, 100) << std::endl;
-
-  std::cout << "Benchmarking Tan ... " << std::endl;
-  std::cout << test_timing<8>(dtan, gen.AsDouble() * 100, 0, 100) << std::endl;
-  std::cout << test_timing<16>(dtan, gen.AsDouble() * 100, 0, 100) << std::endl;
-  std::cout << test_timing<20>(dtan, gen.AsDouble() * 100, 0, 100) << std::endl;
-
-  std::cout << "Benchmarking Exp ... " << std::endl;
-  std::cout << test_timing<8>(dexp, gen.AsDouble() * 100, 0, 100) << std::endl;
-  std::cout << test_timing<16>(dexp, gen.AsDouble() * 100, 0, 100) << std::endl;
-  std::cout << test_timing<20>(dexp, gen.AsDouble() * 100, 0, 100) << std::endl;
-}
-
 int main(int argc, char **argv)
 {
-  if ((argc == 2) && (std::string(argv[1]) == "benchmark"))
-    benchmark();
 
   std::cout << "Testing Sin ... " << std::endl;
   test1<8>(dsin, 0, 2 * 3.14, 2);
