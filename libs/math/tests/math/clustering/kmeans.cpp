cf60f17c9555c957c2340bdcbbcc4787c339e17a
juliamgomes <juliagomes@berkeley.edu>
2018-11-15 22:32:32 +0000

    wip can build sample clusters of n dimensions but still missing assertions

--- libs/math/tests/math/clustering/kmeans.cpp
+++ libs/math/tests/math/clustering/kmeans.cpp
@@ -39,6 +39,9 @@ matrix_type combinations(int n, int r);
 // Helper function
 int factorial(int n)
 {
+  if (n == 0){
+    return 1;
+  }
   int i;
   for(i = n-1; i > 1; i--)
     n *= i;
@@ -49,9 +52,14 @@ int factorial(int n)
 // Helper function
 matrix_type combinations(int n, int r)
 {
+  if (r == 0){
+    matrix_type output_array{};
+    return output_array;
+  }
+
   int n_combinations = factorial(n)/factorial(r)/factorial(n-r);
-  int current_dim = 0;
-  int current_row = 0;
+  std::size_t current_dim = 0;
+  std::size_t current_row = 0;
 
   std::vector<bool> v(static_cast<unsigned long>(n));
   std::fill(v.end() - r, v.end(), true);
@@ -60,25 +68,24 @@ matrix_type combinations(int n, int r)
   do {
     for (int i = 0; i < n; ++i) {
       if (v[static_cast<unsigned long>(i)]) {
-        if (current_dim == 0){
-          int dim0 = (i + 1);
-          // std::cout << "dim0: " << dim0 << " ";
+          int dim = (i+1);
+          output_array.Set(current_row, current_dim, static_cast<data_type>(dim));
+          if (current_dim == static_cast<std::size_t>(r)-1){
+            current_row += 1;
+          }
           current_dim = current_dim + 1;
-          current_dim = current_dim % r;
-          output_array.At(static_cast<std::size_t>(current_row), 0) = dim0;
-        }
-        else if (current_dim == 1){
-          int dim1 = (i + 1);
-          // std::cout << "dim1: " << dim1 << " ";
-          current_dim = current_dim + 1;
-          current_dim = current_dim % r;
-          output_array.At(static_cast<std::size_t>(current_row), 1) = dim1;
-          current_row = current_row + 1;
+          current_dim = current_dim % static_cast<std::size_t>(r);
+
         }
       }
-    }
-    // std::cout << "\n";
-  } while (std::next_permutation(v.begin(), v.end()));
+    }while (std::next_permutation(v.begin(), v.end()));
+    for (std::size_t k = 0; k < output_array.shape()[0]; ++k) {
+        for (std::size_t j = 0;  j< output_array.shape()[1]; ++j)
+        {
+          std::cout << output_array.At(k,j) << "\t\t";
+        }
+        std::cout << std::endl;
+      }
   return output_array;
 }
 
@@ -143,81 +150,60 @@ TEST(clustering_test, kmeans_test_4dimensions)
   float base = 2.0;
   float out = std::pow(base, static_cast<float>(n_dimensions));  // Each dimension will be positive or negative for 2^n combinations
   int n_clusters = static_cast<int>(out);
-  int n_points = n_clusters*5;  // Each cluster will consist of 5 points
+  int n_points_per_cluster = 5;
+  int n_points = n_clusters*n_points_per_cluster;
+  std::size_t row = 0;
   matrix_type A{static_cast<std::size_t>(n_points), static_cast<std::size_t>(n_dimensions)};
 
-  matrix_type com = combinations(4, 2);
+  // Trivial case: all dimensions are negative
+  int val_magnitude = 50;
+  std::vector<int> dimension_signs (static_cast<std::size_t>(n_dimensions), -1);
+  for (std::size_t p=0; p < static_cast<std::size_t>(n_points_per_cluster); ++p){
+    for (std::size_t k=0; k<static_cast<std::size_t>(n_dimensions); ++k)
+    {
+      int sign = dimension_signs[k];
+      int val = val_magnitude * sign;
+      A.Set(row, k, val);
+    }
+    row = row + 1;
+    val_magnitude += 5;
+  }
 
-  for (std::size_t i = 0; i < 6; ++i){
-    for (std::size_t j = 0; j < 2; ++j){
-      std::cout << com.At(i,j);
+  //
+  for (int r = 1; r <= n_dimensions; ++r){
+    matrix_type combinations_array = combinations(n_dimensions, r);
+
+    for (std::size_t i = 0; i < combinations_array.shape()[0]; ++i){
+      std::vector<int> dimension_signs (static_cast<std::size_t>(n_dimensions), -1);
+      for (std::size_t j = 0; j < combinations_array.shape()[1]; ++j){
+        int positive_dimension = static_cast<int>(combinations_array.At(i,j)) - 1;
+        dimension_signs[static_cast<std::size_t>(positive_dimension)] = 1;
+      }
+      std::cout << std::endl;
+      int val_magnitude = 50;
+      for (std::size_t p=0; p < static_cast<std::size_t>(n_points_per_cluster); ++p){
+        for (std::size_t k=0; k<static_cast<std::size_t>(n_dimensions); ++k)
+        {
+          int sign = dimension_signs[k];
+          int val = val_magnitude * sign;
+          A.Set(row, k, val);
+        }
+        row = row + 1;
+        val_magnitude += 5;
+      }
+
+    }
+    std::cout << std::endl << "next starts at row: " << row << " of " << n_points << std::endl;
+  }
+  for (std::size_t l = 0; l < A.shape()[0]; ++l) {
+    for (std::size_t m = 0; m < A.shape()[1]; ++m)
+    {
+      std::cout << A.At(l,m) << "\t\t";
     }
     std::cout << std::endl;
   }
 
-
-//
-//  do {
-//    for (int i = 0; i < n; ++i) {
-//      if (v[static_cast<unsigned long>(i)]) {
-//        std::cout << (i + 1) << " ";
-//      }
-//    }
-//    std::cout << "\n";
-//  } while (std::next_permutation(v.begin(), v.end()));
-
-
-
-
-//  int n = 4;
-//  int r = 2;
-//  int n_combinations = factorial(n)/factorial(r)/factorial(n-r);
-//  int which_dim = 0;
-//  int row = 0;
-//
-//  std::vector<bool> v(static_cast<unsigned long>(n));
-//  std::fill(v.end() - r, v.end(), true);
-//
-//  matrix_type output_array{static_cast<std::size_t>(n_combinations), static_cast<std::size_t>(r)};
-//  do {
-//    for (int i = 0; i < n; ++i) {
-//      if (v[static_cast<unsigned long>(i)]) {
-//        if (which_dim == 0){
-//          int dim0 = (i + 1);
-//          std::cout << "dim0: " << dim0 << " ";
-//          which_dim = which_dim + 1;
-//          which_dim = which_dim % r;
-//          output_array.At(static_cast<std::size_t>(row), 0) = dim0;
-//        }
-//        else if (which_dim == 1){
-//          int dim1 = (i + 1);
-//          std::cout << "dim1: " << dim1 << " ";
-//          which_dim = which_dim + 1;
-//          which_dim = which_dim % r;
-//          output_array.At(static_cast<std::size_t>(row), 1) = dim1;
-//          row = row + 1;
-//        }
-//      }
-//    }
-//    std::cout << "\n";
-//  } while (std::next_permutation(v.begin(), v.end()));
-//
-//  for (std::size_t i = 0; i < 6; ++i){
-//    for (std::size_t j = 0; j < 2; ++j){
-//      std::cout << output_array.At(i,j);
-//    }
-//    std::cout << std::endl;
-//  }
-
-
-
-  for(int c=0; c < n_clusters; ++c)
-  {
-    for (int p = 0; p < n_points; ++p)
-    {
-
-    }
-  }
+  matrix_type combinations_array = combinations(n_dimensions, 0);
 }
 
 TEST(clustering_test, kmeans_test_previous_assignment)
