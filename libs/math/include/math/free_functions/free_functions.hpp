dd61963fe18a8c9ecd40a7c6f7869e32e604e3bf
baykaner <khan.baykaner@fetch.ai>
2018-11-16 14:16:08 +0000

    tweak to free functions implementation

--- libs/math/include/math/free_functions/free_functions.hpp
+++ libs/math/include/math/free_functions/free_functions.hpp
@@ -2168,8 +2168,7 @@ void Softmax(linalg::Matrix<T, C, S> const &array, linalg::Matrix<T, C, S> &ret)
 template <typename T, typename C, typename S>
 linalg::Matrix<T, C, S> Softmax(linalg::Matrix<T, C, S> const &array)
 {
-  linalg::Matrix<T, C, S> ret{array.size()};
-  ret.Reshape(array.shape());
+  linalg::Matrix<T, C, S> ret{array.shape()};
   Softmax(array, ret);
   return ret;
 }
