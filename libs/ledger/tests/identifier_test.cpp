951ca474d73fbaaf2a0569a563df9de0976c2371
Saira <saira.bibi@emumba.com>
2018-11-22 11:08:30 +0500

    test converion to gtest

--- libs/ledger/tests/identifier_test.cpp
+++ libs/ledger/tests/identifier_test.cpp
@@ -17,114 +17,85 @@
 //------------------------------------------------------------------------------
 
 #include "ledger/identifier.hpp"
-#include "testing/unittest.hpp"
+#include <gtest/gtest.h>
 
-using fetch::ledger::Identifier;
+ using fetch::ledger::Identifier;
 
-int main()
+ TEST(identifier_gtest, basic_checks)
 {
-
-  SCENARIO("Parsing")
+  Identifier id("foo.bar.baz");
+  EXPECT_EQ(id.name(), "baz");
+  EXPECT_EQ(id.name_space(), "foo.bar");
+  EXPECT_EQ(id[0], "foo");
+  EXPECT_EQ(id[1], "bar");
+  EXPECT_EQ(id[2], "baz");
+}
+ TEST(identifier_ancestry_checks, direct_parent)
+{
+  Identifier parent("foo");
+  Identifier child("foo.bar");
+  EXPECT_TRUE(parent.IsParentTo(child));
+  EXPECT_TRUE(child.IsChildTo(parent));
+  EXPECT_TRUE(parent.IsDirectParentTo(child));
+  EXPECT_TRUE(child.IsDirectChildTo(parent));
+  EXPECT_FALSE(parent.IsChildTo(child));
+  EXPECT_FALSE(child.IsParentTo(parent));
+}
+ TEST(identifier_ancestry_checks, indirect_Parent)
+{
+  Identifier parent("foo");
+  Identifier child("foo.bar.baz");
+  EXPECT_TRUE(parent.IsParentTo(child));
+  EXPECT_TRUE(child.IsChildTo(parent));
+  EXPECT_FALSE(parent.IsDirectParentTo(child));
+  EXPECT_FALSE(child.IsDirectChildTo(parent));
+  EXPECT_FALSE(parent.IsChildTo(child));
+  EXPECT_FALSE(child.IsParentTo(parent));
+}
+ TEST(identifier_ancestry_checks, Child)
+{
+  Identifier parent("foo.baz");
+  Identifier child("foo.bar");
+  EXPECT_FALSE(parent.IsParentTo(child));
+  EXPECT_FALSE(child.IsChildTo(parent));
+  EXPECT_FALSE(child.IsParentTo(parent));
+  EXPECT_FALSE(parent.IsChildTo(child));
+}
+ TEST(identifier_ancestry_checks, Append)
+{
+  Identifier id;
+  id.Append("foo");
+  id.Append("bar");
+  id.Append("baz");
+  id.Append("x.y.z");
+  EXPECT_EQ(id.full_name(), "foo.bar.baz.x.y.z");
+}
+ TEST(identifier_ancestry_checks, Append_invalid_namespace_at_beginning)
+{
+  Identifier id;
+   bool exception_received = false;
+   try
   {
-    SECTION("Basic Checks")
-    {
-      Identifier id("foo.bar.baz");
-
-      EXPECT(id.name() == "baz");
-      EXPECT(id.name_space() == "foo.bar");
-      EXPECT(id[0] == "foo");
-      EXPECT(id[1] == "bar");
-      EXPECT(id[2] == "baz");
-    };
-  };
-
-  SCENARIO("Ancestry Checks")
+    id.Append(".foo");
+  }
+  catch (std::runtime_error const &ex)
   {
-    SECTION("Direct Parent")
-    {
-      Identifier parent("foo");
-      Identifier child("foo.bar");
-
-      EXPECT(parent.IsParentTo(child));
-      EXPECT(child.IsChildTo(parent));
-      EXPECT(parent.IsDirectParentTo(child));
-      EXPECT(child.IsDirectChildTo(parent));
-      EXPECT(!parent.IsChildTo(child));
-      EXPECT(!child.IsParentTo(parent));
-    };
-
-    SECTION("Indirect Parent")
-    {
-      Identifier parent("foo");
-      Identifier child("foo.bar.baz");
-
-      EXPECT(parent.IsParentTo(child));
-      EXPECT(child.IsChildTo(parent));
-      EXPECT(!parent.IsDirectParentTo(child));
-      EXPECT(!child.IsDirectChildTo(parent));
-      EXPECT(!parent.IsChildTo(child));
-      EXPECT(!child.IsParentTo(parent));
-    };
-
-    SECTION("Child")
-    {
-      Identifier parent("foo.baz");
-      Identifier child("foo.bar");
-
-      EXPECT(!parent.IsParentTo(child));
-      EXPECT(!child.IsChildTo(parent));
-      EXPECT(!child.IsParentTo(parent));
-      EXPECT(!parent.IsChildTo(child));
-    };
-
-    SECTION("Append")
-    {
-      Identifier id;
-      id.Append("foo");
-      id.Append("bar");
-      id.Append("baz");
-      id.Append("x.y.z");
-
-      EXPECT(id.full_name() == "foo.bar.baz.x.y.z");
-    };
-
-    SECTION("Append invalid namespace at beginning")
-    {
-      Identifier id;
-
-      bool exception_received = false;
-
-      try
-      {
-        id.Append(".foo");
-      }
-      catch (std::runtime_error const &ex)
-      {
-        exception_received = true;
-      }
-
-      EXPECT(exception_received);
-    };
-
-    SECTION("Append invalid namespace in the middle")
-    {
-      Identifier id;
-      id.Append("foo");
-
-      bool exception_received = false;
-
-      try
-      {
-        id.Append(".bar");
-      }
-      catch (std::runtime_error const &ex)
-      {
-        exception_received = true;
-      }
-
-      EXPECT(exception_received);
-    };
-  };
-
-  return 0;
+    exception_received = true;
+  }
+  EXPECT_TRUE(exception_received);
+}
+ TEST(identifier_ancestry_checks, Append_invalid_namespace_in_the_middle)
+{
+  Identifier id;
+  id.Append("foo");
+   bool exception_received = false;
+   try
+  {
+    id.Append(".bar");
+  }
+  catch (std::runtime_error const &ex)
+  {
+    exception_received = true;
+  }
+  EXPECT_TRUE(exception_received);
 }
