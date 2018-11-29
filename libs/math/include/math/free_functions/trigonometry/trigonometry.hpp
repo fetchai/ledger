7b1735ca97aae3c1cde02c8412e16d9fd3c92d92
juliamgomes <juliagomes@berkeley.edu>
2018-11-29 17:21:32 +0000

    some modifications wip

--- libs/math/include/math/free_functions/trigonometry/trigonometry.hpp
+++ libs/math/include/math/free_functions/trigonometry/trigonometry.hpp
@@ -17,13 +17,13 @@
 //
 //------------------------------------------------------------------------------
 
-#include "math/kernels/approx_exp.hpp"
-#include "math/kernels/approx_log.hpp"
-#include "math/kernels/approx_logistic.hpp"
-#include "math/kernels/approx_soft_max.hpp"
-#include "math/kernels/basic_arithmetics.hpp"
-#include "math/kernels/relu.hpp"
-#include "math/kernels/sign.hpp"
+//#include "math/kernels/approx_exp.hpp"
+//#include "math/kernels/approx_log.hpp"
+//#include "math/kernels/approx_logistic.hpp"
+//#include "math/kernels/approx_soft_max.hpp"
+//#include "math/kernels/basic_arithmetics.hpp"
+//#include "math/kernels/relu.hpp"
+//#include "math/kernels/sign.hpp"
 #include "math/kernels/standard_functions.hpp"
 
 #include "core/assert.hpp"
@@ -35,10 +35,6 @@
 #include <numeric>
 #include <vector>
 
-/////////////////////////////////
-/// include specific math functions
-/////////////////////////////////
-
 #include "math/free_functions/fundamental_operators.hpp"  // add, subtract etc.
 #include "math/free_functions/standard_functions/abs.hpp"
 #include "math/free_functions/standard_functions/exp.hpp"
@@ -46,17 +42,6 @@
 #include "math/free_functions/standard_functions/log.hpp"
 #include "math/free_functions/standard_functions/remainder.hpp"
 #include "math/free_functions/statistics/normal.hpp"
-
-/////////////////////////////////
-/// blas libraries
-/////////////////////////////////
-
-#include "math/linalg/blas/gemm_nn_vector.hpp"
-#include "math/linalg/blas/gemm_nn_vector_threaded.hpp"
-#include "math/linalg/blas/gemm_nt_vector.hpp"
-#include "math/linalg/blas/gemm_nt_vector_threaded.hpp"
-#include "math/linalg/blas/gemm_tn_vector.hpp"
-#include "math/linalg/blas/gemm_tn_vector_threaded.hpp"
 
 #include "math/meta/type_traits.hpp"
 
