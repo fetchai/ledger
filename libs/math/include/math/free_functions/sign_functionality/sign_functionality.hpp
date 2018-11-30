1eb6c97f96f3e04fc6c6fe30febce5040fa03b79
juliamgomes <juliagomes@berkeley.edu>
2018-11-30 13:57:51 +0000

    removed unnecessary imports in sign_functionality.hpp file

--- libs/math/include/math/free_functions/sign_functionality/sign_functionality.hpp
+++ libs/math/include/math/free_functions/sign_functionality/sign_functionality.hpp
@@ -17,58 +17,10 @@
 //
 //------------------------------------------------------------------------------
 
-#include "math/kernels/approx_exp.hpp"
-#include "math/kernels/approx_log.hpp"
-#include "math/kernels/approx_logistic.hpp"
-#include "math/kernels/approx_soft_max.hpp"
-#include "math/kernels/basic_arithmetics.hpp"
-#include "math/kernels/relu.hpp"
 #include "math/kernels/sign.hpp"
 #include "math/kernels/standard_functions.hpp"
-
-#include "core/assert.hpp"
-#include "math/ndarray_broadcast.hpp"
-#include "meta/type_traits.hpp"
-#include "vectorise/memory/range.hpp"
-#include <algorithm>
 #include <cassert>
-#include <numeric>
-#include <vector>
-
-/////////////////////////////////
-/// include specific math functions
-/////////////////////////////////
-
-#include "math/free_functions/fundamental_operators.hpp"  // add, subtract etc.
-#include "math/free_functions/standard_functions/abs.hpp"
-#include "math/free_functions/standard_functions/exp.hpp"
-#include "math/free_functions/standard_functions/fmod.hpp"
-#include "math/free_functions/standard_functions/log.hpp"
-#include "math/free_functions/standard_functions/remainder.hpp"
-#include "math/free_functions/statistics/normal.hpp"
-
-/////////////////////////////////
-/// blas libraries
-/////////////////////////////////
-
-#include "math/linalg/blas/gemm_nn_vector.hpp"
-#include "math/linalg/blas/gemm_nn_vector_threaded.hpp"
-#include "math/linalg/blas/gemm_nt_vector.hpp"
-#include "math/linalg/blas/gemm_nt_vector_threaded.hpp"
-#include "math/linalg/blas/gemm_tn_vector.hpp"
-#include "math/linalg/blas/gemm_tn_vector_threaded.hpp"
-
 #include "math/meta/type_traits.hpp"
-
-#include "math/free_functions/exponentiation/exponentiation.hpp"
-#include "math/free_functions/trigonometry/trigonometry.hpp"
-#include "math/free_functions/comparison/comparison.hpp"
-#include "math/free_functions/statistics/distributions.hpp"
-#include "math/free_functions/precision/precision.hpp"
-#include "math/free_functions/iteration/iteration.hpp"
-#include "math/free_functions/deep_learning/activation_functions.hpp"
-#include "math/free_functions/deep_learning/loss_functions.hpp"
-#include "math/free_functions/matrix_operations/matrix_operations.hpp"
 
 namespace fetch {
 namespace math {
