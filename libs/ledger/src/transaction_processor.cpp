d8153f1d184c8d4c4adaf313db0a3080ed04260f
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2018-11-30 11:49:14 +0000

    Testing: Reconfigure setting back to transaction verification mode

--- libs/ledger/src/transaction_processor.cpp
+++ libs/ledger/src/transaction_processor.cpp
@@ -45,6 +45,7 @@ void TransactionProcessor::OnTransaction(chain::VerifiedTransaction const &tx)
 {
   FETCH_METRIC_TX_SUBMITTED(tx.digest());
 
+#if 0
   // dispatch the transaction to the storage engine
   storage_.AddTransaction(tx);
 
@@ -54,10 +55,18 @@ void TransactionProcessor::OnTransaction(chain::VerifiedTransaction const &tx)
   miner_.EnqueueTransaction(tx.summary());
 
   FETCH_METRIC_TX_QUEUED(tx.digest());
+#endif
 }
 
 void TransactionProcessor::OnTransactions(TransactionList const &txs)
 {
+#if 1
+  for (auto const &tx : txs)
+  {
+    (void)tx;
+    FETCH_METRIC_TX_SUBMITTED(tx.digest());
+  }
+#else
 #ifdef FETCH_ENABLE_METRICS
   auto const submitted = metrics::Metrics::Clock::now();
 #endif  // FETCH_ENABLE_METRICS
@@ -85,6 +94,7 @@ void TransactionProcessor::OnTransactions(TransactionList const &txs)
     FETCH_METRIC_TX_QUEUED_EX(tx.digest(), queued);
   }
 #endif  // FETCH_ENABLE_METRICS
+#endif
 }
 
 }  // namespace ledger
