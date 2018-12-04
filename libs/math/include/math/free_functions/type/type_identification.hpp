74bf1c132ba1c3a7907c688671bbb4f1b0891444
juliamgomes <juliagomes@berkeley.edu>
2018-12-04 11:11:34 +0000

    moved bitmask operations from type_identification file to matrix_operations file

--- libs/math/include/math/free_functions/type/type_identification.hpp
+++ libs/math/include/math/free_functions/type/type_identification.hpp
@@ -24,13 +24,6 @@
 namespace fetch {
 namespace math {
 
-template <typename T, typename C>
-class ShapeLessArray;
-template <typename T, typename C>
-class NDArray;
-template <typename T, typename C>
-class NDArrayIterator;
-
 namespace linalg {
 template <typename T, typename C, typename S>
 class Matrix;
@@ -38,85 +31,6 @@ class Matrix;
 
 template <typename T, typename C>
 T Max(ShapeLessArray<T, C> const &array);
-
-/**
- * calculates bit mask on this
- * @param x
- */
-namespace details {
-template <typename ArrayType>
-void BooleanMaskImplementation(ArrayType &input_array, ArrayType const &mask, ArrayType &ret)
-{
-  assert(input_array.size() == mask.size());
-
-  std::size_t counter = 0;
-  for (std::size_t i = 0; i < input_array.size(); ++i)
-  {
-    assert((mask[i] == 1) || (mask[i] == 0));
-    // TODO(private issue 193): implement boolean only ndarray to avoid cast
-    if (bool(mask[i]))
-    {
-      ret[counter] = input_array[i];
-      ++counter;
-    }
-  }
-
-  ret.LazyResize(counter);
-}
-}  // namespace details
-template <typename T, typename C>
-void BooleanMask(ShapeLessArray<T, C> &input_array, ShapeLessArray<T, C> const &mask,
-                 ShapeLessArray<T, C> &ret)
-{
-  details::BooleanMaskImplementation(input_array, mask, ret);
-}
-template <typename T, typename C>
-ShapeLessArray<T, C> BooleanMask(ShapeLessArray<T, C> &      input_array,
-                                 ShapeLessArray<T, C> const &mask)
-{
-  ShapeLessArray<T, C> ret;
-  BooleanMask(input_array, mask, ret);
-  return ret;
-}
-template <typename T, typename C>
-void BooleanMask(NDArray<T, C> &input_array, NDArray<T, C> &mask, NDArray<T, C> &ret)
-{
-  assert(input_array.shape().size() >= mask.shape().size());
-  assert(mask.shape().size() > 0);
-
-  // because tensorflow is row major by default - we have to flip the mask and array to get the same
-  // answer
-  // TODO(private issue 208)
-  input_array.MajorOrderFlip();
-  mask.MajorOrderFlip();
-
-  if (mask.shape() == input_array.shape())
-  {
-    details::BooleanMaskImplementation(input_array, mask, ret);
-  }
-  else
-  {
-    for (std::size_t j = 0; j < mask.shape().size(); ++j)
-    {
-      assert(mask.shape()[j] == input_array.shape()[j]);
-    }
-
-    // new shape should be n-k+1 dimensions
-    std::vector<std::size_t> new_shape;
-    NDArray<T, C>            ret{new_shape};
-
-    // TODO(private issue 207): perhaps a little bit hacky to implement boolean mask as a
-    // multiplication
-    Broadcast([](T x, T y) { return x * y; }, input_array, mask, ret);
-  }
-}
-template <typename T, typename C>
-NDArray<T, C> BooleanMask(NDArray<T, C> &input_array, NDArray<T, C> &mask)
-{
-  NDArray<T, C> ret;
-  BooleanMask(input_array, mask, ret);
-  return ret;
-}
 
 /**
  * finite check
