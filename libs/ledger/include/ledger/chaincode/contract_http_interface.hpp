b3f332ae0c327715668bee7bcba5e777e6909d42
Peter Bukva <peter.bukva@fetch.ai>
2018-12-10 15:21:32 +0000

    Incorporating review comments (doxygen doc + reloc. impl. to .cpp unit)

--- libs/ledger/include/ledger/chaincode/contract_http_interface.hpp
+++ libs/ledger/include/ledger/chaincode/contract_http_interface.hpp
@@ -41,241 +41,86 @@ namespace ledger {
 
 class ContractHttpInterface : public http::HTTPModule
 {
-  using SubmitTxRetval = std::pair<std::size_t, std::size_t>;
+
 
 public:
-  static constexpr char const *           LOGGING_NAME = "ContractHttpInterface";
-  static byte_array::ConstByteArray const API_PATH_CONTRACT_PREFIX;
-  static byte_array::ConstByteArray const CONTRACT_NAME_SEPARATOR;
-  static byte_array::ConstByteArray const PATH_SEPARATOR;
+  static constexpr char const *LOGGING_NAME = "ContractHttpInterface";
 
-  ContractHttpInterface(StorageInterface &storage, TransactionProcessor &processor)
-    : storage_{storage}
-    , processor_{processor}
-  {
-    byte_array::ByteArray contract_full_name;
-
-    // create all the contracts
-    auto const &contracts = contract_cache_.factory().GetContracts();
-    for (auto const &contract_name : contracts)
-    {
-      contract_full_name.Resize(0, ResizeParadigm::ABSOLUTE);
-      contract_full_name.Append(contract_name, CONTRACT_NAME_SEPARATOR);
-      std::size_t const contract_full_name_base_size = contract_full_name.size();
-
-      // create the contract
-      auto contract = contract_cache_.factory().Create(contract_name);
-
-      byte_array::ByteArray contract_path{contract_name};
-      contract_path.Replace(static_cast<char const &>(CONTRACT_NAME_SEPARATOR[0]),
-                            static_cast<char const &>(PATH_SEPARATOR[0]));
-
-      byte_array::ByteArray api_path;
-      //* ByteArry from `contract_name` performs deep copy due to const -> non-const
-      api_path.Append(API_PATH_CONTRACT_PREFIX, contract_path, PATH_SEPARATOR);
-      std::size_t const api_path_base_size = api_path.size();
-
-      // enumerate all of the contract query handlers
-      auto const &query_handlers = contract->query_handlers();
-      for (auto const &handler : query_handlers)
-      {
-        byte_array::ConstByteArray const &query_name = handler.first;
-        api_path.Resize(api_path_base_size, ResizeParadigm::ABSOLUTE);
-        api_path.Append(query_name);
-
-        // FETCH_LOG_INFO(LOGGING_NAME, "API: ", api_path);
-        FETCH_LOG_INFO(LOGGING_NAME, "QUERY API HANDLER: ", api_path);
-
-        Post(api_path, [this, contract_name, query_name](http::ViewParameters const &,
-                                                         http::HTTPRequest const &request) {
-          return OnQuery(contract_name, query_name, request);
-        });
-      }
-
-      auto const &transaction_handlers = contract->transaction_handlers();
-      for (auto const &handler : transaction_handlers)
-      {
-        byte_array::ConstByteArray const &query_name = handler.first;
-        api_path.Resize(api_path_base_size, ResizeParadigm::ABSOLUTE);
-        api_path.Append(query_name);
-
-        FETCH_LOG_INFO(LOGGING_NAME, "TX API HANDLER: ", api_path);
-
-        contract_full_name.Resize(contract_full_name_base_size, ResizeParadigm::ABSOLUTE);
-        contract_full_name.Append(query_name);
-
-        Post(api_path, [this, contract_full_name](http::ViewParameters const &params,
-                                                  http::HTTPRequest const &   request) {
-          return OnTransaction(params, request, &contract_full_name);
-        });
-      }
-    }
-
-    // add custom debug handlers
-    Post("/api/debug/submit",
-         [this](http::ViewParameters const &, http::HTTPRequest const &request) {
-           chain::MutableTransaction tx;
-
-           tx.PushResource("foo.bar.baz" + std::to_string(transaction_index_));
-           tx.set_fee(transaction_index_);
-           tx.set_contract_name("fetch.dummy.run");
-           tx.set_data(std::to_string(transaction_index_++));
-
-           processor_.AddTransaction(tx);
-
-           std::ostringstream oss;
-           oss << R"({ "submitted": true })";
-
-           return http::CreateJsonResponse(oss.str());
-         });
-
-    // TODO(issue 414): Remove legacy/unused/legacy endpoints from public HTTP interface
-    Post("/api/contract/fetch/token/submit",
-         [this](http::ViewParameters const &params, http::HTTPRequest const &request) {
-           return OnTransaction(params, request);
-         });
-
-    // TODO(issue 414): Remove legacy/unused/legacy endpoints from public HTTP interface
-    Post("/api/contract/submit",
-         [this](http::ViewParameters const &params, http::HTTPRequest const &request) {
-           return OnTransaction(params, request);
-         });
-  }
+  ContractHttpInterface(StorageInterface &storage, TransactionProcessor &processor);
 
 private:
   http::HTTPResponse OnQuery(byte_array::ConstByteArray const &contract_name,
                              byte_array::ConstByteArray const &query,
-                             http::HTTPRequest const &         request)
-  {
-    try
-    {
-      FETCH_LOG_INFO(LOGGING_NAME, "OnQuery");
-      FETCH_LOG_DEBUG(LOGGING_NAME, request.body());
-
-      // parse the incoming request
-      json::JSONDocument doc;
-      doc.Parse(request.body());
-
-      // dispatch the contract type
-      variant::Variant response;
-      auto             contract = contract_cache_.Lookup(contract_name);
-
-      // attach, dispatch and detach
-      contract->Attach(storage_);
-      auto const status = contract->DispatchQuery(query, doc.root(), response);
-      contract->Detach();
-
-      if (status == Contract::Status::OK)
-      {
-        // encode the response
-        std::ostringstream oss;
-        oss << response;
-
-        // generate the response object
-        return http::CreateJsonResponse(oss.str());
-      }
-      else
-      {
-        FETCH_LOG_WARN(LOGGING_NAME, "Error running query. status = ", static_cast<int>(status));
-      }
-    }
-    catch (std::exception &ex)
-    {
-      FETCH_LOG_WARN(LOGGING_NAME, "Query error: ", ex.what());
-    }
-
-    return JsonBadRequest();
-  }
+                             http::HTTPRequest const &         request);
 
   http::HTTPResponse OnTransaction(
       http::ViewParameters const &, http::HTTPRequest const &request,
-      byte_array::ConstByteArray const *const expected_contract_name = nullptr)
-  {
-    std::ostringstream oss;
-    bool               error_response{true};
-    try
-    {
-      SubmitTxRetval submitted;
-
-      // detect the content format, defaulting to json
-      byte_array::ConstByteArray content_type = "application/json";
-      if (request.header().Has("content-type"))
-      {
-        content_type = request.header()["content-type"];
-      }
-
-      // handle the types of transaction
-      bool unknown_format = false;
-      if (content_type == "application/vnd+fetch.transaction+native")
-      {
-        submitted = SubmitNativeTx(request, expected_contract_name);
-      }
-      else if (content_type == "application/vnd+fetch.transaction+json" ||
-               content_type == "application/json")
-      {
-        submitted = SubmitJsonTx(request, expected_contract_name);
-      }
-      else
-      {
-        unknown_format = true;
-      }
-
-      if (unknown_format)
-      {
-        // format the message
-        std::ostringstream error_msg;
-        error_msg << "Unknown content type: " << content_type;
-
-        oss << R"({ "submitted": false, "error": )" << std::quoted(error_msg.str()) << " }";
-      }
-      else if (submitted.first != submitted.second)
-      {
-        // format the message
-        std::ostringstream error_msg;
-        error_msg << "Some transactions has not submitted due to wrong contract type."
-                  << content_type;
-
-        oss << R"({ "submitted": false, "count": )" << submitted.first << R"(, "expected_count": )"
-            << submitted.second
-            << R"(, "error": "Some transactions have NOT been submitted due to miss-matching contract name."})";
-      }
-      else
-      {
-        // success report the statistics
-        oss << R"({ "submitted": true, "count": )" << submitted.first << " }";
-        error_response = false;
-      }
-    }
-    catch (std::exception const &ex)
-    {
-      oss.clear();
-      oss << R"({ "submitted": false, "error": ")" << std::quoted(ex.what()) << " }";
-    }
-
-    return http::CreateJsonResponse(oss.str(), (error_response)
-                                                   ? http::Status::CLIENT_ERROR_BAD_REQUEST
-                                                   : http::Status::SUCCESS_OK);
-  }
-
-  static http::HTTPResponse JsonBadRequest()
-  {
-    return http::CreateJsonResponse("", http::Status::CLIENT_ERROR_BAD_REQUEST);
-  }
-
-  SubmitTxRetval SubmitJsonTx(
-      http::HTTPRequest const &               request,
       byte_array::ConstByteArray const *const expected_contract_name = nullptr);
-  SubmitTxRetval SubmitNativeTx(
+
+  /** @brief Structure containing status of of multi-transaction submission.
+   *
+   *  The structure is supposed to carry information about how many transactions
+   *  have been successfully processed and how many transactions have been
+   *  actually received for processing.
+   *  The structure is supposed to be used as return value for methods which
+   *  are dedicated to handle HTTP request for bulk transaction (multi-transaction)
+   *  reception, giving caller ability to check status of how request has been
+   *  handled (transaction reception/processing).
+   *
+   * @see SubmitJsonTx
+   * @see SubmitNativeTx
+   */
+  struct SubmitTxStatus
+  {
+    std::size_t processed;
+    std::size_t received;
+  };
+
+  /**
+   * Method handles incoming http request containing single or bulk of JSON formatted
+   * Wire transactions.
+   *
+   * @param request https request containing single or bulk of JSON formatted Wire Transaction(s).
+   * @param expected_contract_name contract name each transaction in request must conform to.
+   *        Transactions which do NOT conform to this contract name will NOT be accepted further
+   *        processing. If the value is `nullptr` (default value) the contract name check is
+   *        DISABLED, and so transactions (each received transaction in bulk) can have any contract name.
+   *
+   * @return submit status, please see the `SubmitTxStatus` structure
+   * @see SubmitTxStatus
+
+   * @see SubmitNativeTx
+   */
+  SubmitTxStatus SubmitJsonTx(
       http::HTTPRequest const &               request,
       byte_array::ConstByteArray const *const expected_contract_name = nullptr);
 
-  std::size_t transaction_index_{0};
+  /**
+   * Method handles incoming http request containing single or bulk of Native formattd
+   * Wire transactions.
+   *
+   * This method was originally designed for benchmark/stress-test purposes, but can be used in
+   * production environment.
+   *
+   * @param request https request containing single or bulk of JSON formatted Wire Transaction(s).
+   * @param expected_contract_name contract name each transaction in request must conform to.
+   *        Transactions which do NOT conform to this contract name will NOT be accepted further
+   *        processing. If the value is `nullptr` (default value) the contract name check is
+   *        DISABLED, and so transactions (each received transaction in bulk) can have any contract name.
+   *
+   * @return submit status, please see the `SubmitTxStatus` structure
+   * @see SubmitTxStatus
 
+   * @see SubmitJsonTx
+   */
+  SubmitTxStatus SubmitNativeTx(
+      http::HTTPRequest const &               request,
+      byte_array::ConstByteArray const *const expected_contract_name = nullptr);
+
+  std::size_t           transaction_index_{0};
   StorageInterface &    storage_;
   TransactionProcessor &processor_;
   ChainCodeCache        contract_cache_;
-
-  // static TransactionHandlerMap const transaction_handlers_;
 };
 
 }  // namespace ledger
