53d8106adc89e9e0e4a3e7f14f9aa321c96819a2
Pierre Wilmot <pierre.wilmot@fetch.ai>
2019-01-07 18:10:58 +0000

    Introduce Nodes & Graph

---
+++ libs/ml/include/ml/node.hpp
@@ -0,0 +1,76 @@
+#pragma once
+//------------------------------------------------------------------------------
+//
+//   Copyright 2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "ops/ops.hpp"
+#include <iostream>
+#include <memory>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace fetch {
+namespace ml {
+
+template <class T>
+class NodeInterface
+{
+public:
+  using ArrayType    = T;
+  using ArrayPtrType = std::shared_ptr<ArrayType>;
+
+  virtual ArrayPtrType Evaluate() = 0;
+  virtual void AddInput(std::shared_ptr<NodeInterface<T>> const &i) = 0;
+};
+
+template <class T, class O>
+class Node : public NodeInterface<T>, public O
+{
+public:
+  using ArrayType    = T;
+  using ArrayPtrType = std::shared_ptr<ArrayType>;
+
+  Node(std::string const name)
+    : name_(std::move(name))
+  {}
+
+  virtual ~Node() = default;
+
+  ArrayPtrType Evaluate()
+  {
+    std::vector<ArrayPtrType> inputs;
+    for (auto const &i : inputs_)
+    {
+      inputs.push_back(i->Evaluate());
+    }
+    std::cout << "Evaluating node [" << name_ << "]" << std::endl;
+    return this->Forward(inputs);
+  }
+
+  void AddInput(std::shared_ptr<NodeInterface<T>> const &i)
+  {
+    inputs_.push_back(i);
+  }
+
+private:
+  std::vector<std::shared_ptr<NodeInterface<T>>> inputs_;
+  std::string                                    name_;
+};
+
+}  // namespace ml
+}  // namespace fetch
---
+++ libs/ml/tests/ml/ops/node.cpp
@@ -0,0 +1,75 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "ml/node.hpp"
+#include "math/ndarray.hpp"
+#include "ml/ops/placeholder.hpp"
+#include "ml/ops/relu.hpp"
+#include <gtest/gtest.h>
+
+TEST(node_test, node_placeholder)
+{
+  fetch::ml::Node<fetch::math::NDArray<int>, fetch::ml::ops::PlaceHolder<fetch::math::NDArray<int>>>
+                                             placeholder("PlaceHolder");
+  std::shared_ptr<fetch::math::NDArray<int>> data =
+      std::make_shared<fetch::math::NDArray<int>>(std::vector<size_t>({5, 5}));
+  placeholder.SetData(data);
+
+  EXPECT_EQ(placeholder.Forward({}), data);  // Testing pointer value
+  EXPECT_EQ(placeholder.Evaluate(), data);   // Testing pointer value
+}
+
+TEST(node_test, node_relu)
+{
+  std::shared_ptr<fetch::ml::Node<fetch::math::NDArray<int>,
+                                  fetch::ml::ops::PlaceHolder<fetch::math::NDArray<int>>>>
+      placeholder =
+          std::make_shared<fetch::ml::Node<fetch::math::NDArray<int>,
+                                           fetch::ml::ops::PlaceHolder<fetch::math::NDArray<int>>>>(
+              "PlaceHolder");
+
+  std::shared_ptr<fetch::ml::Node<fetch::math::NDArray<int>,
+                                  fetch::ml::ops::ReluLayer<fetch::math::NDArray<int>>>>
+      relu =
+          std::make_shared<fetch::ml::Node<fetch::math::NDArray<int>,
+                                           fetch::ml::ops::ReluLayer<fetch::math::NDArray<int>>>>(
+              "Relu");
+
+  relu->AddInput(placeholder);
+
+  std::shared_ptr<fetch::math::NDArray<int>> data =
+      std::make_shared<fetch::math::NDArray<int>>(std::vector<size_t>({4, 4}));
+  std::shared_ptr<fetch::math::NDArray<int>> gt =
+      std::make_shared<fetch::math::NDArray<int>>(std::vector<size_t>({4, 4}));
+  std::vector<int> dataValues({0, -1, 2, -3, 4, -5, 6, -7, 8, -9, 10, -11, 12, -13, 14, -15, 16});
+  std::vector<int> gtValues({0, 0, 2, 0, 4, 0, 6, 0, 8, 0, 10, 0, 12, 0, 14, 0, 16});
+  placeholder->SetData(data);
+
+  for (size_t i(0); i < 4; ++i)
+  {
+    for (size_t j(0); j < 4; ++j)
+    {
+      data->Set(std::vector<size_t>({i, j}), dataValues[i * 4 + j]);
+      gt->Set(std::vector<size_t>({i, j}), gtValues[i * 4 + j]);
+    }
+  }
+
+  EXPECT_EQ(placeholder->Forward({}), data);  // Testing pointer value
+  EXPECT_EQ(placeholder->Evaluate(), data);   // Testing pointer value
+  EXPECT_TRUE(relu->Evaluate()->AllClose(*gt));
+}
