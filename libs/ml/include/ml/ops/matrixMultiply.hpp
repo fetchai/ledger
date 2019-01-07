b786706ff4b1e1f6d1920c2c61f1747b890ced45
Pierre Wilmot <pierre.wilmot@fetch.ai>
2019-01-07 11:49:12 +0000

    Redefine MatrixMultiply as an subclass of ops + tests

--- libs/ml/include/ml/ops/matrixMultiply.hpp
+++ libs/ml/include/ml/ops/matrixMultiply.hpp
@@ -19,6 +19,7 @@
 
 #include "math/free_functions/free_functions.hpp"
 #include "ml/ops/derivatives/derivatives.hpp"
+#include "math/free_functions/matrix_operations/matrix_operations.hpp"
 #include "ml/ops/ops.hpp"
 
 namespace fetch {
@@ -26,42 +27,45 @@ namespace ml {
 namespace ops {
 
   template <class T>
-  class ReluLayer : public fetch::ml::Ops<T>
+  class MatrixMultiply : public fetch::ml::Ops<T>
   {
   public:
     using ArrayType         = T;
     using ArrayPtrType      = std::shared_ptr<ArrayType>;
 
-    ReluLayer() = default;
+    MatrixMultiply() = default;
 
     virtual ArrayPtrType forward(std::vector<ArrayPtrType> const & inputs)
     {
-      assert(inputs.size() == 1);
+      assert(inputs.size() == 2);
+      assert(inputs[0]->shape().size() == 2);
+      assert(inputs[1]->shape().size() == 2);
+      assert(inputs[0]->shape()[1] == inputs[1]->shape()[0]);
 
-      if (!zeroes_ || zeroes_->shape() != inputs[0]->shape())
-      	zeroes_ = std::make_shared<ArrayType>(inputs[0]->shape());
+      // std::cout << "[" << inputs[0]->shape()[0] << " x " << inputs[0]->shape()[1] <<
+      // 	"] * [" << inputs[1]->shape()[0] << " x " << inputs[1]->shape()[1] << "]" <<
+      // 	" => [" << inputs[0]->shape()[0] << " x " <<  inputs[1]->shape()[1] << "]"<< std::endl;	
+      
+      std::vector<size_t> outputShape({inputs[0]->shape()[0], inputs[1]->shape()[1]});
+      if (!this->output_ || this->output_->shape() != outputShape)
+        this->output_ = std::make_shared<ArrayType>(outputShape);
 
-      if (!this->output_ || this->output_->shape() != inputs[0]->shape())
-        this->output_ = std::make_shared<ArrayType>(inputs[0]->shape());
-
-      fetch::math::Maximum(*(inputs[0]), *zeroes_, *this->output_);
+      fetch::math::Dot(*(inputs[0]), *(inputs[1]), *(this->output_));
       
       return this->output_;
     }
     
     virtual std::vector<ArrayPtrType> backward(std::vector<ArrayPtrType> const & inputs, ArrayPtrType errorSignal)
     {
-      assert(inputs.size() == 1);
-      assert(inputs[0]->shape() == errorSignal->shape());
+      assert(inputs.size() == 2);
 
-      for (std::size_t i = 0; i < inputs[0]->size(); ++i)
-	{
-	  if ((*(inputs[0]))[i] <= 0)
-	    {
-	      errorSignal->Set(i, 0);
-	    }
-	}
-      return {errorSignal};
+      ArrayPtrType errorSignal1 = std::make_shared<ArrayType>(inputs[0]->shape());
+      ArrayPtrType errorSignal2 = std::make_shared<ArrayType>(inputs[1]->shape());
+      
+      fetch::math::DotTranspose(*errorSignal, *inputs[1], *errorSignal1);
+      fetch::math::TransposeDot(*inputs[0], *errorSignal, *errorSignal2);
+
+      return {errorSignal1, errorSignal2};
     }
     
   private:
