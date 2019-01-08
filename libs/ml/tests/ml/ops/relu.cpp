1a235b34d2941152a22b7e30f472e7a6222c48eb
Pierre Wilmot <pierre.wilmot@fetch.ai>
2019-01-08 12:09:38 +0000

    Extend relu test to include a mixture of NDArray / linalg::Matrix && int / float / double

--- libs/ml/tests/ml/ops/relu.cpp
+++ libs/ml/tests/ml/ops/relu.cpp
@@ -1,4 +1,3 @@
-
 //------------------------------------------------------------------------------
 //
 //   Copyright 2018-2019 Fetch.AI Limited
@@ -19,12 +18,19 @@
 
 #include "ml/ops/relu.hpp"
 #include "math/ndarray.hpp"
+#include "math/linalg/matrix.hpp"
 #include <gtest/gtest.h>
 
-TEST(relu_test, forward_all_positive_integer_test)
+template <typename T>
+class ReluTest : public ::testing::Test {};
+
+using MyTypes = ::testing::Types<fetch::math::NDArray<int>, fetch::math::NDArray<float>, fetch::math::NDArray<double>, fetch::math::linalg::Matrix<int>, fetch::math::linalg::Matrix<float>, fetch::math::linalg::Matrix<double>>;
+TYPED_TEST_CASE(ReluTest, MyTypes);
+  
+TYPED_TEST(ReluTest, forward_all_positive_test)
 {
-  std::shared_ptr<fetch::math::NDArray<int>> data = std::make_shared<fetch::math::NDArray<int>>(8);
-  std::shared_ptr<fetch::math::NDArray<int>> gt   = std::make_shared<fetch::math::NDArray<int>>(8);
+  std::shared_ptr<TypeParam> data = std::make_shared<TypeParam>(8);
+  std::shared_ptr<TypeParam> gt   = std::make_shared<TypeParam>(8);
   size_t                                     i(0);
   for (int e : {1, 2, 3, 4, 5, 6, 7, 8})
   {
@@ -32,17 +38,17 @@ TEST(relu_test, forward_all_positive_integer_test)
     gt->Set(i, e);
     i++;
   }
-  fetch::ml::ops::ReluLayer<fetch::math::NDArray<int>> op;
-  std::shared_ptr<fetch::math::NDArray<int>>           prediction = op.Forward({data});
+  fetch::ml::ops::ReluLayer<TypeParam> op;
+  std::shared_ptr<TypeParam>           prediction = op.Forward({data});
 
   // test correct values
-  ASSERT_TRUE(prediction->AllClose(*gt));
+  ASSERT_TRUE(prediction->AllClose(*gt));  
 }
 
-TEST(relu_test, forward_all_negative_integer_test)
+TYPED_TEST(ReluTest, forward_all_negative_integer_test)
 {
-  std::shared_ptr<fetch::math::NDArray<int>> data = std::make_shared<fetch::math::NDArray<int>>(8);
-  std::shared_ptr<fetch::math::NDArray<int>> gt   = std::make_shared<fetch::math::NDArray<int>>(8);
+  std::shared_ptr<TypeParam> data = std::make_shared<TypeParam>(8);
+  std::shared_ptr<TypeParam> gt   = std::make_shared<TypeParam>(8);
   size_t                                     i(0);
   for (int e : {-1, -2, -3, -4, -5, -6, -7, -8})
   {
@@ -50,17 +56,17 @@ TEST(relu_test, forward_all_negative_integer_test)
     gt->Set(i, 0);
     i++;
   }
-  fetch::ml::ops::ReluLayer<fetch::math::NDArray<int>> op;
-  std::shared_ptr<fetch::math::NDArray<int>>           prediction = op.Forward({data});
+  fetch::ml::ops::ReluLayer<TypeParam> op;
+  std::shared_ptr<TypeParam>           prediction = op.Forward({data});
 
   // test correct values
   ASSERT_TRUE(prediction->AllClose(*gt));
 }
 
-TEST(relu_test, forward_mixed_integer_test)
+TYPED_TEST(ReluTest, forward_mixed_test)
 {
-  std::shared_ptr<fetch::math::NDArray<int>> data = std::make_shared<fetch::math::NDArray<int>>(8);
-  std::shared_ptr<fetch::math::NDArray<int>> gt   = std::make_shared<fetch::math::NDArray<int>>(8);
+  std::shared_ptr<TypeParam> data = std::make_shared<TypeParam>(8);
+  std::shared_ptr<TypeParam> gt   = std::make_shared<TypeParam>(8);
   std::vector<int>                           dataInput({1, -2, 3, -4, 5, -6, 7, -8});
   std::vector<int>                           gtInput({1, 0, 3, 0, 5, 0, 7, 0});
   for (size_t i(0); i < 8; ++i)
@@ -68,18 +74,18 @@ TEST(relu_test, forward_mixed_integer_test)
     data->Set(i, dataInput[i]);
     gt->Set(i, gtInput[i]);
   }
-  fetch::ml::ops::ReluLayer<fetch::math::NDArray<int>> op;
-  std::shared_ptr<fetch::math::NDArray<int>>           prediction = op.Forward({data});
+  fetch::ml::ops::ReluLayer<TypeParam> op;
+  std::shared_ptr<TypeParam>           prediction = op.Forward({data});
 
   // test correct values
   ASSERT_TRUE(prediction->AllClose(*gt));
 }
 
-TEST(relu_test, backward_mixed_integer_test)
+TYPED_TEST(ReluTest, backward_mixed_test)
 {
-  std::shared_ptr<fetch::math::NDArray<int>> data  = std::make_shared<fetch::math::NDArray<int>>(8);
-  std::shared_ptr<fetch::math::NDArray<int>> error = std::make_shared<fetch::math::NDArray<int>>(8);
-  std::shared_ptr<fetch::math::NDArray<int>> gt    = std::make_shared<fetch::math::NDArray<int>>(8);
+  std::shared_ptr<TypeParam> data  = std::make_shared<TypeParam>(8);
+  std::shared_ptr<TypeParam> error = std::make_shared<TypeParam>(8);
+  std::shared_ptr<TypeParam> gt    = std::make_shared<TypeParam>(8);
   std::vector<int>                           dataInput({1, -2, 3, -4, 5, -6, 7, -8});
   std::vector<int>                           errorInput({-1, 2, 3, -5, -8, 13, -21, -34});
   std::vector<int>                           gtInput({-1, 0, 3, 0, -8, 0, -21, 0});
@@ -89,30 +95,9 @@ TEST(relu_test, backward_mixed_integer_test)
     error->Set(i, errorInput[i]);
     gt->Set(i, gtInput[i]);
   }
-  fetch::ml::ops::ReluLayer<fetch::math::NDArray<int>>    op;
-  std::vector<std::shared_ptr<fetch::math::NDArray<int>>> prediction = op.Backward({data}, error);
+  fetch::ml::ops::ReluLayer<TypeParam>    op;
+  std::vector<std::shared_ptr<TypeParam>> prediction = op.Backward({data}, error);
 
   // test correct values
   ASSERT_TRUE(prediction[0]->AllClose(*gt));
-}
-
-TEST(relu_test, forward_mixed_float_test)
-{
-  std::shared_ptr<fetch::math::NDArray<float>> data =
-      std::make_shared<fetch::math::NDArray<float>>(8);
-  std::shared_ptr<fetch::math::NDArray<float>> gt =
-      std::make_shared<fetch::math::NDArray<float>>(8);
-  std::vector<float> dataInput(
-      {1.1f, -2.22f, 3.333f, -4.4444f, 5.55555f, -6.666666f, 7.7777777f, -8.88888888f});
-  std::vector<float> gtInput({1.1f, 0.0f, 3.333f, 0.0f, 5.55555f, 0.0f, 7.7777777f, 0.0f});
-  for (size_t i(0); i < 8; ++i)
-  {
-    data->Set(i, dataInput[i]);
-    gt->Set(i, gtInput[i]);
-  }
-  fetch::ml::ops::ReluLayer<fetch::math::NDArray<float>> op;
-  std::shared_ptr<fetch::math::NDArray<float>>           prediction = op.Forward({data});
-
-  // test correct values
-  ASSERT_TRUE(prediction->AllClose(*gt));
 }
