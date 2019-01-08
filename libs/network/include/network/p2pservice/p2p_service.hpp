b72c3691f76dd17970116d3d0314ac0648a8fdb8
Katie Lucas <katie.lucas@fetch.ai>
2019-01-08 15:58:54 +0000

    Whups

--- libs/network/include/network/p2pservice/p2p_service.hpp
+++ libs/network/include/network/p2pservice/p2p_service.hpp
@@ -75,7 +75,6 @@ public:
   using PeerTrust            = TrustInterface::PeerTrust;
 
   static constexpr char const *LOGGING_NAME              = "P2PService";
-  static constexpr std::size_t const MAX_PEERS_PER_CYCLE = 32;
 
   // Construction / Destruction
   P2PService(Muddle &muddle, LaneManagement &lane_management, TrustInterface &trust,
