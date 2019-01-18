d8bf5f253599181d29e0ddd75c618e6bb55576c9
Khan Baykaner <khan.baykaner@fetch.ai>
2019-01-18 18:48:24 +0000

    WIP

---
+++ libs/auctions/examples/auction_smart_contract/auction_smart_contract.cpp
@@ -0,0 +1,70 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+//#include "ledger/chaincode/dummy_contract.hpp"
+#include "auctions/auction_smart_contract.hpp"
+#include <chrono>
+#include <random>
+
+static constexpr std::size_t MINIMUM_TIME = 50;
+static constexpr std::size_t MAXIMUM_TIME = 200;
+static constexpr std::size_t DELTA_TIME   = MAXIMUM_TIME - MINIMUM_TIME;
+
+
+fetch::auctions::VickreyAuctionContract::VickreyAuctionContract() : ledger::Contract("fetch.dummy")
+{
+  OnTransaction("wait", this, &VickreyAuctionContract::Wait);
+  OnTransaction("run", this, &VickreyAuctionContract::Run);
+}
+
+fetch::auctions::VickreyAuctionContract::Status fetch::auctions::VickreyAuctionContract::Wait(Transaction const &)
+{
+  std::random_device rd;
+  std::mt19937       rng;
+  rng.seed(rd());
+
+  // generate a random amount of 'work' time
+  std::size_t const work_time = (rng() % DELTA_TIME) + MINIMUM_TIME;
+
+  auto counter = counter_.load();
+
+  FETCH_LOG_INFO(LOGGING_NAME, "Running complicated transaction ", counter, " ... ");
+
+  // wait for the work time
+  std::this_thread::sleep_for(std::chrono::milliseconds(work_time));
+
+  FETCH_LOG_INFO(LOGGING_NAME, "Running complicated transaction ", counter, " ... complete");
+
+  ++counter_;
+
+  return Status::OK;
+}
+
+fetch::auctions::VickreyAuctionContract::Status fetch::auctions::VickreyAuctionContract::Run(Transaction const &)
+{
+  FETCH_LOG_DEBUG(LOGGING_NAME, "Running that contract...");
+  return Status::OK;
+}
+
+int main()
+{
+
+  fetch::auctions::VickreyAuctionContract va{};
+
+  return 0 ;
+}
