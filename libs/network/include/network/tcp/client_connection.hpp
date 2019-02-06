ba07c1dda526fa34392684e48b0982a6f85e6d31
nhutton <nathan.hutton@fetch.ai>
2019-02-06 13:55:05 +0000

    try fixing client connection

--- libs/network/include/network/tcp/client_connection.hpp
+++ libs/network/include/network/tcp/client_connection.hpp
@@ -24,6 +24,7 @@
 #include "core/serializers/byte_array_buffer.hpp"
 #include "network/management/client_manager.hpp"
 #include "network/message.hpp"
+#include "network/management/network_manager.hpp"
 
 #include "network/fetch_asio.hpp"
 #include <atomic>
@@ -37,7 +38,7 @@ namespace network {
  * The class will generically push data to its manager, and also allow pushing
  * data to the connected client.
  */
-class ClientConnection : public AbstractConnection
+class ClientConnection final: public AbstractConnection
 {
 public:
   static constexpr char const *LOGGING_NAME = "ClientConnection";
@@ -47,10 +48,14 @@ public:
   using handle_type = typename AbstractConnection::connection_handle_type;
 
   ClientConnection(std::weak_ptr<asio::ip::tcp::tcp::socket> socket,
-                   std::weak_ptr<ClientManager>              manager)
+                   std::weak_ptr<ClientManager>              manager,
+                   NetworkManager                            network_manager
+                   )
     : socket_(std::move(socket))
     , manager_(std::move(manager))
+    , network_manager_(network_manager)
     , write_mutex_(__LINE__, __FILE__)
+    , write_in_use_mutex_(__LINE__, __FILE__)
   {
     LOG_STACK_TRACE_POINT;
     auto socket_ptr = socket_.lock();
@@ -85,6 +90,13 @@ public:
     }
 
     ptr->Leave(this->handle());
+
+    running_ = false;
+
+    if(thread_)
+    {
+      thread_->join();
+    }
   }
 
   void Start()
@@ -98,10 +110,19 @@ public:
 
     ptr->Join(shared_from_this());
     ReadHeader();
+
+    running_ = true;
+    thread_ = std::make_unique<std::thread>([this]() { this->WriteLoop(); });
   }
 
   void Send(message_type const &msg) override
   {
+
+    if(msg.size() == 2508)
+    {
+      ERROR_BACKTRACE;
+    }
+
     LOG_STACK_TRACE_POINT;
 
     if (shutting_down_)
@@ -109,17 +130,11 @@ public:
       return;
     }
 
+    //FETCH_LOG_INFO(LOGGING_NAME, "Pushing message to queue");
     write_mutex_.lock();
-    bool write_in_progress = !write_queue_.empty();
     write_queue_.push_back(msg);
     write_mutex_.unlock();
-
-    FETCH_LOG_DEBUG(LOGGING_NAME, "Sendin g Message");
-
-    if (!write_in_progress)
-    {
-      Write();
-    }
+    //FETCH_LOG_INFO(LOGGING_NAME, "Finished pushing message to queue");
   }
 
   uint16_t Type() const override
@@ -152,6 +167,155 @@ public:
   }
 
 private:
+
+  void WriteLoop()
+  {
+    FETCH_LOG_INFO(LOGGING_NAME, "Entering write loop");
+
+    while(running_ && !shutting_down_)
+    {
+
+      write_in_use_mutex_.lock();
+
+      // Verify the socket is ready to accept a write
+      if(write_in_use_.use_count() != 1)
+      {
+        /*FETCH_LOG_INFO(LOGGING_NAME, "Failed to get use_count: ", write_in_use_.use_count());*/
+        write_in_use_mutex_.unlock();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        continue;
+      }
+
+      auto write_in_use_copy = write_in_use_;
+
+      /*FETCH_LOG_INFO(LOGGING_NAME, "Now count: ", write_in_use_.use_count());
+      FETCH_LOG_INFO(LOGGING_NAME, "Now count: ", write_in_use_copy.use_count()); */
+
+      write_in_use_mutex_.unlock();
+
+      FETCH_LOG_INFO(LOGGING_NAME, "Writing to socket");
+
+      // Try to get a message from the queue
+      write_mutex_.lock();
+
+      if (write_queue_.empty())
+      {
+        write_mutex_.unlock();
+        std::this_thread::sleep_for(std::chrono::milliseconds(2));
+        continue;
+      }
+
+      auto buffer = write_queue_.front();
+
+      byte_array::ByteArray header;
+      SetHeader(header, buffer.size());
+      write_queue_.pop_front();
+      write_mutex_.unlock();
+
+      FETCH_LOG_INFO(LOGGING_NAME, "Socket write. Size: ", buffer.size());
+
+      // Scope for closures
+      {
+        // Send the message, make sure write_in_use_ use count will be kept alive as long as the CB is.
+        auto self = shared_from_this();
+        auto weak_socket = socket_;
+
+        // Post a closure to asio
+        network_manager_.Post([this, self, weak_socket, buffer, write_in_use_copy, header]
+            {
+              // 
+              auto socket_ptr = socket_.lock();
+
+              if(!socket_ptr)
+              {
+                return;
+              }
+
+              // Note this cb keeps header and buffer alive
+              auto cb   = [this, buffer, socket_ptr, header, self](std::error_code ec, std::size_t) {
+                auto ptr = manager_.lock();
+                if (!ptr)
+                {
+                  FETCH_LOG_WARN(LOGGING_NAME, "Server: Failed to contact manager.");
+                  return;
+                }
+
+                if (!ec)
+                {
+                  FETCH_LOG_DEBUG(LOGGING_NAME, "Server: Successfully wrote message.");
+                }
+                else
+                {
+                  FETCH_LOG_WARN(LOGGING_NAME, "Server: Failed to write message. Leaving.");
+                  ptr->Leave(this->handle());
+                }
+              };
+
+              std::vector<asio::const_buffer> buffers{asio::buffer(header.pointer(), header.size()),
+                                                      asio::buffer(buffer.pointer(), buffer.size())};
+
+              asio::async_write(*socket_ptr, buffers, cb);
+
+            });
+      }
+    }
+  }
+
+  /*
+  void thing()
+  {
+    if (shutting_down_)
+    {
+      return;
+    }
+
+    LOG_STACK_TRACE_POINT;
+    auto socket_ptr = socket_.lock();
+    if (!socket_ptr)
+    {
+      return;
+    }
+
+    write_mutex_.lock();
+
+    if (write_queue_.empty())
+    {
+      write_mutex_.unlock();
+      return;
+    }
+
+    auto buffer = write_queue_.front();
+
+    byte_array::ByteArray header;
+    SetHeader(header, buffer.size());
+    write_queue_.pop_front();
+    write_mutex_.unlock();
+
+    auto self = shared_from_this();
+    auto cb   = [this, buffer, socket_ptr, header, self](std::error_code ec, std::size_t) {
+      auto ptr = manager_.lock();
+      if (!ptr)
+      {
+        return;
+      }
+
+      if (!ec)
+      {
+        FETCH_LOG_DEBUG(LOGGING_NAME, "Server: Wrote message.");
+        Write();
+      }
+      else
+      {
+        ptr->Leave(this->handle());
+      }
+    };
+
+    std::vector<asio::const_buffer> buffers{asio::buffer(header.pointer(), header.size()),
+                                            asio::buffer(buffer.pointer(), buffer.size())};
+
+    asio::async_write(*socket_ptr, buffers, cb);
+  }*/
+
   void ReadHeader()
   {
     if (shutting_down_)
@@ -262,6 +426,7 @@ private:
     }
   }
 
+  /*
   void Write()
   {
     if (shutting_down_)
@@ -314,14 +479,20 @@ private:
                                             asio::buffer(buffer.pointer(), buffer.size())};
 
     asio::async_write(*socket_ptr, buffers, cb);
-  }
+  } */
 
   std::atomic<bool>                         shutting_down_{false};
   std::weak_ptr<asio::ip::tcp::tcp::socket> socket_;
   std::weak_ptr<ClientManager>              manager_;
   message_queue_type                        write_queue_;
+  NetworkManager                            network_manager_;
   fetch::mutex::Mutex                       write_mutex_;
   std::string                               address_;
+  std::unique_ptr<std::thread>              thread_;
+  bool                                      running_{false};
+
+  std::shared_ptr<int>                      write_in_use_ = std::make_shared<int>(0);
+  fetch::mutex::Mutex                       write_in_use_mutex_;
 
   // TODO(issue 17): put this in shared class
   static const uint64_t networkMagic_ = 0xFE7C80A1FE7C80A1;
