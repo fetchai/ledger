8e655c7033e8c5bce14d45d2f3ac67e77caba298
Pierre Wilmot <pierre.wilmot@fetch.ai>
2019-02-07 15:30:27 +0000

    Run Mareix Multiply Test with FP

--- libs/ml/tests/ml/ops/matrix_multiply.cpp
+++ libs/ml/tests/ml/ops/matrix_multiply.cpp
@@ -28,7 +28,9 @@ class MatrixMultiplyTest : public ::testing::Test
 
 using MyTypes = ::testing::Types<fetch::math::Tensor<int>, fetch::math::Tensor<float>,
                                  fetch::math::Tensor<double>, fetch::math::NDArray<int>,
-                                 fetch::math::NDArray<float>, fetch::math::NDArray<double>>;
+                                 fetch::math::NDArray<float>, fetch::math::NDArray<double>,
+                                 fetch::math::Tensor<fetch::fixed_point::FixedPoint<16, 16>>,
+                                 fetch::math::Tensor<fetch::fixed_point::FixedPoint<32, 32>>>;
 TYPED_TEST_CASE(MatrixMultiplyTest, MyTypes);
 
 TYPED_TEST(MatrixMultiplyTest, forward_test)
