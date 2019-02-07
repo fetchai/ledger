7074e5cfaf73740221fe2135cc89615c3fc59c3e
Pierre Wilmot <pierre.wilmot@fetch.ai>
2019-02-07 16:24:20 +0000

    More FP ML [touch #498]

--- libs/ml/tests/ml/ops/softmax.cpp
+++ libs/ml/tests/ml/ops/softmax.cpp
@@ -17,6 +17,7 @@
 //------------------------------------------------------------------------------
 
 #include "ml/ops/softmax.hpp"
+#include "core/fixed_point/fixed_point.hpp"
 #include "math/ndarray.hpp"
 #include "math/tensor.hpp"
 #include <gtest/gtest.h>
@@ -27,7 +28,9 @@ class SoftmaxTest : public ::testing::Test
 };
 
 using MyTypes = ::testing::Types<fetch::math::NDArray<float>, fetch::math::NDArray<double>,
-                                 fetch::math::Tensor<float>, fetch::math::Tensor<double>>;
+                                 fetch::math::Tensor<float>, fetch::math::Tensor<double>,
+                                 fetch::math::Tensor<fetch::fixed_point::FixedPoint<32, 32>>>;
+
 TYPED_TEST_CASE(SoftmaxTest, MyTypes);
 
 TYPED_TEST(SoftmaxTest, forward_test)
--- libs/ml/tests/ml/ops/weigths.cpp
+++ libs/ml/tests/ml/ops/weigths.cpp
@@ -16,6 +16,7 @@
 //
 //------------------------------------------------------------------------------
 
+#include "core/fixed_point/fixed_point.hpp"
 #include "math/ndarray.hpp"
 #include "math/tensor.hpp"
 #include "ml/ops/weights.hpp"
@@ -28,7 +29,10 @@ class WeightsTest : public ::testing::Test
 
 using MyTypes = ::testing::Types<fetch::math::NDArray<int>, fetch::math::NDArray<float>,
                                  fetch::math::NDArray<double>, fetch::math::Tensor<int>,
-                                 fetch::math::Tensor<float>, fetch::math::Tensor<double>>;
+                                 fetch::math::Tensor<float>, fetch::math::Tensor<double>,
+                                 fetch::math::Tensor<fetch::fixed_point::FixedPoint<16, 16>>,
+                                 fetch::math::Tensor<fetch::fixed_point::FixedPoint<32, 32>>>;
+
 TYPED_TEST_CASE(WeightsTest, MyTypes);
 
 TYPED_TEST(WeightsTest, allocation_test)
