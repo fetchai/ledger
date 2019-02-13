fda7ebd86d900ef7eb82340e752ad9631d1f5a78
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-02-13 18:50:03 +0000

    Disable the trust system

--- apps/constellation/main.cpp
+++ apps/constellation/main.cpp
@@ -185,7 +185,7 @@ struct CommandLineArguments
     p.add(args.cfg.verification_threads,  "verifier-threads",      "The number of processor threads",                                               uint32_t{std::thread::hardware_concurrency()});
     p.add(args.cfg.max_peers,             "max-peers",             "The number of maximal peers to send to peer requests.",                         DEFAULT_MAX_PEERS);
     p.add(args.cfg.transient_peers,       "transient-peers",       "The number of the peers which will be random in answer sent to peer requests.", DEFAULT_TRANSIENT_PEERS);
-    p.add(args.cfg.peers_update_cycle_ms, "peers-update-cycle-ms", "How fast to do peering changes.",                                               uint32_t{2000});
+    p.add(args.cfg.peers_update_cycle_ms, "peers-update-cycle-ms", "How fast to do peering changes.",                                               uint32_t{0});
     // clang-format on
 
     // parse the args
