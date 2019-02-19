1b704d826b19afd95990eb04130d8f11dd8acf03
baykaner <40827646+baykaner@users.noreply.github.com>
2019-02-19 09:40:09 +0000

    Bugfix/tensor wrapper fix (#659)
    * WIP to bugfix size_t incompatibility with VM issue
    
    * bugfixed size_t usages in vm, math, and ml libraries
    
    * revoked size_t from IsPrimitive (i.e. undoing temporary fix)
    
    * applied style
    
    * typedef for size type in tensor
    
    * applied style
    
    * bugfixes to typedefs

--- libs/ml/tests/ml/ops/graph.cpp
+++ libs/ml/tests/ml/ops/graph.cpp
@@ -17,28 +17,30 @@
 //------------------------------------------------------------------------------
 
 #include "ml/graph.hpp"
-#include "math/ndarray.hpp"
+#include "math/tensor.hpp"
 #include "ml/ops/placeholder.hpp"
 #include "ml/ops/relu.hpp"
 #include <gtest/gtest.h>
 
+using ArrayType = typename fetch::math::Tensor<int>;
+
 TEST(graph_test, node_placeholder)
 {
-  fetch::ml::Graph<fetch::math::NDArray<int>> g;
-  g.AddNode<fetch::ml::ops::PlaceHolder<fetch::math::NDArray<int>>>("Input", {});
+  fetch::ml::Graph<ArrayType> g;
+  g.AddNode<fetch::ml::ops::PlaceHolder<ArrayType>>("Input", {});
 
-  std::shared_ptr<fetch::math::NDArray<int>> data = std::make_shared<fetch::math::NDArray<int>>(8);
-  std::shared_ptr<fetch::math::NDArray<int>> gt   = std::make_shared<fetch::math::NDArray<int>>(8);
-  std::size_t                                i(0);
+  std::shared_ptr<ArrayType> data = std::make_shared<ArrayType>(8);
+  std::shared_ptr<ArrayType> gt   = std::make_shared<ArrayType>(8);
+  int                        i(0);
   for (int e : {1, 2, 3, 4, 5, 6, 7, 8})
   {
-    data->Set(i, e);
-    gt->Set(i, e);
+    data->Set(std::uint64_t(i), e);
+    gt->Set(std::uint64_t(i), e);
     i++;
   }
 
   g.SetInput("Input", data);
-  std::shared_ptr<fetch::math::NDArray<int>> prediction = g.Evaluate("Input");
+  std::shared_ptr<ArrayType> prediction = g.Evaluate("Input");
 
   // test correct values
   ASSERT_TRUE(prediction->AllClose(*gt));
@@ -46,27 +48,29 @@ TEST(graph_test, node_placeholder)
 
 TEST(graph_test, node_relu)
 {
-  fetch::ml::Graph<fetch::math::NDArray<int>> g;
-  g.AddNode<fetch::ml::ops::PlaceHolder<fetch::math::NDArray<int>>>("Input", {});
-  g.AddNode<fetch::ml::ops::ReluLayer<fetch::math::NDArray<int>>>("Relu", {"Input"});
+  fetch::ml::Graph<ArrayType> g;
+  g.AddNode<fetch::ml::ops::PlaceHolder<ArrayType>>("Input", {});
+  g.AddNode<fetch::ml::ops::ReluLayer<ArrayType>>("Relu", {"Input"});
 
-  std::shared_ptr<fetch::math::NDArray<int>> data =
-      std::make_shared<fetch::math::NDArray<int>>(std::vector<std::size_t>({4, 4}));
-  std::shared_ptr<fetch::math::NDArray<int>> gt =
-      std::make_shared<fetch::math::NDArray<int>>(std::vector<std::size_t>({4, 4}));
+  std::shared_ptr<ArrayType> data =
+      std::make_shared<ArrayType>(std::vector<typename ArrayType::SizeType>({4, 4}));
+  std::shared_ptr<ArrayType> gt =
+      std::make_shared<ArrayType>(std::vector<typename ArrayType::SizeType>({4, 4}));
   std::vector<int> dataValues({0, -1, 2, -3, 4, -5, 6, -7, 8, -9, 10, -11, 12, -13, 14, -15, 16});
   std::vector<int> gtValues({0, 0, 2, 0, 4, 0, 6, 0, 8, 0, 10, 0, 12, 0, 14, 0, 16});
-  for (std::size_t i(0); i < 4; ++i)
+  for (int i(0); i < 4; ++i)
   {
-    for (std::size_t j(0); j < 4; ++j)
+    for (int j(0); j < 4; ++j)
     {
-      data->Set(std::vector<std::size_t>({i, j}), dataValues[i * 4 + j]);
-      gt->Set(std::vector<std::size_t>({i, j}), gtValues[i * 4 + j]);
+      data->Set(std::vector<std::uint64_t>({std::uint64_t(i), std::uint64_t(j)}),
+                dataValues[std::uint64_t(i * 4 + j)]);
+      gt->Set(std::vector<std::uint64_t>({std::uint64_t(i), std::uint64_t(j)}),
+              gtValues[std::uint64_t(i * 4 + j)]);
     }
   }
 
   g.SetInput("Input", data);
-  std::shared_ptr<fetch::math::NDArray<int>> prediction = g.Evaluate("Relu");
+  std::shared_ptr<fetch::math::Tensor<int>> prediction = g.Evaluate("Relu");
 
   // test correct values
   ASSERT_TRUE(prediction->AllClose(*gt));
