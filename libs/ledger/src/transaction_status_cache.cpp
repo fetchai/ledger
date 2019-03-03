d546b296335e89a5f600087c3409b05dbd3e3e33
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-03-03 11:50:48 +0000

    Add temporal transaction status cache

---
+++ libs/ledger/src/transaction_status_cache.cpp
@@ -0,0 +1,107 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "network/generics/milli_timer.hpp"
+#include "ledger/transaction_status_cache.hpp"
+
+static const std::chrono::hours   LIFETIME{24};
+static const std::chrono::minutes INTERVAL{5};
+
+using fetch::generics::MilliTimer;
+
+namespace fetch {
+namespace ledger {
+
+char const *ToString(TransactionStatus status)
+{
+  char const *text = "Unknown";
+
+  switch (status)
+  {
+    case TransactionStatus::UNKNOWN:
+      break;
+    case TransactionStatus::PENDING:
+      text = "Pending";
+      break;
+    case TransactionStatus::MINED:
+      text = "Mined";
+      break;
+    case TransactionStatus::EXECUTED:
+      text = "Executed";
+      break;
+  }
+
+  return text;
+}
+
+TransactionStatus TransactionStatusCache::Query(TxDigest digest) const
+{
+  TransactionStatus status{TransactionStatus::UNKNOWN};
+
+  {
+    FETCH_LOCK(mtx_);
+
+    auto const it = cache_.find(digest);
+    if (cache_.end() != it)
+    {
+      status = it->second.status;
+    }
+  }
+
+  return status;
+}
+
+void TransactionStatusCache::Update(TxDigest digest, TransactionStatus status, Timepoint const &now)
+{
+  FETCH_LOCK(mtx_);
+
+  // update the cache
+  cache_[digest] = Element{status, now};
+
+  // determine if we need to prune the cache
+  auto const delta_prune = now - last_clean_;
+  if (delta_prune > INTERVAL)
+  {
+    PruneCache(now);
+
+    last_clean_ = now;
+  }
+}
+
+void TransactionStatusCache::PruneCache(Timepoint const &now)
+{
+  MilliTimer timer{"TxStatusCache::Prune"};
+
+  auto it = cache_.begin();
+  while (it != cache_.end())
+  {
+    auto const age = now - it->second.timestamp;
+
+    if (age > LIFETIME)
+    {
+      it = cache_.erase(it);
+    }
+    else
+    {
+      ++it;
+    }
+  }
+}
+
+} // namespace ledger
+} // namespace fetch
---
+++ libs/ledger/tests/chain/transaction_status_cache_tests.cpp
@@ -0,0 +1,137 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "core/macros.hpp"
+#include "core/random/lcg.hpp"
+#include "core/byte_array/byte_array.hpp"
+#include "ledger/transaction_status_cache.hpp"
+
+#include "gtest/gtest.h"
+
+#include <memory>
+#include <iostream>
+
+namespace fetch {
+namespace ledger {
+
+std::ostream & operator<<(std::ostream &s, TransactionStatus const &status)
+{
+  s << ToString(status);
+  return s;
+}
+
+} // namespace ledger
+} // namespace fetch
+
+namespace {
+
+using fetch::byte_array::ByteArray;
+using fetch::ledger::TransactionStatusCache;
+using fetch::random::LinearCongruentialGenerator;
+using fetch::ledger::TransactionStatus;
+using fetch::ledger::ToString;
+
+using StatusCachePtr = std::unique_ptr<TransactionStatusCache>;
+using Clock          = TransactionStatusCache::Clock;
+using Timepoint      = TransactionStatusCache::Timepoint;
+using TxDigest       = TransactionStatusCache::TxDigest;
+using RngWord        = LinearCongruentialGenerator::random_type;
+
+class TransactionStatusCacheTests : public ::testing::Test
+{
+protected:
+
+  void SetUp() override
+  {
+    cache_ = std::make_unique<TransactionStatusCache>();
+  }
+
+  void TearDown() override
+  {
+    cache_.reset();
+  }
+
+  TxDigest GenerateDigest()
+  {
+    static constexpr std::size_t DIGEST_BIT_LENGTH  = 256u;
+    static constexpr std::size_t DIGEST_BYTE_LENGTH = DIGEST_BIT_LENGTH / 8u;
+    static constexpr std::size_t RNG_WORD_SIZE      = sizeof(RngWord);
+    static constexpr std::size_t NUM_WORDS          = DIGEST_BYTE_LENGTH / RNG_WORD_SIZE;
+
+    static_assert((DIGEST_BYTE_LENGTH % RNG_WORD_SIZE) == 0, "");
+
+    ByteArray digest;
+    digest.Resize(DIGEST_BYTE_LENGTH);
+
+    auto *digest_raw = reinterpret_cast<RngWord *>(digest.pointer());
+    for (std::size_t i = 0; i < NUM_WORDS; ++i)
+    {
+      *digest_raw++ = rng_();
+    }
+
+    return TxDigest{digest};
+  }
+
+  StatusCachePtr              cache_{};
+  LinearCongruentialGenerator rng_{};
+};
+
+TEST_F(TransactionStatusCacheTests, CheckBasicUsage)
+{
+  auto tx1 = GenerateDigest();
+  auto tx2 = GenerateDigest();
+  auto tx3 = GenerateDigest();
+
+  cache_->Update(tx1, TransactionStatus::PENDING);
+  cache_->Update(tx2, TransactionStatus::MINED);
+  cache_->Update(tx3, TransactionStatus::EXECUTED);
+
+  EXPECT_EQ(TransactionStatus::PENDING, cache_->Query(tx1));
+  EXPECT_EQ(TransactionStatus::MINED, cache_->Query(tx2));
+  EXPECT_EQ(TransactionStatus::EXECUTED, cache_->Query(tx3));
+}
+
+TEST_F(TransactionStatusCacheTests, CheckPruning)
+{
+  auto tx1 = GenerateDigest();
+  auto tx2 = GenerateDigest();
+  auto tx3 = GenerateDigest();
+
+  cache_->Update(tx1, TransactionStatus::PENDING);
+  cache_->Update(tx2, TransactionStatus::MINED);
+
+  EXPECT_EQ(TransactionStatus::PENDING, cache_->Query(tx1));
+  EXPECT_EQ(TransactionStatus::MINED, cache_->Query(tx2));
+
+  Timepoint const future_time_point = Clock::now() + std::chrono::hours{25};
+  cache_->Update(tx3, TransactionStatus::EXECUTED, future_time_point);
+
+  EXPECT_EQ(TransactionStatus::UNKNOWN, cache_->Query(tx1));
+  EXPECT_EQ(TransactionStatus::UNKNOWN, cache_->Query(tx2));
+  EXPECT_EQ(TransactionStatus::EXECUTED, cache_->Query(tx3));
+}
+
+TEST_F(TransactionStatusCacheTests, CheckStatusStrings)
+{
+  EXPECT_STREQ("Unknown", ToString(TransactionStatus::UNKNOWN));
+  EXPECT_STREQ("Pending", ToString(TransactionStatus::PENDING));
+  EXPECT_STREQ("Mined", ToString(TransactionStatus::MINED));
+  EXPECT_STREQ("Executed", ToString(TransactionStatus::EXECUTED));
+}
+
+} // namespace
