09b63a437091e61a375e45525552b3b5cba3d8e0
Pierre Wilmot <pierre.wilmot@fetch.ai>
2019-03-05 14:17:13 +0000

    Style

--- libs/ml/include/ml/ops/convolution.hpp
+++ libs/ml/include/ml/ops/convolution.hpp
@@ -47,39 +47,39 @@ public:
     outputShape.push_back(inputs[0]->shape()[1] - inputs[1]->shape()[2] + 1);
     outputShape.push_back(inputs[0]->shape()[2] - inputs[1]->shape()[3] + 1);
     if (!this->output_ || this->output_->shape() != outputShape)
+    {
+      this->output_ = std::make_shared<ArrayType>(outputShape);
+    }
+
+    for (uint64_t i(0); i < outputShape[0]; ++i)  // Iterate over output channels
+    {
+      for (uint64_t j(0); j < outputShape[1]; ++j)  // Iterate over output height
       {
-	this->output_ = std::make_shared<ArrayType>(outputShape);
+        for (uint64_t k(0); k < outputShape[2]; ++k)  // Iterate over output width
+        {
+          typename ArrayType::Type sum(0);
+          for (uint64_t ki(0); ki < inputs[1]->shape()[1]; ki++)  // Iterate over Input channel
+          {
+            for (uint64_t kj(0); kj < inputs[1]->shape()[2]; kj++)  // Iterate over kernel height
+            {
+              for (uint64_t kk(0); kk < inputs[1]->shape()[3]; kk++)  // Iterate over kernel width
+              {
+                std::vector<typename ArrayType::SizeType> kernelIdx({i, ki, kj, kk});
+                std::vector<typename ArrayType::SizeType> inputIdx(3);
+                inputIdx[0]                = ki;
+                inputIdx[1]                = j + kj;
+                inputIdx[2]                = k + kk;
+                typename ArrayType::Type i = inputs[0]->At(inputIdx);
+                typename ArrayType::Type w = inputs[1]->At(kernelIdx);
+                sum += i * w;
+              }
+            }
+          }
+          this->output_->Set(std::vector<typename ArrayType::SizeType>({i, j, k}), sum);
+        }
       }
-    
-    for (uint64_t i(0) ; i < outputShape[0] ; ++i) // Iterate over output channels
-      {
-	for (uint64_t j(0) ; j < outputShape[1] ; ++j) // Iterate over output height
-	  {
-	    for (uint64_t k(0) ; k < outputShape[2] ; ++k) // Iterate over output width
-	      {
-		typename ArrayType::Type sum(0);
-		for (uint64_t ki(0) ; ki < inputs[1]->shape()[1] ; ki++) // Iterate over Input channel
-		  {
-		    for (uint64_t kj(0) ; kj < inputs[1]->shape()[2] ; kj++) // Iterate over kernel height
-		      {
-			for (uint64_t kk(0) ; kk < inputs[1]->shape()[3] ; kk++) // Iterate over kernel width
-			  {
-			    std::vector<typename ArrayType::SizeType> kernelIdx({i, ki, kj, kk});
-			    std::vector<typename ArrayType::SizeType> inputIdx(3);
-			    inputIdx[0] = ki;
-			    inputIdx[1] = j + kj;
-			    inputIdx[2] = k + kk;
-			    typename ArrayType::Type i = inputs[0]->At(inputIdx);
-			    typename ArrayType::Type w = inputs[1]->At(kernelIdx);
-			    sum += i * w;
-			  }
-		      }
-		  }
-		this->output_->Set(std::vector<typename ArrayType::SizeType>({i, j, k}), sum);
-	      }
-	  }
-      }
-    
+    }
+
     return this->output_;
   }
 
