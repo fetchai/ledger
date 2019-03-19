b83649da46d697d7321a9000fd7018cca1572989
Pierre Wilmot <46316670+pierre-wilmot-fetch-ai@users.noreply.github.com>
2019-03-19 18:04:59 +0000

    Dataloader embedings bugfix (#764)
    * Add missing dataloader test file
    
    * Fix tests
    
    * Fix bug in dataloader
    
    * Fix model saving

--- libs/ml/include/ml/dataloaders/w2v_cbow_dataloader.hpp
+++ libs/ml/include/ml/dataloaders/w2v_cbow_dataloader.hpp
@@ -37,9 +37,7 @@ public:
     : currentSentence_(0)
     , currentWord_(0)
     , window_size_(window_size)
-  {
-    vocab_[""] = 0;
-  }
+  {}
 
   virtual uint64_t Size() const
   {
@@ -128,9 +126,7 @@ private:
       indexes.reserve(strings.size());
       for (std::string const &s : strings)
       {
-        // +1 means that values will start at 1. 0 is reserved for UNKNOWN word
-        auto value =
-            vocab_.insert(std::pair<std::string, uint64_t>(s, (uint64_t)(vocab_.size() + 1)));
+        auto value = vocab_.insert(std::pair<std::string, uint64_t>(s, (uint64_t)(vocab_.size())));
         indexes.push_back((*value.first).second);
       }
     }
