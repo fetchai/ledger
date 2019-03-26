5ee9cc0c1499fa91bb0e9449f5ddf00598c2936f
Ed FitzGerald <40318762+ejfitzgerald@users.noreply.github.com>
2019-03-26 11:29:56 +0000

    VM-Lang JSON State Object (#786)

--- apps/vm-lang/main.cpp
+++ apps/vm-lang/main.cpp
@@ -16,8 +16,12 @@
 //
 //------------------------------------------------------------------------------
 
+#include "core/byte_array/decoders.hpp"
+#include "core/byte_array/encoders.hpp"
 #include "core/commandline/cli_header.hpp"
 #include "core/commandline/params.hpp"
+#include "core/json/document.hpp"
+#include "variant/variant.hpp"
 #include "vm/analyser.hpp"
 #include "vm/compiler.hpp"
 #include "vm/module.hpp"
@@ -39,6 +43,11 @@ using fetch::vm::VM;
 using fetch::vm::String;
 using fetch::vm::TypeId;
 using fetch::vm_modules::VMFactory;
+using fetch::variant::Variant;
+using fetch::json::JSONDocument;
+using fetch::byte_array::ConstByteArray;
+using fetch::byte_array::FromHex;
+using fetch::byte_array::ToHex;
 
 class Parameters
 {
@@ -104,9 +113,20 @@ std::string ReadFileContents(std::string const &path)
   std::ifstream f(path.c_str());
   std::string   str{};
 
+  if (!f.is_open())
+  {
+    return str;
+  }
+
   // pre-allocate the string buffer
   f.seekg(0, std::ios::end);
-  str.reserve(static_cast<std::size_t>(f.tellg()));
+  auto const size = static_cast<std::size_t>(f.tellg());
+  if (size == 0)
+  {
+    return str;
+  }
+
+  str.reserve(size);
   f.seekg(0, std::ios::beg);
 
   // assign the contents
@@ -131,6 +151,99 @@ Ptr<String> Argv(VM *vm, TypeId, int32_t index)
 // placeholder class
 struct System : public fetch::vm::Object
 {
+};
+
+struct JsonStateMap : public fetch::vm::IoObserverInterface
+{
+public:
+  // Construction / Destruction
+  JsonStateMap()                     = default;
+  JsonStateMap(JsonStateMap const &) = delete;
+  JsonStateMap(JsonStateMap &&)      = delete;
+  ~JsonStateMap() override           = default;
+
+  /// @name Save and Restore Operations
+  /// @{
+  void LoadFromFile(char const *filename)
+  {
+    // read the contents of the file
+    ConstByteArray file_contents{ReadFileContents(filename)};
+
+    if (!file_contents.empty())
+    {
+      // parse the contents of the file
+      JSONDocument document{file_contents};
+
+      if (!document.root().IsObject())
+      {
+        throw std::runtime_error("JSON state file is not correct");
+      }
+
+      // load the file
+      data_ = document.root();
+    }
+  }
+
+  void SaveToFile(char const *filename)
+  {
+    std::ofstream file{filename};
+    file << data_;
+  }
+  /// @}
+
+  /// @name Io Observable Interface
+  /// @{
+  Status Read(std::string const &key, void *data, uint64_t &size) override
+  {
+    Status status{Status::ERROR};
+
+    if (data_.Has(key))
+    {
+      auto const value = FromHex(data_[key].As<ConstByteArray>());
+
+      status = Status::BUFFER_TOO_SMALL;
+      if (size >= value.size())
+      {
+        auto *raw_data = reinterpret_cast<uint8_t *>(data);
+        std::memcpy(raw_data, value.pointer(), value.size());
+
+        status = Status::OK;
+      }
+
+      // update the output size
+      size = value.size();
+    }
+
+    return status;
+  }
+
+  Status Write(std::string const &key, void const *data, uint64_t size) override
+  {
+    auto const *raw_data = reinterpret_cast<uint8_t const *>(data);
+
+    // store the data key
+    data_[key] = ToHex(ConstByteArray(raw_data, size));
+
+    return Status::OK;
+  }
+
+  Status Exists(std::string const &key) override
+  {
+    return (data_.Has(key) ? Status::OK : Status::ERROR);
+  }
+  /// @}
+
+  Variant const &data()
+  {
+    return data_;
+  }
+
+  // Operators
+  JsonStateMap &operator=(JsonStateMap const &) = delete;
+  JsonStateMap &operator=(JsonStateMap &&) = delete;
+
+private:
+  Variant data_{Variant::Object()};
 };
 
 }  // namespace
@@ -180,6 +293,17 @@ int main(int argc, char **argv)
   // create the VM instance
   auto vm = VMFactory::GetVM(module);
 
+  std::string const data_path = params.program().GetParam("data", "");
+
+  JsonStateMap state_map;
+  vm->SetIOObserver(state_map);
+
+  // restore and data file that is specified
+  if (!data_path.empty())
+  {
+    state_map.LoadFromFile(data_path.c_str());
+  }
+
   // Execute the requested function
   std::string        error;
   std::string        console;
@@ -191,6 +315,18 @@ int main(int argc, char **argv)
   if (!console.empty())
   {
     std::cout << console << std::endl;
+  }
+
+  // display the error message if the script execution was not successful
+  if (!success)
+  {
+    std::cout << error << std::endl;
+  }
+
+  // save any specified data file
+  if (!data_path.empty())
+  {
+    state_map.SaveToFile(data_path.c_str());
   }
 
   return (success) ? 0 : 1;
