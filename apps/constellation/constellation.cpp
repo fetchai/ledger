ebea2587cd2dfe8c841d92cd486031a82b75cbe8
Ed FitzGerald <40318762+ejfitzgerald@users.noreply.github.com>
2019-04-05 17:00:01 +0100

    State Recovery (#806)

--- apps/constellation/constellation.cpp
+++ apps/constellation/constellation.cpp
@@ -194,7 +194,6 @@ Constellation::Constellation(CertificatePtr &&certificate, Config config)
   FETCH_LOG_INFO(LOGGING_NAME, "");
 
   // attach the services to the reactor
-  reactor_.Attach(block_coordinator_.GetWeakRunnable());
   reactor_.Attach(main_chain_service_->GetWeakRunnable());
 
   // configure all the lane services
@@ -336,6 +335,10 @@ void Constellation::Run(UriList const &initial_peers)
 
   // Finally start the HTTP server
   http_.Start(http_port_);
+
+  // The block coordinator needs to access correctly started lanes to recover state in the case of
+  // a crash.
+  reactor_.Attach(block_coordinator_.GetWeakRunnable());
 
   //---------------------------------------------------------------
   // Step 2. Main monitor loop
