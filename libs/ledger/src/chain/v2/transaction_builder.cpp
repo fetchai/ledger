cb846d1a38c241e155f97868118d317fdee8c383
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-04-16 19:47:21 +0100

    Correct header order

--- libs/ledger/src/chain/v2/transaction_builder.cpp
+++ libs/ledger/src/chain/v2/transaction_builder.cpp
@@ -16,12 +16,12 @@
 //
 //------------------------------------------------------------------------------
 
-#include "ledger/chain/v2/transaction_builder.hpp"
 #include "core/logger.hpp"
 #include "core/macros.hpp"
 #include "crypto/prover.hpp"
 #include "crypto/sha256.hpp"
 #include "ledger/chain/v2/transaction.hpp"
+#include "ledger/chain/v2/transaction_builder.hpp"
 #include "ledger/chain/v2/transaction_serializer.hpp"
 
 #include <algorithm>
