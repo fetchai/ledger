6f82c6bf040b69eec82961c82e2f1d1c02955ee4
Jiri Vestfal <jiri.vestfal@fetch.ai>
2019-04-25 17:50:48 +0100

    Convolution 2D forward pass implemented using GEMM

--- libs/ml/include/ml/ops/convolution_2d.hpp
+++ libs/ml/include/ml/ops/convolution_2d.hpp
@@ -17,6 +17,7 @@
 //
 //------------------------------------------------------------------------------
 
+#include "math/matrix_operations.hpp"
 #include "ml/ops/ops.hpp"
 
 namespace fetch {
@@ -46,32 +47,85 @@ public:
 
     auto outputShape = ComputeOutputShape(inputs);
     ASSERT(output.shape() == outputShape);
-    for (SizeType i(0); i < outputShape[0]; ++i)  // Iterate over output channels
+
+    SizeType input_channels  = inputs.at(0).get().shape().at(0);
+    SizeType output_channels = inputs.at(1).get().shape().at(0);
+    SizeType kernel_height   = inputs.at(1).get().shape().at(2);
+    SizeType kernel_width    = inputs.at(1).get().shape().at(3);
+    SizeType output_height   = outputShape.at(1);
+    SizeType output_width    = outputShape.at(2);
+
+    SizeType horizontal_stride_width  = kernel_width * kernel_height * input_channels;
+    SizeType horizontal_stride_height = output_height * output_width;
+    SizeType vertical_stride_width    = output_channels;
+
+    ArrayType horizontal_stride({horizontal_stride_width, horizontal_stride_height});
+    ArrayType vertical_stride({vertical_stride_width, horizontal_stride_width});
+
+    // Fill horizontal stride
+    SizeType i_s;  // stride width iterator
+    SizeType j_s;  // stride height iterator
+
+    j_s=0;
+    for (SizeType i_o{0}; i_o < output_height; ++i_o)  // Iterate over output height
     {
-      for (SizeType j{0}; j < outputShape[1]; ++j)  // Iterate over output height
+      for (SizeType j_o{0}; j_o < output_width; ++j_o)  // Iterate over output width
       {
-        for (SizeType k{0}; k < outputShape[2]; ++k)  // Iterate over output width
+        i_s = 0;
+        for (SizeType i_ic(0); i_ic < input_channels; ++i_ic)  // Iterate over input channels
         {
-          typename ArrayType::Type sum(0);
-          for (SizeType ki(0); ki < inputs.at(1).get().shape()[1];
-               ki++)  // Iterate over Input channel
+
+          for (SizeType i_k(0); i_k < kernel_height; i_k++)  // Iterate over kernel height
           {
-            for (SizeType kj(0); kj < inputs.at(1).get().shape()[2];
-                 kj++)  // Iterate over kernel height
+            for (SizeType j_k(0); j_k < kernel_width; j_k++)  // Iterate over kernel width
             {
-              for (SizeType kk(0); kk < inputs.at(1).get().shape()[3];
-                   kk++)  // Iterate over kernel width
-              {
-                typename ArrayType::Type w = inputs.at(1).get().At(i, ki, kj, kk);
-                typename ArrayType::Type i = inputs.at(0).get().At(ki, j + kj, k + kk);
-                sum += i * w;
-              }
+
+              horizontal_stride.Set(i_s, j_s, inputs.at(0).get().At(i_ic, i_o + i_k, j_o + j_k));
+              ++i_s;
             }
           }
-          output.Set(i, j, k, sum);
+        }
+        ++j_s;
+      }
+    }
+
+    // Fill vertical stride
+    for (SizeType i_oc{0}; i_oc < output_channels; ++i_oc)  // Iterate over output channels
+    {
+      j_s = 0;
+      for (SizeType i_ic{0}; i_ic < input_channels; ++i_ic)  // Iterate over input channels
+      {
+
+        for (SizeType i_k(0); i_k < kernel_height; i_k++)  // Iterate over kernel height
+        {
+          for (SizeType j_k(0); j_k < kernel_width; j_k++)  // Iterate over kernel width
+          {
+
+            vertical_stride.Set(i_oc, j_s, inputs.at(1).get().At(i_oc, i_ic, i_k, j_k));
+            ++j_s;
+          }
         }
       }
     }
+
+    // Do matmul
+    ArrayType tmp_output = fetch::math::Dot(vertical_stride, horizontal_stride);
+
+    // Put values to output
+    SizeType i_tmp = 0;
+      for (SizeType i_oc{0}; i_oc < output_channels; ++i_oc)  // Iterate over output channels
+    {
+      i_tmp = 0;
+      for (SizeType i_o{0}; i_o < output_height; ++i_o)  // Iterate over output height
+      {
+        for (SizeType j_o{0}; j_o < output_width; ++j_o)  // Iterate over output width
+        {
+          output.Set(i_oc, i_o, j_o, tmp_output.At(i_oc, i_tmp));
+          ++i_tmp;
+        }
+      }
+    }
+
     return output;
   }
 
