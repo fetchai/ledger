16a3e31c71da1c63faf5d1ac358d06de43f64445
juergend <juergen.drachta@7lytix.com>
2019-04-30 18:29:23 +0200

    loader now inheriting from base loader

--- libs/ml/include/ml/dataloaders/code2vec_context_loaders/context_loader.hpp
+++ libs/ml/include/ml/dataloaders/code2vec_context_loaders/context_loader.hpp
@@ -17,6 +17,7 @@
 //
 //------------------------------------------------------------------------------
 
+#include "ml/dataloaders/dataloader.hpp"
 #include <sstream>
 #include <string>
 #include <unordered_map>
@@ -30,27 +31,47 @@ namespace dataloaders {
  * A basic text loader that handles parsing text strings into a vocabulary
  * @tparam T  tensor type
  */
-template <typename T>
-class ContextLoader
+template <typename DataType, typename LabelType>
+class ContextLoader : public DataLoader<DataType, LabelType>
 {
 public:
-  using ArrayType = T;
-  using DataType  = typename T::Type;
-  using SizeType  = typename T::SizeType;
+  using SizeType = typename DataType::value_type;
 
   using WordIdxType   = std::vector<std::vector<SizeType>>;
   using VocabType     = std::unordered_map<std::string, SizeType>;
   using SentencesType = std::vector<std::vector<std::string>>;
-  using umap_str_int = std::unordered_map<std::string, int>;
-  using umap_int_str = std::unordered_map<int, std::string>;
+  using umap_str_int  = std::unordered_map<std::string, SizeType>;
+  using umap_int_str  = std::unordered_map<SizeType, std::string>;
 
-  ContextLoader() = default;
+  ContextLoader()
+    : data_iterator_position(0){};
 
-  void        AddData(std::string const &text);
-  static void addValueToUnorderedMap(umap_str_int &umap, std::string word);
+  std::pair<DataType, LabelType> GetNext()
+  {
+    auto return_value = this->data[this->data_iterator_position];
+    this->data_iterator_position += 1;
+    return this->data[this->data_iterator_position];
+  };
+
+  std::uint64_t Size() const
+  {
+    return this->data.size();
+  };
+  bool IsDone() const
+  {
+    return ((this->data).size() == (this->data_iterator_position));
+  };
+  void Reset()
+  {
+    this->data_iterator_position = SizeType{0};
+  };
+
+  void                            AddData(std::string const &text);
+  static void                     addValueToUnorderedMap(umap_str_int &umap, std::string word);
   static std::vector<std::string> splitStringByChar(std::stringstream input, const char *sep);
+  SizeType addToIdxUMaps(std::string input, umap_str_int &name_to_idx, umap_int_str &idx_to_name);
 
-  void        createIdxUMaps();
+  void createIdxUMaps();
 
   umap_int_str GetUmapIdxToFunctionName();
   umap_int_str GetUmapIdxToPath();
@@ -65,7 +86,6 @@ public:
   umap_str_int GetCounterWords();
 
 private:
-
   umap_str_int function_name_counter;
   umap_str_int path_counter;
   umap_str_int word_counter;
@@ -78,15 +98,15 @@ private:
   umap_int_str idx_to_path;
   umap_int_str idx_to_word;
 
-
-  static void createIdxUMapsFromCounter(umap_str_int &counter,
-                                        umap_str_int &name_to_idx,
+  static void createIdxUMapsFromCounter(umap_str_int &counter, umap_str_int &name_to_idx,
                                         umap_int_str &idx_to_name);
 
+  std::vector<std::pair<DataType, LabelType>> data;
+  SizeType                                    data_iterator_position;
 };
 
-template <typename T>
-void ContextLoader<T>::AddData(std::string const &c2v_input)
+template <typename DataType, typename LabelType>
+void ContextLoader<DataType, LabelType>::AddData(std::string const &c2v_input)
 {
 
   std::stringstream c2v_input_ss(c2v_input);
@@ -102,6 +122,8 @@ void ContextLoader<T>::AddData(std::string const &c2v_input)
     c2v_input_line_ss >> function_name;
 
     addValueToUnorderedMap(function_name_counter, function_name);
+    SizeType function_name_idx =
+        addToIdxUMaps(function_name, function_name_to_idx, idx_to_function_name);
 
     while (c2v_input_line_ss >> context)
     {
@@ -111,13 +133,24 @@ void ContextLoader<T>::AddData(std::string const &c2v_input)
       addValueToUnorderedMap(word_counter, context_string_splitted[0]);
       addValueToUnorderedMap(path_counter, context_string_splitted[1]);
       addValueToUnorderedMap(word_counter, context_string_splitted[2]);
+
+      SizeType source_word_idx =
+          addToIdxUMaps(context_string_splitted[0], word_to_idx, idx_to_word);
+      SizeType path_idx = addToIdxUMaps(context_string_splitted[1], path_to_idx, idx_to_path);
+      SizeType target_word_idx =
+          addToIdxUMaps(context_string_splitted[2], word_to_idx, idx_to_word);
+
+      std::pair<DataType, LabelType> input_data_pair(
+          std::vector<SizeType>{source_word_idx, path_idx, target_word_idx}, function_name_idx);
+
+      this->data.push_back(input_data_pair);
     }
   }
 }
 
-template <typename T>
-void ContextLoader<T>::addValueToUnorderedMap(typename ContextLoader<T>::umap_str_int &umap,
-                                              std::string                           word)
+template <typename DataType, typename LabelType>
+void ContextLoader<DataType, LabelType>::addValueToUnorderedMap(
+    typename ContextLoader<DataType, LabelType>::umap_str_int &umap, std::string word)
 {
   if (umap.find(word) == umap.end())
   {
@@ -128,9 +161,9 @@ void ContextLoader<T>::addValueToUnorderedMap(typename ContextLoader<T>::umap_st
     umap[word] = +1;
   }
 }
-template <typename T>
-std::vector<std::string> ContextLoader<T>::splitStringByChar(std::stringstream input,
-                                                             const char *      sep)
+template <typename DataType, typename LabelType>
+std::vector<std::string> ContextLoader<DataType, LabelType>::splitStringByChar(
+    std::stringstream input, const char *sep)
 {
   std::vector<std::string> splitted_string;
   std::string              segment;
@@ -141,10 +174,29 @@ std::vector<std::string> ContextLoader<T>::splitStringByChar(std::stringstream i
   return splitted_string;
 }
 
-template <typename T>
-void ContextLoader<T>::createIdxUMapsFromCounter(typename ContextLoader<T>::umap_str_int &counter,
-                                                 typename ContextLoader<T>::umap_str_int &name_to_idx,
-                                                 typename ContextLoader<T>::umap_int_str &idx_to_name)
+template <typename DataType, typename LabelType>
+typename ContextLoader<DataType, LabelType>::SizeType
+ContextLoader<DataType, LabelType>::addToIdxUMaps(
+    std::string input, typename ContextLoader<DataType, LabelType>::umap_str_int &name_to_idx,
+    typename ContextLoader<DataType, LabelType>::umap_int_str &idx_to_name)
+{
+  if (name_to_idx.find(input) == name_to_idx.end())
+  {
+    name_to_idx[input]              = name_to_idx.size();
+    idx_to_name[name_to_idx.size()] = input;
+    return name_to_idx.size();
+  }
+  else
+  {
+    return name_to_idx[input];
+  }
+}
+
+template <typename DataType, typename LabelType>
+void ContextLoader<DataType, LabelType>::createIdxUMapsFromCounter(
+    typename ContextLoader<DataType, LabelType>::umap_str_int &counter,
+    typename ContextLoader<DataType, LabelType>::umap_str_int &name_to_idx,
+    typename ContextLoader<DataType, LabelType>::umap_int_str &idx_to_name)
 {
   int idx = 0;
   for (auto kv : counter)
@@ -155,64 +207,76 @@ void ContextLoader<T>::createIdxUMapsFromCounter(typename ContextLoader<T>::umap
   }
 }
 
-template <typename T>
-void ContextLoader<T>::createIdxUMaps()
+template <typename DataType, typename LabelType>
+void ContextLoader<DataType, LabelType>::createIdxUMaps()
 {
   createIdxUMapsFromCounter(function_name_counter, function_name_to_idx, idx_to_function_name);
   createIdxUMapsFromCounter(path_counter, path_to_idx, idx_to_path);
   createIdxUMapsFromCounter(word_counter, word_to_idx, idx_to_word);
 }
 
-template <typename T>
-typename ContextLoader<T>::umap_int_str ContextLoader<T>::GetUmapIdxToFunctionName(){
+template <typename DataType, typename LabelType>
+typename ContextLoader<DataType, LabelType>::umap_int_str
+ContextLoader<DataType, LabelType>::GetUmapIdxToFunctionName()
+{
   return this->idx_to_function_name;
 };
 
-template <typename T>
-typename ContextLoader<T>::umap_int_str ContextLoader<T>::GetUmapIdxToPath(){
+template <typename DataType, typename LabelType>
+typename ContextLoader<DataType, LabelType>::umap_int_str
+ContextLoader<DataType, LabelType>::GetUmapIdxToPath()
+{
   return this->idx_to_path;
 };
 
-template <typename T>
-typename ContextLoader<T>::umap_int_str ContextLoader<T>::GetUmapIdxToWord(){
+template <typename DataType, typename LabelType>
+typename ContextLoader<DataType, LabelType>::umap_int_str
+ContextLoader<DataType, LabelType>::GetUmapIdxToWord()
+{
   return this->idx_to_word;
 };
 
-
-
-
-template <typename T>
-typename ContextLoader<T>::umap_str_int ContextLoader<T>::GetUmapFunctioNameToIdx(){
+template <typename DataType, typename LabelType>
+typename ContextLoader<DataType, LabelType>::umap_str_int
+ContextLoader<DataType, LabelType>::GetUmapFunctioNameToIdx()
+{
   return this->function_name_to_idx;
 };
 
-template <typename T>
-typename ContextLoader<T>::umap_str_int ContextLoader<T>::GetUmapPathToIdx(){
+template <typename DataType, typename LabelType>
+typename ContextLoader<DataType, LabelType>::umap_str_int
+ContextLoader<DataType, LabelType>::GetUmapPathToIdx()
+{
   return this->path_to_idx;
 };
 
-template <typename T>
-typename ContextLoader<T>::umap_str_int ContextLoader<T>::GetUmapWordToIdx(){
+template <typename DataType, typename LabelType>
+typename ContextLoader<DataType, LabelType>::umap_str_int
+ContextLoader<DataType, LabelType>::GetUmapWordToIdx()
+{
   return this->word_to_idx;
 };
 
-
-template <typename T>
-typename ContextLoader<T>::umap_str_int ContextLoader<T>::GetCounterFunctionNames(){
+template <typename DataType, typename LabelType>
+typename ContextLoader<DataType, LabelType>::umap_str_int
+ContextLoader<DataType, LabelType>::GetCounterFunctionNames()
+{
   return this->function_name_counter;
 };
 
-template <typename T>
-typename ContextLoader<T>::umap_str_int ContextLoader<T>::GetCounterPaths(){
+template <typename DataType, typename LabelType>
+typename ContextLoader<DataType, LabelType>::umap_str_int
+ContextLoader<DataType, LabelType>::GetCounterPaths()
+{
   return this->path_counter;
 };
 
-template <typename T>
-typename ContextLoader<T>::umap_str_int ContextLoader<T>::GetCounterWords(){
+template <typename DataType, typename LabelType>
+typename ContextLoader<DataType, LabelType>::umap_str_int
+ContextLoader<DataType, LabelType>::GetCounterWords()
+{
   return this->word_counter;
 };
-
-
 
 }  // namespace dataloaders
 }  // namespace ml
