95a48ae61e21b4c6ca6ab584464732e5f6adadb1
Khan Baykaner <khan.baykaner@fetch.ai>
2019-05-02 15:51:32 +0100

    increased print frequency slightly

--- libs/ml/examples/word2vec_SGNS/main.cpp
+++ libs/ml/examples/word2vec_SGNS/main.cpp
@@ -53,7 +53,7 @@ struct TrainingParams
   SizeType    training_epochs = 15;             // total number of training epochs
   double      learning_rate   = 0.025;          // alpha - the learning rate
   SizeType    k               = 10;             // how many nearest neighbours to compare against
-  SizeType    print_freq      = 100000;         // how often to print status
+  SizeType    print_freq      = 10000;          // how often to print status
   std::string test_word       = "action";       // test word to consider
   std::string save_loc        = "./model.fba";  // save file location for exporting graph
 };
