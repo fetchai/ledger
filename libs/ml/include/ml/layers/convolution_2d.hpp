17e51291d65a66dcb1ed0f6f518e17990af1f1da
Jiri Vestfal <jiri.vestfal@fetch.ai>
2019-05-03 10:28:37 +0100

    Fixed copy/paste typo bug

--- libs/ml/include/ml/layers/convolution_2d.hpp
+++ libs/ml/include/ml/layers/convolution_2d.hpp
@@ -78,15 +78,13 @@ public:
     if (output_shape_.size() != 0)
       return output_shape_;
     // output_shape_[0]=number of output channels
-    output_shape_.emplace_back(inputs.at(1).get().shape()[0]);
+    output_shape_.emplace_back(output_channels_);
     // output_shape_[1]=number of stride_size steps on input height
-    output_shape_.emplace_back(
-        (inputs.at(0).get().shape()[1] - inputs.at(1).get().shape()[2] + stride_size_) /
-        stride_size_);
+    output_shape_.emplace_back((inputs.at(0).get().shape()[1] - kernel_size_ + stride_size_) /
+                               stride_size_);
     // output_shape_[2]=number of stride_size steps on input width
-    output_shape_.emplace_back(
-        (inputs.at(0).get().shape()[2] - inputs.at(1).get().shape()[3] + stride_size_) /
-        stride_size_);
+    output_shape_.emplace_back((inputs.at(0).get().shape()[2] - kernel_size_ + stride_size_) /
+                               stride_size_);
     return output_shape_;
   }
 
