6ea1c176ffb2de070236e7aacf8dcc4074d22d91
nhutton <nathan.hutton@fetch.ai>
2019-05-03 17:14:00 +0100

    Updated cluster to indicate standalone network for E2E test

--- scripts/ci/install-test-dependencies.sh
+++ scripts/ci/install-test-dependencies.sh
@@ -5,5 +5,4 @@ set -e
 pip3 install -e git+https://github.com/fetchai/ledger-api-py.git@develop#egg=fetchai-ledger-api
 
 # install the latest version of the net utils
-#pip3 install -i https://test.pypi.org/simple/ fetchai-netutils==0.0.1
-pip3 install -i https://test.pypi.org/simple/ fetchai-netutils==0.0.2a1
+pip3 install -i https://test.pypi.org/simple/ fetchai-netutils==0.0.3a1
