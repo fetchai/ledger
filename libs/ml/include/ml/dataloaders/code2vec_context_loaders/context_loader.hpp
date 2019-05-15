6aba8bc09b331260881e5ab7673ae71f7e61c9ed
juergend <juergen.drachta@7lytix.com>
2019-05-15 12:33:38 +0200

    newline added

--- libs/ml/include/ml/dataloaders/code2vec_context_loaders/context_loader.hpp
+++ libs/ml/include/ml/dataloaders/code2vec_context_loaders/context_loader.hpp
@@ -16,6 +16,7 @@
 //   limitations under the License.
 //
 //------------------------------------------------------------------------------
+
 #include "math/base_types.hpp"
 #include "ml/dataloaders/dataloader.hpp"
 #include <sstream>
