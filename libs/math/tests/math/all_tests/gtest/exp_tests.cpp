b28bb92590083d017cd0b2bd9cb495fb3d8d6832
Khan Baykaner <khan.baykaner@fetch.ai>
2019-05-16 12:44:48 +0100

    rework approx_exp test

--- libs/math/tests/math/all_tests/gtest/exp_tests.cpp
+++ libs/math/tests/math/all_tests/gtest/exp_tests.cpp
@@ -21,76 +21,98 @@
 #include <iostream>
 
 #include "core/random/lcg.hpp"
+
 #include "math/approx_exp.hpp"
+#include "math/fixed_point/fixed_point.hpp"
 #include <gtest/gtest.h>
 
-template <uint8_t N, uint64_t C>
-void test1(double max)
+// TODO: NOTES: should this work with float?
+// TODO: NOTES: should it work with FixedPoint?
+// TODO: which tests may not be necessary?
+
+template <uint8_t N, uint64_t C, typename T>
+void test1(T max)
 {
   fetch::math::ApproxExpImplementation<N, C> fexp;
   double                                     me = 0;
   for (double x = -300; x < 300; x += 0.1)
   {
-    double y0 = fexp(x);
+    T y0 = static_cast<T>(fexp(static_cast<T>(x)));
+    //    T y0 = fexp(x);
     double y1 = exp(x);
-    double r  = fabs(y0 - y1) / y1 * 100;
-    me        = std::max(r, me);
+    double r  = (fabs(static_cast<double>(y0) - y1) / y1 * 100);
+    //    double r  = (fabs(y0 - y1) / y1 * 100);
+    me = std::max(r, me);
   }
-  ASSERT_LE(me, max) << "expected: " << max;
+  //  ASSERT_LE(me, static_cast<double>(max)) << "expected: " << static_cast<double>(max);
+  EXPECT_LE(me, static_cast<double>(max));
 }
 
-template <uint8_t N, uint64_t C, std::size_t MAX = 100000000>
-double test_timing(double x_value)
+template <typename T>
+class ExpTests : public ::testing::Test
 {
-  fetch::math::ApproxExpImplementation<N, C> fexp;
-  volatile double                            x = 1;
+};
 
-  auto t1a = std::chrono::high_resolution_clock::now();
-  for (std::size_t i = 0; i < MAX; ++i)
-  {
-    x = x_value;
-  }
-  auto t2a = std::chrono::high_resolution_clock::now();
+// using MyTypes = ::testing::Types<float, double, fetch::fixed_point::FixedPoint<32, 32>>;
+using MyTypes = ::testing::Types<float, double>;
+TYPED_TEST_CASE(ExpTests, MyTypes);
 
-  double us1 = double(std::chrono::duration_cast<std::chrono::milliseconds>(t2a - t1a).count());
-
-  auto t1b = std::chrono::high_resolution_clock::now();
-  for (std::size_t i = 0; i < MAX; ++i)
-  {
-    x = x_value;
-    x = fexp(x);
-  }
-  auto t2b = std::chrono::high_resolution_clock::now();
-
-  double us2 = double(std::chrono::duration_cast<std::chrono::milliseconds>(t2b - t1b).count());
-
-  auto t1c = std::chrono::high_resolution_clock::now();
-  for (std::size_t i = 0; i < MAX; ++i)
-  {
-    x = x_value;
-    x = exp(x);
-  }
-  auto t2c = std::chrono::high_resolution_clock::now();
-
-  double us3 = double(std::chrono::duration_cast<std::chrono::milliseconds>(t2c - t1c).count());
-
-  return (us3 - us1) / (us2 - us1);
-  //  auto t3 = std::chrono::high_resolution_clock::now();
+TYPED_TEST(ExpTests, exp_0_0)
+{
+  TypeParam x = TypeParam(7);
+  test1<0, 0>(x);
 }
-// TODO(private issue 332): Move to a benchmark
 
-TEST(exp_gtest, testing_exp)
+TYPED_TEST(ExpTests, exp_0_60801)
 {
+  TypeParam x = TypeParam{5};
+  test1<0, 60801>(x);
+}
 
-  test1<0, 0>(7);
-  test1<0, 60801>(5);
-  test1<8, 60801>(0.08);
-  test1<12, 60801>(0.005);
-  test1<16, 60801>(0.0003);
-  test1<20, 60801>(0.00004);
+TYPED_TEST(ExpTests, exp_8_60801)
+{
+  TypeParam x = TypeParam(0.08);
+  test1<8, 60801>(x);
+}
 
-  test1<8, 0>(0.08);
-  test1<12, 0>(0.005);
-  test1<16, 0>(0.0003);
-  test1<20, 0>(0.00004);
+TYPED_TEST(ExpTests, exp_12_60801)
+{
+  TypeParam x = TypeParam(0.005);
+  test1<12, 60801>(x);
+}
+
+TYPED_TEST(ExpTests, exp_16_60801)
+{
+  TypeParam x = TypeParam(0.0003);
+  test1<16, 60801>(x);
+}
+
+TYPED_TEST(ExpTests, exp_20_60801)
+{
+  TypeParam x = TypeParam(0.00004);
+  test1<20, 60801>(x);
+}
+
+TYPED_TEST(ExpTests, exp_8_0)
+{
+  TypeParam x = TypeParam(0.08);
+  test1<8, 0>(x);
+}
+
+TYPED_TEST(ExpTests, exp_12_0)
+{
+  TypeParam x = TypeParam(0.005);
+  test1<12, 0>(x);
+}
+
+TYPED_TEST(ExpTests, exp_16_0)
+{
+  TypeParam x = TypeParam(0.0003);
+  test1<16, 0>(x);
+}
+
+TYPED_TEST(ExpTests, exp_20_0)
+{
+  TypeParam x = TypeParam(0.00004);
+  test1<20, 0>(x);
 }
