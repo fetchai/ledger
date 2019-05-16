f3892de7fe7f5f62f8e7d9ed7cd9df9cea43a2c0
Pierre Wilmot <pierre.wilmot@fetch.ai>
2019-05-16 12:33:20 +0100

    Fix merge conflict

--- libs/ml/examples/word2vec_CBOW/averaged_embeddings.hpp
+++ libs/ml/examples/word2vec_CBOW/averaged_embeddings.hpp
@@ -136,7 +136,7 @@ public:
   }
 
   virtual std::vector<SizeType> ComputeOutputShape(
-      std::vector<std::reference_wrapper<ArrayType const>> const &inputs)
+      std::vector<std::reference_wrapper<ArrayType const>> const &inputs) const
   {
     (void)inputs;
     auto outputShape = this->output_->shape();
