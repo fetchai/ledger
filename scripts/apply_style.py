94d7029eca3360a5d3b4c986155f4ee08687c32d
Wojciech Kaluza <w-kaluza@tlen.pl>
2019-05-31 21:50:12 +0100

    Deduplicate license

--- scripts/apply_style.py
+++ scripts/apply_style.py
@@ -55,30 +55,11 @@ EXCLUDED_DIRS = find_excluded_dirs()
 
 CURRENT_YEAR = int(time.localtime().tm_year)
 
-RE_LICENSE = """//------------------------------------------------------------------------------
-//
-//   Copyright 2018(-\\d+)? Fetch.AI Limited
-//
-//   Licensed under the Apache License, Version 2.0 \\(the "License"\\);
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
-//
-//------------------------------------------------------------------------------([\\s\\n]*)
-"""
-
-LICENSE = """//------------------------------------------------------------------------------
+LICENSE_TEMPLATE = """//------------------------------------------------------------------------------
 //
 //   Copyright 2018-{} Fetch.AI Limited
 //
-//   Licensed under the Apache License, Version 2.0 (the "License");
+//   Licensed under the Apache License, Version 2.0 {}the "License"{};
 //   you may not use this file except in compliance with the License.
 //   You may obtain a copy of the License at
 //
@@ -90,9 +71,10 @@ LICENSE = """//-----------------------------------------------------------------
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 //
-//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------{}"""
 
-""".format(CURRENT_YEAR)
+LICENSE = LICENSE_TEMPLATE.format(CURRENT_YEAR, '(', ')', '\n\n')
+RE_LICENSE = LICENSE_TEMPLATE.format('(-\\d+)?', '\\(', '\\)', '([\\s\\n]*)')
 
 SUPPORTED_LANGUAGES = {
     'cpp': {
