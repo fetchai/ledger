75ebc07c3f4245addc003ab1d6d2e6f528628e8c
Wojciech Kaluza <w-kaluza@tlen.pl>
2019-05-31 18:47:45 +0100

    Simplify main

--- scripts/apply_style.py
+++ scripts/apply_style.py
@@ -31,8 +31,6 @@ PROJECT_ROOT = abspath(dirname(dirname(__file__)))
 CHECKED_IN_BINARY_FILE_PATTERNS = ('*.png', '*.npy')
 INCLUDE_GUARD = '#pragma once'
 DISALLOWED_HEADER_FILE_EXTENSIONS = ('*.h', '*.hxx', '*.hh')
-NUM_PROCESSED_FILES = 0
-TOTAL_FILES_TO_PROCESS = 0
 CMAKE_VERSION_REQUIREMENT = 'cmake_minimum_required(VERSION 3.5 FATAL_ERROR)'
 
 
@@ -97,6 +95,28 @@ LICENSE = """//-----------------------------------------------------------------
 
 """.format(CURRENT_YEAR)
 
+NUM_FILES_PROCESSEFD_SO_FAR = 0  # Global variable
+TOTAL_FILES_TO_PROCESS = 0  # Global variable
+
+output_lock = threading.Lock()
+file_count_lock = threading.Lock()
+
+
+def output(text):
+    with output_lock:
+        sys.stdout.write('{}\n'.format(text))
+        sys.stdout.flush()
+
+
+def increment_file_count():
+    global NUM_FILES_PROCESSEFD_SO_FAR
+
+    with file_count_lock:
+        NUM_FILES_PROCESSEFD_SO_FAR += 1
+        if NUM_FILES_PROCESSEFD_SO_FAR % 100 == 0:
+            output('Processed {}\t of {} files'.format(
+                NUM_FILES_PROCESSEFD_SO_FAR, TOTAL_FILES_TO_PROCESS))
+
 
 def include_patterns(*filename_patterns):
     def decorator(func):
@@ -114,30 +134,6 @@ def include_patterns(*filename_patterns):
 
 def has_include_guard(text):
     return text.startswith(INCLUDE_GUARD)
-
-
-@include_patterns('*.cpp', '*.hpp')
-def fix_license_header(text, path_to_file):
-    if LICENSE in text:
-        return text
-
-    # determine if an old license is already present
-    existing_license_present = bool(re.search(RE_LICENSE, text, re.MULTILINE))
-
-    if existing_license_present:
-        # replace the contents of the license
-        return re.sub(RE_LICENSE, LICENSE, text)
-    elif fnmatch.fnmatch(basename(path_to_file), '*.cpp'):
-        # add license to the top of the file
-        return LICENSE + text
-    elif fnmatch.fnmatch(basename(path_to_file), '*.hpp'):
-        if has_include_guard(text):
-            # add the license after the header guard
-            return re.sub(r'{}\s+'.format(INCLUDE_GUARD), '{}\n{}'.format(INCLUDE_GUARD, LICENSE), text)
-        else:
-            return LICENSE + text
-
-    return text
 
 
 def find_clang_format():
@@ -184,18 +180,9 @@ SUPPORTED_LANGUAGES = {
     }
 }
 
-output_lock = threading.Lock()
-file_count_lock = threading.Lock()
-
-
-def output(text):
-    with output_lock:
-        sys.stdout.write('{}\n'.format(text))
-        sys.stdout.flush()
-
 
 def walk_source_directories(project_root, excluded_dirs):
-    """Walk directory tree, skip listed subtrees"""
+    """Walk directory tree, skip excluded subtrees"""
     for root, _, files in os.walk(project_root):
         if any([os.path.commonpath([root, excluded_dir]) == excluded_dir for excluded_dir in excluded_dirs]):
             continue
@@ -203,7 +190,8 @@ def walk_source_directories(project_root, excluded_dirs):
         yield root, _, files
 
 
-def format_language(cmd_prefix, filename_patterns):
+def external_formatter(cmd_prefix, filename_patterns):
+
     @include_patterns(*filename_patterns)
     def reformat_as_pipe(text, path_to_file):
         input_bytes = text.encode()
@@ -222,16 +210,36 @@ def format_language(cmd_prefix, filename_patterns):
     return reformat_as_pipe
 
 
+@include_patterns('*.cpp', '*.hpp')
+def fix_license_header(text, path_to_file):
+    if LICENSE in text:
+        return text
+
+    # determine if an old license is already present
+    existing_license_present = bool(re.search(RE_LICENSE, text, re.MULTILINE))
+
+    if existing_license_present:
+        # replace the contents of the license
+        return re.sub(RE_LICENSE, LICENSE, text)
+    elif fnmatch.fnmatch(basename(path_to_file), '*.cpp'):
+        # add license to the top of the file
+        return LICENSE + text
+    elif fnmatch.fnmatch(basename(path_to_file), '*.hpp'):
+        if has_include_guard(text):
+            # add the license after the header guard
+            return re.sub(r'{}\s+'.format(INCLUDE_GUARD), '{}\n{}'.format(INCLUDE_GUARD, LICENSE), text)
+        else:
+            return LICENSE + text
+
+    return text
+
+
 @include_patterns('*.py')
 def format_python(text, path_to_file):
     if text.strip():
         return autopep8.fix_code(text)
 
     return text.strip()
-
-
-def get_diff():
-    return subprocess.check_output(['git', 'diff'], cwd=PROJECT_ROOT).decode().strip()
 
 
 @include_patterns('*.hpp')
@@ -266,57 +274,24 @@ def fix_terminal_newlines(text, path_to_file):
     return text
 
 
-TRANSFORMATIONS = [
-    fix_cmake_version_requirements,
-    fix_license_header,
-    fix_missing_include_guards,
-    fix_terminal_newlines,
-    format_language(**SUPPORTED_LANGUAGES['cpp']),
-    format_language(**SUPPORTED_LANGUAGES['cmake']),
-    format_python
-]
-
-
-def apply_transformations_to_file(path_to_file):
-    global NUM_PROCESSED_FILES
-    global TOTAL_FILES_TO_PROCESS
-
-    try:
-        with open(path_to_file, 'r+', encoding='utf-8') as f:
-            original_text = f.read()
-            text = original_text
-
-            for transformation in TRANSFORMATIONS:
-                text = transformation(text, path_to_file)
-
-            if text != original_text:
-                f.seek(0)
-                f.truncate(0)
-                f.write(text)
-
-        with file_count_lock:
-            NUM_PROCESSED_FILES += 1
-            if NUM_PROCESSED_FILES % 100 == 0:
-                output('Processed {}\t of {} files'.format(
-                    NUM_PROCESSED_FILES, TOTAL_FILES_TO_PROCESS))
-
-    except:
-        output(traceback.format_exc())
-
-
 def check_for_headers_with_non_hpp_extension(file_paths):
+    output('Checking for header files with incorrect extensions ...')
+
     invalid_file_groups = [fnmatch.filter(
         file_paths, pattern) for pattern in DISALLOWED_HEADER_FILE_EXTENSIONS]
 
     invalid_files = [file for group in invalid_file_groups for file in group]
     if invalid_files:
-        output("Error: Fetch header files should have the extension hpp")
+        output("Error: Fetch header files should have the extension '.hpp':")
+        list_counter = 1
         for path in invalid_files:
-            output(path)
+            output('  {}. {}'.format(list_counter, path))
+            list_counter = list_counter + 1
+
         sys.exit(1)
 
 
-def is_binary_file(file_name):
+def is_checked_in_binary_file(file_name):
     return any([fnmatch.fnmatch(file_name, pattern) for pattern in CHECKED_IN_BINARY_FILE_PATTERNS])
 
 
@@ -328,18 +303,33 @@ def get_changed_paths_from_git(commit):
 
 
 def files_of_interest(commit):
+    global TOTAL_FILES_TO_PROCESS
+
+    output('Composing list of files ...')
     ret = []
-    changes = [] if commit is None else get_changed_paths_from_git(commit)
+    changes = None if commit is None else get_changed_paths_from_git(commit)
 
     for root, _, files in walk_source_directories(PROJECT_ROOT, EXCLUDED_DIRS):
         for file_name in files:
-            if not is_binary_file(file_name):
+            if not is_checked_in_binary_file(file_name):
                 absolute_path = abspath(join(root, file_name))
                 if isfile(absolute_path):
                     if commit is None or absolute_path in changes:
                         ret.append(absolute_path)
 
+    TOTAL_FILES_TO_PROCESS = len(ret)
+
     return ret
+
+
+def print_diff_and_fail():
+    diff = subprocess.check_output(
+        ['git', 'diff'], cwd=PROJECT_ROOT).decode().strip()
+    if diff:
+        output('*' * 80)
+        output(diff)
+        output('*' * 80)
+        sys.exit(1)
 
 
 def parse_commandline():
@@ -372,32 +362,54 @@ def parse_commandline():
         args.jobs
 
 
-def main():
-    global TOTAL_FILES_TO_PROCESS
+TRANSFORMATIONS = [
+    fix_cmake_version_requirements,
+    fix_license_header,
+    fix_missing_include_guards,
+    fix_terminal_newlines,
+    external_formatter(**SUPPORTED_LANGUAGES['cpp']),
+    external_formatter(**SUPPORTED_LANGUAGES['cmake']),
+    format_python
+]
 
-    commit_arg, diff_arg, jobs_arg = parse_commandline()
 
-    output('Composing list of files ...')
-    files_to_process = files_of_interest(commit_arg)
-    TOTAL_FILES_TO_PROCESS = len(files_to_process)
+def apply_transformations_to_file(path_to_file):
+    try:
+        with open(path_to_file, 'r+', encoding='utf-8') as f:
+            original_text = f.read()
+            text = original_text
 
-    output('Checking for header files with incorrect extensions ...')
-    check_for_headers_with_non_hpp_extension(files_to_process)
+            for transformation in TRANSFORMATIONS:
+                text = transformation(text, path_to_file)
 
+            if text != original_text:
+                f.seek(0)
+                f.truncate(0)
+                f.write(text)
+
+        increment_file_count()
+    except:
+        output(traceback.format_exc())
+
+
+def process_in_parallel(files_to_process, jobs):
     output('Processing {} file(s) with {} job(s) ...'
-           .format(TOTAL_FILES_TO_PROCESS, jobs_arg))
-    with ThreadPoolExecutor(max_workers=jobs_arg) as pool:
+           .format(TOTAL_FILES_TO_PROCESS, jobs))
+    with ThreadPoolExecutor(max_workers=jobs) as pool:
         pool.map(apply_transformations_to_file, files_to_process)
 
     output('Done.')
 
-    if diff_arg:
-        diff = get_diff()
-        if diff:
-            output('*' * 80)
-            output(diff)
-            output('*' * 80)
-            sys.exit(1)
+
+def main():
+    commit, fail_if_changes, jobs = parse_commandline()
+
+    files_to_process = files_of_interest(commit)
+    check_for_headers_with_non_hpp_extension(files_to_process)
+    process_in_parallel(files_to_process, jobs)
+
+    if fail_if_changes:
+        print_diff_and_fail()
 
 
 if __name__ == '__main__':
