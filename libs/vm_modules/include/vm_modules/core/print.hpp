7dc6d20b06518f9b741fb91bbbe5008efd1e733f
Wojciech Kaluza <wojciech.kaluza@fetch.ai>
2019-06-01 19:41:41 +0100

    Deduplicate

--- libs/vm_modules/include/vm_modules/core/print.hpp
+++ libs/vm_modules/include/vm_modules/core/print.hpp
@@ -23,56 +23,55 @@
 namespace fetch {
 namespace vm_modules {
 
+template <bool APPEND_LINEBREAK = false>
 inline void PrintString(fetch::vm::VM *vm, fetch::vm::Ptr<fetch::vm::String> const &s)
 {
   auto &out = vm->GetOutputDevice(vm::VM::STDOUT);
   out << s->str;
+
+  if (APPEND_LINEBREAK)
+  {
+    out << std::endl;
+  }
 }
 
-inline void PrintLnString(fetch::vm::VM *vm, fetch::vm::Ptr<fetch::vm::String> const &s)
-{
-  auto &out = vm->GetOutputDevice(vm::VM::STDOUT);
-  out << s->str << std::endl;
-}
-template <typename T>
-inline void Print(fetch::vm::VM *vm, T const &s)
+template <typename T, bool APPEND_LINEBREAK = false>
+inline void PrintNumber(fetch::vm::VM *vm, T const &s)
 {
   auto &out = vm->GetOutputDevice(vm::VM::STDOUT);
   out << s;
+
+  if (APPEND_LINEBREAK)
+  {
+    out << std::endl;
+  }
 }
 
-template <typename T>
-inline void PrintLn(fetch::vm::VM *vm, T const &s)
-{
-  auto &out = vm->GetOutputDevice(vm::VM::STDOUT);
-  out << s << std::endl;
-}
-
+template <bool APPEND_LINEBREAK = false>
 inline void PrintByte(fetch::vm::VM *vm, int8_t const &s)
 {
   auto &out = vm->GetOutputDevice(vm::VM::STDOUT);
   out << static_cast<int32_t>(s);
+
+  if (APPEND_LINEBREAK)
+  {
+    out << std::endl;
+  }
 }
 
-inline void PrintByteLn(fetch::vm::VM *vm, int8_t const &s)
-{
-  auto &out = vm->GetOutputDevice(vm::VM::STDOUT);
-  out << static_cast<int32_t>(s) << std::endl;
-}
-
+template <bool APPEND_LINEBREAK = false>
 inline void PrintUnsignedByte(fetch::vm::VM *vm, uint8_t const &s)
 {
   auto &out = vm->GetOutputDevice(vm::VM::STDOUT);
   out << static_cast<uint32_t>(s);
+
+  if (APPEND_LINEBREAK)
+  {
+    out << std::endl;
+  }
 }
 
-inline void PrintUnsignedByteLn(fetch::vm::VM *vm, uint8_t const &s)
-{
-  auto &out = vm->GetOutputDevice(vm::VM::STDOUT);
-  out << static_cast<uint32_t>(s) << std::endl;
-}
-
-template <typename T, bool NL = false>
+template <typename T, bool APPEND_LINEBREAK = false>
 inline void PrintArrayPrimitive(fetch::vm::VM *vm, vm::Ptr<vm::Array<T>> const &g)
 {
   auto &out = vm->GetOutputDevice(vm::VM::STDOUT);
@@ -94,7 +93,8 @@ inline void PrintArrayPrimitive(fetch::vm::VM *vm, vm::Ptr<vm::Array<T>> const &
     out << g->elements[i];
   }
   out << "]";
-  if (NL)
+
+  if (APPEND_LINEBREAK)
   {
     out << std::endl;
   }
@@ -102,45 +102,55 @@ inline void PrintArrayPrimitive(fetch::vm::VM *vm, vm::Ptr<vm::Array<T>> const &
 
 inline void CreatePrint(vm::Module &module)
 {
-  module.CreateFreeFunction("print", &PrintString);
-  module.CreateFreeFunction("printLn", &PrintLnString);
+  module.CreateFreeFunction("print", &PrintString<>);
+  module.CreateFreeFunction("printLn", &PrintString<true>);
 
-  module.CreateFreeFunction("print", &PrintUnsignedByte);
-  module.CreateFreeFunction("printLn", &PrintUnsignedByteLn);
-  module.CreateFreeFunction("print", &PrintByte);
-  module.CreateFreeFunction("printLn", &PrintByteLn);
+  module.CreateFreeFunction("print", &PrintUnsignedByte<>);
+  module.CreateFreeFunction("printLn", &PrintUnsignedByte<true>);
 
-  module.CreateFreeFunction("print", &Print<uint16_t>);
-  module.CreateFreeFunction("printLn", &PrintLn<uint16_t>);
-  module.CreateFreeFunction("print", &Print<int16_t>);
-  module.CreateFreeFunction("printLn", &PrintLn<int16_t>);
+  module.CreateFreeFunction("print", &PrintByte<>);
+  module.CreateFreeFunction("printLn", &PrintByte<true>);
 
-  module.CreateFreeFunction("print", &Print<uint32_t>);
-  module.CreateFreeFunction("printLn", &PrintLn<uint32_t>);
-  module.CreateFreeFunction("print", &Print<int32_t>);
-  module.CreateFreeFunction("printLn", &PrintLn<int32_t>);
+  module.CreateFreeFunction("print", &PrintNumber<uint16_t>);
+  module.CreateFreeFunction("printLn", &PrintNumber<uint16_t, true>);
 
-  module.CreateFreeFunction("print", &Print<uint64_t>);
-  module.CreateFreeFunction("printLn", &PrintLn<uint64_t>);
-  module.CreateFreeFunction("print", &Print<int64_t>);
-  module.CreateFreeFunction("printLn", &PrintLn<int64_t>);
+  module.CreateFreeFunction("print", &PrintNumber<int16_t>);
+  module.CreateFreeFunction("printLn", &PrintNumber<int16_t, true>);
 
-  module.CreateFreeFunction("print", &Print<float>);
-  module.CreateFreeFunction("printLn", &PrintLn<float>);
-  module.CreateFreeFunction("print", &Print<double>);
-  module.CreateFreeFunction("printLn", &PrintLn<double>);
+  module.CreateFreeFunction("print", &PrintNumber<uint32_t>);
+  module.CreateFreeFunction("printLn", &PrintNumber<uint32_t, true>);
+
+  module.CreateFreeFunction("print", &PrintNumber<int32_t>);
+  module.CreateFreeFunction("printLn", &PrintNumber<int32_t, true>);
+
+  module.CreateFreeFunction("print", &PrintNumber<uint64_t>);
+  module.CreateFreeFunction("printLn", &PrintNumber<uint64_t, true>);
+
+  module.CreateFreeFunction("print", &PrintNumber<int64_t>);
+  module.CreateFreeFunction("printLn", &PrintNumber<int64_t, true>);
+
+  module.CreateFreeFunction("print", &PrintNumber<float>);
+  module.CreateFreeFunction("printLn", &PrintNumber<float, true>);
+
+  module.CreateFreeFunction("print", &PrintNumber<double>);
+  module.CreateFreeFunction("printLn", &PrintNumber<double, true>);
+
+  module.CreateFreeFunction("print", &PrintArrayPrimitive<uint32_t>);
+  module.CreateFreeFunction("printLn", &PrintArrayPrimitive<uint32_t, true>);
 
   module.CreateFreeFunction("print", &PrintArrayPrimitive<int32_t>);
-  module.CreateFreeFunction("print", &PrintArrayPrimitive<int64_t>);
-  module.CreateFreeFunction("print", &PrintArrayPrimitive<uint32_t>);
-  module.CreateFreeFunction("print", &PrintArrayPrimitive<uint64_t>);
-  module.CreateFreeFunction("print", &PrintArrayPrimitive<float>);
-  module.CreateFreeFunction("print", &PrintArrayPrimitive<double>);
   module.CreateFreeFunction("printLn", &PrintArrayPrimitive<int32_t, true>);
-  module.CreateFreeFunction("printLn", &PrintArrayPrimitive<int64_t, true>);
-  module.CreateFreeFunction("printLn", &PrintArrayPrimitive<uint32_t, true>);
+
+  module.CreateFreeFunction("print", &PrintArrayPrimitive<uint64_t>);
   module.CreateFreeFunction("printLn", &PrintArrayPrimitive<uint64_t, true>);
+
+  module.CreateFreeFunction("print", &PrintArrayPrimitive<int64_t>);
+  module.CreateFreeFunction("printLn", &PrintArrayPrimitive<int64_t, true>);
+
+  module.CreateFreeFunction("print", &PrintArrayPrimitive<float>);
   module.CreateFreeFunction("printLn", &PrintArrayPrimitive<float, true>);
+
+  module.CreateFreeFunction("print", &PrintArrayPrimitive<double>);
   module.CreateFreeFunction("printLn", &PrintArrayPrimitive<double, true>);
 }
 
