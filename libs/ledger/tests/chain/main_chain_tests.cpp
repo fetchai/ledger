207ffddbc7d5a5b6cc804db65bfd9f2ccd79cf9d
Wojciech Kaluza <wojciech.kaluza@fetch.ai>
2019-06-07 11:38:47 +0100

    Introduce block timestamps

--- libs/ledger/tests/chain/main_chain_tests.cpp
+++ libs/ledger/tests/chain/main_chain_tests.cpp
@@ -50,7 +50,8 @@ static bool IsSameBlock(Block const &a, Block const &b)
   return (a.body.hash == b.body.hash) && (a.body.previous_hash == b.body.previous_hash) &&
          (a.body.merkle_hash == b.body.merkle_hash) &&
          (a.body.block_number == b.body.block_number) && (a.body.miner == b.body.miner) &&
-         (a.body.log2_num_lanes == b.body.log2_num_lanes) && (a.body.slices == b.body.slices) &&
+         (a.body.log2_num_lanes == b.body.log2_num_lanes) &&
+         (a.body.timestamp == b.body.timestamp) && (a.body.slices == b.body.slices) &&
          (a.proof == b.proof) && (a.nonce == b.nonce);
 }
 
@@ -141,7 +142,6 @@ TEST_P(MainChainTests, CheckSideChainSwitching)
   ASSERT_EQ(chain_->GetHeaviestBlockHash(), side2->body.hash);
 
   ASSERT_EQ(BlockStatus::ADDED, chain_->AddBlock(*main2));
-  ASSERT_EQ(chain_->GetHeaviestBlockHash(), side2->body.hash);  // because of hash values
 
   ASSERT_EQ(BlockStatus::ADDED, chain_->AddBlock(*main3));
   ASSERT_EQ(chain_->GetHeaviestBlockHash(), main3->body.hash);
@@ -177,7 +177,7 @@ TEST_P(MainChainTests, CheckChainBlockInvalidation)
   ASSERT_EQ(BlockStatus::ADDED, chain_->AddBlock(*main1));
   ASSERT_EQ(chain_->GetHeaviestBlockHash(), side2->body.hash);
   ASSERT_EQ(BlockStatus::ADDED, chain_->AddBlock(*main2));
-  ASSERT_EQ(chain_->GetHeaviestBlockHash(), side2->body.hash);  // because of hash values
+
   ASSERT_EQ(BlockStatus::ADDED, chain_->AddBlock(*main3));
   ASSERT_EQ(chain_->GetHeaviestBlockHash(), main3->body.hash);
 
