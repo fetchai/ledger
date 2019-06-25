f0a1678395d0c29e0204b5ecce023b0791489b89
Emma Smith <emma.smith@fetch.ai>
2019-06-25 18:10:29 +0100

    Used make_unique to make pointer to compiler

--- libs/vm_modules/examples/commodity_prediction/main.cpp
+++ libs/vm_modules/examples/commodity_prediction/main.cpp
@@ -142,7 +142,7 @@ int main(int argc, char **argv)
   module->CreateFreeFunction("read_csv", &read_csv);
 
   // Setting compiler up
-  fetch::vm::Compiler *    compiler = new fetch::vm::Compiler(module.get());
+  auto compiler = std::make_unique<fetch::vm::Compiler>(module.get());
   fetch::vm::Executable    executable;
   fetch::vm::IR            ir;
   std::vector<std::string> errors;
@@ -189,6 +189,5 @@ int main(int argc, char **argv)
   {
     std::cout << "Runtime error on line " << error << std::endl;
   }
-  delete compiler;
   return 0;
 }
