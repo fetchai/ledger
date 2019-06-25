8dea744a6be4295fc3a0d21c4940cb2381adbc84
Khan Baykaner <khan.baykaner@fetch.ai>
2019-06-25 13:35:59 +0100

    fixed point vm_module tests

--- libs/vm_modules/tests/unit/fixed_point.cpp
+++ libs/vm_modules/tests/unit/fixed_point.cpp
@@ -28,21 +28,175 @@ public:
   VmTestToolkit     toolkit{&stdout};
 };
 
-TEST_F(FixedPointTest, create_fixed_point)
+bool RunTest(VmTestToolkit &toolkit, std::stringstream &stdout, char const *TEXT, double gt)
 {
   auto m = toolkit.module();
+  EXPECT_TRUE(toolkit.Compile(TEXT));
+  EXPECT_TRUE(toolkit.Run());
+  EXPECT_EQ(std::stod(stdout.str()), gt);
+  return true;
+}
 
-  static char const *TEXT = R"(
+TEST_F(FixedPointTest, create_fixed_point)
+{
+  char const *TEXT = R"(
     function main()
       print(1.0fp32);
     endfunction
   )";
-
-  ASSERT_TRUE(toolkit.Compile(TEXT));
-  ASSERT_TRUE(toolkit.Run());
-
-  double gt = static_cast<double>(fetch::fixed_point::fp32_t(1));
-  EXPECT_EQ(std::stod(stdout.str()), gt);
+  double      gt   = static_cast<double>(fetch::fixed_point::fp32_t(1));
+  EXPECT_TRUE(RunTest(toolkit, stdout, TEXT, gt));
 }
+
+TEST_F(FixedPointTest, addition_fixed_point)
+{
+  char const *TEXT = R"(
+    function main()
+      var a = 2.0fp32;
+      var b = 3.0fp32;
+      a += b;
+      print(a);
+    endfunction
+  )";
+  double      gt   = static_cast<double>(fetch::fixed_point::fp32_t(5));
+  EXPECT_TRUE(RunTest(toolkit, stdout, TEXT, gt));
+}
+
+TEST_F(FixedPointTest, subtraction_fixed_point)
+{
+  char const *TEXT = R"(
+    function main()
+      var a = 3.0fp32;
+      var b = 2.0fp32;
+      a -= b;
+      print(a);
+    endfunction
+  )";
+  double      gt   = static_cast<double>(fetch::fixed_point::fp32_t(1));
+  EXPECT_TRUE(RunTest(toolkit, stdout, TEXT, gt));
+}
+
+TEST_F(FixedPointTest, multiplication_fixed_point)
+{
+  char const *TEXT = R"(
+    function main()
+      var a = 3.0fp32;
+      var b = 2.0fp32;
+      a *= b;
+      print(a);
+    endfunction
+  )";
+  double      gt   = static_cast<double>(fetch::fixed_point::fp32_t(6));
+  EXPECT_TRUE(RunTest(toolkit, stdout, TEXT, gt));
+}
+
+TEST_F(FixedPointTest, divide_fixed_point)
+{
+  char const *TEXT = R"(
+    function main()
+      var a = 3.0fp32;
+      var b = 2.0fp32;
+      a /= b;
+      print(a);
+    endfunction
+  )";
+  double      gt   = static_cast<double>(fetch::fixed_point::fp32_t(1.5));
+  EXPECT_TRUE(RunTest(toolkit, stdout, TEXT, gt));
+}
+
+TEST_F(FixedPointTest, array_32_fixed_point)
+{
+  char const *TEXT = R"(
+    function main()
+      var myArray = Array<Fixed32>(5);
+
+      for (i in 0:4)
+        myArray[i] = toFixed32(i);
+        printLn(myArray[i]);
+      endfor
+      print(myArray[3]);
+    endfunction
+  )";
+  double      gt   = static_cast<double>(fetch::fixed_point::fp32_t(3));
+  EXPECT_TRUE(RunTest(toolkit, stdout, TEXT, gt));
+}
+
+TEST_F(FixedPointTest, array_64_fixed_point)
+{
+  char const *TEXT = R"(
+    function main()
+      var myArray = Array<Fixed64>(5);
+
+      for (i in 0:4)
+        myArray[i] = toFixed64(i);
+        printLn(myArray[i]);
+      endfor
+      print(myArray[3]);
+    endfunction
+  )";
+  double      gt   = static_cast<double>(fetch::fixed_point::fp64_t(3));
+  EXPECT_TRUE(RunTest(toolkit, stdout, TEXT, gt));
+}
+
+TEST_F(FixedPointTest, map_32_fixed_point)
+{
+  char const *TEXT = R"(
+    function main()
+      var mymap = Map<Fixed32, Fixed32>();
+      mymap[0] = 1;
+      print(mymap[0]);
+    endfunction
+  )";
+  double      gt   = static_cast<double>(fetch::fixed_point::fp32_t(1));
+  EXPECT_TRUE(RunTest(toolkit, stdout, TEXT, gt));
+}
+//
+// TEST_F(FixedPointTest, sin_pi_32_fixed_point)
+//{
+//  char const *TEXT = R"(
+//    function main()
+//      var pi : Fixed64 = 3.1415fp64;
+//      print(sin(pi));
+//    endfunction
+//  )";
+//  double gt = static_cast<double>(fetch::fixed_point::fp32_t(0));
+//  EXPECT_TRUE(RunTest(toolkit, stdout, TEXT, gt));
+//}
+//
+// TEST_F(FixedPointTest, cos_pi_32_fixed_point)
+//{
+//  char const *TEXT = R"(
+//    function main()
+//      var pi : Fixed64 = 3.1415fp64;
+//      print(cos(pi));
+//    endfunction
+//  )";
+//  double gt = static_cast<double>(fetch::fixed_point::fp32_t(1));
+//  EXPECT_TRUE(RunTest(toolkit, stdout, TEXT, gt));
+//}
+//
+// TEST_F(FixedPointTest, exp_64_fixed_point)
+//{
+//  char const *TEXT = R"(
+//    function main()
+//      var val : Fixed64 = 1fp64;
+//      print(exp(val));
+//    endfunction
+//  )";
+//  double gt = static_cast<double>(fetch::fixed_point::fp64_t(2.71828182846));
+//  EXPECT_TRUE(RunTest(toolkit, stdout, TEXT, gt));
+//}
+//
+// TEST_F(FixedPointTest, pow_64_fixed_point)
+//{
+//  char const *TEXT = R"(
+//    function main()
+//      var val : Fixed64 = 2fp64;
+//      print(pow(val, val));
+//    endfunction
+//  )";
+//  double gt = static_cast<double>(fetch::fixed_point::fp64_t(4));
+//  EXPECT_TRUE(RunTest(toolkit, stdout, TEXT, gt));
+//}
 
 }  // namespace
