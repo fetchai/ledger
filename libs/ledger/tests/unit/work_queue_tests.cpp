b4b8e2d7c766368d908c56bd3db06aea8a0a5b2a
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-07-02 15:50:50 +0100

    Add failing test

---
+++ libs/ledger/tests/unit/work_queue_tests.cpp
@@ -0,0 +1,83 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "ledger/upow/work_queue.hpp"
+#include "ledger/upow/work.hpp"
+
+#include "gtest/gtest.h"
+
+#include <memory>
+
+#include <iostream>
+
+namespace {
+
+using fetch::byte_array::ConstByteArray;
+using fetch::ledger::WorkQueue;
+using fetch::ledger::Work;
+using fetch::ledger::WorkScore;
+using fetch::ledger::WorkPtr;
+
+using UInt256 = Work::UInt256;
+using WorkQueuePtr = std::unique_ptr<WorkQueue>;
+
+class WorkQueueTests : public ::testing::Test
+{
+protected:
+
+  void SetUp() override
+  {
+    work_queue_ = std::make_unique<WorkQueue>();
+  }
+
+  void TearDown() override
+  {
+    work_queue_.reset();
+  }
+
+  WorkPtr CreateWork(WorkScore score, uint64_t nonce)
+  {
+    auto work = std::make_shared<Work>();
+    work->UpdateScore(score);
+    work->UpdateNonce(UInt256{nonce});
+    return work;
+  }
+
+  WorkQueuePtr work_queue_;
+};
+
+
+TEST_F(WorkQueueTests, CheckOrdering)
+{
+  auto item1 = CreateWork(2000, 12);
+  auto item2 = CreateWork(2000, 12321321);
+
+  work_queue_->push(item2);
+  work_queue_->push(item1);
+
+  auto first = work_queue_->top();
+  work_queue_->pop();
+
+  auto second = work_queue_->top();
+  work_queue_->pop();
+
+  EXPECT_EQ(first.get(), item1.get());
+  EXPECT_EQ(second.get(), item2.get());
+}
+
+} // namespace
