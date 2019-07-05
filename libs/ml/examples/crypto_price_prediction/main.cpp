3d00c0321dd984b623a47498f608c080c05fe6ae
Khan Baykaner <khan.baykaner@fetch.ai>
2019-07-05 12:31:38 +0100

    WIP - initial implementation compiles

--- libs/ml/examples/crypto_price_prediction/main.cpp
+++ libs/ml/examples/crypto_price_prediction/main.cpp
@@ -17,22 +17,23 @@
 //------------------------------------------------------------------------------
 
 #include "math/tensor.hpp"
-#include "ml/dataloaders/mnist_loaders/mnist_loader.hpp"
+//#include "ml/dataloaders/mnist_loaders/mnist_loader.hpp"
 #include "ml/graph.hpp"
 #include "ml/layers/convolution_1d.hpp"
 #include "ml/ops/activation.hpp"
 #include "ml/ops/loss_functions/mean_square_error.hpp"
 #include "ml/optimisation/adam_optimiser.hpp"
-#include "ml/regularisers/l1_regulariser.hpp"
-#include "ml/regularisers/regularisation.hpp"
+//#include "ml/regularisers/l1_regulariser.hpp"
+//#include "ml/regularisers/regularisation.hpp"
+#include "ml/dataloaders/ReadCSV.hpp"
 
-#include <cstddef>
-#include <cstdint>
-#include <iostream>
-#include <string>
-#include <unordered_map>
-#include <utility>
-#include <vector>
+//#include <cstddef>
+//#include <cstdint>
+//#include <iostream>
+//#include <string>
+//#include <unordered_map>
+//#include <utility>
+//#include <vector>
 
 using namespace fetch::ml::ops;
 using namespace fetch::ml::layers;
@@ -70,11 +71,17 @@ std::shared_ptr<GraphType> BuildModel(std::string &input_name, std::string &outp
   output_name         = g->AddNode<fetch::ml::layers::Convolution1D<ArrayType>>(
       "Conv1D_2", {layer_2}, conv1D_2_filters, conv1D_2_input_channels, conv1D_2_kernel_size,
       conv1D_2_stride);
+
+  return g;
 }
 
 // TODO: convert to a dataloader
-void LoadData(std::string const &data_filename, std::string const &labels_filename)
-{}
+std::vector<ArrayType> LoadData(std::string const &data_filename, std::string const &labels_filename)
+{
+  auto data_tensor = fetch::ml::dataloaders::ReadCSV<ArrayType>(data_filename);
+  auto labels_tensor = fetch::ml::dataloaders::ReadCSV<ArrayType>(labels_filename);
+  return {data_tensor, labels_tensor};
+}
 
 int main(int ac, char **av)
 {
@@ -83,8 +90,6 @@ int main(int ac, char **av)
   SizeType batch_size{8};
   //  fetch::ml::details::RegularisationType regulariser =
   //  fetch::ml::details::RegularisationType::L1; DataType reg_rate{0.01f};
-  std::string input_name  = "";
-  std::string output_name = "";
 
   if (ac < 3)
   {
@@ -95,18 +100,20 @@ int main(int ac, char **av)
   std::cout << "FETCH Crypto price prediction demo" << std::endl;
 
   // Load training data
-  LoadData(av[1], av[2]);
+  std::vector<ArrayType> data_and_labels = LoadData(av[1], av[2]);
   //  DataLoaderType data_loader(av[1], av[2]);
 
   // Prepare graph & optimiser
+  std::string input_name  = "";
+  std::string output_name = "";
   std::shared_ptr<GraphType> g = BuildModel(input_name, output_name);
-  OptimiserType              optimiser(g, {input}, output);
+  OptimiserType              optimiser(g, {input_name}, output_name);
 
   // Training loop
   DataType loss;
   for (SizeType i{0}; i < epochs; i++)
   {
-    loss = optimiser.Run(data_loader, batch_size, subset_size);
+    loss = optimiser.Run({data_and_labels.at(0)}, data_and_labels.at(1), batch_size);
     std::cout << "Loss: " << loss << std::endl;
   }
 
