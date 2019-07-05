0af6fa031e27871c759864880892c865d6bcf81f
Khan Baykaner <khan.baykaner@fetch.ai>
2019-07-05 12:36:46 +0100

    refactoring

--- libs/ml/examples/crypto_price_prediction/main.cpp
+++ libs/ml/examples/crypto_price_prediction/main.cpp
@@ -17,35 +17,27 @@
 //------------------------------------------------------------------------------
 
 #include "math/tensor.hpp"
-//#include "ml/dataloaders/mnist_loaders/mnist_loader.hpp"
 #include "ml/graph.hpp"
 #include "ml/layers/convolution_1d.hpp"
 #include "ml/ops/activation.hpp"
 #include "ml/ops/loss_functions/mean_square_error.hpp"
 #include "ml/optimisation/adam_optimiser.hpp"
-//#include "ml/regularisers/l1_regulariser.hpp"
-//#include "ml/regularisers/regularisation.hpp"
 #include "ml/dataloaders/ReadCSV.hpp"
 
-//#include <cstddef>
-//#include <cstdint>
-//#include <iostream>
-//#include <string>
-//#include <unordered_map>
-//#include <utility>
-//#include <vector>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace fetch::ml::ops;
 using namespace fetch::ml::layers;
 
 using DataType  = float;
-using ArrayType = fetch::math::Tensor<DataType>;
-using SizeType  = typename ArrayType::SizeType;
+using TensorType = fetch::math::Tensor<DataType>;
+using SizeType  = typename TensorType::SizeType;
 
-using GraphType        = typename fetch::ml::Graph<ArrayType>;
-using CostFunctionType = typename fetch::ml::ops::MeanSquareError<ArrayType>;
-using OptimiserType    = typename fetch::ml::optimisers::AdamOptimiser<ArrayType, CostFunctionType>;
-// using DataLoaderType   = typename fetch::ml::dataloaders::MNISTLoader<ArrayType, ArrayType>;
+using GraphType        = typename fetch::ml::Graph<TensorType>;
+using CostFunctionType = typename fetch::ml::ops::MeanSquareError<TensorType>;
+using OptimiserType    = typename fetch::ml::optimisers::AdamOptimiser<TensorType, CostFunctionType>;
 
 std::shared_ptr<GraphType> BuildModel(std::string &input_name, std::string &output_name)
 {
@@ -56,40 +48,36 @@ std::shared_ptr<GraphType> BuildModel(std::string &input_name, std::string &outp
   SizeType conv1D_1_kernel_size    = 20;
   SizeType conv1D_1_stride         = 3;
 
-  typename ArrayType::Type keep_prob = 0.5;
+  typename TensorType::Type keep_prob = 0.5;
 
   SizeType conv1D_2_filters        = 1;
   SizeType conv1D_2_input_channels = 1;
   SizeType conv1D_2_kernel_size    = 16;
   SizeType conv1D_2_stride         = 4;
 
-  input_name          = g->AddNode<PlaceHolder<ArrayType>>("Input", {});
-  std::string layer_1 = g->AddNode<fetch::ml::layers::Convolution1D<ArrayType>>(
+  input_name          = g->AddNode<PlaceHolder<TensorType>>("Input", {});
+  std::string layer_1 = g->AddNode<fetch::ml::layers::Convolution1D<TensorType>>(
       "Conv1D_1", {input_name}, conv1D_1_filters, conv1D_1_input_channels, conv1D_1_kernel_size,
       conv1D_1_stride, fetch::ml::details::ActivationType::RELU);
-  std::string layer_2 = g->AddNode<Dropout<ArrayType>>("Dropout_1", {layer_1}, keep_prob);
-  output_name         = g->AddNode<fetch::ml::layers::Convolution1D<ArrayType>>(
+  std::string layer_2 = g->AddNode<Dropout<TensorType>>("Dropout_1", {layer_1}, keep_prob);
+  output_name         = g->AddNode<fetch::ml::layers::Convolution1D<TensorType>>(
       "Conv1D_2", {layer_2}, conv1D_2_filters, conv1D_2_input_channels, conv1D_2_kernel_size,
       conv1D_2_stride);
 
   return g;
 }
 
-// TODO: convert to a dataloader
-std::vector<ArrayType> LoadData(std::string const &data_filename, std::string const &labels_filename)
+std::vector<TensorType> LoadData(std::string const &data_filename, std::string const &labels_filename)
 {
-  auto data_tensor = fetch::ml::dataloaders::ReadCSV<ArrayType>(data_filename);
-  auto labels_tensor = fetch::ml::dataloaders::ReadCSV<ArrayType>(labels_filename);
+  auto data_tensor = fetch::ml::dataloaders::ReadCSV<TensorType>(data_filename);
+  auto labels_tensor = fetch::ml::dataloaders::ReadCSV<TensorType>(labels_filename);
   return {data_tensor, labels_tensor};
 }
 
 int main(int ac, char **av)
 {
-  //  SizeType                               subset_size{100};
   SizeType epochs{10};
   SizeType batch_size{8};
-  //  fetch::ml::details::RegularisationType regulariser =
-  //  fetch::ml::details::RegularisationType::L1; DataType reg_rate{0.01f};
 
   if (ac < 3)
   {
@@ -99,17 +87,16 @@ int main(int ac, char **av)
 
   std::cout << "FETCH Crypto price prediction demo" << std::endl;
 
-  // Load training data
-  std::vector<ArrayType> data_and_labels = LoadData(av[1], av[2]);
-  //  DataLoaderType data_loader(av[1], av[2]);
+  std::cout << "Loading crypto price data... " << std::endl;
+  std::vector<TensorType> data_and_labels = LoadData(av[1], av[2]);
 
-  // Prepare graph & optimiser
-  std::string input_name  = "";
-  std::string output_name = "";
+  std::cout << "Build model & optimiser... "<< std::endl;
+  std::string input_name;
+  std::string output_name;
   std::shared_ptr<GraphType> g = BuildModel(input_name, output_name);
   OptimiserType              optimiser(g, {input_name}, output_name);
 
-  // Training loop
+  std::cout << "Begin training loop... "<< std::endl;
   DataType loss;
   for (SizeType i{0}; i < epochs; i++)
   {
