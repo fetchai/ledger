96e8689047f70fcd5ea50540a8a3f7bb1baaeea4
Khan Baykaner <khan.baykaner@fetch.ai>
2019-07-08 17:07:17 +0100

    WIP normalise implemented - denormalise not yet ready

--- libs/ml/examples/crypto_price_prediction/main.cpp
+++ libs/ml/examples/crypto_price_prediction/main.cpp
@@ -17,13 +17,15 @@
 //------------------------------------------------------------------------------
 
 #include "math/tensor.hpp"
+#include "math/normalize_array.hpp"
+
 #include "ml/dataloaders/ReadCSV.hpp"
+#include "ml/dataloaders/tensor_dataloader.hpp"
 #include "ml/graph.hpp"
 #include "ml/layers/convolution_1d.hpp"
 #include "ml/ops/activation.hpp"
 #include "ml/ops/loss_functions/mean_square_error.hpp"
 #include "ml/optimisation/adam_optimiser.hpp"
-#include "ml/dataloaders/tensor_dataloader.hpp"
 
 #include <iostream>
 #include <string>
@@ -38,8 +40,8 @@ using SizeType   = typename TensorType::SizeType;
 
 using GraphType        = typename fetch::ml::Graph<TensorType>;
 using CostFunctionType = typename fetch::ml::ops::MeanSquareError<TensorType>;
-using OptimiserType = typename fetch::ml::optimisers::AdamOptimiser<TensorType, CostFunctionType>;
-using DataLoaderType   = typename fetch::ml::dataloaders::TensorDataLoader<TensorType, TensorType>;
+using OptimiserType  = typename fetch::ml::optimisers::AdamOptimiser<TensorType, CostFunctionType>;
+using DataLoaderType = typename fetch::ml::dataloaders::TensorDataLoader<TensorType, TensorType>;
 
 std::shared_ptr<GraphType> BuildModel(std::string &input_name, std::string &output_name)
 {
@@ -74,36 +76,103 @@ std::vector<TensorType> LoadData(std::string const &train_data_filename,
                                  std::string const &test_data_filename,
                                  std::string const &test_labels_filename)
 {
-  auto train_data_tensor   = fetch::ml::dataloaders::ReadCSV<TensorType>(train_data_filename, 0, 0, true);
-  auto train_labels_tensor = fetch::ml::dataloaders::ReadCSV<TensorType>(train_labels_filename, 0, 0, true);
-  auto test_data_tensor   = fetch::ml::dataloaders::ReadCSV<TensorType>(test_data_filename, 0, 0, true);
-  auto test_labels_tensor = fetch::ml::dataloaders::ReadCSV<TensorType>(test_labels_filename, 0, 0, true);
+  auto train_data_tensor =
+      fetch::ml::dataloaders::ReadCSV<TensorType>(train_data_filename, 0, 0, true);
+  auto train_labels_tensor =
+      fetch::ml::dataloaders::ReadCSV<TensorType>(train_labels_filename, 0, 0, true);
+  auto test_data_tensor =
+      fetch::ml::dataloaders::ReadCSV<TensorType>(test_data_filename, 0, 0, true);
+  auto test_labels_tensor =
+      fetch::ml::dataloaders::ReadCSV<TensorType>(test_labels_filename, 0, 0, true);
 
   train_data_tensor.Reshape({1, train_data_tensor.shape().at(0), train_data_tensor.shape().at(1)});
-  train_labels_tensor.Reshape({1, train_labels_tensor.shape().at(0), train_labels_tensor.shape().at(1)});
+  train_labels_tensor.Reshape(
+      {1, train_labels_tensor.shape().at(0), train_labels_tensor.shape().at(1)});
   test_data_tensor.Reshape({1, test_data_tensor.shape().at(0), test_data_tensor.shape().at(1)});
-  test_labels_tensor.Reshape({1, test_labels_tensor.shape().at(0), test_labels_tensor.shape().at(1)});
+  test_labels_tensor.Reshape(
+      {1, test_labels_tensor.shape().at(0), test_labels_tensor.shape().at(1)});
   return {train_data_tensor, train_labels_tensor, test_data_tensor, test_labels_tensor};
 }
 
-void NormaliseFeatures()
+/**
+ * MinMax scaler normaliser
+ * @param input_data
+ * @param reference_data
+ * @return
+ */
+TensorType NormaliseFeatures(TensorType &input_data, TensorType const & reference_data)
 {
+  // set up return tensor
+  TensorType ret(input_data.shape());
 
+  // set up min, max, and range tensors
+  TensorType x_min({1, reference_data.shape(1)});
+  TensorType x_max({1, reference_data.shape(1)});
+  TensorType x_range({1, reference_data.shape(1)});
+
+  // calculate min, max, and range of each feature
+  for (std::size_t i = 0; i < reference_data.shape(2); ++i)
+  {
+    auto x_min_it = x_min.begin();
+    auto x_max_it = x_max.begin();
+    auto x_range_it = x_range.begin();
+    auto train_it = input_data.Slice(i, 2).begin();
+    while (x_min_it.is_valid())
+    {
+      if (*x_min_it > *train_it)
+      {
+        *x_min_it = *train_it;
+      }
+
+      if (*x_max_it < *train_it)
+      {
+        *x_max_it = *train_it;
+      }
+
+      if (*x_range_it < (*x_max_it - *x_min_it))
+      {
+        *x_range_it = (*x_max_it - *x_min_it);
+      }
+
+      ++train_it;
+      ++x_min_it;
+      ++x_max_it;
+      ++x_range_it;
+    }
+  }
+
+  // apply normalisation to each feature according to scale -1, 1
+  for (std::size_t i = 0; i < reference_data.shape(2); ++i)
+  {
+    auto x_min_it = x_min.begin();
+    auto x_range_it = x_range.begin();
+    auto ret_it = ret.Slice(i, 2).begin();
+    while (ret_it.is_valid())
+    {
+      *ret_it = (*ret_it - *x_min_it) / (*x_range_it);
+
+      ++ret_it;
+      ++x_min_it;
+      ++x_range_it;
+    }
+  }
+  return ret;
 }
-
-void DeNormalisePrediction()
-{
-
-}
+//
+//void DeNormalisePrediction()
+//{}
 
 int main(int ac, char **av)
 {
-  SizeType epochs{3};
+  SizeType epochs{10};
   SizeType batch_size{8};
+  bool normalise = false;
 
   if (ac < 5)
   {
-    std::cout << "Usage : " << av[0] << " PATH/TO/train-data PATH/TO/train-labels PATH/TO/test-data PATH/TO/test-labels" << std::endl;
+    std::cout << "Usage : " << av[0]
+              << " PATH/TO/train-data PATH/TO/train-labels PATH/TO/test-data PATH/TO/test-labels"
+              << std::endl;
     return 1;
   }
 
@@ -111,13 +180,27 @@ int main(int ac, char **av)
 
   std::cout << "Loading crypto price data... " << std::endl;
   std::vector<TensorType> data_and_labels = LoadData(av[1], av[2], av[3], av[4]);
-  TensorType train_data = data_and_labels.at(0);
-  TensorType train_label = data_and_labels.at(1);
-  TensorType test_data = data_and_labels.at(2);
-  TensorType test_label = data_and_labels.at(3);
+  TensorType              train_data      = data_and_labels.at(0);
+  TensorType              train_label     = data_and_labels.at(1);
+  TensorType              test_data       = data_and_labels.at(2);
+  TensorType              test_label      = data_and_labels.at(3);
 
-//  DataLoaderType loader;
-//  loader.AddData(train_data, train_label);
+  if (normalise)
+  {
+    TensorType orig_train_data = train_data.Copy();
+    TensorType orig_train_label = train_label.Copy();
+    TensorType orig_test_data = test_data.Copy();
+    TensorType orig_test_label = test_label.Copy();
+
+    // normalise training and test data with respect to training data range
+    train_data = NormaliseFeatures(train_data, train_data);
+    train_label = NormaliseFeatures(train_label, train_data);
+    test_data = NormaliseFeatures(test_data, train_data);
+    test_label = NormaliseFeatures(test_label, train_data);
+  }
+
+  //  DataLoaderType loader;
+  //  loader.AddData(train_data, train_label);
 
   std::cout << "Build model & optimiser... " << std::endl;
   std::string                input_name;
@@ -139,7 +222,6 @@ int main(int ac, char **av)
   test_label.Reshape({test_label.shape().at(1), test_label.shape().at(2)});
   auto result = fetch::math::MeanSquareError(prediction, test_label);
   std::cout << "result: " << result << std::endl;
-//  std::cout << "Begin testing: " << std::endl;
 
   return 0;
 }
--- libs/vm_modules/examples/crypto_price_prediction/script/bitcoin.cpp
+++ libs/vm_modules/examples/crypto_price_prediction/script/bitcoin.cpp
@@ -1,59 +1,86 @@
-function build_graph(graph: Graph)
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
 
-var conv1D_1_filters = 8;
+function build_graph(graph
+                     : Graph)
+
+    var conv1D_1_filters    = 8;
 var conv1D_1_input_channels = 1;
-var conv1D_1_kernel_size = 20;
-var conv1D_1_stride = 3;
+var conv1D_1_kernel_size    = 20;
+var conv1D_1_stride         = 3;
 
 var keep_prob = 0.5f;
 
-var conv1D_2_filters = 1;
+var conv1D_2_filters        = 1;
 var conv1D_2_input_channels = conv1D_1_filters;
-var conv1D_2_kernel_size = 16;
-var conv1D_2_stride = 4;
+var conv1D_2_kernel_size    = 16;
+var conv1D_2_stride         = 4;
 
 graph.AddPlaceholder("Input");
-graph.AddConv1D("hidden_conv1D_1", "Input", conv1D_1_filters, conv1D_1_input_channels, conv1D_1_kernel_size, conv1D_1_stride);
+graph.AddConv1D("hidden_conv1D_1", "Input", conv1D_1_filters, conv1D_1_input_channels,
+                conv1D_1_kernel_size, conv1D_1_stride);
 graph.AddDropout("dropout_1", "hidden_conv1D_1", keep_prob);
-graph.AddFullyConnected("hidden_conv1D_1", "dropout_1", conv1D_2_filters, conv1D_2_input_channels, conv1D_2_kernel_size, conv1D_2_stride);
+graph.AddFullyConnected("hidden_conv1D_1", "dropout_1", conv1D_2_filters, conv1D_2_input_channels,
+                        conv1D_2_kernel_size, conv1D_2_stride);
 endfunction
-//
-//
-//function read_weights(graph: Graph)
-//// read in weights
-//var file_weights0 = System.Argv(0) +  "/output/keras_aluminium_px_last_us/model_weights/hidden_dense_1/hidden_dense_1_12/kernel:0.csv";
-//var file_bias0    = System.Argv(0) +  "/output/keras_aluminium_px_last_us/model_weights/hidden_dense_1/hidden_dense_1_12/bias:0.csv";
-//var file_weights1 = System.Argv(0) +  "/output/keras_aluminium_px_last_us/model_weights/hidden_dense_2/hidden_dense_2_4/kernel:0.csv";
-//var file_bias1    = System.Argv(0) +  "/output/keras_aluminium_px_last_us/model_weights/hidden_dense_2/hidden_dense_2_4/bias:0.csv";
-//var file_weights2 = System.Argv(0) +  "/output/keras_aluminium_px_last_us/model_weights/output_dense/output_dense_12/kernel:0.csv";
-//var file_bias2    = System.Argv(0) +  "/output/keras_aluminium_px_last_us/model_weights/output_dense/output_dense_12/bias:0.csv";
-//
-//var weights0 = read_csv(file_weights0, true);
-//var bias0    = read_csv(file_bias0, false);
-//var weights1 = read_csv(file_weights1, true);
-//var bias1    = read_csv(file_bias1, false);
-//var weights2 = read_csv(file_weights2, true);
-//var bias2    = read_csv(file_bias2, false);
-//
-//// load weights into graph
-//var sd = graph.StateDict();
-//sd.SetWeights("hidden_dense_1" + "_FC_Weights", weights0);
-//sd.SetWeights("hidden_dense_1" + "_FC_Bias", bias0);
-//sd.SetWeights("hidden_dense_2" + "_FC_Weights", weights1);
-//sd.SetWeights("hidden_dense_2" + "_FC_Bias", bias1);
-//sd.SetWeights("output_dense" + "_FC_Weights", weights2);
-//sd.SetWeights("output_dense" + "_FC_Bias", bias2);
-//graph.LoadStateDict(sd);
-//endfunction
+    //
+    //
+    // function read_weights(graph: Graph)
+    //// read in weights
+    // var file_weights0 = System.Argv(0) +
+    // "/output/keras_aluminium_px_last_us/model_weights/hidden_dense_1/hidden_dense_1_12/kernel:0.csv";
+    // var file_bias0    = System.Argv(0) +
+    // "/output/keras_aluminium_px_last_us/model_weights/hidden_dense_1/hidden_dense_1_12/bias:0.csv";
+    // var file_weights1 = System.Argv(0) +
+    // "/output/keras_aluminium_px_last_us/model_weights/hidden_dense_2/hidden_dense_2_4/kernel:0.csv";
+    // var file_bias1    = System.Argv(0) +
+    // "/output/keras_aluminium_px_last_us/model_weights/hidden_dense_2/hidden_dense_2_4/bias:0.csv";
+    // var file_weights2 = System.Argv(0) +
+    // "/output/keras_aluminium_px_last_us/model_weights/output_dense/output_dense_12/kernel:0.csv";
+    // var file_bias2    = System.Argv(0) +
+    // "/output/keras_aluminium_px_last_us/model_weights/output_dense/output_dense_12/bias:0.csv";
+    //
+    // var weights0 = read_csv(file_weights0, true);
+    // var bias0    = read_csv(file_bias0, false);
+    // var weights1 = read_csv(file_weights1, true);
+    // var bias1    = read_csv(file_bias1, false);
+    // var weights2 = read_csv(file_weights2, true);
+    // var bias2    = read_csv(file_bias2, false);
+    //
+    //// load weights into graph
+    // var sd = graph.StateDict();
+    // sd.SetWeights("hidden_dense_1" + "_FC_Weights", weights0);
+    // sd.SetWeights("hidden_dense_1" + "_FC_Bias", bias0);
+    // sd.SetWeights("hidden_dense_2" + "_FC_Weights", weights1);
+    // sd.SetWeights("hidden_dense_2" + "_FC_Bias", bias1);
+    // sd.SetWeights("output_dense" + "_FC_Weights", weights2);
+    // sd.SetWeights("output_dense" + "_FC_Bias", bias2);
+    // graph.LoadStateDict(sd);
+    // endfunction
 
-function main()
+    function
+    main()
 
-if (System.Argc() != 3)
-  print("Usage: VM SCRIPT_FILE PATH/TO/INPUT.csv /PATH/TO/LABEL.csv");
-  return;
+        if (System.Argc() != 3) print("Usage: VM SCRIPT_FILE PATH/TO/INPUT.csv /PATH/TO/LABEL.csv");
+return;
 endif
 
-var loader = DataLoader();
+    var loader = DataLoader();
 loader.AddData("tensor", System.Argv(1), System.Argv(2));
 
 var graph = Graph();
@@ -61,15 +88,13 @@ build_graph(graph);
 
 var testing = true;
 
-//read_weights(graph);
+// read_weights(graph);
 
-while(!loader.IsDone())
-    var input_data = loader.GetNext();
-    graph.SetInput("num_input", input_data.Data());
-    var pred = graph.Evaluate("softmax_3");
-    print(pred.ToString());
+while (!loader.IsDone())
+  var input_data = loader.GetNext();
+graph.SetInput("num_input", input_data.Data());
+var pred = graph.Evaluate("softmax_3");
+print(pred.ToString());
 endwhile
 
-
-
-endfunction
+    endfunction
