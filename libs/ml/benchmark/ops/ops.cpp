8646139dbf836db3210c2b38c7586d13a07fed71
Jiri Vestfal <jiri.vestfal@fetch.ai>
2019-07-09 14:15:18 +0100

    Multiply benchmark

--- libs/ml/benchmark/ops/ops.cpp
+++ libs/ml/benchmark/ops/ops.cpp
@@ -27,6 +27,7 @@
 #include "ml/ops/log.hpp"
 #include "ml/ops/exp.hpp"
 #include "ml/ops/divide.hpp"
+#include "ml/ops/multiply.hpp"
 
 #include "vectorise/fixed_point/fixed_point.hpp"
 
@@ -227,5 +228,30 @@ BENCHMARK_TEMPLATE(BM_DivideForward, double, 512)->Unit(benchmark::kMicrosecond)
 BENCHMARK_TEMPLATE(BM_DivideForward, double, 1024)->Unit(benchmark::kMicrosecond);
 BENCHMARK_TEMPLATE(BM_DivideForward, double, 2048)->Unit(benchmark::kMicrosecond);
 BENCHMARK_TEMPLATE(BM_DivideForward, double, 4096)->Unit(benchmark::kMicrosecond);
+
+template <class T, int N>
+void BM_MultiplyForward(benchmark::State &state)
+{
+    fetch::math::Tensor<T> input_1({1, N});
+    fetch::math::Tensor<T> input_2({1, N});
+    fetch::math::Tensor<T> output({1, N});
+
+    std::vector<std::reference_wrapper<fetch::math::Tensor<T> const>> inputs;
+    inputs.push_back(input_1);
+    inputs.push_back(input_2);
+    fetch::ml::ops::Multiply<fetch::math::Tensor<T>> mul1;
+
+    for (auto _ : state)
+    {
+        mul1.Forward(inputs, output);
+    }
+}
+
+BENCHMARK_TEMPLATE(BM_MultiplyForward, double, 2)->Unit(benchmark::kNanosecond);
+BENCHMARK_TEMPLATE(BM_MultiplyForward, double, 256)->Unit(benchmark::kMicrosecond);
+BENCHMARK_TEMPLATE(BM_MultiplyForward, double, 512)->Unit(benchmark::kMicrosecond);
+BENCHMARK_TEMPLATE(BM_MultiplyForward, double, 1024)->Unit(benchmark::kMicrosecond);
+BENCHMARK_TEMPLATE(BM_MultiplyForward, double, 2048)->Unit(benchmark::kMicrosecond);
+BENCHMARK_TEMPLATE(BM_MultiplyForward, double, 4096)->Unit(benchmark::kMicrosecond);
 
 BENCHMARK_MAIN();
