e00e336904400b9076d981f1eaf3c07de0374933
Khan Baykaner <khan.baykaner@fetch.ai>
2019-07-09 11:11:43 +0100

    removed leftover include

--- libs/vm_modules/include/vm_modules/ml/ml.hpp
+++ libs/vm_modules/include/vm_modules/ml/ml.hpp
@@ -21,8 +21,6 @@
 #include "vm_modules/math/tensor.hpp"
 #include "vm_modules/ml/dataloaders/dataloader.hpp"
 #include "vm_modules/ml/graph.hpp"
-#include "vm_modules/ml/ops/loss_functions/cross_entropy_loss.hpp"
-#include "vm_modules/ml/ops/loss_functions/mean_square_error_loss.hpp"
 #include "vm_modules/ml/optimisation/adam_optimiser.hpp"
 #include "vm_modules/ml/state_dict.hpp"
 #include "vm_modules/ml/training_pair.hpp"
