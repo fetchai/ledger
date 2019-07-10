c1ef20c134c8e7cdf8d2cb1c5cc4c0871f0878a5
Jiri Vestfal <45360788+MissingNO57@users.noreply.github.com>
2019-07-10 15:39:58 +0100

    Added tests and benchmarks (#1323)
    Tests for OPS: abs, divide, exp, log, maximum, sqrt
    Fixed Divide gradient
    Implemented OPS: abs, exp, log, maximum

--- libs/ml/tests/ml/ops/abs.cpp
+++ libs/ml/tests/ml/ops/abs.cpp
@@ -16,19 +16,16 @@
 //
 //------------------------------------------------------------------------------
 
-#include "ml/ops/divide.hpp"
+#include "math/base_types.hpp"
+#include "ml/ops/abs.hpp"
 
 #include "math/tensor.hpp"
 #include "vectorise/fixed_point/fixed_point.hpp"
 
 #include "gtest/gtest.h"
 
-#include <cstdint>
-#include <vector>
-
-namespace {
 template <typename T>
-class DivideTest : public ::testing::Test
+class AbsTest : public ::testing::Test
 {
 };
 
@@ -36,67 +33,52 @@ using MyTypes = ::testing::Types<fetch::math::Tensor<float>, fetch::math::Tensor
                                  fetch::math::Tensor<fetch::fixed_point::fp32_t>,
                                  fetch::math::Tensor<fetch::fixed_point::fp64_t>>;
 
-TYPED_TEST_CASE(DivideTest, MyTypes);
+TYPED_TEST_CASE(AbsTest, MyTypes);
 
-TYPED_TEST(DivideTest, forward_test)
+TYPED_TEST(AbsTest, forward_test)
 {
   using ArrayType = TypeParam;
   using DataType  = typename TypeParam::Type;
 
-  ArrayType data_1 = ArrayType::FromString(
+  ArrayType data = ArrayType::FromString(
       "1, -2, 3,-4, 5,-6, 7,-8;"
       "1,  2, 3, 4, 5, 6, 7, 8");
 
-  ArrayType data_2 = ArrayType::FromString(
-      " 8, -7, 6,-5, 4,-3, 2,-1;"
-      "-8,  7,-6, 5,-4, 3,-2, 1");
-
   ArrayType gt = ArrayType::FromString(
-      "0.125,	0.285714285714286,	0.5,	0.8,	1.25,	2,	3.5,	8;"
-      "-0.125, 0.285714285714286,	-0.5,	0.8,	-1.25,	2,	-3.5,	8");
+      "1, 2, 3, 4, 5, 6, 7, 8;"
+      "1, 2, 3, 4, 5, 6, 7, 8");
 
-  fetch::ml::ops::Divide<ArrayType> op;
+  fetch::ml::ops::Abs<ArrayType> op;
 
-  TypeParam prediction(op.ComputeOutputShape({data_1, data_2}));
-  op.Forward({data_1, data_2}, prediction);
+  TypeParam prediction(op.ComputeOutputShape({data}));
+  op.Forward({data}, prediction);
 
   // test correct values
   ASSERT_TRUE(prediction.AllClose(gt, fetch::math::function_tolerance<DataType>(),
                                   fetch::math::function_tolerance<DataType>()));
 }
 
-TYPED_TEST(DivideTest, backward_test)
+TYPED_TEST(AbsTest, backward_test)
 {
   using ArrayType = TypeParam;
   using DataType  = typename TypeParam::Type;
 
-  ArrayType data_1 = ArrayType::FromString(
+  ArrayType data = ArrayType::FromString(
       "1, -2, 3,-4, 5,-6, 7,-8;"
       "1,  2, 3, 4, 5, 6, 7, 8");
 
-  ArrayType data_2 = ArrayType::FromString(
-      "8, -7, 6,-5, 4,-3, 2,-1;"
-      "8,  7,-6, 5,-4, 3,-2, 1");
-
-  ArrayType gt_1 = ArrayType::FromString(
-      "0.125, 0.142857142857143, 0.333333333333333, 0.4, 0.75, 1, 2, 4;"
-      "0.625, -0.714285714285714, -1, -1.2, -1.75, -2.33333333333333, -4, -8");
-
-  ArrayType gt_2 = ArrayType::FromString(
-      "0.015625, 0.040816326530612, 0.166666666666667, 0.32, 0.9375, 2, 7, 32;"
-      "0.078125, -0.204081632653061, 0.5, -0.96, 2.1875, -4.66666666666667, 14, -64");
+  ArrayType gt = ArrayType::FromString(
+      "1, 1, 2, 2, 3, 3, 4, 4;"
+      "5, -5, 6, -6, 7, -7, 8, -8");
 
   ArrayType error = ArrayType::FromString(
       "1, -1, 2, -2, 3, -3, 4, -4;"
       "5, -5, 6, -6, 7, -7, 8, -8");
 
-  fetch::ml::ops::Divide<ArrayType> op;
-  std::vector<ArrayType>            prediction = op.Backward({data_1, data_2}, error);
+  fetch::ml::ops::Abs<ArrayType> op;
+  std::vector<ArrayType>         prediction = op.Backward({data}, error);
 
   // test correct values
-  ASSERT_TRUE(prediction[0].AllClose(gt_1, fetch::math::function_tolerance<DataType>(),
-                                     fetch::math::function_tolerance<DataType>()));
-  ASSERT_TRUE(prediction[1].AllClose(gt_2, fetch::math::function_tolerance<DataType>(),
+  ASSERT_TRUE(prediction[0].AllClose(gt, fetch::math::function_tolerance<DataType>(),
                                      fetch::math::function_tolerance<DataType>()));
 }
-}  // namespace
--- libs/ml/tests/ml/ops/multiply.cpp
+++ libs/ml/tests/ml/ops/multiply.cpp
@@ -16,19 +16,15 @@
 //
 //------------------------------------------------------------------------------
 
-#include "ml/ops/divide.hpp"
+#include "ml/ops/multiply.hpp"
 
 #include "math/tensor.hpp"
 #include "vectorise/fixed_point/fixed_point.hpp"
 
 #include "gtest/gtest.h"
 
-#include <cstdint>
-#include <vector>
-
-namespace {
 template <typename T>
-class DivideTest : public ::testing::Test
+class MultiplyTest : public ::testing::Test
 {
 };
 
@@ -36,36 +32,9 @@ using MyTypes = ::testing::Types<fetch::math::Tensor<float>, fetch::math::Tensor
                                  fetch::math::Tensor<fetch::fixed_point::fp32_t>,
                                  fetch::math::Tensor<fetch::fixed_point::fp64_t>>;
 
-TYPED_TEST_CASE(DivideTest, MyTypes);
+TYPED_TEST_CASE(MultiplyTest, MyTypes);
 
-TYPED_TEST(DivideTest, forward_test)
-{
-  using ArrayType = TypeParam;
-  using DataType  = typename TypeParam::Type;
-
-  ArrayType data_1 = ArrayType::FromString(
-      "1, -2, 3,-4, 5,-6, 7,-8;"
-      "1,  2, 3, 4, 5, 6, 7, 8");
-
-  ArrayType data_2 = ArrayType::FromString(
-      " 8, -7, 6,-5, 4,-3, 2,-1;"
-      "-8,  7,-6, 5,-4, 3,-2, 1");
-
-  ArrayType gt = ArrayType::FromString(
-      "0.125,	0.285714285714286,	0.5,	0.8,	1.25,	2,	3.5,	8;"
-      "-0.125, 0.285714285714286,	-0.5,	0.8,	-1.25,	2,	-3.5,	8");
-
-  fetch::ml::ops::Divide<ArrayType> op;
-
-  TypeParam prediction(op.ComputeOutputShape({data_1, data_2}));
-  op.Forward({data_1, data_2}, prediction);
-
-  // test correct values
-  ASSERT_TRUE(prediction.AllClose(gt, fetch::math::function_tolerance<DataType>(),
-                                  fetch::math::function_tolerance<DataType>()));
-}
-
-TYPED_TEST(DivideTest, backward_test)
+TYPED_TEST(MultiplyTest, forward_test)
 {
   using ArrayType = TypeParam;
   using DataType  = typename TypeParam::Type;
@@ -76,22 +45,49 @@ TYPED_TEST(DivideTest, backward_test)
 
   ArrayType data_2 = ArrayType::FromString(
       "8, -7, 6,-5, 4,-3, 2,-1;"
-      "8,  7,-6, 5,-4, 3,-2, 1");
+      "-8,  7,-6, 5,-4, 3,-2, 1");
+
+  ArrayType gt = ArrayType::FromString(
+      "8, 14, 18,20, 20,18, 14,8;"
+      "-8,  14,-18, 20,-20, 18,-14, 8");
+
+  fetch::ml::ops::Multiply<ArrayType> op;
+
+  TypeParam prediction(op.ComputeOutputShape({data_1, data_2}));
+  op.Forward({data_1, data_2}, prediction);
+
+  // test correct values
+  ASSERT_TRUE(prediction.AllClose(gt, fetch::math::function_tolerance<DataType>(),
+                                  fetch::math::function_tolerance<DataType>()));
+}
+
+TYPED_TEST(MultiplyTest, backward_test)
+{
+  using ArrayType = TypeParam;
+  using DataType  = typename TypeParam::Type;
+
+  ArrayType data_1 = ArrayType::FromString(
+      "1, -2, 3,-4, 5,-6, 7,-8;"
+      "1,  2, 3, 4, 5, 6, 7, 8");
+
+  ArrayType data_2 = ArrayType::FromString(
+      "8, -7, 6,-5, 4,-3, 2,-1;"
+      "-8,  7,-6, 5,-4, 3,-2, 1");
 
   ArrayType gt_1 = ArrayType::FromString(
-      "0.125, 0.142857142857143, 0.333333333333333, 0.4, 0.75, 1, 2, 4;"
-      "0.625, -0.714285714285714, -1, -1.2, -1.75, -2.33333333333333, -4, -8");
+      "8,	   7,  12,  10,  12,   9,   8,  4;"
+      "-40, -35, -36, -30,	-28, -21, -16, -8");
 
   ArrayType gt_2 = ArrayType::FromString(
-      "0.015625, 0.040816326530612, 0.166666666666667, 0.32, 0.9375, 2, 7, 32;"
-      "0.078125, -0.204081632653061, 0.5, -0.96, 2.1875, -4.66666666666667, 14, -64");
+      "1,   2,	 6,	  8, 15,  18, 28,  32;"
+      "5, -10, 18,	-24, 35, -42, 56, -64");
 
   ArrayType error = ArrayType::FromString(
       "1, -1, 2, -2, 3, -3, 4, -4;"
       "5, -5, 6, -6, 7, -7, 8, -8");
 
-  fetch::ml::ops::Divide<ArrayType> op;
-  std::vector<ArrayType>            prediction = op.Backward({data_1, data_2}, error);
+  fetch::ml::ops::Multiply<ArrayType> op;
+  std::vector<ArrayType>              prediction = op.Backward({data_1, data_2}, error);
 
   // test correct values
   ASSERT_TRUE(prediction[0].AllClose(gt_1, fetch::math::function_tolerance<DataType>(),
@@ -99,4 +95,3 @@ TYPED_TEST(DivideTest, backward_test)
   ASSERT_TRUE(prediction[1].AllClose(gt_2, fetch::math::function_tolerance<DataType>(),
                                      fetch::math::function_tolerance<DataType>()));
 }
-}  // namespace
