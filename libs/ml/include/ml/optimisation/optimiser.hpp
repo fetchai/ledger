e93e644b252f741853d8847375dbdcd38888466a
Xiaodong Wu <xiaodong.wu@fetch.ai>
2019-07-10 17:27:54 +0100

    copyright

--- libs/ml/include/ml/optimisation/optimiser.hpp
+++ libs/ml/include/ml/optimisation/optimiser.hpp
@@ -16,6 +16,7 @@
 //   limitations under the License.
 //
 //------------------------------------------------------------------------------
+
 #include "math/base_types.hpp"
 #include "math/statistics/mean.hpp"
 #include "ml/dataloaders/dataloader.hpp"
