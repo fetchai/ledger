d6f65b049471067f44e5fcafebb1cdeae2585761
Xiaodong Wu <xiaodong.wu@fetch.ai>
2019-07-10 17:24:12 +0100

    copyright thing

--- libs/ml/include/ml/optimisation/optimiser.hpp
+++ libs/ml/include/ml/optimisation/optimiser.hpp
@@ -16,24 +16,6 @@
 //   limitations under the License.
 //
 //------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-//
-//   Copyright 2018-2019 Fetch.AI Limited
-//
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
-//
-//------------------------------------------------------------------------------
 #include "math/base_types.hpp"
 #include "math/statistics/mean.hpp"
 #include "ml/dataloaders/dataloader.hpp"
@@ -408,10 +390,6 @@ void Optimiser<T>::PrintStats(SizeType batch_size, SizeType subset_size)
   // print it in log
   FETCH_LOG_INFO("ML_LIB", "Training speed", stat_string_);
   FETCH_LOG_INFO("ML_LIB", "Batch loss: ", loss_sum_ / static_cast<DataType>(step_ / batch_size));
-  //      // print it in cout
-  //  std::cout << "\r" << stat_string_
-  //            << " batch loss: " << loss_sum_ / static_cast<DataType>(step_ / batch_size)
-  //            << std::flush;
 }
 /**
  *
