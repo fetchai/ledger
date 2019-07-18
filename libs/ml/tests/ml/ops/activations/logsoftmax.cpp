3ddd3b3de4ac2bcdcd2df164728ef2981a787516
Xiaodong Wu <xiaodong.wu@fetch.ai>
2019-07-18 17:35:42 +0100

    fix softmax tests

--- libs/ml/tests/ml/ops/activations/logsoftmax.cpp
+++ libs/ml/tests/ml/ops/activations/logsoftmax.cpp
@@ -84,12 +84,12 @@ TYPED_TEST(LogSoftmaxTest, backward_test)
   using DataType  = typename TypeParam::Type;
   using ArrayType = TypeParam;
 
-  ArrayType data  = ArrayType::FromString(R"(1, -2, 3, -4, 5, -6, 7, -8)");
-  ArrayType error = ArrayType::FromString(R"(0, 0, 0, 1, 1, 1, 0, 0)");
+  ArrayType data  = ArrayType::FromString("1; -2; 3; -4; 5; -6; 7; -8");
+  ArrayType error = ArrayType::FromString("0; 0; 0; 1; 1; 1; 0; 0");
   ArrayType gt    = ArrayType::FromString(
-      R"(-6.4312e-03, -3.2019e-04, -4.7521e-02,  9.9996e-01,  6.4887e-01, 9.9999e-01, -2.59454, -7.9368e-07)");
+      "-6.4312e-03; -3.2019e-04; -4.7521e-02;  9.9996e-01;  6.4887e-01; 9.9999e-01; -2.59454; -7.9368e-07");
 
-  fetch::ml::ops::LogSoftmax<ArrayType> op;
+  fetch::ml::ops::LogSoftmax<ArrayType> op{1};
   std::vector<ArrayType>                prediction = op.Backward({data}, error);
 
   // test correct values
