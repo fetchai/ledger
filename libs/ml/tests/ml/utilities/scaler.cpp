b8a5e5e8e9560b7373cc56179eb808a22fc194bd
Khan Baykaner <khan.baykaner@fetch.ai>
2019-07-19 16:07:29 +0100

    added min_max_scaler test

---
+++ libs/ml/tests/ml/utilities/scaler.cpp
@@ -0,0 +1,54 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "math/tensor.hpp"
+#include "ml/utilities/scaler.hpp"
+#include "vectorise/fixed_point/fixed_point.hpp"
+
+#include "gtest/gtest.h"
+
+template <typename T>
+class ScalerTest : public ::testing::Test
+{
+};
+
+using MyTypes = ::testing::Types<fetch::math::Tensor<int>, fetch::math::Tensor<float>,
+                                 fetch::math::Tensor<double>,
+                                 fetch::math::Tensor<fetch::fixed_point::FixedPoint<16, 16>>,
+                                 fetch::math::Tensor<fetch::fixed_point::FixedPoint<32, 32>>>;
+TYPED_TEST_CASE(ScalerTest, MyTypes);
+
+TYPED_TEST(ScalerTest, min_max_test)
+{
+  TypeParam data(std::vector<fetch::math::SizeType>({8, 6, 4}));
+  data.FillUniformRandom();
+
+  TypeParam norm_data(std::vector<fetch::math::SizeType>({8, 6, 4}));
+  TypeParam de_norm_data(std::vector<fetch::math::SizeType>({8, 6, 4}));
+
+  fetch::ml::utilities::MinMaxScaler<TypeParam> scaler;
+  scaler.setScale(data);
+
+  scaler.Normalise(data, norm_data);
+  scaler.deNormalise(norm_data, de_norm_data);
+
+  EXPECT_EQ(data.shape(), norm_data.shape());
+  EXPECT_EQ(de_norm_data.shape(), norm_data.shape());
+
+  EXPECT_TRUE(data.AllCLose(de_norm_data));
+}
