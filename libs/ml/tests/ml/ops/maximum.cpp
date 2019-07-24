83d159daada2faa90be78fd28da7d0c283bd0b5a
Khan Baykaner <khan.baykaner@fetch.ai>
2019-07-24 13:03:59 +0100

    maximum test

--- libs/ml/tests/ml/ops/maximum.cpp
+++ libs/ml/tests/ml/ops/maximum.cpp
@@ -95,3 +95,58 @@ TYPED_TEST(MaximumTest, backward_test)
   ASSERT_TRUE(prediction[1].AllClose(gt_2, fetch::math::function_tolerance<DataType>(),
                                      fetch::math::function_tolerance<DataType>()));
 }
+
+TYPED_TEST(MaximumTest, saveparams_test)
+{
+  using ArrayType     = TypeParam;
+  using DataType      = typename TypeParam::Type;
+  using VecTensorType = typename fetch::ml::Ops<ArrayType>::VecTensorType;
+  using SPType        = typename fetch::ml::ops::Maximum<ArrayType>::SPType;
+  using OpType        = typename fetch::ml::ops::Maximum<ArrayType>;
+
+
+  ArrayType data_1 = ArrayType::FromString(
+      "1, -2, 3,-4, 5,-6, 7,-8;"
+      "1,  2, 3, 4, 5, 6, 7, 8");
+
+  ArrayType data_2 = ArrayType::FromString(
+      "8, -7, 6,-5, 4,-3, 2,-1;"
+      "-8,  7,-6, 5,-4, 3,-2, 1");
+
+  ArrayType gt = ArrayType::FromString(
+      "8,	-2, 6, -4, 5, -3, 7, -1;"
+      "1,  7, 3,  5, 5,  6, 7,  8");
+
+  OpType op;
+
+  ArrayType     prediction(op.ComputeOutputShape({data_1, data_2}));
+  VecTensorType vec_data({data_1, data_2});
+
+  op.Forward(vec_data, prediction);
+
+  // extract saveparams
+  std::shared_ptr<fetch::ml::SaveableParams> sp = op.GetOpSaveableParams();
+
+  // downcast to correct type
+  auto dsp = std::dynamic_pointer_cast<SPType>(sp);
+
+  // serialize
+  fetch::serializers::ByteArrayBuffer b;
+  b << *dsp;
+
+  // deserialize
+  b.seek(0);
+  auto dsp2 = std::make_shared<SPType>();
+  b >> *dsp2;
+
+  // rebuild node
+  OpType new_op(*dsp2);
+
+  // check that new predictions match the old
+  ArrayType new_prediction(op.ComputeOutputShape({data_1, data_2}));
+  new_op.Forward(vec_data, new_prediction);
+
+  // test correct values
+  EXPECT_TRUE(new_prediction.AllClose(prediction, fetch::math::function_tolerance<DataType>(),
+                                      fetch::math::function_tolerance<DataType>()));
+}
