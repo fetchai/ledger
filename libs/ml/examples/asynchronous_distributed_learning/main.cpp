43c036a480d9287463787d0b778a9b73306297bf
Khan Baykaner <khan.baykaner@fetch.ai>
2019-07-25 16:59:09 +0100

    replacing existing standard library random number generation and shuffling with lfg

--- libs/ml/examples/asynchronous_distributed_learning/main.cpp
+++ libs/ml/examples/asynchronous_distributed_learning/main.cpp
@@ -16,6 +16,7 @@
 //
 //------------------------------------------------------------------------------
 
+#include "core/random.hpp"
 #include "math/statistics/mean.hpp"
 #include "math/tensor.hpp"
 #include "ml/dataloaders/mnist_loaders/mnist_loader.hpp"
@@ -57,6 +58,7 @@ using namespace fetch::ml::layers;
 
 using DataType  = float;
 using ArrayType = fetch::math::Tensor<DataType>;
+using SizeType = fetch::math::SizeType;
 
 class TrainingClient
 {
@@ -122,9 +124,7 @@ public:
         peers_.push_back(p);
       }
     }
-    std::random_device rd;
-    std::mt19937       g(rd());
-    std::shuffle(peers_.begin(), peers_.end(), g);
+    fetch::random::Shuffle(gen_, peers_, peers_);
   }
 
   void UpdateWeights()
@@ -142,9 +142,8 @@ public:
       g_.LoadStateDict(g_.StateDict().Merge(averageStateDict, MERGE_RATIO));
     }
     // Shuffle the peers list to get new contact for next update
-    std::random_device rd;
-    std::mt19937       g(rd());
-    std::shuffle(peers_.begin(), peers_.end(), g);
+    fetch::random::Shuffle(gen_, peers_, peers_);
+
   }
 
   std::vector<float> const &GetLossesValues() const
@@ -167,6 +166,9 @@ private:
 
   // Mutex to protect weight access
   std::mutex m_;
+
+  // random number generator for shuffling peers
+  fetch::random::LaggedFibonacciGenerator<> gen_;
 };
 
 int main(int ac, char **av)
