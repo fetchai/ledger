b55c18617ff026cda5b3ae63c3b2d11c6df7afbc
Wojciech Kaluza <w-kaluza@tlen.pl>
2019-07-27 05:07:46 +0100

    start ml

---
+++ libs/vm_modules/src/ml/graph.cpp
@@ -0,0 +1,163 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "ml/graph.hpp"
+#include "ml/layers/convolution_1d.hpp"
+#include "ml/layers/fully_connected.hpp"
+#include "ml/ops/activation.hpp"
+#include "ml/ops/loss_functions/cross_entropy_loss.hpp"
+#include "ml/ops/loss_functions/mean_square_error_loss.hpp"
+#include "vm/module.hpp"
+#include "vm_modules/math/tensor.hpp"
+#include "vm_modules/ml/graph.hpp"
+#include "vm_modules/ml/state_dict.hpp"
+
+namespace fetch {
+namespace vm_modules {
+namespace ml {
+
+class VMGraph : public fetch::vm::Object
+{
+  using SizeType       = fetch::math::SizeType;
+  using DataType       = fetch::vm_modules::math::DataType;
+  using MathTensorType = fetch::math::Tensor<DataType>;
+  using VMTensorType   = fetch::vm_modules::math::VMTensor;
+  using GraphType      = fetch::ml::Graph<MathTensorType>;
+  using VMPtrString    = fetch::vm::Ptr<fetch::vm::String>;
+
+public:
+  VMGraph(fetch::vm::VM *vm, fetch::vm::TypeId type_id)
+    : fetch::vm::Object(vm, type_id)
+    , graph_()
+  {}
+
+  static fetch::vm::Ptr<VMGraph> Constructor(fetch::vm::VM *vm, fetch::vm::TypeId type_id)
+  {
+    return new VMGraph(vm, type_id);
+  }
+
+  void SetInput(VMPtrString const &name, fetch::vm::Ptr<VMTensorType> const &input)
+  {
+    graph_.SetInput(name->str, (*input).GetTensor());
+  }
+
+  fetch::vm::Ptr<VMTensorType> Evaluate(VMPtrString const &name)
+  {
+    MathTensorType               t   = graph_.Evaluate(name->str);
+    fetch::vm::Ptr<VMTensorType> ret = this->vm_->CreateNewObject<math::VMTensor>(t.shape());
+    (*ret).Copy(t);
+    return ret;
+  }
+
+  void BackPropagateError(VMPtrString const &name)
+  {
+    graph_.BackPropagateError(name->str);
+  }
+
+  void Step(DataType lr)
+  {
+    graph_.Step(lr);
+  }
+
+  void AddPlaceholder(VMPtrString const &name)
+  {
+    graph_.AddNode<fetch::ml::ops::PlaceHolder<MathTensorType>>(name->str, {});
+  }
+
+  void AddFullyConnected(VMPtrString const &name, VMPtrString const &input_name, int in, int out)
+  {
+    graph_.AddNode<fetch::ml::layers::FullyConnected<MathTensorType>>(
+        name->str, {input_name->str}, std::size_t(in), std::size_t(out));
+  }
+
+  void AddConv1D(VMPtrString const &name, VMPtrString const &input_name, int filters,
+                 int in_channels, int kernel_size, int stride_size)
+  {
+    graph_.AddNode<fetch::ml::layers::Convolution1D<MathTensorType>>(
+        name->str, {input_name->str}, static_cast<SizeType>(filters),
+        static_cast<SizeType>(in_channels), static_cast<SizeType>(kernel_size),
+        static_cast<SizeType>(stride_size));
+  }
+
+  void AddRelu(VMPtrString const &name, VMPtrString const &input_name)
+  {
+    graph_.AddNode<fetch::ml::ops::Relu<MathTensorType>>(name->str, {input_name->str});
+  }
+
+  void AddSoftmax(VMPtrString const &name, VMPtrString const &input_name)
+  {
+    graph_.AddNode<fetch::ml::ops::Softmax<fetch::math::Tensor<DataType>>>(name->str,
+                                                                           {input_name->str});
+  }
+
+  void AddCrossEntropyLoss(VMPtrString const &name, VMPtrString const &input_name,
+                           VMPtrString const &label_name)
+  {
+    graph_.AddNode<fetch::ml::ops::CrossEntropyLoss<fetch::math::Tensor<DataType>>>(
+        name->str, {input_name->str, label_name->str});
+  }
+
+  void AddMeanSquareErrorLoss(VMPtrString const &name, VMPtrString const &input_name,
+                              VMPtrString const &label_name)
+  {
+    graph_.AddNode<fetch::ml::ops::MeanSquareErrorLoss<fetch::math::Tensor<DataType>>>(
+        name->str, {input_name->str, label_name->str});
+  }
+
+  void AddDropout(VMPtrString const &name, VMPtrString const &input_name, DataType const &prob)
+  {
+    graph_.AddNode<fetch::ml::ops::Dropout<MathTensorType>>(name->str, {input_name->str}, prob);
+  }
+
+  void LoadStateDict(fetch::vm::Ptr<VMStateDict> const &sd)
+  {
+    graph_.LoadStateDict(sd->state_dict_);
+  }
+
+  fetch::vm::Ptr<VMStateDict> StateDict()
+  {
+    fetch::vm::Ptr<VMStateDict> ret = this->vm_->CreateNewObject<VMStateDict>(graph_.StateDict());
+    return ret;
+  }
+
+  static void Bind(fetch::vm::Module &module)
+  {
+    module.CreateClassType<VMGraph>("Graph")
+        .CreateConstructor<>()
+        .CreateMemberFunction("setInput", &VMGraph::SetInput)
+        .CreateMemberFunction("evaluate", &VMGraph::Evaluate)
+        .CreateMemberFunction("backPropagate", &VMGraph::BackPropagateError)
+        .CreateMemberFunction("step", &VMGraph::Step)
+        .CreateMemberFunction("addPlaceholder", &VMGraph::AddPlaceholder)
+        .CreateMemberFunction("addFullyConnected", &VMGraph::AddFullyConnected)
+        .CreateMemberFunction("addConv1D", &VMGraph::AddConv1D)
+        .CreateMemberFunction("addRelu", &VMGraph::AddRelu)
+        .CreateMemberFunction("addSoftmax", &VMGraph::AddSoftmax)
+        .CreateMemberFunction("addDropout", &VMGraph::AddDropout)
+        .CreateMemberFunction("addCrossEntropyLoss", &VMGraph::AddCrossEntropyLoss)
+        .CreateMemberFunction("addMeanSquareErrorLoss", &VMGraph::AddMeanSquareErrorLoss)
+        .CreateMemberFunction("loadStateDict", &VMGraph::LoadStateDict)
+        .CreateMemberFunction("stateDict", &VMGraph::StateDict);
+  }
+
+  GraphType graph_;
+};
+
+}  // namespace ml
+}  // namespace vm_modules
+}  // namespace fetch
---
+++ libs/vm_modules/src/ml/optimisation/optimiser.cpp
@@ -0,0 +1,145 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "ml/graph.hpp"
+#include "ml/optimisation/adagrad_optimiser.hpp"
+#include "ml/optimisation/adam_optimiser.hpp"
+#include "ml/optimisation/momentum_optimiser.hpp"
+#include "ml/optimisation/optimiser.hpp"
+#include "ml/optimisation/rmsprop_optimiser.hpp"
+#include "ml/optimisation/sgd_optimiser.hpp"
+#include "vm_modules/math/tensor.hpp"
+#include "vm_modules/math/type.hpp"
+#include "vm_modules/ml/dataloaders/dataloader.hpp"
+#include "vm_modules/ml/graph.hpp"
+#include "vm_modules/ml/optimisation/optimiser.hpp"
+#include "vm_modules/ml/training_pair.hpp"
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace fetch {
+namespace vm_modules {
+namespace ml {
+
+class VMOptimiser : public fetch::vm::Object
+{
+public:
+  using DataType  = fetch::vm_modules::math::DataType;
+  using ArrayType = fetch::math::Tensor<DataType>;
+  using GraphType = fetch::ml::Graph<ArrayType>;
+
+  using OptimiserType         = fetch::ml::optimisers::Optimiser<ArrayType>;
+  using AdagradOptimiserType  = fetch::ml::optimisers::AdaGradOptimiser<ArrayType>;
+  using AdamOptimiserType     = fetch::ml::optimisers::AdamOptimiser<ArrayType>;
+  using MomentumOptimiserType = fetch::ml::optimisers::MomentumOptimiser<ArrayType>;
+  using RmspropOptimiserType  = fetch::ml::optimisers::RMSPropOptimiser<ArrayType>;
+  using SgdOptimiserType      = fetch::ml::optimisers::SGDOptimiser<ArrayType>;
+
+  VMOptimiser(fetch::vm::VM *vm, fetch::vm::TypeId type_id, std::string const &mode,
+              GraphType const &graph, std::vector<std::string> const &input_node_names,
+              std::string const &label_node_name, std::string const &output_node_name)
+    : fetch::vm::Object(vm, type_id)
+  {
+    if (mode == "adagrad")
+    {
+      AdagradOptimiserType optimiser(std::make_shared<GraphType>(graph), input_node_names,
+                                     label_node_name, output_node_name);
+      optimiser_ = std::make_shared<AdagradOptimiserType>(optimiser);
+    }
+    else if (mode == "adam")
+    {
+      AdamOptimiserType optimiser(std::make_shared<GraphType>(graph), input_node_names,
+                                  label_node_name, output_node_name);
+      optimiser_ = std::make_shared<AdamOptimiserType>(optimiser);
+    }
+    else if (mode == "momentum")
+    {
+      MomentumOptimiserType optimiser(std::make_shared<GraphType>(graph), input_node_names,
+                                      label_node_name, output_node_name);
+      optimiser_ = std::make_shared<MomentumOptimiserType>(optimiser);
+    }
+    else if (mode == "rmsprop")
+    {
+      RmspropOptimiserType optimiser(std::make_shared<GraphType>(graph), input_node_names,
+                                     label_node_name, output_node_name);
+      optimiser_ = std::make_shared<RmspropOptimiserType>(optimiser);
+    }
+    else if (mode == "sgd")
+    {
+      SgdOptimiserType optimiser(std::make_shared<GraphType>(graph), input_node_names,
+                                 label_node_name, output_node_name);
+      optimiser_ = std::make_shared<SgdOptimiserType>(optimiser);
+    }
+    else
+    {
+      throw std::runtime_error("unrecognised optimiser mode");
+    }
+  }
+
+  static void Bind(vm::Module &module)
+  {
+    module.CreateClassType<fetch::vm_modules::ml::VMOptimiser>("Optimiser")
+        .CreateConstructor<fetch::vm::Ptr<fetch::vm::String>,
+                           fetch::vm::Ptr<fetch::vm_modules::ml::VMGraph>,
+                           fetch::vm::Ptr<fetch::vm::String>, fetch::vm::Ptr<fetch::vm::String>,
+                           fetch::vm::Ptr<fetch::vm::String>>()
+        .CreateMemberFunction("run", &fetch::vm_modules::ml::VMOptimiser::RunData)
+        .CreateMemberFunction("run", &fetch::vm_modules::ml::VMOptimiser::RunLoader)
+        .CreateMemberFunction("run", &fetch::vm_modules::ml::VMOptimiser::RunLoaderNoSubset);
+  }
+
+  static fetch::vm::Ptr<VMOptimiser> Constructor(
+      fetch::vm::VM *vm, fetch::vm::TypeId type_id, fetch::vm::Ptr<fetch::vm::String> const &mode,
+      fetch::vm::Ptr<fetch::vm_modules::ml::VMGraph> const &graph,
+      fetch::vm::Ptr<fetch::vm::String> const &             input_node_names,
+      fetch::vm::Ptr<fetch::vm::String> const &             label_node_name,
+      fetch::vm::Ptr<fetch::vm::String> const &             output_node_names)
+  {
+    return new VMOptimiser(vm, type_id, mode->str, graph->graph_, {input_node_names->str},
+                           label_node_name->str, output_node_names->str);
+  }
+
+  DataType RunData(fetch::vm::Ptr<fetch::vm_modules::math::VMTensor> const &data,
+                   fetch::vm::Ptr<fetch::vm_modules::math::VMTensor> const &labels,
+                   uint64_t                                                 batch_size)
+  {
+    return optimiser_->Run({(data->GetTensor())}, labels->GetTensor(), batch_size);
+  }
+
+  DataType RunLoader(fetch::vm::Ptr<fetch::vm_modules::ml::VMDataLoader> const &loader,
+                     uint64_t batch_size, uint64_t subset_size)
+  {
+    return optimiser_->Run(*(loader->loader_), batch_size, subset_size);
+  }
+
+  DataType RunLoaderNoSubset(fetch::vm::Ptr<fetch::vm_modules::ml::VMDataLoader> const &loader,
+                             uint64_t                                                   batch_size)
+  {
+    return optimiser_->Run(*(loader->loader_), batch_size);
+  }
+
+private:
+  std::shared_ptr<OptimiserType> optimiser_;
+};
+
+}  // namespace ml
+}  // namespace vm_modules
+}  // namespace fetch
