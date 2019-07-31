f763a60ac63fa1f259283a7040f9edcfacc80f6f
Jiri Vestfal <jiri.vestfal@fetch.ai>
2019-07-31 14:32:48 +0100

    Applied style

--- libs/math/tests/math/all_tests/matrix_operations/matrix_operations.cpp
+++ libs/math/tests/math/all_tests/matrix_operations/matrix_operations.cpp
@@ -987,6 +987,41 @@ TYPED_TEST(FreeFunctionsTest, ReduceMax3D_axis_2)
               static_cast<double>(function_tolerance<DataType>()));
 }
 
+TYPED_TEST(FreeFunctionsTest, ReduceMax3D_axes_0_2)
+{
+  using SizeType = typename TypeParam::SizeType;
+  using DataType = typename TypeParam::Type;
+
+  SizeType n_height{4};
+  SizeType n_width{4};
+  SizeType n_features{2};
+
+  TypeParam array1{{n_height, n_width, n_features}};
+
+  // Fill input
+  auto     it    = array1.begin();
+  SizeType count = 1;
+  while (it.is_valid())
+  {
+    *it = static_cast<DataType>(count);
+    count++;
+    ++it;
+  }
+
+  TypeParam output{{1, n_width, 1}};
+  fetch::math::ReduceMax(array1, {0, 2}, output);
+
+  // Test values
+  EXPECT_NEAR(static_cast<double>(output(0, 0, 0)), 20.,
+              static_cast<double>(function_tolerance<DataType>()));
+  EXPECT_NEAR(static_cast<double>(output(0, 1, 0)), 24.,
+              static_cast<double>(function_tolerance<DataType>()));
+  EXPECT_NEAR(static_cast<double>(output(0, 2, 0)), 28.,
+              static_cast<double>(function_tolerance<DataType>()));
+  EXPECT_NEAR(static_cast<double>(output(0, 3, 0)), 32.,
+              static_cast<double>(function_tolerance<DataType>()));
+}
+
 TYPED_TEST(FreeFunctionsTest, Dot)
 {
   using SizeType = typename TypeParam::SizeType;
