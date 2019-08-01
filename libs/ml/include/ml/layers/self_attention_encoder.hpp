e8839667b286020aa7d027790c07bcd8643fe166
Xiaodong Wu <xiaodong.wu@fetch.ai>
2019-08-01 17:42:55 +0100

    halfway throught the encoder part

--- libs/ml/include/ml/layers/self_attention_encoder.hpp
+++ libs/ml/include/ml/layers/self_attention_encoder.hpp
@@ -20,7 +20,7 @@
 #include "layers.hpp"
 #include "math/standard_functions/sqrt.hpp"
 #include "ml/layers/fully_connected.hpp"
-#include "ml/layers/scaled_dot_product_attention.hpp"
+#include "ml/layers/normalisation/layer_norm.hpp"
 #include "ml/ops/add.hpp"
 #include "ml/ops/concatenate.hpp"
 #include "ml/ops/divide.hpp"
@@ -28,6 +28,7 @@
 #include "ml/ops/matrix_multiply.hpp"
 #include "ml/ops/placeholder.hpp"
 #include "ml/ops/transpose.hpp"
+#include "multihead_attention.hpp"
 
 #include <cmath>
 #include <cstdint>
@@ -53,42 +54,60 @@ public:
   using RegType         = fetch::ml::details::RegularisationType;
   using WeightsInitType = fetch::ml::ops::WeightsInitialisation;
   using ActivationType  = fetch::ml::details::ActivationType;
+  
+  std::string positionwise_feedforward(std::string name, std::string const &input){
+  	// position wise feedforward with relu acitvation
+	  std::string ff_first_layer = this->template AddNode<fetch::ml::layers::FullyConnected<ArrayType>>(
+	   name + "_Feedforward_No_1", {input}, static_cast<SizeType>(model_dim_),
+	   static_cast<SizeType>(ff_dim_), ActivationType::RELU, RegType::NONE,
+	   static_cast<DataType>(0), WeightsInitType::XAVIER_GLOROT, true);
+	  
+	  // do dropout
+	  std::string ff_first_layer_dropout = this->template AddNode<fetch::ml::ops::Dropout>(name + "_Dropout", {ff_first_layer}, feedforward_dropout_);
+	  
+	  // position wise feedforward stage 2
+	  std::string ff_second_layer = this->template AddNode<fetch::ml::layers::FullyConnected<ArrayType>>(
+	   name + "_Feedforward_No_2", {ff_first_layer_dropout}, static_cast<SizeType>(ff_dim_),
+	   static_cast<SizeType>(model_dim_), ActivationType::NOTHING, RegType::NONE,
+	   static_cast<DataType>(0), WeightsInitType::XAVIER_GLOROT, true);
+	  
+	  return ff_second_layer;
+  }
+  
+  std::string residual_connection(std::string name, std::string const &prev_layer_input, std::string const &prev_layer_output){
+		
+  	std::string dropout_output = this->template AddNode<fetch::ml::ops::Dropout>(name + "_Dropout", {prev_layer_output}, residual_dropout_);
+	  std::string residual_addition = this->template AddNode<fetch::ml::ops::Add>(name + "_Residual_Addition", {prev_layer_input, dropout_output});
+	  
+  	std::string normalized_residual = this->template AddNode<fetch::ml::layers::LayerNorm<ArrayType>>(name + "_LayerNorm", {residual_addition});
+  	
+  }
 
-  SelfAttentionEncoder(SizeType n_heads, SizeType model_dim, DataType dropout = 0.9)
+  SelfAttentionEncoder(SizeType n_heads, SizeType model_dim, SizeType ff_dim, DataType residual_dropout = 0.9, DataType attention_dropout = 0.9, DataType feedforward_dropout = 0.9)
     : n_heads_(n_heads)
     , model_dim_(model_dim)
-    , dropout_(dropout)
+    , ff_dim_(ff_dim)
+    , residual_dropout_(residual_dropout)
+    , attention_dropout_(attention_dropout)
+    , feedforward_dropout_(feedforward_dropout)
   {
     // make sure all heads can be concatenate together to form model_dim
     assert(model_dim_ % n_heads_ == 0);
-    key_dim_ = model_dim_ / n_heads_;
-
-    // assuming key_dim is the same as value_dim
-    value_dim_ = key_dim_;
 
     std::string name = DESCRIPTOR;
 
     // all input shapes are (feature_length, model_dim, batch_num)
-    std::string query =
-        this->template AddNode<fetch::ml::ops::PlaceHolder<ArrayType>>(name + "_Query", {});
-    std::string key =
-        this->template AddNode<fetch::ml::ops::PlaceHolder<ArrayType>>(name + "_Key", {});
-    std::string value =
-        this->template AddNode<fetch::ml::ops::PlaceHolder<ArrayType>>(name + "_Value", {});
+    std::string input =
+        this->template AddNode<fetch::ml::ops::PlaceHolder<ArrayType>>(name + "_Input", {});
 
-    // do n_heads time linear transformation
-    std::vector<std::string> heads;
-    for (SizeType i = 0; i < n_heads_; i++)
-    {
-      std::string head_name             = name + "_Head_No_" + std::to_string(static_cast<int>(i));
-      std::string head_attention_output = create_one_attention_head(head_name, query, key, value);
-      heads.emplace_back(head_attention_output);
-    }
-
-    // concatenate all attention head outputs
-    std::string concatenated_attention_heads =
-        this->template AddNode<fetch::ml::ops::Concatenate<ArrayType>>(
-            name + "_Concatenated_Heads", heads, static_cast<SizeType>(0));
+    // multihead attention on input time series vector
+    std::string multihead_self_attention =
+        this->template AddNode<fetch::ml::layers::MultiheadAttention<ArrayType>>(
+            name + "_Multihead_Attention", {input, input, input}, n_heads, model_dim_, attention_dropout_);
+    
+    // make residual connection
+    
+    
     // do the final transformation
     std::string transformed_multihead =
         this->template AddNode<fetch::ml::layers::FullyConnected<ArrayType>>(
@@ -138,11 +157,12 @@ public:
   static constexpr char const *DESCRIPTOR = "SelfAttentionEncoder";
 
 private:
-  SizeType key_dim_;
-  SizeType value_dim_;
   SizeType n_heads_;
   SizeType model_dim_;
-  DataType dropout_;
+  SizeType ff_dim_;
+  DataType residual_dropout_;
+  DataType attention_dropout_;
+  DataType feedforward_dropout_;
 };
 
 }  // namespace layers
