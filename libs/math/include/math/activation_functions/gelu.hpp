9e677db17bcbc1fbd252b57b91d7607be41b6c16
Xiaodong Wu <xiaodong.wu@fetch.ai>
2019-08-06 16:53:37 +0100

    finish math backend for gelu

---
+++ libs/math/include/math/activation_functions/gelu.hpp
@@ -0,0 +1,66 @@
+#pragma once
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "math/comparison.hpp"
+#include "math/fundamental_operators.hpp"
+#include "math/standard_functions/exp.hpp"
+#include "math/standard_functions/pow.hpp"
+#include "math/trigonometry.hpp"
+
+#include <cassert>
+
+namespace fetch {
+namespace math {
+
+/**
+ * Guassian error linear unit (approximated)
+ * 0.5x(1+tanh(0.797885x+0.035677x^3))
+ * @tparam ArrayType
+ * @param t
+ * @param ret
+ */
+template <typename ArrayType>
+void Gelu(ArrayType const &t,  ArrayType &ret)
+{
+  assert(t.size() == ret.size());
+  using DataType = typename ArrayType::Type;
+
+  DataType one{1}, half{static_cast<DataType>(0.5)}, three{3}, coeff1{static_cast<DataType>(0.797885)}, coeff2{static_cast<DataType>(0.035677)};
+  ArrayType intermediate = t.Copy();
+  
+  Multiply(t, coeff1, intermediate);
+  Pow(t, three, ret);
+  Multiply(ret, coeff2, ret);
+  Add(intermediate, ret, ret);
+  TanH(ret, ret);
+  Add(ret, one, ret);
+  Multiply(t, ret, ret);
+  Multiply(ret, half, ret);
+}
+
+template <typename ArrayType>
+ArrayType Gelu(ArrayType const &t)
+{
+  ArrayType ret(t.shape());
+  Gelu(t, ret);
+  return ret;
+}
+
+}  // namespace math
+}  // namespace fetch
---
+++ libs/ml/include/ml/ops/activations/gelu.hpp
@@ -0,0 +1,96 @@
+#pragma once
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "math/activation_functions/elu.hpp"
+#include "math/fundamental_operators.hpp"
+#include "math/matrix_operations.hpp"
+#include "ml/ops/ops.hpp"
+
+#include <cassert>
+#include <vector>
+
+namespace fetch {
+namespace ml {
+namespace ops {
+
+template <class T>
+class Gelu : public fetch::ml::Ops<T>
+{
+public:
+  using ArrayType     = T;
+  using DataType      = typename ArrayType::Type;
+  using SizeType      = typename ArrayType::SizeType;
+  using VecTensorType = typename Ops<T>::VecTensorType;
+
+  Gelu() = default;
+  ~Gelu() = default;
+  
+  void Forward(VecTensorType const &inputs, ArrayType &output) override
+  {
+    assert(inputs.size() == 1);
+    fetch::math::Elu((*inputs.front()), a_, output);
+  }
+
+  std::vector<ArrayType> Backward(VecTensorType const &inputs,
+                                  ArrayType const &    error_signal) override
+  {
+    assert(inputs.size() == 1);
+    assert(inputs.front()->shape() == error_signal.shape());
+    ArrayType ret{error_signal.shape()};
+
+    DataType zero{0};
+    DataType one{1};
+
+    // gradient of elu function is a*e^x where x<0; and 1.0 where x>=0
+    auto it  = inputs.front()->cbegin();
+    auto rit = ret.begin();
+    while (it.is_valid())
+    {
+      if (*it >= zero)
+      {
+        // f(x)=x for x>=0
+        *rit = one;
+      }
+      else
+      {
+        // f(x)=a*e^x
+        fetch::math::Exp(*it, *rit);
+        fetch::math::Multiply(a_, *rit, *rit);
+      }
+      ++it;
+      ++rit;
+    }
+
+    // multiply by error_signal (chain rule)
+    fetch::math::Multiply(error_signal, ret, ret);
+
+    return {ret};
+  }
+
+  std::vector<SizeType> ComputeOutputShape(VecTensorType const &inputs) const override
+  {
+    return inputs.front()->shape();
+  }
+
+  static constexpr char const *DESCRIPTOR = "Gelu";
+};
+
+}  // namespace ops
+}  // namespace ml
+}  // namespace fetch
