2c9c4cad290a2596390bdf7affb452ddbc8c8349
Khan Baykaner <khan.baykaner@fetch.ai>
2019-08-07 13:07:32 +0100

    bugfixed multihead attention signature

--- libs/ml/include/ml/layers/multihead_attention.hpp
+++ libs/ml/include/ml/layers/multihead_attention.hpp
@@ -46,7 +46,7 @@ public:
   using ActivationType  = fetch::ml::details::ActivationType;
   using SPType          = fetch::ml::LayerMultiHeadSaveableParams<T>;
 
-  MultiheadAttention(SizeType n_heads, SizeType model_dim, DataType dropout = 0.9)
+  MultiheadAttention(SizeType n_heads, SizeType model_dim, DataType dropout = static_cast<DataType>(0.9))
     : n_heads_(n_heads)
     , model_dim_(model_dim)
     , dropout_(dropout)
