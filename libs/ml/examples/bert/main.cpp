f25a09ccc7f71808476cf3c296c1d35f92a8ceac
Xiaodong Wu <xiaodong.wu@fetch.ai>
2019-08-08 14:01:00 +0100

    start ensembling bert

---
+++ libs/ml/examples/bert/main.cpp
@@ -0,0 +1,89 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "math/tensor.hpp"
+#include "ml/graph.hpp"
+#include "ml/ops/loss_functions/cross_entropy_loss.hpp"
+#include "ml/optimisation/adam_optimiser.hpp"
+
+#include <iostream>
+#include <string>
+
+using namespace fetch::ml::ops;
+using namespace fetch::ml::layers;
+
+using DataType  = float;
+using ArrayType = fetch::math::Tensor<DataType>;
+using SizeType  = typename ArrayType::SizeType;
+
+using GraphType      = typename fetch::ml::Graph<ArrayType>;
+using OptimiserType  = typename fetch::ml::optimisers::AdamOptimiser<ArrayType>;
+
+int main(int ac, char **av)
+{
+
+  if (ac < 3)
+  {
+    std::cout << "Usage : " << av[0]
+              << " PATH/TO/train-images-idx3-ubyte PATH/TO/train-labels-idx1-ubyte" << std::endl;
+    return 1;
+  }
+
+  std::cout << "FETCH BERT Demo" << std::endl;
+  
+  // Prepare input with segment embedding, position embedding and token embedding
+  
+  
+  // Ensemble the whole bert model
+  
+  
+  // Create a sudo data for input
+
+  // Prepare graph
+  //  Input -> FC -> Relu -> FC -> Relu -> FC -> Softmax
+  auto g = std::make_shared<GraphType>();
+
+  std::string input = g->AddNode<PlaceHolder<ArrayType>>("Input", {});
+  std::string label = g->AddNode<PlaceHolder<ArrayType>>("Label", {});
+
+  std::string layer_1 = g->AddNode<FullyConnected<ArrayType>>(
+      "FC1", {input}, 28u * 28u, 10u, fetch::ml::details::ActivationType::RELU, regulariser,
+      reg_rate);
+  std::string layer_2 = g->AddNode<FullyConnected<ArrayType>>(
+      "FC2", {layer_1}, 10u, 10u, fetch::ml::details::ActivationType::RELU, regulariser, reg_rate);
+  std::string output = g->AddNode<FullyConnected<ArrayType>>(
+      "FC3", {layer_2}, 10u, 10u, fetch::ml::details::ActivationType::SOFTMAX, regulariser,
+      reg_rate);
+  std::string error = g->AddNode<CrossEntropyLoss<ArrayType>>("Error", {output, label});
+
+  // Initialise MNIST loader
+  DataLoaderType data_loader(av[1], av[2]);
+
+  // Initialise Optimiser
+  OptimiserType optimiser(g, {input}, label, error, learning_rate);
+
+  // Training loop
+  DataType loss;
+  for (SizeType i{0}; i < epochs; i++)
+  {
+    loss = optimiser.Run(data_loader, batch_size, subset_size);
+    std::cout << "Loss: " << loss << std::endl;
+  }
+
+  return 0;
+}
