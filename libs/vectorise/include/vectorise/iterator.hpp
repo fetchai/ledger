44567e4b684539768e1c528917a3be2cd4a2948a
Konstantinos Margaritis <markos@freevec.org>
2019-08-13 22:03:55 +0300

    add copy constructor from a different Iterator

--- libs/vectorise/include/vectorise/iterator.hpp
+++ libs/vectorise/include/vectorise/iterator.hpp
@@ -20,6 +20,7 @@
 #include "vectorise/register.hpp"
 
 #include <cassert>
+#include <iostream>
 
 namespace fetch {
 namespace vectorise {
@@ -36,19 +37,44 @@ public:
     : ptr_(nullptr)
     , end_(nullptr)
   {}
+
+  template <typename Y, std::size_t M>
+  VectorRegisterIterator(VectorRegisterIterator<Y, M> &o, std::size_t const offset)
+    : ptr_(reinterpret_cast<mm_register_type *>(o.pointer()))
+    , end_(reinterpret_cast<mm_register_type *>(o.end()))
+  {
+    ptr_ += offset;
+    std::cout << "ptr_ = " << std::hex << ptrdiff_t(ptr_) << std::endl;
+    std::cout << "end_ = " << std::hex << ptrdiff_t(end_) << std::endl;
+  }
+
   VectorRegisterIterator(type const *d, std::size_t size)
     : ptr_((mm_register_type *)d)
     , end_((mm_register_type *)(d + size))
-  {}
+  {
+    std::cout << "ptr_ = " << std::hex << ptrdiff_t(ptr_) << std::endl;
+    std::cout << "end_ = " << std::hex << ptrdiff_t(end_) << std::endl;
+  }
 
   void Next(VectorRegisterType &m)
   {
     assert((end_ == nullptr) || (ptr_ < end_));
+    std::cout << "ptr_ = " << std::hex << ptrdiff_t(ptr_) << std::endl;
 
     m.data() = *ptr_;
     ++ptr_;
+    std::cout << "ptr_ = " << std::hex << ptrdiff_t(ptr_) << std::endl;
   }
 
+  mm_register_type *pointer() const
+  {
+    return ptr_;
+  }
+
+  mm_register_type *end() const
+  {
+    return end_;
+  }
 private:
   mm_register_type *ptr_;
   mm_register_type *end_;
