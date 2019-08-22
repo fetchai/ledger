c710878ff9a9ff6ce33cfbb9660d26feb2dcf327
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-08-22 19:24:06 +0000

    Style

--- libs/beacon/tests/dkg/full_cycle.cpp
+++ libs/beacon/tests/dkg/full_cycle.cpp
@@ -246,7 +246,7 @@ void RunHonestComitteeRenewal(uint16_t delay = 100, uint16_t total_renewals = 4,
 
 TEST(beacon, full_cycle)
 {
-//  SetGlobalLogLevel(LogLevel::CRITICAL);
+  //  SetGlobalLogLevel(LogLevel::CRITICAL);
   // TODO(tfr): Heuristically fails atm. RunHonestComitteeRenewal(100, 4, 4, 4, 10, 0.5);
   RunHonestComitteeRenewal(800, 4, 2, 2, 10, 0.5);
 }
