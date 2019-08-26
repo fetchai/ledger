6f6986bea3bccca31884f50b774528cfbcda3d34
Denis Trapeznikov <denin@mail.ru>
2019-08-26 23:18:58 +0200

    +Last

--- libs/meta/include/meta/type_util.hpp
+++ libs/meta/include/meta/type_util.hpp
@@ -161,5 +161,13 @@ using CopyReferenceKind =
 template <class Source, class Dest>
 using CopyReferenceKindT = typename CopyReferenceKind<Source, Dest>::type;
 
+template <class... Ts>
+struct Last : std::tuple_element<sizeof...(Ts) - 1, std::tuple<Ts...>>
+{
+};
+
+template <class... Ts>
+using LastT = typename Last<Ts...>::type;
+
 }  // namespace type_util
 }  // namespace fetch
