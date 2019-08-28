1696c83123c18150f261cd15ac4feccd43037752
Denis Trapeznikov <denin@mail.ru>
2019-08-28 07:15:17 +0200

    Style applied.

--- libs/vm/include/vm/variant.hpp
+++ libs/vm/include/vm/variant.hpp
@@ -268,16 +268,14 @@ struct Variant
   }
 
   template <typename T>
-  std::enable_if_t<IsPrimitive<T>::value> Construct(T      other,
-                                                    TypeId other_type_id) noexcept
+  std::enable_if_t<IsPrimitive<T>::value> Construct(T other, TypeId other_type_id) noexcept
   {
     primitive.Set(other);
     type_id = other_type_id;
   }
 
   template <typename T>
-  std::enable_if_t<IsPtr<T>::value> Construct(T const &other,
-                                              TypeId   other_type_id) noexcept
+  std::enable_if_t<IsPtr<T>::value> Construct(T const &other, TypeId other_type_id) noexcept
   {
     new (&object) Ptr<Object>(other);
     type_id = other_type_id;
@@ -413,15 +411,13 @@ struct Variant
   }
 
   template <typename T>
-  std::enable_if_t<IsVariant<T>::value> Assign(T const &other,
-                                               TypeId /* other_type_id */) noexcept
+  std::enable_if_t<IsVariant<T>::value> Assign(T const &other, TypeId /* other_type_id */) noexcept
   {
     operator=(other);
   }
 
   template <typename T>
-  std::enable_if_t<IsVariant<T>::value> Assign(T &&other,
-                                               TypeId /* other_type_id */) noexcept
+  std::enable_if_t<IsVariant<T>::value> Assign(T &&other, TypeId /* other_type_id */) noexcept
   {
     operator=(std::forward<T>(other));
   }
