1efa5c0aa49469b077f89c3b915ad70d49b50899
Khan Baykaner <khan.baykaner@fetch.ai>
2019-08-29 18:47:04 +0100

    wip - trying to get around overflow

--- libs/ml/examples/crypto_price_prediction/main.cpp
+++ libs/ml/examples/crypto_price_prediction/main.cpp
@@ -25,9 +25,12 @@
 #include "ml/layers/convolution_1d.hpp"
 #include "ml/ops/activation.hpp"
 #include "ml/ops/loss_functions/mean_square_error_loss.hpp"
-#include "ml/optimisation/adam_optimiser.hpp"
+//#include "ml/optimisation/adam_optimiser.hpp"
+#include "ml/optimisation/sgd_optimiser.hpp"
 #include "ml/utilities/min_max_scaler.hpp"
 #include "vectorise/fixed_point/fixed_point.hpp"
+
+#include "ml/serializers/ml_types.hpp"
 
 #include <iostream>
 #include <memory>
@@ -38,44 +41,81 @@ using namespace fetch::ml::ops;
 using namespace fetch::ml::layers;
 
 using DataType   = fetch::fixed_point::fp64_t;
+//using DataType   = double;
 using TensorType = fetch::math::Tensor<DataType>;
 using SizeType   = TensorType::SizeType;
 
 using GraphType        = fetch::ml::Graph<TensorType>;
 using CostFunctionType = fetch::ml::ops::MeanSquareErrorLoss<TensorType>;
-using OptimiserType    = fetch::ml::optimisers::AdamOptimiser<TensorType>;
+using OptimiserType    = fetch::ml::optimisers::SGDOptimiser<TensorType>;
+//using OptimiserType    = fetch::ml::optimisers::AdamOptimiser<TensorType>;
 using DataLoaderType   = fetch::ml::dataloaders::TensorDataLoader<TensorType, TensorType>;
 
 struct TrainingParams
 {
-  SizeType epochs{10};
-  SizeType batch_size{1000};
-  bool     normalise = true;
+  SizeType epochs{100};
+  SizeType batch_size{64};
+  bool     normalise = false;
 };
 
 std::shared_ptr<GraphType> BuildModel(std::string &input_name, std::string &output_name,
                                       std::string &label_name, std::string &error_name)
 {
+//  auto g = std::make_shared<GraphType>();
+//
+//  SizeType conv1D_1_filters        = 16;
+//  SizeType conv1D_1_input_channels = 1;
+//  SizeType conv1D_1_kernel_size    = 96;
+//  SizeType conv1D_1_stride         = 3;
+//
+//  typename TensorType::Type keep_prob_1{0.9};
+//
+//  SizeType conv1D_2_filters        = 8;
+//  SizeType conv1D_2_input_channels = conv1D_1_filters;
+//  SizeType conv1D_2_kernel_size    = 48;
+//  SizeType conv1D_2_stride         = 2;
+//
+//  typename TensorType::Type keep_prob_2{0.9};
+//
+//  SizeType conv1D_3_filters        = 1;
+//  SizeType conv1D_3_input_channels = conv1D_2_filters;
+//  SizeType conv1D_3_kernel_size    = 47;
+//  SizeType conv1D_3_stride         = 1;
+//
+//  input_name = g->AddNode<PlaceHolder<TensorType>>("Input", {});
+//  label_name = g->AddNode<PlaceHolder<TensorType>>("Label", {});
+//
+//  std::string layer_1 = g->AddNode<fetch::ml::layers::Convolution1D<TensorType>>(
+//      "Conv1D_1", {input_name}, conv1D_1_filters, conv1D_1_input_channels, conv1D_1_kernel_size,
+//      conv1D_1_stride, fetch::ml::details::ActivationType::RELU);
+//  std::string layer_2 = g->AddNode<Dropout<TensorType>>("Dropout_1", {layer_1}, keep_prob_1);
+//
+//  std::string layer_3 = g->AddNode<fetch::ml::layers::Convolution1D<TensorType>>(
+//      "Conv1D_2", {layer_2}, conv1D_2_filters, conv1D_2_input_channels, conv1D_2_kernel_size,
+//      conv1D_2_stride, fetch::ml::details::ActivationType::RELU);
+//  std::string layer_4 = g->AddNode<Dropout<TensorType>>("Dropout_2", {layer_3}, keep_prob_2);
+//
+//  output_name = g->AddNode<fetch::ml::layers::Convolution1D<TensorType>>(
+//      "Conv1D_3", {layer_4}, conv1D_3_filters, conv1D_3_input_channels, conv1D_3_kernel_size,
+//      conv1D_3_stride);
+//
+//  error_name = g->AddNode<fetch::ml::ops::MeanSquareErrorLoss<TensorType>>(
+//      "error_name", {output_name, label_name});
+//  return g;
+
   auto g = std::make_shared<GraphType>();
 
-  SizeType conv1D_1_filters        = 16;
+  SizeType conv1D_1_filters        = 8;
   SizeType conv1D_1_input_channels = 1;
-  SizeType conv1D_1_kernel_size    = 96;
-  SizeType conv1D_1_stride         = 3;
+  SizeType conv1D_1_kernel_size    = 192;
+  SizeType conv1D_1_stride         = 2;
 
-  typename TensorType::Type keep_prob_1{0.9};
+  typename TensorType::Type keep_prob_1{0.5};
 
-  SizeType conv1D_2_filters        = 8;
+  SizeType conv1D_2_filters        = 1;
   SizeType conv1D_2_input_channels = conv1D_1_filters;
-  SizeType conv1D_2_kernel_size    = 48;
-  SizeType conv1D_2_stride         = 2;
-
-  typename TensorType::Type keep_prob_2{0.9};
-
-  SizeType conv1D_3_filters        = 1;
-  SizeType conv1D_3_input_channels = conv1D_2_filters;
-  SizeType conv1D_3_kernel_size    = 47;
-  SizeType conv1D_3_stride         = 1;
+  SizeType conv1D_2_kernel_size    = 164;
+  SizeType conv1D_2_stride         = 3;
 
   input_name = g->AddNode<PlaceHolder<TensorType>>("Input", {});
   label_name = g->AddNode<PlaceHolder<TensorType>>("Label", {});
@@ -85,14 +125,9 @@ std::shared_ptr<GraphType> BuildModel(std::string &input_name, std::string &outp
       conv1D_1_stride, fetch::ml::details::ActivationType::RELU);
   std::string layer_2 = g->AddNode<Dropout<TensorType>>("Dropout_1", {layer_1}, keep_prob_1);
 
-  std::string layer_3 = g->AddNode<fetch::ml::layers::Convolution1D<TensorType>>(
-      "Conv1D_2", {layer_2}, conv1D_2_filters, conv1D_2_input_channels, conv1D_2_kernel_size,
-      conv1D_2_stride, fetch::ml::details::ActivationType::RELU);
-  std::string layer_4 = g->AddNode<Dropout<TensorType>>("Dropout_2", {layer_3}, keep_prob_2);
-
   output_name = g->AddNode<fetch::ml::layers::Convolution1D<TensorType>>(
-      "Conv1D_3", {layer_4}, conv1D_3_filters, conv1D_3_input_channels, conv1D_3_kernel_size,
-      conv1D_3_stride);
+      "Conv1D_2", {layer_2}, conv1D_2_filters, conv1D_2_input_channels, conv1D_2_kernel_size,
+      conv1D_2_stride);
 
   error_name = g->AddNode<fetch::ml::ops::MeanSquareErrorLoss<TensorType>>(
       "error_name", {output_name, label_name});
@@ -128,6 +163,28 @@ std::vector<TensorType> LoadData(std::string const &train_data_filename,
   test_labels_tensor.Reshape(
       {1, test_labels_tensor.shape().at(0), test_labels_tensor.shape().at(1)});
   return {train_data_tensor, train_labels_tensor, test_data_tensor, test_labels_tensor};
+}
+
+void SaveGraphToFile(GraphType &g, std::string const file_name)
+{
+
+  // start serializing and writing to file
+  fetch::ml::GraphSaveableParams<TensorType> gsp1 = g.GetGraphSaveableParams();
+  std::cout << "got saveable params" << std::endl;
+
+  fetch::serializers::SizeCounter       counter;
+  fetch::serializers::MsgPackSerializer b;
+  counter << gsp1;
+  std::cout << "finish counting" << std::endl;
+  b.Reserve(counter.size());
+  b << gsp1;
+  std::cout << "finish serializing" << std::endl;
+
+  std::ofstream outFile(file_name, std::ios::out | std::ios::binary);
+  outFile.write(b.data().char_pointer(), std::streamsize(b.size()));
+  outFile.close();
+  std::cout << b.size() << std::endl;
+  std::cout << "finish writing to file" << std::endl;
 }
 
 int main(int ac, char **av)
@@ -197,6 +254,10 @@ int main(int ac, char **av)
     auto result = fetch::math::MeanAbsoluteError(prediction, orig_test_label);
     std::cout << "mean absolute validation error: " << result << std::endl;
   }
+
+  SaveGraphToFile(*g, "./bitcoin_price_prediction_graph.bin");
+
+
 
   return 0;
 }
