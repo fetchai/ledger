a203a79845e4ee5fb218bf0ddcb5536b5194a32a
baykaner <40827646+baykaner@users.noreply.github.com>
2019-08-30 13:20:27 +0100

    Dataloader prepare batch compliance (#1573)
    add test for tensor dataloader prepare batch usage

--- libs/ml/tests/ml/dataloader/tensor_dataloader.cpp
+++ libs/ml/tests/ml/dataloader/tensor_dataloader.cpp
@@ -100,3 +100,30 @@ TYPED_TEST(TensorDataloaderTest, test_validation_splitting_dataloader_test)
   EXPECT_THROW(tdl.SetMode(DataLoaderMode::TEST), std::runtime_error);
   EXPECT_THROW(tdl.SetMode(DataLoaderMode::VALIDATE), std::runtime_error);
 }
+
+TYPED_TEST(TensorDataloaderTest, prepare_batch_test)
+{
+  using SizeType = fetch::math::SizeType;
+
+  SizeType feature_size_1 = 2;
+  SizeType feature_size_2 = 3;
+  SizeType batch_size     = 2;
+  SizeType n_data         = 10;
+
+  TypeParam label_tensor = TypeParam::UniformRandom(n_data);
+  TypeParam data1_tensor = TypeParam::UniformRandom(feature_size_1 * feature_size_2 * n_data);
+  label_tensor.Reshape({1, n_data});
+  data1_tensor.Reshape({feature_size_1, feature_size_2, n_data});
+
+  // generate a plausible tensor data loader
+  auto tdl = fetch::ml::dataloaders::TensorDataLoader<TypeParam, TypeParam>(label_tensor.shape(),
+                                                                            {data1_tensor.shape()});
+  // add some data
+  tdl.AddData(data1_tensor, label_tensor);
+
+  bool is_done_set = false;
+  auto batch       = tdl.PrepareBatch(batch_size, is_done_set).second;
+
+  EXPECT_EQ(batch.at(0).shape(),
+            std::vector<SizeType>({feature_size_1, feature_size_2, batch_size}));
+}
