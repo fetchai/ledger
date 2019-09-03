9f6c780e6f7fb8126d83232ed060c7274a75db39
nhutton <nathan.hutton@fetch.ai>
2019-09-03 18:18:21 +0100

    style

--- libs/beacon/src/beacon_setup_service.cpp
+++ libs/beacon/src/beacon_setup_service.cpp
@@ -1281,7 +1281,7 @@ uint64_t GetExpectedDKGTime(uint64_t cabinet_size)
     expected_dkg_time_s = 30;
   }
 
-  FETCH_LOG_INFO(LOGGING_NAME, "Note: Expect DKG time to be ", expected_dkg_time_s, " s");
+  FETCH_LOG_INFO(BeaconSetupService::LOGGING_NAME, "Note: Expect DKG time to be ", expected_dkg_time_s, " s");
 
   return expected_dkg_time_s;
 }
