21e85db4cced49c16ce1ee70cce4ce281262e812
Wojciech Kaluza <w-kaluza@tlen.pl>
2019-09-10 13:31:05 +0100

    Revert troublesome files

--- libs/crypto/include/crypto/bls_base.hpp
+++ libs/crypto/include/crypto/bls_base.hpp
@@ -56,16 +56,46 @@ enum
 
 void Init();
 
-PrivateKey PrivateKeyByCSPRNG();
+inline PrivateKey PrivateKeyByCSPRNG()
+{
+  PrivateKey ret;
 
-PublicKey PublicKeyFromPrivate(PrivateKey const &priv);
+  int32_t error = blsSecretKeySetByCSPRNG(&ret);
 
-Signature Sign(PrivateKey const &priv, byte_array::ConstByteArray const &msg);
+  if (error != 0)
+  {
+    throw std::runtime_error("Failed at generating BLS secret key.");
+  }
 
-bool Verify(Signature const &signature, PublicKey const &pub,
-            byte_array::ConstByteArray const &msg);
+  return ret;
+}
 
-PrivateKey HashToPrivateKey(byte_array::ConstByteArray const &seed);
+inline PublicKey PublicKeyFromPrivate(PrivateKey const &priv)
+{
+  PublicKey ret;
+  blsGetPublicKey(&ret, &priv);
+  return ret;
+}
+
+inline Signature Sign(PrivateKey const &priv, byte_array::ConstByteArray const &msg)
+{
+  Signature ret;
+  blsSign(&ret, &priv, msg.pointer(), msg.size());
+  return ret;
+}
+
+inline bool Verify(Signature const &signature, PublicKey const &pub,
+                   byte_array::ConstByteArray const &msg)
+{
+  return blsVerify(&signature, &pub, msg.pointer(), msg.size());
+}
+
+inline PrivateKey HashToPrivateKey(byte_array::ConstByteArray const &seed)
+{
+  PrivateKey priv;
+  blsHashToSecretKey(&priv, seed.pointer(), seed.size());
+  return priv;
+}
 
 template <typename KeyType>
 KeyType PrivateKeyShare(std::vector<KeyType> &kl, Id const &id)
@@ -81,19 +111,62 @@ KeyType PrivateKeyShare(std::vector<KeyType> &kl, Id const &id)
   return ret;
 }
 
-void AddKeys(PrivateKey &lhs, PrivateKey const &rhs);
+inline void AddKeys(PrivateKey &lhs, PrivateKey const &rhs)
+{
+  blsSecretKeyAdd(&lhs, &rhs);
+}
 
-void AddKeys(PublicKey &lhs, PublicKey const &rhs);
+inline void AddKeys(PublicKey &lhs, PublicKey const &rhs)
+{
+  blsPublicKeyAdd(&lhs, &rhs);
+}
 
-bool PublicKeyIsEqual(PublicKey const &pk1, PublicKey const &pk2);
+inline bool PublicKeyIsEqual(PublicKey const &pk1, PublicKey const &pk2)
+{
+  return blsPublicKeyIsEqual(&pk1, &pk2);
+}
 
-PublicKey GetPublicKey(PrivateKey const &sk);
+inline PublicKey GetPublicKey(PrivateKey const &sk)
+{
+  PublicKey ret;
+  blsGetPublicKey(&ret, &sk);
+  return ret;
+}
 
-PublicKey PublicKeyShare(PublicKeyList const &master_keys, Id const &id);
+inline PublicKey PublicKeyShare(PublicKeyList const &master_keys, Id const &id)
+{
+  PublicKey ret;
+  blsPublicKeyShare(&ret, master_keys.data(), master_keys.size(), &id);
+  return ret;
+}
 
-Signature RecoverSignature(SignatureList const &sigs, IdList const &ids);
+inline Signature RecoverSignature(SignatureList const &sigs, IdList const &ids)
+{
+  Signature ret;
+  if (blsSignatureRecover(&ret, sigs.data(), ids.data(), sigs.size()) != 0)
+  {
+    throw std::runtime_error("Unable to recover signature");
+  }
+  return ret;
+}
 
-byte_array::ConstByteArray ToBinary(Signature const &sig);
+inline byte_array::ConstByteArray ToBinary(Signature const &sig)
+{
+  byte_array::ByteArray buffer{};
+  buffer.Resize(1024);
+#ifdef BLS_SWAP_G
+  std::size_t n = mclBnG2_getStr(buffer.char_pointer(), buffer.size(), &sig.v, 0);
+#else
+  std::size_t n = mclBnG1_getStr(buffer.char_pointer(), buffer.size(), &sig.v, 0);
+#endif
+  if (n == 0)
+  {
+    throw std::runtime_error("Signature:tgetStr");
+  }
+  buffer.Resize(n);
+
+  return {buffer};
+}
 
 }  // namespace bls
 }  // namespace crypto
