22cf9148e4ec6295db0e92fd8509057fe590b53b
Lokman Rahmani <lokman.rahmani@fetch.ai>
2019-09-11 10:10:56 +0100

    [minor] Add IUpdate and Update internals

--- libs/dmlf/include/dmlf/iupdate.hpp
+++ libs/dmlf/include/dmlf/iupdate.hpp
@@ -1,4 +1,26 @@
 #pragma once
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+
+#include "core/byte_array/byte_array.hpp"
+
+#include <cstdint>
 
 namespace fetch {
 namespace dmlf {
@@ -6,9 +28,23 @@ namespace dmlf {
 class IUpdate
 {
 public:
+  using TimeStampType = std::uint64_t; 
+
   IUpdate()
   {
   }
+  
+  // API
+  virtual byte_array::ByteArray serialise()        = 0;
+  virtual void deserialise(byte_array::ByteArray&) = 0;
+  virtual TimeStampType TimeStamp() const          = 0; 
+  
+  // Queue ordering
+  bool operator>(const IUpdate& other) const
+  {
+    return TimeStamp() > other.TimeStamp();
+  }
+
   virtual ~IUpdate()
   {
   }
@@ -20,5 +56,5 @@ private:
   bool operator<(const IUpdate &other) = delete;
 };
 
-}
-}
+}  // namespace dmlf
+}  // namespace fetch
--- libs/dmlf/include/dmlf/update.hpp
+++ libs/dmlf/include/dmlf/update.hpp
@@ -1,14 +1,61 @@
 #pragma once
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "dmlf/iupdate.hpp"
+
+#include <vector>
+#include <cstdint>
+#include <chrono>
 
 namespace fetch {
 namespace dmlf {
 
-class Update
+template <typename T>
+class Update : public IUpdate
 {
 public:
-  Update()
+  using TensorType       = T;
+  using VectorTensorType = std::vector<TensorType>;
+  using TimeStampType    = IUpdate::TimeStampType; 
+  
+  explicit Update()
+    : stamp_{CurrentTime()}
   {
   }
+  explicit Update(VectorTensorType gradients)
+    : gradients_{gradients}
+    , stamp_{CurrentTime()}
+  {
+  }
+  
+  virtual byte_array::ByteArray serialise() override
+  {
+    return byte_array::ByteArray{};
+  }
+  virtual void deserialise(byte_array::ByteArray&) override
+  {
+  }
+  virtual TimeStampType TimeStamp() const override 
+  {
+    return stamp_;
+  }
+
   virtual ~Update()
   {
   }
@@ -18,7 +65,16 @@ private:
   Update &operator=(const Update &other) = delete;
   bool operator==(const Update &other) = delete;
   bool operator<(const Update &other) = delete;
+  
+  TimeStampType CurrentTime() 
+  {
+    return static_cast<TimeStampType>
+      (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
+  }
+  
+  VectorTensorType gradients_;
+  TimeStampType stamp_;
 };
 
-}
-}
+}  // namespace dmlf
+}  // namespace fetch
