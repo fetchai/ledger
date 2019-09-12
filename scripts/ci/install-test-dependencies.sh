1c5399fa8c405853c4a381ce4cd77e350d7fda51
Wojciech Kaluza <w-kaluza@tlen.pl>
2019-09-12 20:55:56 +0100

    pip packages

--- scripts/ci/install-test-dependencies.sh
+++ scripts/ci/install-test-dependencies.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-pip3 install --force-reinstall wheel
+pip3 install --force-reinstall wheel pyyaml requests
 
 #???
 pip3 install --force-reinstall git+https://github.com/wkaluza/ledger-api-py.git@wip_consolidate_smart_and_synergetic_contracts#egg=fetchai-ledger-api
 
 pip3 install --force-reinstall -i https://test.pypi.org/simple/ fetchai-netutils==0.0.6a1
-
-pip3 install --force-reinstall pyyaml
