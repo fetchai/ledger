99de7595c165363c875edd054e1d382e1b56ee10
nhutton <nathan.hutton@fetch.ai>
2019-09-12 11:24:26 +0100

    revert fully option

--- libs/core/src/logging.cpp
+++ libs/core/src/logging.cpp
@@ -21,27 +21,8 @@
 #include "telemetry/counter.hpp"
 #include "telemetry/registry.hpp"
 
-#if defined(__GNUC__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wsign-conversion"
-#endif
-
-#if defined(__clang__)
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wsign-conversion"
-#endif
-
-#include "spdlog/sinks/dup_filter_sink.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/spdlog.h"
-
-#if defined(__clang__)
-#pragma clang diagnostic pop
-#endif
-
-#if defined(__GNUC__)
-#pragma GCC diagnostic pop
-#endif
 
 #include <unordered_map>
 
@@ -167,7 +148,11 @@ spdlog::level::level_enum ConvertFromLevel(LogLevel level)
 }
 
 LogRegistry::LogRegistry()
-{}
+{
+  spdlog::set_level(
+      spdlog::level::trace);  // this should be kept in sync with the compilation level
+  spdlog::set_pattern("%^[%L]%$ %Y/%m/%d %T | %-30n : %v");
+}
 
 void LogRegistry::Log(LogLevel level, char const *name, std::string &&message)
 {
@@ -242,17 +227,9 @@ LogRegistry::Logger &LogRegistry::GetLogger(char const *name)
   auto it = registry_.find(name);
   if (it == registry_.end())
   {
-    // create the new logger instance - note it suppresses duplicate messages
-    auto dup_filter =
-        std::make_shared<spdlog::sinks::dup_filter_sink_mt>(std::chrono::milliseconds(100));
-    dup_filter->add_sink(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-
-    auto logger = std::make_shared<spdlog::logger>(name, dup_filter);
-
+    // create the new logger instance
+    auto logger = spdlog::stdout_color_mt(name, spdlog::color_mode::automatic);
     logger->set_level(ConvertFromLevel(DEFAULT_LEVEL));
-    logger->set_pattern("%^[%L]%$ %Y/%m/%d %T | %-30n : %v");
-    logger->set_level(
-        spdlog::level::trace);  // this should be kept in sync with the compilation level
 
     // keep a reference of it
     registry_[name] = logger;
