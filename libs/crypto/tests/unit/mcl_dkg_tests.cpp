479403158e6ab7b55582933447dfddcafe676fca
jinmannwong <jinmannwong@gmail.com>
2019-09-12 08:40:59 +0100

    Tests for mcl crypto

--- libs/crypto/tests/unit/mcl_dkg_tests.cpp
+++ libs/crypto/tests/unit/mcl_dkg_tests.cpp
@@ -25,7 +25,7 @@
 #include <iostream>
 #include <ostream>
 
-using namespace fetch::dkg;
+using namespace fetch::crypto::mcl;
 using namespace fetch::byte_array;
 
 TEST(MclTests, BaseMcl)
@@ -84,51 +84,34 @@ TEST(MclTests, BaseMcl)
   EXPECT_EQ(sizeof(mcl::fp::Unit), 8);
 }
 
-TEST(MclDkgTests, ComputeLhsTrivialSubgroup)
+void SetGenerator(bn::G2 &group_g)
 {
-  // Construct polynomial of degree 2 (threshold = 1)
-  uint32_t            threshold = 1;
-  std::vector<bn::Fr> vec_a;
-  Init(vec_a, threshold + 1);
-  std::vector<bn::Fr> vec_b;
-  Init(vec_b, threshold + 1);
-
-  // Randomly initialise the values of a but leave b uninitialised
-  for (auto &a_i : vec_a)
-  {
-    a_i.setRand();
-  }
-
-  std::vector<bn::G2> coefficients;
-  Init(coefficients, threshold + 1);
-
-  bn::G2 group_g, group_h;
   group_g.clear();
-  group_h.clear();
+
   const bn::Fp2 g("1380305877306098957770911920312855400078250832364663138573638818396353623780",
                   "14633108267626422569982187812838828838622813723380760182609272619611213638781");
+
   bn::mapToG2(group_g, g);
-  for (std::size_t ii = 0; ii <= threshold; ++ii)
-  {
-    coefficients[ii] = ComputeLHS(group_g, group_h, vec_a[ii], vec_b[ii]);
-  }
-
-  // Check ComputeLHS with trivial subgroup h
-  std::vector<bn::G2> coefficients_test;
-  Init(coefficients_test, threshold + 1);
-  for (std::size_t jj = 0; jj <= threshold; ++jj)
-  {
-    bn::G2 tmpG;
-    tmpG.clear();
-    bn::G2::mul(tmpG, group_g, vec_a[jj]);
-    bn::G2::add(coefficients_test[jj], coefficients_test[jj], tmpG);
-  }
-
-  EXPECT_EQ(coefficients, coefficients_test);
 }
 
-TEST(MclDkgTests, ComputeLhsNonTrivialSubgroup)
+void SetGenerators(bn::G2 &group_g, bn::G2 &group_h)
 {
+  group_g.clear();
+  group_h.clear();
+
+  const bn::Fp2 g("1380305877306098957770911920312855400078250832364663138573638818396353623780",
+                  "14633108267626422569982187812838828838622813723380760182609272619611213638781");
+  const bn::Fp2 h("6798148801244076840612542066317482178930767218436703568023723199603978874964",
+                  "12726557692714943631796519264243881146330337674186001442981874079441363994424");
+
+  bn::mapToG2(group_g, g);
+  bn::mapToG2(group_h, h);
+}
+
+TEST(MclDkgTests, ComputeLhsRhs)
+{
+  mcl::bn::initPairing();
+
   // Construct polynomial of degree 2 (threshold = 1)
   uint32_t            threshold = 1;
   std::vector<bn::Fr> vec_a;
@@ -147,21 +130,14 @@ TEST(MclDkgTests, ComputeLhsNonTrivialSubgroup)
   Init(coefficients, threshold + 1);
 
   bn::G2 group_g, group_h;
-  group_g.clear();
-  group_h.clear();
-  const bn::Fp2 g("1380305877306098957770911920312855400078250832364663138573638818396353623780",
-                  "14633108267626422569982187812838828838622813723380760182609272619611213638781");
-  const bn::Fp2 h("6798148801244076840612542066317482178930767218436703568023723199603978874964",
-                  "12726557692714943631796519264243881146330337674186001442981874079441363994424");
-  bn::mapToG2(group_g, g);
-  bn::mapToG2(group_h, h);
+  SetGenerators(group_g, group_h);
 
   for (std::size_t ii = 0; ii <= threshold; ++ii)
   {
     coefficients[ii] = ComputeLHS(group_g, group_h, vec_a[ii], vec_b[ii]);
   }
 
-  // Check ComputeLHS with trivial subgroup h
+  // Check compute LHS with direct computation
   std::vector<bn::G2> coefficients_test;
   Init(coefficients_test, threshold + 1);
   for (std::size_t jj = 0; jj <= threshold; ++jj)
@@ -176,52 +152,143 @@ TEST(MclDkgTests, ComputeLhsNonTrivialSubgroup)
   }
 
   EXPECT_EQ(coefficients, coefficients_test);
+
+  bn::G2   rhs;
+  uint32_t rank = 2;
+  rhs           = ComputeRHS(rank, coefficients);
+
+  // Check compute RHS with direct computation (increment rank as not allowed to be 0)
+  bn::G2 rhs_test;
+  rhs_test.clear();
+  for (std::size_t jj = 0; jj <= threshold; ++jj)
+  {
+    bn::G2 tmpG;
+    tmpG.clear();
+    bn::G2::mul(tmpG, coefficients[jj], static_cast<int64_t>(pow(rank + 1, jj)));
+    bn::G2::add(rhs_test, rhs_test, tmpG);
+  }
+
+  EXPECT_EQ(rhs, rhs_test);
 }
 
-TEST(MclDkgTests, ComputeZi)
+TEST(MclDkgTests, Interpolation)
 {
-    mcl::bn::initPairing();
+  mcl::bn::initPairing();
 
-    // Construct polynomial of degree 2
-    uint32_t            degree = 2;
-    std::vector<bn::Fr> vec_a;
-    Init(vec_a, degree + 1);
+  // Construct polynomial of degree 2
+  uint32_t            degree = 2;
+  std::vector<bn::Fr> vec_a;
+  Init(vec_a, degree + 1);
 
-    for (std::size_t ii = 0; ii <= degree; ++ii)
+  for (std::size_t ii = 0; ii <= degree; ++ii)
+  {
+    vec_a[ii].setRand();
+  }
+
+  bn::G2 group_g, group_h;
+  SetGenerators(group_g, group_h);
+
+  // Compute values of polynomial evaluated at degree + 1 points
+  uint32_t           cabinet_size = 5;
+  std::set<uint32_t> member_set{0, 1, 2};
+  assert(member_set.size() >= degree + 1);
+  std::vector<bn::Fr> points;
+  std::vector<bn::Fr> values;
+  std::vector<bn::Fr> values2;
+  values.resize(cabinet_size);
+  for (auto index : member_set)
+  {
+    points.emplace_back(index + 1);
+    bn::Fr pow, tmpF, s_i;
+    s_i = vec_a[0];
+    for (uint32_t k = 1; k < vec_a.size(); k++)
     {
-        vec_a[ii].setRand();
+      bn::Fr::pow(pow, index + 1, k);    // adjust index in computation
+      bn::Fr::mul(tmpF, pow, vec_a[k]);  // j^k * a_i[k]
+      bn::Fr::add(s_i, s_i, tmpF);
     }
 
-    std::vector<bn::G2> coefficients;
-    Init(coefficients, degree + 1);
+    // Compare with values from ComputeSecretShare
+    bn::Fr secret_share, secret_share1;
+    ComputeShares(secret_share, secret_share1, vec_a, vec_a, index);
+    EXPECT_EQ(secret_share, s_i);
+    EXPECT_EQ(secret_share1, s_i);
 
-    bn::G2 group_g, group_h;
-    group_g.clear();
-    group_h.clear();
-    const bn::Fp2 g("1380305877306098957770911920312855400078250832364663138573638818396353623780",
-                    "14633108267626422569982187812838828838622813723380760182609272619611213638781");
-    const bn::Fp2 h("6798148801244076840612542066317482178930767218436703568023723199603978874964",
-                    "12726557692714943631796519264243881146330337674186001442981874079441363994424");
-    bn::mapToG2(group_g, g);
-    bn::mapToG2(group_h, h);
+    values[index] = s_i;
+    values2.emplace_back(s_i);
+  }
 
-    //Compute values of polynomial evaluated at degree + 1 points
-    uint32_t cabinet_size = 5;
-    std::set<uint32_t> member_set {0,1,2};
-    assert(member_set.size() >= degree + 1);
-    std::vector<bn::Fr> values;
-    values.resize(cabinet_size);
-    for (auto index : member_set)
+  EXPECT_EQ(vec_a[0], ComputeZi(member_set, values));
+  EXPECT_EQ(vec_a, InterpolatePolynom(points, values2));
+}
+
+TEST(MclDkgTests, Signing)
+{
+  mcl::bn::initPairing();
+
+  uint32_t committee_size = 5;
+  uint32_t threshold      = 3;
+  bn::G2   group_g;
+  SetGenerator(group_g);
+
+  // Construct polynomial of degree threshold - 1
+  std::vector<bn::Fr> vec_a;
+  Init(vec_a, threshold);
+  for (std::size_t ii = 0; ii <= threshold; ++ii)
+  {
+    vec_a[ii].setRand();
+  }
+
+  PublicKey group_public_key;
+  group_public_key.clear();
+
+  std::string                             message = "Hello";
+  std::unordered_map<uint32_t, Signature> threshold_signatures;
+
+  // Generate random selection of committee members
+  std::set<uint32_t> members;
+  while (members.size() < threshold)
+  {
+    members.insert(static_cast<uint32_t>(rand()) % committee_size);
+  }
+
+  // Group secret key is polynomial evaluated at 0
+  PrivateKey group_private_key = vec_a[0];
+  bn::G2::mul(group_public_key, group_g, group_private_key);
+
+  // Compute group signature and check validity
+  Signature group_sig = SignShare(message, group_private_key);
+  EXPECT_EQ(true, VerifySign(group_public_key, message, group_sig, group_g));
+
+  // Generate committee public keys from their private key contributions
+  for (uint32_t i = 0; i < committee_size; ++i)
+  {
+    PrivateKey pow, tmpF, private_key;
+    // Private key is polynomial evaluated at index i
+    private_key = vec_a[0];
+    for (uint32_t k = 1; k < vec_a.size(); k++)
     {
-        bn::Fr pow, tmpF, s_i;
-        s_i      = vec_a[0];
-        for (uint32_t k = 1; k < vec_a.size(); k++) {
-            bn::Fr::pow(pow, index + 1, k);  // adjust index in computation
-            bn::Fr::mul(tmpF, pow, vec_a[k]);  // j^k * a_i[k]
-            bn::Fr::add(s_i, s_i, tmpF);
-        }
-        values[index] = s_i;
+      bn::Fr::pow(pow, i + 1, k);        // adjust index in computation
+      bn::Fr::mul(tmpF, pow, vec_a[k]);  // j^k * a_i[k]
+      bn::Fr::add(private_key, private_key, tmpF);
     }
+    // Public key from private
+    PublicKey public_key;
+    public_key.clear();
+    bn::G2::mul(public_key, group_g, private_key);
 
-    EXPECT_EQ(vec_a[0], ComputeZi(member_set, values));
+    // Compute signature share and validate
+    Signature sig = SignShare(message, private_key);
+    EXPECT_EQ(true, VerifySign(public_key, message, sig, group_g));
+
+    // Accumulate signature shares from some random threshold number of members
+    if (members.find(i) != members.end())
+    {
+      threshold_signatures.insert({i, sig});
+    }
+  }
+
+  // Compute group signature from combining signature shares and validate
+  Signature group_signature = LagrangeInterpolation(threshold_signatures);
+  EXPECT_EQ(true, VerifySign(group_public_key, message, group_signature, group_g));
 }
