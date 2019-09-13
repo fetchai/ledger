7f4b3818b535f8e14db802bb6e9db00ba19adfa6
Wojciech Kaluza <w-kaluza@tlen.pl>
2019-09-13 03:01:45 +0100

    Trigger CI

--- scripts/ci/install-test-dependencies.sh
+++ scripts/ci/install-test-dependencies.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 
-pip3 install --force-reinstall wheel pyyaml requests
+echo "??? changes"
+
+pip3 uninstall --yes wheel pyyaml requests fetchai-ledger-api fetchai-netutils
+
+pip3 install --ignore-installed --force-reinstall wheel pyyaml requests
 
 #???
-pip3 install --force-reinstall git+https://github.com/wkaluza/ledger-api-py.git@wip_consolidate_smart_and_synergetic_contracts#egg=fetchai-ledger-api
+pip3 install --ignore-installed --force-reinstall git+https://github.com/wkaluza/ledger-api-py.git@wip_consolidate_smart_and_synergetic_contracts#egg=fetchai-ledger-api
 
-pip3 install --force-reinstall -i https://test.pypi.org/simple/ fetchai-netutils==0.0.6a1
+pip3 install --ignore-installed --force-reinstall --index-url https://test.pypi.org/simple/ fetchai-netutils==0.0.6a1
