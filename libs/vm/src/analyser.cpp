85dc0e765e285e32e63b46da216e64469870b0fc
Robert Dickson <roberto.dickson@gmail.com>
2019-09-18 20:42:33 +0100

    Etch multi file (#1681)
    * added multi-file support to etch compiler
    
    * apply style
    
    * static analysis fix

--- libs/vm/src/analyser.cpp
+++ libs/vm/src/analyser.cpp
@@ -339,8 +339,7 @@ bool Analyser::Analyse(BlockNodePtr const &root, std::vector<std::string> &error
 void Analyser::AddError(uint16_t line, std::string const &message)
 {
   std::ostringstream stream;
-  stream << "line " << line << ": "
-         << "error: " << message;
+  stream << filename_ << ": line " << line << ": error: " << message;
   errors_.push_back(stream.str());
 }
 
@@ -363,7 +362,7 @@ void Analyser::BuildBlock(BlockNodePtr const &block_node)
     }
     case NodeKind::FunctionDefinitionStatement:
     {
-      BuildFunctionDefinition(block_node, ConvertToBlockNodePtr(child));
+      BuildFunctionDefinition(ConvertToBlockNodePtr(child));
       break;
     }
     case NodeKind::WhileStatement:
@@ -392,6 +391,7 @@ void Analyser::BuildBlock(BlockNodePtr const &block_node)
 
 void Analyser::BuildFile(BlockNodePtr const &file_node)
 {
+  filename_          = file_node->text;
   file_node->symbols = CreateSymbolTable();
   BuildBlock(file_node);
 }
@@ -445,8 +445,7 @@ void Analyser::BuildPersistentStatement(NodePtr const &node)
   state_definitions_.Add(state_name, instantation_type);
 }
 
-void Analyser::BuildFunctionDefinition(BlockNodePtr const &parent_block_node,
-                                       BlockNodePtr const &function_definition_node)
+void Analyser::BuildFunctionDefinition(BlockNodePtr const &function_definition_node)
 {
   function_definition_node->symbols = CreateSymbolTable();
   ExpressionNodePtr identifier_node =
@@ -509,7 +508,7 @@ void Analyser::BuildFunctionDefinition(BlockNodePtr const &parent_block_node,
     return;
   }
   FunctionGroupPtr fg;
-  SymbolPtr        symbol = parent_block_node->symbols->Find(name);
+  SymbolPtr        symbol = root_->symbols->Find(name);
   if (symbol)
   {
     fg = ConvertToFunctionGroupPtr(symbol);
@@ -524,7 +523,7 @@ void Analyser::BuildFunctionDefinition(BlockNodePtr const &parent_block_node,
   else
   {
     fg = CreateFunctionGroup(name);
-    parent_block_node->symbols->Add(fg);
+    root_->symbols->Add(fg);
   }
   FunctionPtr function =
       CreateUserDefinedFreeFunction(name, parameter_types, parameter_variables, return_type);
@@ -709,6 +708,7 @@ void Analyser::AnnotateBlock(BlockNodePtr const &block_node)
 
 void Analyser::AnnotateFile(BlockNodePtr const &file_node)
 {
+  filename_ = file_node->text;
   AnnotateBlock(file_node);
 }
 
