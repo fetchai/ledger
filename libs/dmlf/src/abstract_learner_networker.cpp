45ae8846908fadfffac4cf149b4aa07ef0ef1e57
Katie Lucas <katie.lucas@fetch.ai>
2019-10-03 12:41:38 +0100

    style fixes

---
+++ libs/dmlf/src/abstract_learner_networker.cpp
@@ -0,0 +1,69 @@
+#include "dmlf/abstract_learner_networker.hpp"
+
+namespace fetch {
+namespace dmlf {
+  void AbstractLearnerNetworker::PushUpdateType(const std::string & /*key*/,
+                                                const std::shared_ptr<UpdateInterface> & /*update*/)
+  {
+    // do nothing in the base case.
+  }
+
+  std::size_t AbstractLearnerNetworker::GetUpdateCount() const
+  {
+    Lock l{queue_m_};
+    ThrowIfNotInitialized();
+    return queue_->size();
+  }
+
+  void AbstractLearnerNetworker::SetShuffleAlgorithm(
+       const std::shared_ptr<ShuffleAlgorithmInterface> &alg)
+  {
+    alg_ = alg;
+  }
+
+  std::size_t AbstractLearnerNetworker::GetUpdateTypeCount(const std::string &key) const
+  {
+    Lock l{queue_map_m_};
+    auto iter = queue_map_.find(key);
+    if (iter != queue_map_.end())
+    {
+      return iter->second->size();
+    }
+    throw std::runtime_error{"Requesting UpdateCount for unregistered type"};
+  }
+
+  void AbstractLearnerNetworker::NewMessage(const Bytes &msg)
+  {
+    Lock l{queue_m_};
+    ThrowIfNotInitialized();
+    queue_->PushNewMessage(msg);
+  }
+
+  void AbstractLearnerNetworker::NewDmlfMessage(const Bytes &msg)
+  {
+    serializers::MsgPackSerializer serializer{msg};
+    std::string                    key;
+    Bytes                          update;
+
+    serializer >> key;
+    serializer >> update;
+
+    Lock l{queue_map_m_};
+    auto iter = queue_map_.find(key);
+    if (iter != queue_map_.end())
+    {
+      iter->second->PushNewMessage(update);
+      return;
+    }
+    throw std::runtime_error{"Received Update with a non-registered type"};
+  }
+
+  void AbstractLearnerNetworker::ThrowIfNotInitialized() const
+  {
+    if (!queue_)
+    {
+      throw std::runtime_error{"Learner is not initialized"};
+    }
+  }
+}  // namespace dmlf
+}  // namespace fetch
