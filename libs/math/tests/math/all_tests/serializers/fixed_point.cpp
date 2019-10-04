6a83280aaf4f2e95ebdad7e54cc3953fcaaa6a65
Cristina Yenyxe Gonzalez Garcia <cristina.gonzalez@fetch.ai>
2019-10-04 09:55:47 +0100

    Removed floating point loop counters from fixed-point tests

--- libs/math/tests/math/all_tests/serializers/fixed_point.cpp
+++ libs/math/tests/math/all_tests/serializers/fixed_point.cpp
@@ -21,6 +21,8 @@
 
 #include "gtest/gtest.h"
 
+#include <random>
+
 namespace fetch {
 namespace serializers {
 
@@ -41,9 +43,13 @@ TEST(FixedPointSerialisationTest, IntegerSerialisation)
 
 TEST(FixedPointSerialisationTest, DecimalSerialisation)
 {
-  for (float i(-10); i < 10; i += 0.12345f)
+  std::random_device random_device;
+  std::mt19937 generator(random_device());
+  std::uniform_real_distribution<> distribution(-100.0, 100.0);
+
+  for (int i(0); i < 100; ++i)
   {
-    fetch::fixed_point::FixedPoint<32, 32> a(i);
+    fetch::fixed_point::FixedPoint<32, 32> a(distribution(generator));
     fetch::serializers::MsgPackSerializer  b;
     b << a;
     b.seek(0);
