c75232b20e625bbb03c69b5da5ffc168131fc708
nhutton <nathan.hutton@fetch.ai>
2019-10-04 12:34:51 +0100

    style + lint

--- libs/beacon/include/beacon/beacon_manager.hpp
+++ libs/beacon/include/beacon/beacon_manager.hpp
@@ -57,7 +57,7 @@ public:
     Identity  identity;
   };
 
-  explicit BeaconManager(CertificatePtr cert = CertificatePtr{});
+  explicit BeaconManager(CertificatePtr certificate = CertificatePtr{});
 
   BeaconManager(BeaconManager const &) = delete;
   BeaconManager &operator=(BeaconManager const &) = delete;
