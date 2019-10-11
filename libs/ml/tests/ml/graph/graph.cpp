d81ebab878b89a2655da274a0d520baefb4c4820
Jiri Vestfal <jiri.vestfal@fetch.ai>
2019-10-11 12:17:46 +0100

    Shared layer tests

--- libs/ml/tests/ml/graph/graph.cpp
+++ libs/ml/tests/ml/graph/graph.cpp
@@ -416,6 +416,98 @@ TYPED_TEST(GraphTest, variable_freezing_subgraph)
                                         static_cast<DataType>(0)));
 }
 
+TYPED_TEST(GraphTest, variable_freezing_shared_layer)
+{
+  using TensorType = TypeParam;
+  using DataType   = typename TypeParam::Type;
+
+  // Dummy values
+  TensorType data = TensorType::FromString("1; -2; 3");
+  TensorType gt   = TensorType::FromString("1; -2; 3");
+
+  // Create a graph
+  fetch::ml::Graph<TensorType> g;
+
+  std::string input = g.template AddNode<fetch::ml::ops::PlaceHolder<TensorType>>("Input", {});
+  std::string label = g.template AddNode<fetch::ml::ops::PlaceHolder<TensorType>>("Label", {});
+  std::string layer_1 =
+      g.template AddNode<fetch::ml::layers::FullyConnected<TensorType>>("FC1", {"Input"}, 3u, 3u);
+  std::string layer_2 =
+      g.template AddNode<fetch::ml::layers::FullyConnected<TensorType>>("FC2", {"FC1"}, 3u, 3u);
+  std::string layer_3 =
+      g.template AddNode<fetch::ml::layers::FullyConnected<TensorType>>("FC1", {"FC2"});
+
+  // Add loss function
+  std::string error_output = g.template AddNode<fetch::ml::ops::MeanSquareErrorLoss<TypeParam>>(
+      "num_error", {layer_3, label});
+
+  g.Compile();
+
+  // Calculate Gradient
+  g.SetInput(input, data);
+  g.SetInput(label, gt);
+  TypeParam output = g.Evaluate(error_output);
+  g.BackPropagate(error_output);
+
+  // Freeze variables
+  g.SetFrozenState(layer_1, true);
+
+  // Get weights before applying gradient
+  auto weights_1 = g.GetWeights();
+
+  // Apply gradient
+  auto gradient_vector = g.GetGradients();
+  for (auto grad : gradient_vector)
+  {
+    grad.Fill(static_cast<DataType>(2.0));
+  }
+  g.ApplyGradients(gradient_vector);
+
+  // Get weights after applying gradient
+  auto weights_2 = g.GetWeights();
+
+  // Test if layer1 and copy of layer1 is frozen
+  ASSERT_TRUE(weights_1.at(0).AllClose(weights_2.at(0), static_cast<DataType>(0),
+                                       static_cast<DataType>(0)));
+  ASSERT_TRUE(weights_1.at(1).AllClose(weights_2.at(1), static_cast<DataType>(0),
+                                       static_cast<DataType>(0)));
+  ASSERT_TRUE(weights_1.at(2).AllClose(weights_2.at(2), static_cast<DataType>(0),
+                                       static_cast<DataType>(0)));
+  ASSERT_TRUE(weights_1.at(3).AllClose(weights_2.at(3), static_cast<DataType>(0),
+                                       static_cast<DataType>(0)));
+  ASSERT_FALSE(weights_1.at(4).AllClose(weights_2.at(4), static_cast<DataType>(0),
+                                        static_cast<DataType>(0)));
+  ASSERT_FALSE(weights_1.at(5).AllClose(weights_2.at(5), static_cast<DataType>(0),
+                                        static_cast<DataType>(0)));
+
+  // Un-freeze variables
+  g.SetFrozenState(layer_1, false);
+
+  // Apply gradient
+  auto gradient_vector_2 = g.GetGradients();
+  for (auto grad : gradient_vector_2)
+  {
+    grad.Fill(static_cast<DataType>(2.0));
+  }
+  g.ApplyGradients(gradient_vector);
+
+  auto weights_3 = g.GetWeights();
+
+  // Test if everything is unfrozen
+  ASSERT_FALSE(weights_2.at(0).AllClose(weights_3.at(0), static_cast<DataType>(0),
+                                        static_cast<DataType>(0)));
+  ASSERT_FALSE(weights_2.at(1).AllClose(weights_3.at(1), static_cast<DataType>(0),
+                                        static_cast<DataType>(0)));
+  ASSERT_FALSE(weights_2.at(2).AllClose(weights_3.at(2), static_cast<DataType>(0),
+                                        static_cast<DataType>(0)));
+  ASSERT_FALSE(weights_2.at(3).AllClose(weights_3.at(3), static_cast<DataType>(0),
+                                        static_cast<DataType>(0)));
+  ASSERT_FALSE(weights_2.at(4).AllClose(weights_3.at(4), static_cast<DataType>(0),
+                                        static_cast<DataType>(0)));
+  ASSERT_FALSE(weights_2.at(5).AllClose(weights_3.at(5), static_cast<DataType>(0),
+                                        static_cast<DataType>(0)));
+}
+
 TYPED_TEST(GraphTest,
            diamond_graph_forward)  // Evaluate graph output=(input1*input2)-(input1^2)
 {
