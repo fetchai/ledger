52fd2a32bc1ab9c319088e16fbd5820683f8d017
Katie Lucas <katie.lucas@fetch.ai>
2019-10-15 11:38:20 +0100

    Style it.

--- libs/core/tests/serializers/variants.cpp
+++ libs/core/tests/serializers/variants.cpp
@@ -1,3 +1,21 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
 //
 //   Copyright 2018-2019 Fetch.AI Limited
 //
@@ -34,9 +52,9 @@ TEST(MsgPacker, VariantInt)
 
   {
     fetch::serializers::MsgPackSerializer stream;
-    fetch::variant::Variant i1, i2;
+    fetch::variant::Variant               i1, i2;
 
-    i1 = 123456;
+    i1     = 123456;
     stream = fetch::serializers::MsgPackSerializer();
     stream << i1;
     stream.seek(0);
@@ -48,9 +66,9 @@ TEST(MsgPacker, VariantFloat)
 {
   {
     fetch::serializers::MsgPackSerializer stream;
-    fetch::variant::Variant fp1, fp2;
+    fetch::variant::Variant               fp1, fp2;
 
-    fp1 = 1.25; // should be repn exactly.
+    fp1    = 1.25;  // should be repn exactly.
     stream = fetch::serializers::MsgPackSerializer();
     stream << fp1;
     stream.seek(0);
@@ -62,9 +80,9 @@ TEST(MsgPacker, VariantString)
 {
   {
     fetch::serializers::MsgPackSerializer stream;
-    fetch::variant::Variant s1, s2;
+    fetch::variant::Variant               s1, s2;
 
-    s1 = "123456";
+    s1     = "123456";
     stream = fetch::serializers::MsgPackSerializer();
     stream << s1;
     stream.seek(0);
@@ -76,9 +94,9 @@ TEST(MsgPacker, VariantNull)
 {
   {
     fetch::serializers::MsgPackSerializer stream;
-    fetch::variant::Variant null1, null2;
+    fetch::variant::Variant               null1, null2;
 
-    null1 = fetch::variant::Variant::Null();
+    null1  = fetch::variant::Variant::Null();
     stream = fetch::serializers::MsgPackSerializer();
     stream << null1;
     stream.seek(0);
@@ -90,14 +108,14 @@ TEST(MsgPacker, VariantArray)
 {
   {
     fetch::serializers::MsgPackSerializer stream;
-    fetch::variant::Variant arr1, arr2;
+    fetch::variant::Variant               arr1, arr2;
 
-    arr1 = fetch::variant::Variant::Array(4);
-    arr1[0]=fetch::variant::Variant::Null();
-    arr1[1]=123456;
-    arr1[2]=1.25;
-    arr1[3]="123456";
-    stream = fetch::serializers::MsgPackSerializer();
+    arr1    = fetch::variant::Variant::Array(4);
+    arr1[0] = fetch::variant::Variant::Null();
+    arr1[1] = 123456;
+    arr1[2] = 1.25;
+    arr1[3] = "123456";
+    stream  = fetch::serializers::MsgPackSerializer();
     stream << arr1;
     stream.seek(0);
     stream >> arr2;
@@ -113,19 +131,19 @@ TEST(MsgPacker, VariantArrayOfArray)
 
   {
     fetch::serializers::MsgPackSerializer stream;
-    fetch::variant::Variant arr1, arr2;
+    fetch::variant::Variant               arr1, arr2;
 
-    arr1 = fetch::variant::Variant::Array(4);
-    arr1[0]=fetch::variant::Variant::Array(4);
-    arr1[1]=fetch::variant::Variant::Array(4);
-    arr1[2]=fetch::variant::Variant::Array(4);
-    arr1[3]=fetch::variant::Variant::Array(4);
+    arr1    = fetch::variant::Variant::Array(4);
+    arr1[0] = fetch::variant::Variant::Array(4);
+    arr1[1] = fetch::variant::Variant::Array(4);
+    arr1[2] = fetch::variant::Variant::Array(4);
+    arr1[3] = fetch::variant::Variant::Array(4);
 
-    for(std::size_t i=0;i<4;i++)
+    for (std::size_t i = 0; i < 4; i++)
     {
-      for(std::size_t j=0;j<4;j++)
+      for (std::size_t j = 0; j < 4; j++)
       {
-        arr1[i][j] = (i==j)?1:0;
+        arr1[i][j] = (i == j) ? 1 : 0;
       }
     }
 
@@ -135,12 +153,12 @@ TEST(MsgPacker, VariantArrayOfArray)
     stream >> arr2;
 
     EXPECT_EQ(arr2.IsArray(), true);
-    for(std::size_t i=0;i<4;i++)
+    for (std::size_t i = 0; i < 4; i++)
     {
       EXPECT_EQ(arr2[i].IsArray(), true);
-      for(std::size_t j=0;j<4;j++)
+      for (std::size_t j = 0; j < 4; j++)
       {
-        EXPECT_EQ(arr2[i][j].As<int>(),  (i==j)?1:0);
+        EXPECT_EQ(arr2[i][j].As<int>(), (i == j) ? 1 : 0);
       }
     }
   }
@@ -149,12 +167,12 @@ TEST(MsgPacker, VariantObject)
 {
   {
     fetch::serializers::MsgPackSerializer stream;
-    fetch::variant::Variant obj1, obj2;
+    fetch::variant::Variant               obj1, obj2;
 
     obj1 = fetch::variant::Variant::Object();
 
-    obj1["foo"]=1;
-    obj1["bar"]=2;
+    obj1["foo"] = 1;
+    obj1["bar"] = 2;
 
     stream = fetch::serializers::MsgPackSerializer();
     stream << obj1;
@@ -162,7 +180,7 @@ TEST(MsgPacker, VariantObject)
     stream >> obj2;
 
     EXPECT_EQ(obj2.IsObject(), true);
-    EXPECT_EQ(obj2["foo"].As<int>(),  1);
-    EXPECT_EQ(obj2["bar"].As<int>(),  2);
+    EXPECT_EQ(obj2["foo"].As<int>(), 1);
+    EXPECT_EQ(obj2["bar"].As<int>(), 2);
   }
 }
