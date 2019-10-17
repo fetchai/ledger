a486600ed505b2469129f8fa92429d969b9fe997
Lokman Rahmani <lokman.rahmani@fetch.ai>
2019-10-17 17:17:30 +0100

    Add back muddle-chatter app from Katie's repo

---
+++ apps/dmlf-muddle-chatter/launcher.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+# pip3 install fetchai-ledger-api
+
+import sys
+import json
+import subprocess
+
+from fetchai.ledger.crypto import Entity, Address
+
+
+def main():
+
+    application_binary, count = tuple(sys.argv[1:])
+
+    idents = [
+        (x, Entity())
+        for x
+        in range(0, int(count))
+    ]
+    peers = [
+        {
+            "uri": "tcp://127.0.0.1:{}".format(8000 + instance_number),
+            "key": ident.private_key,
+            "pub": ident.public_key,
+        }
+        for instance_number, ident
+        in idents
+    ]
+    config = {
+        "peers": peers
+    }
+
+    config_str = json.dumps(config)
+
+    process_lines = [
+        [
+            application_binary,
+            config_str,
+            str(instance_number),
+        ]
+        for instance_number
+        in range(0, len(peers))
+    ]
+
+    processes = [
+        subprocess.Popen(pl)
+        for pl
+        in process_lines
+    ]
+
+    results = [
+        x.communicate()
+        for x
+        in processes
+    ]
+
+    if all([process.returncode == 0 for process in processes]):
+        print("ok")
+    else:
+        print("failed")
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ apps/dmlf-muddle-chatter/main.cpp
@@ -0,0 +1,109 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2019 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "core/byte_array/const_byte_array.hpp"
+#include "core/byte_array/decoders.hpp"
+#include "core/byte_array/encoders.hpp"
+#include "core/commandline/parameter_parser.hpp"
+#include "core/serializers/main_serializer.hpp"
+#include "dmlf/networkers/filepassing_learner_networker.hpp"
+#include "dmlf/networkers/local_learner_networker.hpp"
+#include "dmlf/networkers/muddle_learner_networker.hpp"
+#include "dmlf/simple_cycling_algorithm.hpp"
+#include "dmlf/update.hpp"
+#include "dmlf/update_interface.hpp"
+#include "json/document.hpp"
+#include "math/matrix_operations.hpp"
+#include "math/tensor.hpp"
+#include "variant/variant.hpp"
+
+#include <chrono>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <ostream>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using DataType             = fetch::fixed_point::FixedPoint<32, 32>;
+using TensorType           = fetch::math::Tensor<DataType>;
+using UpdateTypeForTesting = fetch::dmlf::Update<TensorType>;
+
+class Learner
+{
+public:
+  std::shared_ptr<fetch::dmlf::MuddleLearnerNetworker>   actual;
+  std::shared_ptr<fetch::dmlf::AbstractLearnerNetworker> interface;
+
+  Learner(const std::string &cloud_config, std::size_t instance_number)
+  {
+    actual = std::make_shared<fetch::dmlf::MuddleLearnerNetworker>(cloud_config, instance_number);
+    interface = actual;
+    interface->Initialize<UpdateTypeForTesting>();
+  }
+
+  void PretendToLearn()
+  {
+    static int sequence_number = 1;
+    auto       t               = TensorType(TensorType::SizeType(2));
+    t.Fill(DataType(sequence_number++));
+    auto r = std::vector<TensorType>();
+    r.push_back(t);
+    interface->PushUpdate(std::make_shared<UpdateTypeForTesting>(r));
+  }
+};
+using MuddleLearnerNetworker = fetch::dmlf::MuddleLearnerNetworker;
+
+int main(int /*argc*/, char **argv)
+{
+  auto config          = std::string(argv[1]);
+  auto instance_number = std::atoi(argv[2]);
+
+  std::cout << "CONFIG:" << config << std::endl;
+
+  auto learner = std::make_shared<Learner>(config, instance_number);
+
+  sleep(1);
+  if (instance_number == 0)
+  {
+    learner->PretendToLearn();
+  }
+
+  sleep(1);
+
+  int r;
+  if (std::size_t(instance_number) == learner->actual->GetUpdateCount())
+  {
+    std::cout << "yes" << std::endl;
+    r = 0;
+  }
+  else
+  {
+    std::cout << "no" << std::endl;
+    r = 1;
+  }
+
+  return r;
+}
