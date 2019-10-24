d23cdea66bc3398f1ff20a081b5a22ff7eb87c08
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-10-24 18:53:09 +0100

    Correct beacon network port name

--- libs/constellation/src/constellation.cpp
+++ libs/constellation/src/constellation.cpp
@@ -493,7 +493,12 @@ void Constellation::Run(UriSet const &initial_peers, core::WeakRunnable bootstra
   // beacon network
   if (beacon_network_)
   {
-    beacon_network_->Start({LookupLocalPort(cfg_.manifest, ServiceIdentifier::Type::DKG)});
+    uint16_t const beacon_bind_port = LookupLocalPort(cfg_.manifest, ServiceIdentifier::Type::DKG);
+    uint16_t const beacon_ext_port = LookupRemotePort(cfg_.manifest, ServiceIdentifier::Type::DKG);
+
+    muddle::MuddleInterface::PortMapping const beacon_port_mapping{{beacon_bind_port, beacon_ext_port}};
+
+    beacon_network_->Start({}, beacon_port_mapping);
   }
 
   // BEFORE the block coordinator starts its state set up special genesis
