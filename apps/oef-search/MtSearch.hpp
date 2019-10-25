e909a74e810bb1972b92f758fb8494a0f714f0a5
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-10-25 14:30:36 +0100

    Add missing binary targets for OEF

---
+++ apps/oef-search/MtSearch.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <set>
+
+#include "oef-base/comms/Core.hpp"
+#include "oef-base/comms/OefListenerSet.hpp"
+#include "oef-base/conversation/OutboundConversations.hpp"
+#include "oef-base/threading/Taskpool.hpp"
+#include "oef-base/threading/Threadpool.hpp"
+#include "oef-base/comms/IOefTaskFactory.hpp"
+#include "logging/logging.hpp"
+
+#include "oef-messages/search_config.hpp"
+#include "oef-search/functions/SearchTaskFactory.hpp"
+#include "oef-search/dap_manager/DapStore.hpp"
+#include "oef-search/dap_manager/DapManager.hpp"
+#include "oef-search/search_comms/SearchPeerStore.hpp"
+#include "oef-search/comms/IAddSearchPeer.hpp"
+
+class Core;
+class OefSearchEndpoint;
+
+class MtSearch : public IAddSearchPeer, public std::enable_shared_from_this<MtSearch>
+{
+public:
+  
+  static constexpr char const *LOGGING_NAME = "MtSearch";
+
+  MtSearch() = default;
+  virtual ~MtSearch() = default;
+
+  bool configure(const std::string &config_file="", const std::string &config_json="");
+  int run();
+
+  void AddPeer(const std::string&) override;
+
+protected:
+private:
+  std::shared_ptr<Core> core;
+  std::shared_ptr<Taskpool> tasks;
+  std::shared_ptr<OefListenerSet<IOefTaskFactory<OefSearchEndpoint>, OefSearchEndpoint>> listeners;
+  std::shared_ptr<OutboundConversations> outbounds;
+  std::shared_ptr<DapStore> dap_store_;
+  std::shared_ptr<DapManager> dap_manager_;
+  std::shared_ptr<SearchPeerStore> search_peer_store_;
+  fetch::oef::pb::SearchConfig config_;
+
+  Threadpool comms_runners;
+  Threadpool tasks_runners;
+
+  void startListeners();
+  bool configureFromJsonFile(const std::string &config_file);
+  bool configureFromJson(const std::string &config_json);
+
+
+  MtSearch(const MtSearch &other) = delete;
+  MtSearch &operator=(const MtSearch &other) = delete;
+  bool operator==(const MtSearch &other) = delete;
+  bool operator<(const MtSearch &other) = delete;
+};
---
+++ apps/oef-search/main.cpp
@@ -0,0 +1,37 @@
+#include "MtSearch.hpp"
+
+
+int main(int argc, char *argv[])
+{
+  // parse args
+
+  std::string config_file;
+
+  if (argc != 2)
+  {
+    FETCH_LOG_ERROR("MTCoreApp", "Failed to run binary, because exactly 1 argument ",
+                    "(path to config file) should be passed!");
+    return -1;
+  }
+
+  config_file = argv[1];
+
+  auto mtSearch = std::make_shared<MtSearch>();
+
+  if (config_file.empty())
+  {
+    FETCH_LOG_WARN("MAIN", "Configuration not provided!");
+    return 1;
+  }
+
+
+  if (!mtSearch->configure(config_file, ""))
+  {
+    FETCH_LOG_WARN("MAIN", "Configuration failed, shutting down...");
+    return 1;
+  }
+
+  mtSearch->run();
+
+  return 0;
+}
