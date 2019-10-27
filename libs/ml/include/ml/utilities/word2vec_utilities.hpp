1f750f0a08bae1a0c4c0460d187e099a82b84f58
Jiri Vestfal <45360788+MissingNO57@users.noreply.github.com>
2019-10-27 21:27:12 +0000

    Distributed networking clients examples (#1835)
    * vocab transfering using dmlf update
    * Moved some missed functions outside class definition
    * Code cleanup
    * Namespace corrected for dmlf ditributed learning and moved utilities to reduce reusing
    * Added comments

--- libs/ml/include/ml/utilities/word2vec_utilities.hpp
+++ libs/ml/include/ml/utilities/word2vec_utilities.hpp
@@ -270,6 +270,11 @@ void TestEmbeddings(Graph<TensorType> const &g, std::string const &skip_gram_nam
 inline std::string ReadFile(std::string const &path)
 {
   std::ifstream t(path);
+  if (t.fail())
+  {
+    throw ml::exceptions::InvalidFile("Cannot open file " + path);
+  }
+
   return std::string((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
 }
 
