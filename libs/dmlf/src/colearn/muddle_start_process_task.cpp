c40f772230b00bda8f752486b4575a8dfdcb6ec1
Katie Lucas <katie.lucas@fetch.ai>
2019-11-07 15:08:58 +0000

    unbreaking tasks

--- libs/dmlf/src/colearn/muddle_start_process_task.cpp
+++ libs/dmlf/src/colearn/muddle_start_process_task.cpp
@@ -1,14 +1,10 @@
 #include "dmlf/colearn/muddle_start_process_task.hpp"
 
 namespace fetch {
+namespace dmlf {
 namespace colearn {
-  
-//std::ostream& operator<<(std::ostream& os, const MuddleStartProcessTask &output) {}
 
-//void swap(MuddleStartProcessTask& v1, MuddleStartProcessTask& v2) {
-//    v1.swap(v2);
-//}
-
+}
 }
 }
 
--- libs/dmlf/src/colearn/muddle_update_task.cpp
+++ libs/dmlf/src/colearn/muddle_update_task.cpp
@@ -1,14 +1,10 @@
 #include "dmlf/colearn/muddle_update_task.hpp"
 
 namespace fetch {
+namespace dmlf {
 namespace colearn {
-  
-//std::ostream& operator<<(std::ostream& os, const MuddleUpdateTask &output) {}
 
-//void swap(MuddleUpdateTask& v1, MuddleUpdateTask& v2) {
-//    v1.swap(v2);
-//}
-
+}
 }
 }
 
