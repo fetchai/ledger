7331d192da4f2b4e041be04c353b60d2f07f9227
Katie Lucas <katie.lucas@fetch.ai>
2019-11-07 15:10:53 +0000

    unbreaking tasks

--- libs/dmlf/src/colearn/muddle_message_task.cpp
+++ libs/dmlf/src/colearn/muddle_message_task.cpp
@@ -1,14 +1,10 @@
 #include "dmlf/colearn/muddle_message_task.hpp"
 
 namespace fetch {
+namespace dmlf {
 namespace colearn {
-  
-//std::ostream& operator<<(std::ostream& os, const MuddleMessageTask &output) {}
 
-//void swap(MuddleMessageTask& v1, MuddleMessageTask& v2) {
-//    v1.swap(v2);
-//}
-
+}
 }
 }
 
