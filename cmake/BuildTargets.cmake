7230a7fefe18e13cb4366b3add29ef8d5c447582
nhutton <nathan.hutton@fetch.ai>
2019-11-08 21:50:41 +0000

    disable for apple builds (oef)

--- cmake/BuildTargets.cmake
+++ cmake/BuildTargets.cmake
@@ -309,6 +309,15 @@ function (configure_library_targets)
 
   file(GLOB children RELATIVE ${library_root} ${library_root}/*)
 
+  if (APPLE)
+    # Temporarily disable oef builds
+    list(FILTER
+         children
+         EXCLUDE
+         REGEX
+         ".*oef.*")
+  endif (APPLE)
+
   set(dirlist "")
   foreach (child ${children})
     set(element_path ${library_root}/${child})
