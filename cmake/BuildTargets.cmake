9e66b45d9f1e8db9fc3edb550f5ea56e38fe9899
Attila Bagoly <b.attila.93@gmail.com>
2019-11-08 17:06:24 +0000

    Re-enable oef build

--- cmake/BuildTargets.cmake
+++ cmake/BuildTargets.cmake
@@ -309,13 +309,6 @@ function (configure_library_targets)
 
   file(GLOB children RELATIVE ${library_root} ${library_root}/*)
 
-  # Temporarily disable oef builds
-  list(FILTER
-       children
-       EXCLUDE
-       REGEX
-       ".*oef.*")
-
   set(dirlist "")
   foreach (child ${children})
     set(element_path ${library_root}/${child})
