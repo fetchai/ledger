79913ad3384b68bfaa1a1148ea21e0d22b4a08f9
nhutton <nathan.hutton@fetch.ai>
2019-11-12 15:03:08 +0000

    minor update to main chain sync

--- libs/ledger/src/protocols/main_chain_rpc_service.cpp
+++ libs/ledger/src/protocols/main_chain_rpc_service.cpp
@@ -462,7 +462,7 @@ MainChainRpcService::State MainChainRpcService::OnSynchronised(State current, St
   MainChain::BlockPtr head_of_chain = chain_.GetHeaviestBlock();
   uint64_t const seconds_since_last_block = GetTime(fetch::moment::GetClock("default", fetch::moment::ClockType::SYSTEM)) - head_of_chain->timestamp;
 
-  if(seconds_since_last_block > 100)
+  if(seconds_since_last_block > 100 && head_of_chain->block_number != 0)
   {
     FETCH_LOG_INFO(LOGGING_NAME, "Synchronisation appears to be lost - chain is old.");
     state_machine_->Delay(std::chrono::milliseconds{1000});
