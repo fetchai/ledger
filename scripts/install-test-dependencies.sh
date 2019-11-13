fbf4151f2757f1f85ed3081a0a621f9c506df810
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-11-13 13:39:25 +0000

    Update to interim version for malablity fix

--- scripts/install-test-dependencies.sh
+++ scripts/install-test-dependencies.sh
@@ -2,5 +2,6 @@
 set -e
 
 pip3 install --no-cache-dir --ignore-installed --force-reinstall wheel pyyaml requests
-pip3 install --no-cache-dir --ignore-installed --force-reinstall fetchai-ledger-api==0.10.0a3
+#pip3 install --no-cache-dir --ignore-installed --force-reinstall fetchai-ledger-api==0.10.0a3
+pip3 install --no-cache-dir --ignore-installed --force-reinstall git+https://github.com/fetchai/ledger-api-py.git@a17e3160673636494f12eb60e115b6ca520de44d#egg=fetchai-ledger-api
 pip3 install --no-cache-dir --ignore-installed --force-reinstall ./scripts/fetchai_netutils
