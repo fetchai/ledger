8a2a973bdfccfd137697af89a82c9f2c39513eb3
Stefanos Malliaros <stefmalli89@gmail.com>
2019-11-15 13:25:19 +0000

    Applied code style

--- scripts/end_to_end_test/smart_contract_tests/malleability_test.py
+++ scripts/end_to_end_test/smart_contract_tests/malleability_test.py
@@ -44,8 +44,10 @@ def run(options):
     mal_tx = 'a1440000c5ab20e3ab845cb4a1d2c3e4c3b08f5ff42a6ff2a71d7697ba8f32c415b77c7f8d850b3ef025b189a2d9bb4a515a84c3673db6d3ef25385d2c8d1e34b06e2de1c0fac08501140000000000000000040bafbc61a08524372f495d9dee08adbc39824e980506947091395cece16636ddc094b9d409d5b34ef0bbd9c99c5caf21fc373802472cf96a8a280f84e833f99240f1a0311cc9b42edaa41d537e3d1f9d54ac159504276c27be2973cea85c8e8a17148a2b1dc3e188f911c6b05f163d95b964ae594a347870973e59b3c93d35b895'
 
     # Creating jsons for the above mentioned transactions
-    legit_trans = submit_json_transaction(host="localhost", port=8000, tx_data=dict(ver="1.2", data=base64.b64encode(binascii.unhexlify(orig_tx)).decode()), endpoint=ENDPOINT)
-    mal_trans = submit_json_transaction(host="localhost", port=8000, tx_data=dict(ver="1.2", data=base64.b64encode(binascii.unhexlify(mal_tx)).decode()), endpoint=ENDPOINT)
+    legit_trans = submit_json_transaction(host="localhost", port=8000, tx_data=dict(
+        ver="1.2", data=base64.b64encode(binascii.unhexlify(orig_tx)).decode()), endpoint=ENDPOINT)
+    mal_trans = submit_json_transaction(host="localhost", port=8000, tx_data=dict(
+        ver="1.2", data=base64.b64encode(binascii.unhexlify(mal_tx)).decode()), endpoint=ENDPOINT)
 
     # Sending the transactions to the ledger
     assert legit_trans == mal_trans, "Malleable transactions have different transaction hash"
@@ -53,4 +55,5 @@ def run(options):
 
     # If transaction malleability is feasible, id2 should have 500 FET.
     # If balance of id2 is more than 250 raise an exception
-    assert api.tokens.balance(id2) == 250, "Vulnerable to transaction malleability attack"
+    assert api.tokens.balance(
+        id2) == 250, "Vulnerable to transaction malleability attack"
