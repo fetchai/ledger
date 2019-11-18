0369cbd5b0716c7a142c87607ce147f2a2f8fc19
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-11-18 18:03:14 +0000

    Add extra multiple of block size check

--- libs/crypto/tests/unit/aes_tests.cpp
+++ libs/crypto/tests/unit/aes_tests.cpp
@@ -50,4 +50,25 @@ TEST(AesTests, BasicAes256CbcTest)
   ASSERT_EQ(plain_text, recovered_text);
 }
 
+TEST(AesTests, ExactMultipleOfBlockSize)
+{
+  // define the inputs to the object
+  ConstByteArray plain_text = "The quick brown fox jumps over the lazy dog.....";
+  ConstByteArray key = FromHex("0102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F20");
+  ConstByteArray iv  = FromHex("0102030405060708090A0B0C0D0E0F10");
+
+  // check the message size is as expected
+  ASSERT_EQ(plain_text.size() % 16, 0);
+
+  // encrypt the plain text
+  ConstByteArray cipher_text{};
+  ASSERT_TRUE(AesBlockCipher::Encrypt(BlockCipher::AES_256_CBC, key, iv, plain_text, cipher_text));
+
+  ConstByteArray recovered_text{};
+  ASSERT_TRUE(
+      AesBlockCipher::Decrypt(BlockCipher::AES_256_CBC, key, iv, cipher_text, recovered_text));
+
+  ASSERT_EQ(plain_text, recovered_text);
+}
+
 }  // namespace
