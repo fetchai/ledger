7c3082940e5610624acaa37f09f4a191136db526
Volodymyr Havrylov <volodymyr.havrylov@n-cube.co.uk>
2019-11-19 22:41:20 +0200

    Codestyle correction

--- libs/vm/src/generator.cpp
+++ libs/vm/src/generator.cpp
@@ -1,4 +1,3 @@
-
 //------------------------------------------------------------------------------
 //
 //   Copyright 2018-2019 Fetch.AI Limited
@@ -1308,9 +1307,9 @@ void Generator::HandleInteger64(IRExpressionNodePtr const &node)
 void Generator::HandleUnsignedInteger64(IRExpressionNodePtr const &node)
 {
   Executable::Instruction instruction(Opcodes::PushConstant);
-  auto value        = static_cast<uint64_t>(std::stoull(node->text));
-  instruction.index = AddConstant(Variant(value, TypeIds::UInt64));
-  uint16_t pc       = function_->AddInstruction(instruction);
+  auto                    value = static_cast<uint64_t>(std::stoull(node->text));
+  instruction.index             = AddConstant(Variant(value, TypeIds::UInt64));
+  uint16_t pc                   = function_->AddInstruction(instruction);
   AddLineNumber(node->line, pc);
 }
 
