87172b6030e3fffff9c8d9de6cb0817cbadb5ff8
n-hutton <nathan.cameron.hutton@gmail.com>
2019-11-21 10:11:29 +0000

    Avoid unneccessary python backtraces when running ci tool (#1998)

--- scripts/fetchai_netutils/fetch/testing/testcase.py
+++ scripts/fetchai_netutils/fetch/testing/testcase.py
@@ -5,6 +5,7 @@ import datetime
 import glob
 import shutil
 import subprocess
+from pathlib import Path
 
 from fetch.cluster.instance import ConstellationInstance, DmlfEtchInstance
 from fetch.cluster.utils import output, verify_file
