37d8604cd008c1bc4f26afc8842d5c3c1e04ba70
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-11-27 23:34:27 +0000

    Auto style apply

--- libs/ledger/tests/unit/storage/transaction_memory_pool_tests.cpp
+++ libs/ledger/tests/unit/storage/transaction_memory_pool_tests.cpp
@@ -14,7 +14,7 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 //
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
 #include "chain/transaction.hpp"
 #include "chain/transaction_builder.hpp"
