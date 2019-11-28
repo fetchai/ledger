808c11199c039a1d7e1165f453e244c956f05dd1
jinmannwong <jinmannwong@gmail.com>
2019-11-28 11:57:05 +0000

    Stop collection of stutter notarisations after removal

--- libs/ledger/include/ledger/protocols/notarisation_service.hpp
+++ libs/ledger/include/ledger/protocols/notarisation_service.hpp
@@ -169,6 +169,8 @@ private:
   uint64_t notarised_chain_height_{0};          ///< Current highest notarised block number in chain
   uint64_t notarisation_collection_height_{0};  ///< Block number current collecting signatures for
   static const uint32_t cutoff_ = 2;            ///< Number of blocks behind
+  std::unordered_set<BlockHash>
+      stutter_blocks_;  ///< Set of stutter blocks to not collect notarisations for
   /// @}
 };
 }  // namespace ledger
