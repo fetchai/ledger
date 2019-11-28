ffc491a680f8813d3b0ea949d7093075aadd3b24
Khan Baykaner <khan.baykaner@fetch.ai>
2019-11-28 18:18:48 +0000

    bugfixed + character handling by tensor

--- libs/math/include/math/tensor.hpp
+++ libs/math/include/math/tensor.hpp
@@ -714,7 +714,6 @@ Tensor<T, C> Tensor<T, C>::FromString(byte_array::ConstByteArray const &c)
     default:
       if (byte_array::consumers::NumberConsumer<1, 2>(c, i) == -1)
       {
-        std::cout << "c[i]: " << c[i] << std::endl;
         throw exceptions::InvalidNumericCharacter("invalid character used in string to set tensor");
       }
       else
