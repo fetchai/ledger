2e2a8d5237b307e2c3465cdd55843cfb5b2b8a9d
Konstantinos Margaritis <markos@freevec.org>
2019-12-03 09:16:05 +0200

    initial Abs() vectorisation attempt

--- libs/vectorise/include/vectorise/arch/avx2/math/abs.hpp
+++ libs/vectorise/include/vectorise/arch/avx2/math/abs.hpp
@@ -23,32 +23,76 @@
 namespace fetch {
 namespace vectorise {
 
-inline VectorRegister<float, 256> abs(VectorRegister<float, 256> const &a)
+inline VectorRegister<int32_t, 128> Abs(VectorRegister<int32_t, 128> const &a)
 {
-  const __m256 sign = _mm256_castsi256_ps(_mm256_set1_epi32(1 << 31));
-  auto const   ret  = VectorRegister<float, 256>(_mm256_andnot_ps(sign, a.data()));
+  auto const ret = VectorRegister<int32_t, 128>(_mm_abs_epi32(a.data()));
   return ret;
 }
 
-inline VectorRegister<float, 128> abs(VectorRegister<float, 128> const &a)
+inline VectorRegister<int32_t, 256> Abs(VectorRegister<int32_t, 256> const &a)
 {
-  const __m128 sign = _mm_castsi128_ps(_mm_set1_epi32(1 << 31));
-  auto const   ret  = VectorRegister<float, 128>(_mm_andnot_ps(sign, a.data()));
+  auto const ret = VectorRegister<int32_t, 256>(_mm256_abs_epi32(a.data()));
   return ret;
 }
 
-inline VectorRegister<double, 256> abs(VectorRegister<double, 256> const &a)
+inline VectorRegister<int64_t, 128> Abs(VectorRegister<int64_t, 128> const &a)
 {
-  const __m256d mask = _mm256_castsi256_pd(_mm256_set1_epi64x(std::numeric_limits<int64_t>::max()));
-  auto const    ret  = VectorRegister<double, 256>(_mm256_and_pd(mask, a.data()));
+  auto const ret  = VectorRegister<int64_t, 128>(_mm_abs_epi64(a.data()));
   return ret;
 }
 
-inline VectorRegister<double, 128> abs(VectorRegister<double, 128> const &a)
+inline VectorRegister<int64_t, 256> Abs(VectorRegister<int64_t, 256> const &a)
+{
+  auto const ret  = VectorRegister<int64_t, 256>(_mm256_abs_epi64(a.data()));
+  return ret;
+}
+
+inline VectorRegister<fixed_point::fp32_t, 128> Abs(VectorRegister<fixed_point::fp32_t, 128> const &a)
+{
+  VectorRegister<int32_t, 128> abs_int32 = Abs(VectorRegister<int32_t, 128>(a.data()));
+  return {abs_int32.data()};
+}
+
+inline VectorRegister<fixed_point::fp32_t, 256> Abs(VectorRegister<fixed_point::fp32_t, 256> const &a)
+{
+  VectorRegister<int32_t, 256> abs_int32 = Abs(VectorRegister<int32_t, 256>(a.data()));
+  return {abs_int32.data()};
+}
+
+inline VectorRegister<fixed_point::fp64_t, 128> Abs(VectorRegister<fixed_point::fp64_t, 128> const &a)
+{
+  VectorRegister<int64_t, 128> abs_int64 = Abs(VectorRegister<int64_t, 128>(a.data()));
+  return {abs_int64.data()};
+}
+
+inline VectorRegister<fixed_point::fp64_t, 256> Abs(VectorRegister<fixed_point::fp64_t, 256> const &a)
+{
+  VectorRegister<int64_t, 256> abs_int64 = Abs(VectorRegister<int64_t, 256>(a.data()));
+  return {abs_int64.data()};
+}
+
+inline VectorRegister<float, 128> Abs(VectorRegister<float, 128> const &a)
+{
+  const __m128 sign = _mm_castsi128_ps(_mm_set1_epi32(std::numeric_limits<int32_t>::max()));
+  return {_mm_andnot_ps(sign, a.data())};
+}
+
+inline VectorRegister<float, 256> Abs(VectorRegister<float, 256> const &a)
+{
+  const __m256 sign = _mm256_castsi256_ps(_mm256_set1_epi32(std::numeric_limits<int32_t>::max()));
+  return {_mm256_andnot_ps(sign, a.data())};
+}
+
+inline VectorRegister<double, 128> Abs(VectorRegister<double, 128> const &a)
 {
   const __m128d mask = _mm_castsi128_pd(_mm_set1_epi64x(std::numeric_limits<int64_t>::max()));
-  auto const    ret  = VectorRegister<double, 128>(_mm_and_pd(mask, a.data()));
-  return ret;
+  return {_mm_and_pd(mask, a.data())};
+}
+
+inline VectorRegister<double, 256> Abs(VectorRegister<double, 256> const &a)
+{
+  const __m256d mask = _mm256_castsi256_pd(_mm256_set1_epi64x(std::numeric_limits<int64_t>::max()));
+  return {_mm256_and_pd(mask, a.data())};
 }
 
 }  // namespace vectorise
