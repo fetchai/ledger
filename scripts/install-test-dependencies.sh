3aeee0a8d6754c7ddba515703eae3ee5614ec018
nhutton <nathan.hutton@fetch.ai>
2019-12-04 16:49:20 +0000

    force removal of python api to guarantee version install

--- scripts/install-test-dependencies.sh
+++ scripts/install-test-dependencies.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+pip3 uninstall -y fetchai-ledger-api
+
 pip3 install --no-cache-dir --ignore-installed --force-reinstall wheel pyyaml requests
 pip3 install --no-cache-dir --ignore-installed --force-reinstall fetchai-ledger-api==0.10.2
 pip3 install --no-cache-dir --ignore-installed --force-reinstall ./scripts/fetchai_netutils
