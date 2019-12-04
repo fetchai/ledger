99ca3f67cf00b8856dd74edd16749984f7ecdb14
nhutton <nathan.hutton@fetch.ai>
2019-12-04 14:18:00 +0000

    Test whether jenkins has tags

--- scripts/install-test-dependencies.sh
+++ scripts/install-test-dependencies.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+git config --global core.pager cat
+git log --oneline -n 10
+
 pip3 install --no-cache-dir --ignore-installed --force-reinstall wheel pyyaml requests
 pip3 install --no-cache-dir --ignore-installed --force-reinstall fetchai-ledger-api==0.10.0a5
 pip3 install --no-cache-dir --ignore-installed --force-reinstall ./scripts/fetchai_netutils
