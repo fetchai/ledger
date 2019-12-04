5db5c97001235a6e48a27338e4f2254e92823482
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-12-04 12:07:56 +0000

    Static Analysis fixes

--- libs/semanticsearch/include/semanticsearch/schema/vocabulary_instance.hpp
+++ libs/semanticsearch/include/semanticsearch/schema/vocabulary_instance.hpp
@@ -36,7 +36,8 @@ public:
   static Vocabulary New(T data)
   {
     // TODO(private issue 143): add destructor
-    return Vocabulary{new VocabularyInstance(std::type_index(typeid(T)), new T(data))};  // NOLINT
+    // NOLINTNEXTLINE
+    return Vocabulary{new VocabularyInstance(std::type_index(typeid(T)), new T(data))};
   }
 
   VocabularyInstance()                                = delete;
