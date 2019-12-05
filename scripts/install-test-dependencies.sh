55b71b957cbe187b0f03a716929604fa2b01aadb
nhutton <nathan.hutton@fetch.ai>
2019-12-05 12:19:19 +0000

    allow removal of api to fail

--- scripts/install-test-dependencies.sh
+++ scripts/install-test-dependencies.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-set -e
 
+# Allowed to fail.
 pip3 uninstall -y fetchai-ledger-api
+
+set -e
 
 pip3 install --no-cache-dir --ignore-installed --force-reinstall wheel pyyaml requests
 pip3 install --no-cache-dir --ignore-installed --force-reinstall fetchai-ledger-api==0.10.2
