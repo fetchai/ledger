02bcc99bfdae4f7a3bab5b9e0109dfeb9c753726
Wojciech Kaluza <w-kaluza@tlen.pl>
2019-12-06 00:38:37 +0000

    Point e2e tests at fork

--- scripts/install-test-dependencies.sh
+++ scripts/install-test-dependencies.sh
@@ -2,5 +2,7 @@
 set -e
 
 pip3 install --no-cache-dir --ignore-installed --force-reinstall wheel pyyaml requests
-pip3 install --no-cache-dir --ignore-installed --force-reinstall fetchai-ledger-api==0.10.2
+pip3 install --no-cache-dir --ignore-installed --force-reinstall --editable git+https://github.com/wkaluza/ledger-api-py.git@pr_parser_c2c_fix#egg=fetchai-ledger-api
+# ??? Restore and bump version when changes merged into SDK
+# pip3 install --no-cache-dir --ignore-installed --force-reinstall fetchai-ledger-api==0.10.2
 pip3 install --no-cache-dir --ignore-installed --force-reinstall ./scripts/fetchai_netutils
