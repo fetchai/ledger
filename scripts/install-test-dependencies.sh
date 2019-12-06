1a5a7b549245036610d4c643105f5c9950d955ce
Attila Bagoly <b.attila.93@gmail.com>
2019-12-06 09:47:55 +0000

    Change ledger API version

--- scripts/install-test-dependencies.sh
+++ scripts/install-test-dependencies.sh
@@ -2,5 +2,6 @@
 set -e
 
 pip3 install --no-cache-dir --ignore-installed --force-reinstall wheel pyyaml requests
-pip3 install --no-cache-dir --ignore-installed --force-reinstall fetchai-ledger-api==0.10.2
+#pip3 install --no-cache-dir --ignore-installed --force-reinstall fetchai-ledger-api==0.10.2
+pip3 install  --no-cache-dir --ignore-installed --force-reinstall --editable git+https://github.com/fetchai/ledger-api-py.git@refactor/remove_contract_digest#egg=fetchai-ledger-api
 pip3 install --no-cache-dir --ignore-installed --force-reinstall ./scripts/fetchai_netutils
