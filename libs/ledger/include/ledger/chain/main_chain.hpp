a1dc3323520229835f995af6444e696bda6c1f91
denis-trapeznikov <denis.trapeznikov@fetch.ai>
2019-12-06 14:20:17 +0000

    Initial revert to more original GetChainPreceding.

--- libs/ledger/include/ledger/chain/main_chain.hpp
+++ libs/ledger/include/ledger/chain/main_chain.hpp
@@ -172,8 +172,8 @@ public:
   /// @{
   BlockPtr   GetHeaviestBlock() const;
   BlockHash  GetHeaviestBlockHash() const;
-  Blocks     GetHeaviestChain(uint64_t lowest_block_number = 0) const;
-  Blocks     GetChainPreceding(BlockHash start, uint64_t lowest_block_number = 0) const;
+  Blocks     GetHeaviestChain(uint64_t limit = UPPER_BOUND) const;
+  Blocks     GetChainPreceding(BlockHash start, uint64_t limit = UPPER_BOUND) const;
   Travelogue TimeTravel(BlockHash current_hash) const;
   bool       GetPathToCommonAncestor(
             Blocks &blocks, BlockHash tip_hash, BlockHash node_hash, uint64_t limit = UPPER_BOUND,
