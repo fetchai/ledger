80b9b8faec1f72dc354ae5a67bfbc40f57ac55aa
n-hutton <nathan.cameron.hutton@gmail.com>
2019-12-06 00:44:39 +0000

    Update libs/ledger/src/protocols/main_chain_rpc_service.cpp
    Co-Authored-By: Ed FitzGerald <40318762+ejfitzgerald@users.noreply.github.com>

--- libs/ledger/src/protocols/main_chain_rpc_service.cpp
+++ libs/ledger/src/protocols/main_chain_rpc_service.cpp
@@ -429,7 +429,7 @@ MainChainRpcService::State MainChainRpcService::OnSynchronising()
     }
 
     FETCH_LOG_INFO(LOGGING_NAME, "Requesting chain from muddle://", ToBase64(current_peer_address_),
-                   " for block ", current_missing_block_.ToHex());
+                   " for block 0x", current_missing_block_.ToHex());
 
     // make the RPC call to the block source with a request for the chain
     current_request_ = rpc_client_.CallSpecificAddress(
