50474494d92833ac0cdf6d1ae051ff058940cd0e
Wojciech Kaluza <w-kaluza@tlen.pl>
2019-12-09 10:08:43 +0000

    Use main repo master

--- scripts/install-test-dependencies.sh
+++ scripts/install-test-dependencies.sh
@@ -2,7 +2,7 @@
 set -e
 
 pip3 install  wheel pyyaml requests --force-reinstall
-pip3 install --no-cache-dir --ignore-installed --force-reinstall --editable git+https://github.com/wkaluza/ledger-api-py.git@pr_parser_c2c_fix#egg=fetchai-ledger-api
+pip3 install --no-cache-dir --ignore-installed --force-reinstall --editable git+https://github.com/fetchai/ledger-api-py.git@master#egg=fetchai-ledger-api
 # ??? Restore and bump version when changes merged into SDK
 # pip3 install  fetchai-ledger-api==0.10.2 --force-reinstall
 pip3 install ./scripts/fetchai_netutils --force-reinstall
