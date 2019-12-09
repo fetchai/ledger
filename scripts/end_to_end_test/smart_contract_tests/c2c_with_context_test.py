23b83ec72dbc9ce651fc129eb1bc3e8098cb9b42
Wojciech Kaluza <w-kaluza@tlen.pl>
2019-12-09 08:30:09 +0000

    Remove dead code

--- scripts/end_to_end_test/smart_contract_tests/c2c_with_context_test.py
+++ scripts/end_to_end_test/smart_contract_tests/c2c_with_context_test.py
@@ -23,11 +23,8 @@ from fetchai.ledger.crypto import Entity
 
 CONTRACT_TEXT = """
 persistent block_number_state : Int64;
-// persistent contract_address_state : Address;
 
 contract c1_interface
-  // @action
-  // function contract_address() : Int64;
   @action
   function block_number() : Int64;
 endcontract
@@ -35,24 +32,17 @@ endcontract
 @action
 function c2c_call(contract_name : String)
   use block_number_state;
-  // use contract_address_state;
 
   contract c = c1_interface(contract_name);
 
   var c2c_block_number = c.block_number();
   block_number_state.set(c2c_block_number);
 
-  // c.contract_address();
-  // get contract address from query when supported: var c2c_addr =
-  // contract_address_state.set(c2c_addr);
-
   var context = getContext();
   var block = context.block();
   var transaction = context.transaction();
   var block_number = block.blockNumber();
-  // var addr = transaction.contractAddress();
 
-  // assert(c2c_addr == addr);
   assert(c2c_block_number == toInt64(block_number));
 endfunction
 
@@ -62,25 +52,9 @@ function query_block_number_state() : Int64
 
   return block_number_state.get();
 endfunction
-
-// @query
-// function query_contract_address_state() : Address
-//   use contract_address_state;
-//
-//   return contract_address_state.get();
-// endfunction
 """
 
 CONTRACT_TEXT2 = """
-// @action // convert to action-query pair
-// function contract_address() : Address
-//   var context = getContext();
-//   var transaction = context.transaction();
-//   var addr = transaction.contractAddress();
-//
-//   return addr;
-// endfunction
-
 @action
 function block_number() : Int64
   var context = getContext();
@@ -118,10 +92,3 @@ def run(options):
     assert later_block > current_block, \
         'Expected number larger than {}, found {}'.format(
             current_block, later_block)
-
-    # result = contract.query(api, 'query_contract_address_state')
-    # assert len(str(result)) > 0, \
-    #     'Expected non-empty address'
-    # assert str(result) == str(contract.address), \
-    #     'Expected query_contract_address_state to return calling contract\'s ' \
-    #     'address ({}), but instead got {}'.format(contract.address, result)
