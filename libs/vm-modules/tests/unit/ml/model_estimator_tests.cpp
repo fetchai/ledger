c1f9ba46703356cf89193a153e5164dabebfd110
Lokman Rahmani <lokman.rahmani@fetch.ai>
2019-12-09 18:54:56 +0000

    Add consistency unit tests for AddLayer[Activation] operation

--- libs/vm-modules/tests/unit/ml/model_estimator_tests.cpp
+++ libs/vm-modules/tests/unit/ml/model_estimator_tests.cpp
@@ -19,25 +19,50 @@
 #include "gmock/gmock.h"
 #include "vm_modules/ml/model/model.hpp"
 #include "vm_modules/ml/model/model_estimator.hpp"
-#include "vm_test_toolkit.hpp"
+
+#include "vm_modules/math/utilities/factory_with_estimator.hpp"
+#include "vm_modules/ml/utilities/factory_with_estimator.hpp"
 
 namespace {
 
 using SizeType         = fetch::math::SizeType;
 using DataType         = fetch::vm_modules::math::DataType;
-using VmPtr            = fetch::vm::Ptr<fetch::vm::String>;
+using vmStringPtr            = fetch::vm::Ptr<fetch::vm::String>;
 using VmModel          = fetch::vm_modules::ml::model::VMModel;
 using VmModelEstimator = fetch::vm_modules::ml::model::ModelEstimator;
 using DataType         = fetch::vm_modules::ml::model::ModelEstimator::DataType;
+using ChargeAmount     = fetch::vm::ChargeAmount;
+
+namespace maf = vm_modules::math::factory_with_estimator;
+namespace mlf = vm_modules::ml::factory_with_estimator;
 
 class VMModelEstimatorTests : public ::testing::Test
 {
 public:
-  std::stringstream stdout;
-  VmTestToolkit     toolkit{&stdout};
+  std::shared_ptr<fetch::vm::VM> vm;
+
+  void SetUp() override
+  {
+    // setup vm
+    vm = maf::NewVM();
+  }
+  
+  ChargeAmount LayerAddDenseCharge(fetch::vm::Ptr<VmModel> model, vmStringPtr layer_type, SizeType input_size, SizeType output_size)
+  {
+    return model->Estimator().LayerAddDense(layer_type, input_size, output_size);
+  }
+  
+  ChargeAmount LayerAddDenseActivationCharge(fetch::vm::Ptr<VmModel> model, vmStringPtr layer_type, SizeType input_size, SizeType output_size, vmStringPtr activation)
+  {
+    return model->Estimator().LayerAddDenseActivation(layer_type, input_size, output_size, activation);
+  }
+
 };
 
-// sanity check that estimator behaves as intended
+/////////////////////////////////////////////////////
+// sanity check that estimator behaves as intended //
+/////////////////////////////////////////////////////
+
 TEST_F(VMModelEstimatorTests, add_dense_layer_test)
 {
   std::string model_type = "sequential";
@@ -50,9 +75,9 @@ TEST_F(VMModelEstimatorTests, add_dense_layer_test)
   SizeType max_output_size = 1000;
   SizeType output_step     = 10;
 
-  VmPtr             vm_ptr_layer_type{new fetch::vm::String(&toolkit.vm(), layer_type)};
+  vmStringPtr             vm_ptr_layer_type{new fetch::vm::String(vm.get(), layer_type)};
   fetch::vm::TypeId type_id = 0;
-  VmModel           model(&toolkit.vm(), type_id, model_type);
+  VmModel           model(vm.get(), type_id, model_type);
   VmModelEstimator  model_estimator(model);
 
   for (SizeType inputs = min_input_size; inputs < max_input_size; inputs += input_step)
@@ -70,7 +95,6 @@ TEST_F(VMModelEstimatorTests, add_dense_layer_test)
   }
 }
 
-// sanity check that estimator behaves as intended
 TEST_F(VMModelEstimatorTests, add_dense_layer_activation_test)
 {
   std::string model_type      = "sequential";
@@ -84,10 +108,10 @@ TEST_F(VMModelEstimatorTests, add_dense_layer_activation_test)
   SizeType max_output_size = 1000;
   SizeType output_step     = 10;
 
-  VmPtr             vm_ptr_layer_type{new fetch::vm::String(&toolkit.vm(), layer_type)};
-  VmPtr             vm_ptr_activation_type{new fetch::vm::String(&toolkit.vm(), activation_type)};
+  vmStringPtr             vm_ptr_layer_type{new fetch::vm::String(vm.get(), layer_type)};
+  vmStringPtr             vm_ptr_activation_type{new fetch::vm::String(vm.get(), activation_type)};
   fetch::vm::TypeId type_id = 0;
-  VmModel           model(&toolkit.vm(), type_id, model_type);
+  VmModel           model(vm.get(), type_id, model_type);
   VmModelEstimator  model_estimator(model);
 
   for (SizeType inputs = min_input_size; inputs < max_input_size; inputs += input_step)
@@ -106,7 +130,6 @@ TEST_F(VMModelEstimatorTests, add_dense_layer_activation_test)
   }
 }
 
-// sanity check that estimator behaves as intended
 TEST_F(VMModelEstimatorTests, add_conv_layer_test)
 {
   std::string model_type = "sequential";
@@ -128,9 +151,9 @@ TEST_F(VMModelEstimatorTests, add_conv_layer_test)
   SizeType max_stride_size = 100;
   SizeType stride_step     = 10;
 
-  VmPtr             vm_ptr_layer_type{new fetch::vm::String(&toolkit.vm(), layer_type)};
+  vmStringPtr             vm_ptr_layer_type{new fetch::vm::String(vm.get(), layer_type)};
   fetch::vm::TypeId type_id = 0;
-  VmModel           model(&toolkit.vm(), type_id, model_type);
+  VmModel           model(vm.get(), type_id, model_type);
   VmModelEstimator  model_estimator(model);
 
   for (SizeType output_channels = min_output_size; output_channels < max_output_size;
@@ -155,7 +178,6 @@ TEST_F(VMModelEstimatorTests, add_conv_layer_test)
   }
 }
 
-// sanity check that estimator behaves as intended
 TEST_F(VMModelEstimatorTests, add_conv_layer_activation_test)
 {
   std::string model_type      = "sequential";
@@ -178,10 +200,10 @@ TEST_F(VMModelEstimatorTests, add_conv_layer_activation_test)
   SizeType max_stride_size = 100;
   SizeType stride_step     = 10;
 
-  VmPtr             vm_ptr_layer_type{new fetch::vm::String(&toolkit.vm(), layer_type)};
-  VmPtr             vm_ptr_activation_type{new fetch::vm::String(&toolkit.vm(), activation_type)};
+  vmStringPtr             vm_ptr_layer_type{new fetch::vm::String(vm.get(), layer_type)};
+  vmStringPtr             vm_ptr_activation_type{new fetch::vm::String(vm.get(), activation_type)};
   fetch::vm::TypeId type_id = 0;
-  VmModel           model(&toolkit.vm(), type_id, model_type);
+  VmModel           model(vm.get(), type_id, model_type);
   VmModelEstimator  model_estimator(model);
 
   for (SizeType output_channels = min_output_size; output_channels < max_output_size;
@@ -207,7 +229,6 @@ TEST_F(VMModelEstimatorTests, add_conv_layer_activation_test)
   }
 }
 
-// sanity check that estimator behaves as intended
 TEST_F(VMModelEstimatorTests, compile_sequential_test)
 {
   std::string model_type = "sequential";
@@ -224,15 +245,15 @@ TEST_F(VMModelEstimatorTests, compile_sequential_test)
 
   fetch::vm::TypeId type_id = 0;
 
-  VmPtr vm_ptr_layer_type{new fetch::vm::String(&toolkit.vm(), layer_type)};
-  VmPtr vm_ptr_loss_type{new fetch::vm::String(&toolkit.vm(), loss_type)};
-  VmPtr vm_ptr_opt_type{new fetch::vm::String(&toolkit.vm(), opt_type)};
+  vmStringPtr vm_ptr_layer_type{new fetch::vm::String(vm.get(), layer_type)};
+  vmStringPtr vm_ptr_loss_type{new fetch::vm::String(vm.get(), loss_type)};
+  vmStringPtr vm_ptr_opt_type{new fetch::vm::String(vm.get(), opt_type)};
 
   for (SizeType inputs = min_input_size; inputs < max_input_size; inputs += input_step)
   {
     for (SizeType outputs = min_output_size; outputs < max_output_size; outputs += output_step)
     {
-      VmModel          model(&toolkit.vm(), type_id, model_type);
+      VmModel          model(vm.get(), type_id, model_type);
       VmModelEstimator model_estimator(model);
 
       // add some layers
@@ -262,7 +283,6 @@ TEST_F(VMModelEstimatorTests, compile_sequential_test)
   }
 }
 
-// sanity check that estimator behaves as intended
 TEST_F(VMModelEstimatorTests, compile_simple_test)
 {
   std::string model_type = "regressor";
@@ -274,13 +294,13 @@ TEST_F(VMModelEstimatorTests, compile_simple_test)
 
   fetch::vm::TypeId type_id = 0;
 
-  VmPtr vm_ptr_opt_type{new fetch::vm::String(&toolkit.vm(), opt_type)};
+  vmStringPtr vm_ptr_opt_type{new fetch::vm::String(vm.get(), opt_type)};
 
   for (SizeType layers = min_layer_size; layers < max_layer_size; layers += layer_step)
   {
 
     fetch::vm::Ptr<fetch::vm::Array<SizeType>> vm_ptr_layers{};
-    VmModel                                    model(&toolkit.vm(), type_id, model_type);
+    VmModel                                    model(vm.get(), type_id, model_type);
     VmModelEstimator                           model_estimator(model);
 
     EXPECT_TRUE(model_estimator.CompileSimple(vm_ptr_opt_type, vm_ptr_layers) ==
@@ -288,7 +308,6 @@ TEST_F(VMModelEstimatorTests, compile_simple_test)
   }
 }
 
-// sanity check that estimator behaves as intended
 TEST_F(VMModelEstimatorTests, estimator_fit_and_predict_test)
 {
   std::string model_type      = "sequential";
@@ -313,10 +332,10 @@ TEST_F(VMModelEstimatorTests, estimator_fit_and_predict_test)
   SizeType batch_size_step = 23;
 
   fetch::vm::TypeId type_id = 0;
-  VmPtr             vm_ptr_layer_type{new fetch::vm::String(&toolkit.vm(), layer_type)};
-  VmPtr             vm_ptr_loss_type{new fetch::vm::String(&toolkit.vm(), loss_type)};
-  VmPtr             vm_ptr_opt_type{new fetch::vm::String(&toolkit.vm(), opt_type)};
-  VmPtr             vm_ptr_activation_type{new fetch::vm::String(&toolkit.vm(), activation_type)};
+  vmStringPtr             vm_ptr_layer_type{new fetch::vm::String(vm.get(), layer_type)};
+  vmStringPtr             vm_ptr_loss_type{new fetch::vm::String(vm.get(), loss_type)};
+  vmStringPtr             vm_ptr_opt_type{new fetch::vm::String(vm.get(), opt_type)};
+  vmStringPtr             vm_ptr_activation_type{new fetch::vm::String(vm.get(), activation_type)};
 
   for (SizeType data_size_1 = min_data_size_1; data_size_1 < max_data_size_1;
        data_size_1 += data_size_1_step)
@@ -335,11 +354,11 @@ TEST_F(VMModelEstimatorTests, estimator_fit_and_predict_test)
           std::vector<uint64_t>                             data_shape{{data_size_1, n_data}};
           std::vector<uint64_t>                             label_shape{{label_size_1, n_data}};
           fetch::vm::Ptr<fetch::vm_modules::math::VMTensor> vm_ptr_tensor_data{
-              new fetch::vm_modules::math::VMTensor(&toolkit.vm(), type_id, data_shape)};
+              new fetch::vm_modules::math::VMTensor(vm.get(), type_id, data_shape)};
           fetch::vm::Ptr<fetch::vm_modules::math::VMTensor> vm_ptr_tensor_labels{
-              new fetch::vm_modules::math::VMTensor(&toolkit.vm(), type_id, label_shape)};
+              new fetch::vm_modules::math::VMTensor(vm.get(), type_id, label_shape)};
 
-          VmModel          model(&toolkit.vm(), type_id, model_type);
+          VmModel          model(vm.get(), type_id, model_type);
           VmModelEstimator model_estimator(model);
 
           model_estimator.LayerAddDenseActivation(vm_ptr_layer_type, data_size_1, label_size_1,
@@ -407,5 +426,111 @@ TEST_F(VMModelEstimatorTests, estimator_fit_and_predict_test)
     }
   }
 }
+
+///////////////////////////////////////
+// Operations estimators consistency //
+///////////////////////////////////////
+
+TEST_F(VMModelEstimatorTests, layeradd_charge_correlate_with_input_size)
+{
+  SizeType output_size = 10;
+  auto layer_type = maf::vmString(vm, "dense");
+  
+  auto model_small = mlf::vmSequentialModel(vm);
+  SizeType input_size_small = 10;
+  auto layeradd_charge_small = LayerAddDenseCharge(model_small, layer_type, input_size_small, output_size);
+
+  auto model_big = mlf::vmSequentialModel(vm);
+  SizeType input_size_big = 100;
+  auto layeradd_charge_big = LayerAddDenseCharge(model_big, layer_type, input_size_big, output_size);
+
+  EXPECT_LT(layeradd_charge_small, layeradd_charge_big);
+}
+
+TEST_F(VMModelEstimatorTests, layeradd_correlate_with_output_size)
+{
+  SizeType input_size = 10;
+  auto layer_type = maf::vmString(vm, "dense");
+  
+  auto model_small = mlf::vmSequentialModel(vm);
+  SizeType output_size_small = 10;
+  auto layeradd_charge_small = LayerAddDenseCharge(model_small, layer_type, input_size, output_size_small);
+
+  auto model_big = mlf::vmSequentialModel(vm);
+  SizeType output_size_big = 100;
+  auto layeradd_charge_big = LayerAddDenseCharge(model_big, layer_type, input_size, output_size_big);
+
+  EXPECT_LT(layeradd_charge_small, layeradd_charge_big);
+}
+
+TEST_F(VMModelEstimatorTests, layeradd_correlate_with_input_size_output_size)
+{
+  auto layer_type = maf::vmString(vm, "dense");
+  
+  auto model_small = mlf::vmSequentialModel(vm);
+  SizeType input_size_small = 10;
+  SizeType output_size_small = 10;
+  auto layeradd_charge_small = LayerAddDenseCharge(model_small, layer_type, input_size_small, output_size_small);
+
+  auto model_big = mlf::vmSequentialModel(vm);
+  SizeType input_size_big = 100;
+  SizeType output_size_big = 100;
+  auto layeradd_charge_big = LayerAddDenseCharge(model_big, layer_type, input_size_big, output_size_big);
+
+  EXPECT_LT(layeradd_charge_small, layeradd_charge_big);
+}
+
+TEST_F(VMModelEstimatorTests, layeraddactivation_correlate_with_input_size)
+{
+  SizeType output_size = 10;
+  auto layer_type = maf::vmString(vm, "dense");
+  auto activation_type = maf::vmString(vm, "relu");
+  
+  auto model_small = mlf::vmSequentialModel(vm);
+  SizeType input_size_small = 10;
+  auto layeradd_charge_small = LayerAddDenseActivationCharge(model_small, layer_type, input_size_small, output_size, activation_type);
+
+  auto model_big = mlf::vmSequentialModel(vm);
+  SizeType input_size_big = 100;
+  auto layeradd_charge_big = LayerAddDenseActivationCharge(model_big, layer_type, input_size_big, output_size, activation_type);
+
+  EXPECT_LT(layeradd_charge_small, layeradd_charge_big);
+}
+
+TEST_F(VMModelEstimatorTests, layeraddactivation_correlate_with_output_size)
+{
+  SizeType input_size = 10;
+  auto layer_type = maf::vmString(vm, "dense");
+  auto activation_type = maf::vmString(vm, "relu");
+  
+  auto model_small = mlf::vmSequentialModel(vm);
+  SizeType output_size_small = 10;
+  auto layeradd_charge_small = LayerAddDenseActivationCharge(model_small, layer_type, input_size, output_size_small, activation_type);
+
+  auto model_big = mlf::vmSequentialModel(vm);
+  SizeType output_size_big = 100;
+  auto layeradd_charge_big = LayerAddDenseActivationCharge(model_big, layer_type, input_size, output_size_big, activation_type);
+
+  EXPECT_LT(layeradd_charge_small, layeradd_charge_big);
+}
+
+TEST_F(VMModelEstimatorTests, layeraddactivation_correlate_with_input_size_output_size)
+{
+  auto layer_type = maf::vmString(vm, "dense");
+  auto activation_type = maf::vmString(vm, "relu");
+  
+  auto model_small = mlf::vmSequentialModel(vm);
+  SizeType input_size_small = 10;
+  SizeType output_size_small = 10;
+  auto layeradd_charge_small = LayerAddDenseActivationCharge(model_small, layer_type, input_size_small, output_size_small, activation_type);
+
+  auto model_big = mlf::vmSequentialModel(vm);
+  SizeType input_size_big = 100;
+  SizeType output_size_big = 100;
+  auto layeradd_charge_big = LayerAddDenseActivationCharge(model_big, layer_type, input_size_big, output_size_big, activation_type);
+
+  EXPECT_LT(layeradd_charge_small, layeradd_charge_big);
+}
+
 
 }  // namespace
