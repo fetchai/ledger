c28e9e644e45f35c83161b54219cf3d3c4f147a1
Khan Baykaner <khan.baykaner@fetch.ai>
2019-12-11 10:28:45 +0000

    bugfix namespace ambiguity

--- libs/ml/examples/mnist/main.cpp
+++ libs/ml/examples/mnist/main.cpp
@@ -31,8 +31,8 @@
 
 using namespace fetch::ml::ops;
 using namespace fetch::ml::layers;
-using namespace fetch::math;
 
+using SizeType   = fetch::math::SizeType;
 using DataType   = fetch::fixed_point::FixedPoint<32, 32>;
 using TensorType = fetch::math::Tensor<DataType>;
 
@@ -42,7 +42,7 @@ using DataLoaderType = typename fetch::ml::dataloaders::TensorDataLoader<TensorT
 
 int main(int ac, char **av)
 {
-  DataType learning_rate = Type<DataType>("0.01");
+  DataType learning_rate = fetch::math::Type<DataType>("0.01");
   SizeType subset_size{100};
   SizeType epochs{10};
   SizeType batch_size{10};
