546c09a7d470656d0d05d6f3dc97e94110224bf1
Khan Baykaner <khan.baykaner@fetch.ai>
2019-12-12 16:57:04 +0000

    tensor fromstring + handling

--- libs/math/include/math/tensor.hpp
+++ libs/math/include/math/tensor.hpp
@@ -786,6 +786,7 @@ Tensor<T, C> Tensor<T, C>::FromString(byte_array::ConstByteArray const &c)
       ++i;
       break;
     case ',':
+    case '+':
     case ' ':
     case '\t':
       prev_backslash = false;
