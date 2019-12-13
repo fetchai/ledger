c3302e0baaff76ff3ab10ceefce6afc04ed55a73
Jiri Vestfal <jiri.vestfal@fetch.ai>
2019-12-13 11:55:15 +0000

    AsDouble removed from fillrandom

--- libs/math/include/math/tensor.hpp
+++ libs/math/include/math/tensor.hpp
@@ -1576,7 +1576,7 @@ Tensor<T, C> &Tensor<T, C>::FillUniformRandom()
 {
   for (SizeType i = 0; i < this->size(); ++i)
   {
-    this->operator[](i) = Type(random::Random::generator.AsDouble());
+    this->operator[](i) = Type(random::Random::generator.AsFP64());
   }
   return *this;
 }
