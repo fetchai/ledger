1a7aba3518089104dfd45388fcb532b80dd3e827
void* <peter.bukva@fetch.ai>
2019-12-18 16:23:39 +0000

    Endianess handling of UInt<T> <-> ByteArray conversion (#2010)

--- libs/vm-modules/src/ledger/transaction.cpp
+++ libs/vm-modules/src/ledger/transaction.cpp
@@ -91,7 +91,7 @@ void Transaction::Bind(vm::Module &module)
 Transaction::Transaction(vm::VM *vm, vm::TypeId type_id, fetch::chain::Transaction const &tx)
   : Object{vm, type_id}
   , tx_{std::make_shared<fetch::chain::Transaction>(tx)}
-  , digest_{vm->CreateNewObject<math::UInt256Wrapper>(tx.digest())}
+  , digest_{vm->CreateNewObject<math::UInt256Wrapper>(tx.digest(), platform::Endian::BIG)}
   , from_{CreateAddress(vm, tx.from(), tx.signatories())}
   , transfers_{CreateTransfers(vm, tx)}
   , contract_address_{CreateAddress(vm, tx.contract_address(), tx.signatories())}
