b2abfa1915e79dc1d724c3711614dce3cd730ad0
Iryna Melnyk <iryna.melnyk@n-cube.co.uk>
2020-01-03 18:49:43 +0200

    WIP: fixed model tests where Tensor.fromString() was used

--- libs/vm-modules/tests/unit/ml/ml_model_tests.cpp
+++ libs/vm-modules/tests/unit/ml/ml_model_tests.cpp
@@ -52,10 +52,7 @@ std::string const ACTIVATION_LAYER_TEST_SOURCE = R"(
          model.add("activation", "<<ACTIVATION>>");
          model.compile("mse", "sgd");
 
-         var shape = Array<UInt64>(2);
-         shape[0] = 1u64;
-         shape[1] = 1u64;
-         var x = Tensor(shape);
+         var x = Tensor();
 
          x.fromString("<<INPUT>>");
 
@@ -1096,9 +1093,7 @@ TEST_F(VMModelTests, model_sequential_flatten_tensor_data)
 {
   static char const *SRC_METRIC = R"(
         function main() : Tensor
-            var shape = Array<UInt64>(1);
-            shape[0] = 1u64;
-            var x = Tensor(shape);
+          var x = Tensor();
           var str_vals = "0.5, 7.1, 9.1; 6.2, 7.1, 4.; -99.1, 14328.1, 10.0;";
           x.fromString(str_vals);
           var data = x.unsqueeze();
@@ -1137,9 +1132,7 @@ TEST_F(VMModelTests, model_sequential_flatten_2d_in_2d_out)
 {
   static char const *SRC_METRIC = R"(
               function main() : Tensor
-                 var shape = Array<UInt64>(1);
-                 shape[0] = 1u64;
-                 var x = Tensor(shape);
+                var x = Tensor();
                 var str_vals = "0.5, 7.1, 9.1; 6.2, 7.1, 4.;";
                 x.fromString(str_vals);
 
@@ -1236,9 +1229,7 @@ TEST_F(VMModelTests, model_sequential_reshape_3d_in_2d_out)
 {
   static char const *SRC_METRIC = R"(
               function main() : Tensor
-                var shape = Array<UInt64>(1);
-                shape[0] = 1u64;
-                var x = Tensor(shape);
+                var x = Tensor();
                 var str_vals = "0.5, 7.1, 9.1; 6.2, 7.1, 4.;";
                 x.fromString(str_vals);
                 x = x.unsqueeze();
