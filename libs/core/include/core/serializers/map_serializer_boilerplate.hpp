5984f1ec464231120d4276e6377d8a673b89de25
denis-trapeznikov <denis.trapeznikov@fetch.ai>
2020-01-06 16:28:19 +0000

    Current state.

---
+++ libs/core/include/core/serializers/map_serializer_boilerplate.hpp
@@ -0,0 +1,52 @@
+#pragma once
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2020 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "meta/value_util.hpp"
+
+namespace fetch {
+namespace serializers {
+
+template<class T, class D, class... Fields>
+struct MapSerializerBoilerplate {
+  using Type = T;
+  using DriverType = D;
+
+  template<class Constructor>
+  static constexpr void Serialize(Constructor &map_constructor, Type const &v)
+  {
+	  static constexpr auto map_size = sizeof...(Fields);
+
+	  value_util::ForEach(
+		  [map = map_constructor(map_size), &v](auto field) {
+			  field.Serialize(v, map);
+		  }, Fields{}...);
+  }
+
+  template<class MapDeserializer>
+  static constexpr void Deserialize(MapDeserializer &map, Type &v)
+  {
+	  value_util::ForEach(
+		  [&map, &v](auto field) {
+			  field.Deserialize(v, map);
+		  }, Fields{}...);
+  }
+};
+
+}  // namespace serializers
+}  // namespace fetch
