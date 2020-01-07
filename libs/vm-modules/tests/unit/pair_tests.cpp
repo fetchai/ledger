b09cbb8ddfc5a24d5d7b997646da237a377f7cda
Jiri Vestfal <jiri.vestfal@fetch.ai>
2020-01-07 10:23:39 +0000

    Added more VMPair tests

--- libs/vm-modules/tests/unit/pair_tests.cpp
+++ libs/vm-modules/tests/unit/pair_tests.cpp
@@ -73,4 +73,136 @@ TEST_F(PairTests, assign_string_u32_test)
   ASSERT_EQ(stdout.str(), "TEST-2");
 }
 
+TEST_F(PairTests, assign_bool_int8_test)
+{
+  static char const *TEXT = R"(
+    function main()
+      var data = Pair<Bool, Int8>();
+
+      data.first(true);
+      data.second(-4i8);
+
+      print(data.first());
+      print(',');
+      print(data.second());
+
+    endfunction
+  )";
+
+  ASSERT_TRUE(toolkit.Compile(TEXT));
+  ASSERT_TRUE(toolkit.Run());
+
+  ASSERT_EQ(stdout.str(), "true,-4");
+}
+
+TEST_F(PairTests, assign_uint8_int16_test)
+{
+  static char const *TEXT = R"(
+    function main()
+      var data = Pair<UInt8, Int16>();
+
+      data.first(255u8);
+      data.second(-12345i16);
+
+      print(data.first());
+      print(',');
+      print(data.second());
+
+    endfunction
+  )";
+
+  ASSERT_TRUE(toolkit.Compile(TEXT));
+  ASSERT_TRUE(toolkit.Run());
+
+  ASSERT_EQ(stdout.str(), "255,-12345");
+}
+
+TEST_F(PairTests, assign_uint32_int32_test)
+{
+  static char const *TEXT = R"(
+    function main()
+      var data = Pair<UInt32, Int32>();
+
+      data.first(1234567u32);
+      data.second(-1234567i32);
+
+      print(data.first());
+      print(',');
+      print(data.second());
+
+    endfunction
+  )";
+
+  ASSERT_TRUE(toolkit.Compile(TEXT));
+  ASSERT_TRUE(toolkit.Run());
+
+  ASSERT_EQ(stdout.str(), "1234567,-1234567");
+}
+
+TEST_F(PairTests, assign_uint64_int64_test)
+{
+  static char const *TEXT = R"(
+    function main()
+      var data = Pair<UInt64, Int64>();
+
+      data.first(1234567890u32);
+      data.second(-1234567890i32);
+
+      print(data.first());
+      print(',');
+      print(data.second());
+
+    endfunction
+  )";
+
+  ASSERT_TRUE(toolkit.Compile(TEXT));
+  ASSERT_TRUE(toolkit.Run());
+
+  ASSERT_EQ(stdout.str(), "1234567890,-1234567890");
+}
+
+TEST_F(PairTests, assign_fixed32_fixed64_test)
+{
+  static char const *TEXT = R"(
+    function main()
+      var data = Pair<Fixed32, Fixed64>();
+
+      data.first(-234.567fp32);
+      data.second(-123.456fp64);
+
+      print(data.first());
+      print(',');
+      print(data.second());
+
+    endfunction
+  )";
+
+  ASSERT_TRUE(toolkit.Compile(TEXT));
+  ASSERT_TRUE(toolkit.Run());
+
+  ASSERT_EQ(stdout.str(), "-234.5669,-123.455999999");
+}
+
+TEST_F(PairTests, assign_fixed128_uint256_test)
+{
+  static char const *TEXT = R"(
+    function main()
+      var data = Pair<Fixed128, String>();
+
+      data.first(-234.567fp128);
+      data.second("Hello");
+
+      print(data.first());
+      print(',');
+      print(data.second());
+
+    endfunction
+  )";
+
+  ASSERT_TRUE(toolkit.Compile(TEXT));
+  ASSERT_TRUE(toolkit.Run());
+
+  ASSERT_EQ(stdout.str(), "-234.5669999999999999999,Hello");
+}
+
 }  // namespace
