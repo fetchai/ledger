d9c8fde4e61426ba9e9d31f1f53d41427157d6a3
Attila Bagoly <b.attila.93@gmail.com>
2020-01-13 11:07:28 +0000

    Enable synergetic end to end tests. Change ledger api to master

--- scripts/install-test-dependencies.sh
+++ scripts/install-test-dependencies.sh
@@ -3,5 +3,6 @@ set -e
 
 pip3 install wheel --force-reinstall
 pip3 install pyyaml requests --force-reinstall
-pip3 install fetchai-ledger-api==0.11.0a4 --force-reinstall
+#pip3 install fetchai-ledger-api==0.11.0a4 --force-reinstall
+pip3 install  --no-cache-dir --ignore-installed --force-reinstall --editable git+https://github.com/fetchai/ledger-api-py.git@master#egg=fetchai-ledger-api
 pip3 install ./scripts/fetchai_netutils --force-reinstall
