2285d2010da1abeae44801b4dd5f5d35052605b6
Khan Baykaner <khan.baykaner@fetch.ai>
2020-01-15 13:25:01 +0000

    integer casting fixes

--- libs/math/src/linalg/blas/gemv_t_novector.cpp
+++ libs/math/src/linalg/blas/gemv_t_novector.cpp
@@ -105,7 +105,7 @@ void Blas<S, Signature(_y <= _alpha, _A, _x, _n, _beta, _y, _m),
         for (i = 0; i < leny; ++i)
         {
           y[iy] = beta * y[iy];
-          iy    = static_cast<Type>(iy + incy);
+          iy    = iy + incy;
         }
       }
     }
