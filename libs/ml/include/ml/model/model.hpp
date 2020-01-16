54aa534813487d7843f2b23eff15a9a62d8cca5c
Khan Baykaner <khan.baykaner@fetch.ai>
2020-01-16 12:57:36 +0000

    fixed missing includes

--- libs/ml/include/ml/model/model.hpp
+++ libs/ml/include/ml/model/model.hpp
@@ -26,6 +26,10 @@
 #include "ml/ops/metrics/categorical_accuracy.hpp"
 #include "ml/ops/metrics/types.hpp"
 
+#include "ml/ops/loss_functions/cross_entropy_loss.hpp"
+#include "ml/ops/loss_functions/mean_square_error_loss.hpp"
+#include "ml/ops/loss_functions/softmax_cross_entropy_loss.hpp"
+
 #include "ml/optimisation/optimiser.hpp"
 #include "ml/optimisation/types.hpp"
 
@@ -35,22 +39,6 @@
 #include <utility>
 
 namespace fetch {
-namespace ml {
-
-namespace ops {
-
-template <typename TensorType>
-class CrossEntropyLoss;
-
-template <typename TensorType>
-class MeanSquareErrorLoss;
-
-template <typename TensorType>
-class SoftmaxCrossEntropyLoss;
-}  // namespace ops
-
-}  // namespace ml
-
 namespace vm_modules {
 namespace ml {
 namespace model {
