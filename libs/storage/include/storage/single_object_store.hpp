615be5c071848b9f441c0bd6ed1d620d5111b1d1
nhutton <nathan.hutton@fetch.ai>
2020-01-21 10:53:37 +0000

    fix linting issue

--- libs/storage/include/storage/single_object_store.hpp
+++ libs/storage/include/storage/single_object_store.hpp
@@ -45,7 +45,7 @@ public:
    * false if the file is not the correct version and format, or if
    * loading fails due to corruption
    */
-  bool Load(std::string const &doc_file);
+  bool Load(std::string const &file_name);
 
   /**
    * Get the version of the file that has been loaded
