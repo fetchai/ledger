60ee4fbbe8c87fc091fba3d1851cfb00cd38df6a
Ed FitzGerald <40318762+ejfitzgerald@users.noreply.github.com>
2020-01-23 00:42:20 +0000

    Pin lark parser dependency (quick fix) (#2340)

--- scripts/install-test-dependencies.sh
+++ scripts/install-test-dependencies.sh
@@ -3,5 +3,9 @@ set -e
 
 pip3 install wheel --force-reinstall
 pip3 install pyyaml requests --force-reinstall
+pip3 install lark-parser==0.7.8 --force-reinstall
 pip3 install fetchai-ledger-api==0.11.0a4 --force-reinstall
 pip3 install ./scripts/fetchai_netutils --force-reinstall
+
+# display the list of dependencies that can be inspected later
+pip3 freeze
