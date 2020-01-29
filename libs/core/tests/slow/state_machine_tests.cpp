98b1a2295048f156896f30282b6ac5e369d4e979
nhutton <nathan.hutton@fetch.ai>
2020-01-29 10:54:20 +0000

    add missing test

---
+++ libs/core/tests/slow/state_machine_tests.cpp
@@ -0,0 +1,207 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2020 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include <utility>
+
+#include "core/reactor.hpp"
+#include "core/state_machine.hpp"
+
+#include "gmock/gmock.h"
+
+using namespace fetch;
+using namespace testing;
+
+// Test class allows accessing protected state of the reactor
+class ReactorTest : public core::Reactor
+{
+public:
+  explicit ReactorTest(std::string name)
+    : Reactor(std::move(name))
+  {
+    // Redefine these so the test doesn't take too long
+    execution_too_long_ms_   = 50;
+    thread_watcher_check_ms_ = 200;
+  }
+
+  uint64_t ExecutionTooLongMs() const
+  {
+    return execution_too_long_ms_;
+  }
+
+  uint64_t ThreadWatcherCheckMs() const
+  {
+    return thread_watcher_check_ms_;
+  }
+
+  uint32_t ExecutionsTooLongCounter() const
+  {
+    return executions_too_long_;
+  }
+
+  uint32_t ExecutionsWayTooLongCounter() const
+  {
+    return executions_way_too_long_;
+  }
+};
+
+class ReactorTests : public Test
+{
+public:
+  enum class State : uint8_t
+  {
+    A,
+    B,
+    C,
+  };
+
+  ReactorTests()
+    : reactor_{"Reactor"}
+  {}
+
+  using StateMachine    = core::StateMachine<State>;
+  using StateMachinePtr = std::shared_ptr<StateMachine>;
+
+  void SetUp() override
+  {
+    state_machine_ = std::make_shared<StateMachine>("TestStateMachine", State::A, ToString);
+  }
+
+  void TearDown() override
+  {
+    reactor_.Stop();
+  }
+
+  State OnA()
+  {
+    state_seen_ = static_cast<uint8_t>(State::A);
+    return State::B;
+  }
+
+  State OnB()
+  {
+    state_seen_ = static_cast<uint8_t>(State::B);
+    return State::C;
+  }
+
+  State OnC()
+  {
+    state_seen_ = static_cast<uint8_t>(State::C);
+    return State::A;
+  }
+
+  State OnSlowC()
+  {
+    state_seen_ = static_cast<uint8_t>(State::C);
+    std::this_thread::sleep_for(std::chrono::milliseconds(reactor_.ExecutionTooLongMs()));
+    return State::A;
+  }
+
+  State OnVerySlowC()
+  {
+    state_seen_ = static_cast<uint8_t>(State::C);
+    std::this_thread::sleep_for(std::chrono::milliseconds(reactor_.ThreadWatcherCheckMs() * 3));
+    return State::A;
+  }
+
+  static char const *ToString(State state)
+  {
+    char const *text = "unknown";
+
+    switch (state)
+    {
+    case State::A:
+      text = "A";
+      break;
+    case State::B:
+      text = "B";
+      break;
+    case State::C:
+      text = "C";
+      break;
+    }
+
+    return text;
+  }
+
+protected:
+  StateMachinePtr      state_machine_;
+  ReactorTest          reactor_;
+  std::atomic<uint8_t> state_seen_{std::numeric_limits<uint8_t>::max()};
+};
+
+// Basic test - does the reactor drive the state machine through all states
+TEST_F(ReactorTests, ReactorPassesThroughStates)
+{
+  // Note: because it is a fixture you need to upcast the this pointer
+  // clang-format off
+  state_machine_->RegisterHandler(State::A, static_cast<ReactorTests *>(this), &ReactorTests::OnA);
+  state_machine_->RegisterHandler(State::B, static_cast<ReactorTests *>(this), &ReactorTests::OnB);
+  state_machine_->RegisterHandler(State::C, static_cast<ReactorTests *>(this), &ReactorTests::OnC);
+  // clang-format on
+
+  reactor_.Attach(state_machine_);
+  reactor_.Start();
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(5));
+
+  EXPECT_NE(state_seen_, std::numeric_limits<uint8_t>::max());
+  EXPECT_EQ(reactor_.ExecutionsTooLongCounter(), 0);
+  EXPECT_EQ(reactor_.ExecutionsWayTooLongCounter(), 0);
+}
+
+// Test the state machine registers states that are taking too long
+TEST_F(ReactorTests, DISABLED_ReactorNoticesTooLongStates)
+{
+
+  // Note: because it is a fixture you need to upcast the this pointer
+  // clang-format off
+  state_machine_->RegisterHandler(State::A, static_cast<ReactorTests *>(this), &ReactorTests::OnA);
+  state_machine_->RegisterHandler(State::B, static_cast<ReactorTests *>(this), &ReactorTests::OnB);
+  state_machine_->RegisterHandler(State::C, static_cast<ReactorTests *>(this), &ReactorTests::OnSlowC);
+  // clang-format on
+
+  reactor_.Attach(state_machine_);
+  reactor_.Start();
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(reactor_.ExecutionTooLongMs() * 10));
+
+  EXPECT_NE(state_seen_, std::numeric_limits<uint8_t>::max());
+  EXPECT_NE(reactor_.ExecutionsTooLongCounter(), 0);
+  EXPECT_EQ(reactor_.ExecutionsWayTooLongCounter(), 0);
+}
+
+// Test the state machine registers states that are taking *way* too long
+TEST_F(ReactorTests, DISABLED_ReactorNoticesWayTooLongStates)
+{
+
+  // Note: because it is a fixture you need to upcast the this pointer
+  // clang-format off
+  state_machine_->RegisterHandler(State::A, static_cast<ReactorTests *>(this), &ReactorTests::OnA);
+  state_machine_->RegisterHandler(State::B, static_cast<ReactorTests *>(this), &ReactorTests::OnB);
+  state_machine_->RegisterHandler(State::C, static_cast<ReactorTests *>(this), &ReactorTests::OnVerySlowC);
+  // clang-format on
+
+  reactor_.Attach(state_machine_);
+  reactor_.Start();
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(reactor_.ThreadWatcherCheckMs() * 4));
+
+  EXPECT_NE(state_seen_, std::numeric_limits<uint8_t>::max());
+  EXPECT_NE(reactor_.ExecutionsTooLongCounter(), 0);
+  EXPECT_NE(reactor_.ExecutionsWayTooLongCounter(), 0);
+}
