151444e03699349b470b316f6a2856f1b44df50f
denis-trapeznikov <denis.trapeznikov@fetch.ai>
2020-01-29 15:03:50 +0000

    Current state.

--- libs/ledger/include/ledger/consensus/consensus.hpp
+++ libs/ledger/include/ledger/consensus/consensus.hpp
@@ -135,8 +135,8 @@ private:
   telemetry::CounterPtr         consensus_validate_block_failures_total_;
   telemetry::CounterPtr         consensus_non_heaviest_blocks_total_;
 
-  mutable std::unique_map<std::string, telemetry::CounterPtr> block_generation_fails_;
-  mutable Mutex                                               block_generation_fails_mutex_;
+  mutable std::unordered_map<std::string, telemetry::CounterPtr> block_generation_fails_;
+  mutable Mutex                                                  block_generation_fails_mutex_;
   telemetry::CounterPtr TelemetryOnFail(std::string const &key) const;
 };
 
