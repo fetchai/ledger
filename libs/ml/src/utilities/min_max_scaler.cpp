713a11882725fcf63df34c3758de654c3a71119b
Khan Baykaner <khan.baykaner@fetch.ai>
2020-01-30 16:10:26 +0000

    bugfix shadowed type

--- libs/ml/src/utilities/min_max_scaler.cpp
+++ libs/ml/src/utilities/min_max_scaler.cpp
@@ -64,8 +64,6 @@ void MinMaxScaler<TensorType>::SetScale(TensorType const &reference_tensor)
 template <typename TensorType>
 void MinMaxScaler<TensorType>::Normalise(TensorType const &input_tensor, TensorType &output_tensor)
 {
-  using DataType = typename TensorType::Type;
-
   output_tensor.Reshape(input_tensor.shape());
   SizeType batch_dim = input_tensor.shape().size() - 1;
 
