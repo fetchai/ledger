1ab6c36fa4bffeb2ecca981e26ebd63b2b92a3cf
Wojciech Kaluza <w-kaluza@tlen.pl>
2020-01-31 21:30:04 +0000

    Fix serializer element count; typos (#2361)

--- libs/ledger/include/ledger/consensus/stake_manager.hpp
+++ libs/ledger/include/ledger/consensus/stake_manager.hpp
@@ -164,7 +164,7 @@ public:
   template <typename Constructor>
   static void Serialize(Constructor &map_constructor, Type const &stake_manager)
   {
-    auto map = map_constructor(5);
+    auto map = map_constructor(4);
     map.Append(UPDATE_QUEUE, stake_manager.update_queue_);
     map.Append(STAKE_HISTORY, stake_manager.stake_history_);
     map.Append(CURRENT_SNAPSHOT, stake_manager.current_);
@@ -182,5 +182,4 @@ public:
 };
 
 }  // namespace serializers
-
 }  // namespace fetch
