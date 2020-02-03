554ec03b4f0bfbbc2690ee55d9cf65a6bbc1be08
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2020-02-03 13:44:10 +0200

    Added backward charge estimation to losses (#2365)

--- libs/ml/src/ops/loss_functions/softmax_cross_entropy_loss.cpp
+++ libs/ml/src/ops/loss_functions/softmax_cross_entropy_loss.cpp
@@ -95,9 +95,19 @@ std::vector<math::SizeType> SoftmaxCrossEntropyLoss<TensorType>::ComputeOutputSh
 template <typename TensorType>
 OperationsCount SoftmaxCrossEntropyLoss<TensorType>::ChargeForward()
 {
-  assert(!this->batch_output_shape_.empty());
+  assert(!this->batch_input_shapes_.empty());
   OperationsCount cost = fetch::ml::charge_estimation::ops::SOFTMAX_CROSS_ENTROPY_PER_ELEMENT *
                          this->TotalElementsIn({this->batch_input_shapes_});
+  return cost;
+}
+
+template <typename TensorType>
+OperationsCount SoftmaxCrossEntropyLoss<TensorType>::ChargeBackward()
+{
+  assert(!this->batch_input_shapes_.empty());
+  OperationsCount cost =
+      fetch::ml::charge_estimation::ops::SOFTMAX_CROSS_ENTROPY_BACKWARD_PER_ELEMENT *
+      this->TotalElementsIn({this->batch_input_shapes_.at(0)});
   return cost;
 }
 
