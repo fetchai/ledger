cf6087ff39b1fcf9c09fc6de69650e6fdd35aa8e
Wojciech Kaluza <w-kaluza@tlen.pl>
2020-02-11 14:37:02 +0000

    Include monitoring

--- scripts/apply_style.py
+++ scripts/apply_style.py
@@ -77,8 +77,7 @@ def find_excluded_dirs():
     exclusions = (
         ('vendor'),
         ('libs', 'oef-base', 'include', 'oef-base', 'comms'),
-        ('libs', 'oef-base', 'include', 'oef-base', 'conversation'),
-        ('libs', 'oef-base', 'include', 'oef-base', 'monitoring'))
+        ('libs', 'oef-base', 'include', 'oef-base', 'conversation'))
     directories_to_exclude = [abspath(join(PROJECT_ROOT, *path))
                               for path in exclusions]
 
