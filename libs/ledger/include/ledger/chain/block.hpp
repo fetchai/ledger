ea940dea2aa9818e47e5acb12288d39822fd49a8
nhutton <nathan.hutton@fetch.ai>
2020-02-12 17:03:01 +0000

    fix style.

--- libs/ledger/include/ledger/chain/block.hpp
+++ libs/ledger/include/ledger/chain/block.hpp
@@ -91,8 +91,8 @@ public:
   bool        IsValid() const;
 
 private:
-  static constexpr char const *LOGGING_NAME    = "Block";
-  SystemClock clock_ = moment::GetClock("block:body", moment::ClockType::SYSTEM);
+  static constexpr char const *LOGGING_NAME = "Block";
+  SystemClock                  clock_ = moment::GetClock("block:body", moment::ClockType::SYSTEM);
 };
 
 }  // namespace ledger
