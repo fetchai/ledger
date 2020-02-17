09e31e40e0781c7f4500e8f2db4eddd9a24542ad
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2020-02-17 18:07:53 +0000

    Add helper python script for generating hash like sets

---
+++ scripts/bloom-filter/generate-hashes.py
@@ -0,0 +1,23 @@
+import os
+import tqdm
+
+
+TOTAL_HASHES = 100 * int(1e6)
+
+hash_check = set()
+hashes = []
+
+with tqdm.tqdm(total=TOTAL_HASHES) as pbar:
+	while len(hashes) < TOTAL_HASHES:
+		digest = os.urandom(32)
+		if digest in hash_check:
+			continue
+
+		hash_check.add(digest)
+		hashes.append(digest)
+		pbar.update()
+
+
+with open('output.bin', 'wb') as output_file:
+	for raw_hash in hashes:
+		output_file.write(raw_hash)
