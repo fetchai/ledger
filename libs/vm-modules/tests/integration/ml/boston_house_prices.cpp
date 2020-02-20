b6850c3a750e3c6aa55bde3ca109fbecd3fc351a
baykaner <khan.baykaner@fetch.ai>
2020-02-20 14:44:56 +0000

    reworked vm-modules ml boston housing example to re-use etch script generator

--- libs/vm-modules/tests/integration/ml/boston_house_prices.cpp
+++ libs/vm-modules/tests/integration/ml/boston_house_prices.cpp
@@ -18,6 +18,7 @@
 
 #include "vm_modules/math/tensor/tensor.hpp"
 #include "vm_modules/math/type.hpp"
+#include "vm_modules/scripts/ml/boston_house_prices.hpp"
 #include "vm_modules/test_utilities/vm_test_toolkit.hpp"
 
 #include "gmock/gmock.h"
@@ -29,33 +30,6 @@ using namespace fetch::vm;
 namespace {
 
 using DataType = fetch::vm_modules::math::DataType;
-
-std::string const BOSTON_HOUSING_SOURCE = R"(
-    function main()
-
-      // read in training and test data
-      var data = Tensor('0.00632,18.0;2.31,0.0;0.538,6.575;65.2,4.09;1.0,296.0;15.3,396.9;4.98,0.00632;18.0,2.31;0.0,0.538;6.575,65.2;4.09,1.0;296.0,15.3;396.9,4.98;');
-      var label = Tensor('24.0,24.0;');
-
-      // set up a model architecture
-      var model = Model("sequential");
-      model.add("dense", 13u64, 10u64, "relu");
-      model.add("dense", 10u64, 10u64, "relu");
-      model.add("dense", 10u64, 1u64);
-      model.compile("mse", "adam");
-
-      // train the model
-      var batch_size = 2u64;
-      model.fit(data, label, batch_size);
-
-      // evaluate performance
-      var loss = model.evaluate();
-
-      // make predictions on data
-      var predictions = model.predict(data);
-
-    endfunction
-  )";
 
 constexpr bool IGNORE_CHARGE_ESTIMATION = true;
 
@@ -70,7 +44,8 @@ public:
 
 TEST_F(VMBostonTests, model_add_dense_noact)
 {
-  std::string const src = BOSTON_HOUSING_SOURCE;
+  std::string const src = vm_modules::scripts::ml::BostonHousingScript();
+
   ASSERT_TRUE(toolkit.Compile(src));
   if (IGNORE_CHARGE_ESTIMATION)
   {
