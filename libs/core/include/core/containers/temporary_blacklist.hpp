4e12a1f39caa82b6497f624225912a898c289b7e
denis-trapeznikov <denis.trapeznikov@fetch.ai>
2020-02-26 18:52:03 +0000

    Temporary blacklist structure.

---
+++ libs/core/include/core/containers/temporary_blacklist.hpp
@@ -0,0 +1,97 @@
+#pragma once
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2020 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "core/containers/is_in.hpp"
+#include "core/mutex.hpp"
+
+#include <chrono>
+#include <map>
+#include <unordered_set>
+
+namespace fetch {
+namespace core {
+
+template<class T, std::size_t cooldown_ms = 5000>
+class TemporaryBlacklist
+{
+public:
+	using type = T;
+
+	TemporaryBlacklist() = default;
+	TemporaryBlacklist(TemporaryBlacklist const &) = delete;
+	TemporaryBlacklist(TemporaryBlacklist &&) = delete;
+
+	TemporaryBlacklist &operator=(TemporaryBlacklist const &) = delete;
+	TemporaryBlacklist &operator=(TemporaryBlacklist &&) = delete;
+
+	void Blacklist(T t)
+	{
+		FETCH_LOCK(lock_);
+		auto now = Clock::now();
+		Cleanup(now);
+		chronology_.emplace(now, t);
+		blacklisted_.insert(std::move(t));
+	}
+
+	bool IsBlacklisted(T const &t) const
+	{
+		FETCH_LOCK(lock_);
+		Cleanup(Clock::now());
+		return IsIn(blacklisted_, t);
+	}
+
+	std::size_t size() const
+	{
+		FETCH_LOCK(lock_);
+		Cleanup(Clock::now());
+		return blacklisted_.size();
+	}
+private:
+	using Clock = std::chrono::steady_clock;
+	using TimePoint = Clock::time_point;
+	using Duration = Clock::duration;
+
+	using Chronology = std::map<TimePoint, type>;
+	using Blacklisted = std::unordered_set<type>;
+
+	static constexpr Duration COOLDOWN_PERIOD = std::chrono::milliseconds(cooldown_ms);
+
+	void Cleanup(TimePoint t) const
+	{
+		const_cast<TemporaryBlacklist *>(this)->Cleanup(t);
+	}
+
+	void Cleanup(TimePoint t)
+	{
+		t -= COOLDOWN_PERIOD;
+		auto earliest_surviving_record = chronology_.upper_bound(t);
+		for (auto chrono_it = chronology_.begin(); chrono_it != earliest_surviving_record; ++chrono_it)
+		{
+			blacklisted_.erase(chrono_it->second);
+		}
+		chronology_.erase(chronology_.begin(), earliest_surviving_record);
+	}
+
+	Chronology chronology_;
+	Blacklisted blacklisted_;
+	mutable Mutex lock_;
+};
+
+}  // namespace core
+}  // namespace fetch
---
+++ libs/core/tests/unit/temporary_blacklist_tests.cpp
@@ -0,0 +1,57 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2020 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "core/containers/temporary_blacklist.hpp"
+
+#include "gtest/gtest.h"
+
+#include <chrono>
+#include <thread>
+
+namespace {
+
+using namespace std::chrono_literals;
+
+using TestedClass = fetch::core::TemporaryBlacklist<int, 100>;
+
+TEST(TemporaryBlacklistTests, Main)
+{
+  TestedClass blacklist;
+
+  blacklist.Blacklist(10);
+  std::this_thread::sleep_for(20ms);
+  blacklist.Blacklist(42);
+
+  ASSERT_TRUE(blacklist.IsBlacklisted(10));
+  ASSERT_TRUE(blacklist.IsBlacklisted(42));
+  ASSERT_EQ(blacklist.size(), 2);
+
+  std::this_thread::sleep_for(90ms);
+
+  ASSERT_FALSE(blacklist.IsBlacklisted(10));
+  ASSERT_TRUE(blacklist.IsBlacklisted(42));
+  ASSERT_EQ(blacklist.size(), 1);
+
+  std::this_thread::sleep_for(20ms);
+
+  ASSERT_FALSE(blacklist.IsBlacklisted(10));
+  ASSERT_FALSE(blacklist.IsBlacklisted(42));
+  ASSERT_EQ(blacklist.size(), 0);
+}
+
+}  // namespace
