5774d4cf4ba0e1bb07eb6f1191564695b458aeec
denis-trapeznikov <denis.trapeznikov@fetch.ai>
2020-03-04 12:47:37 +0000

    0_o

---
+++ libs/chain/benchmark/tx_generation.hpp
@@ -0,0 +1,79 @@
+#pragma once
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2020 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "chain/transaction.hpp"
+#include "chain/transaction_builder.hpp"
+#include "core/random/lcg.hpp"
+#include "crypto/ecdsa.hpp"
+#include "meta/type_traits.hpp"
+
+#include <cstdint>
+
+using fetch::byte_array::ByteArray;
+using fetch::chain::Transaction;
+using fetch::chain::TransactionBuilder;
+using fetch::crypto::ECDSASigner;
+using TransactionList = std::vector<Transaction>;
+
+template <typename Word = uint64_t>
+fetch::meta::IfIsUnsignedInteger<Word, ByteArray> GenerateRandomArray(
+    std::size_t num_of_words, fetch::random::LinearCongruentialGenerator rng)
+{
+  ByteArray array(sizeof(Word) * num_of_words);
+  auto      raw_array = reinterpret_cast<Word *>(array.pointer());
+
+  for (std::size_t i = 0; i < num_of_words; ++i)
+  {
+    raw_array[i] = rng();
+  }
+  return array;
+}
+
+inline TransactionList GenerateTransactions(std::size_t count, ECDSASigner const &signer,
+                                            bool large_packets = false)
+{
+  using fetch::BitVector;
+  using fetch::chain::Address;
+
+  using fetch::random::LinearCongruentialGenerator;
+
+  using Word                                    = uint64_t;
+  static constexpr std::size_t TX_SIZE_IN_WORDS = 256ull;
+
+  static fetch::random::LinearCongruentialGenerator rng;
+
+  TransactionList list;
+  list.reserve(count);
+
+  for (std::size_t i = 0; i < count; ++i)
+  {
+    auto tx = TransactionBuilder()
+                  .From(Address{signer.identity()})
+                  .TargetChainCode("fetch.token", BitVector{})
+                  .Data(GenerateRandomArray<Word>(large_packets ? TX_SIZE_IN_WORDS : 1ull, rng))
+                  .Signer(signer.identity())
+                  .Seal()
+                  .Sign(signer)
+                  .Build();
+
+    list.emplace_back(std::move(*tx));
+  }
+
+  return list;
+}
---
+++ libs/chain/benchmark/tx_serialisation.cpp
@@ -0,0 +1,102 @@
+//------------------------------------------------------------------------------
+//
+//   Copyright 2018-2020 Fetch.AI Limited
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//------------------------------------------------------------------------------
+
+#include "tx_generation.hpp"
+
+#include "chain/transaction_serializer.hpp"
+#include "core/byte_array/byte_array.hpp"
+#include "core/byte_array/const_byte_array.hpp"
+#include "core/random/lcg.hpp"
+#include "crypto/ecdsa.hpp"
+#include "ledger/storage_unit/lane_service.hpp"
+
+#include "benchmark/benchmark.h"
+
+#include <vector>
+
+using fetch::crypto::ECDSASigner;
+using fetch::chain::TransactionSerializer;
+using Storage = std::vector<fetch::core::byte_array::ConstByteArray>;
+
+void TxSerialisation(benchmark::State &state)
+{
+  ECDSASigner const signer;
+
+  // create the store
+
+  bool small_tx = state.range(0) == 0;
+
+  std::string number_of_tx;
+  {
+    std::ostringstream oss;
+    oss << std::setprecision(3) << std::scientific << float(state.range(1)) << " Tx,";
+    number_of_tx = oss.str();
+  }
+
+  if (small_tx)
+  {
+    number_of_tx += " small_tx";
+    state.SetLabel(number_of_tx);
+  }
+  else
+  {
+    number_of_tx += " large_tx";
+    state.SetLabel(number_of_tx);
+  }
+
+  for (auto _ : state)
+  {
+    // create X new unique transactions to write per test
+    state.PauseTiming();
+
+    TransactionList input =
+        GenerateTransactions(std::size_t(state.range(1)), signer, small_tx);
+
+    const auto num_txs = input.size();
+    Transactions output(num_txs);
+    Storage cells(num_txs);
+    TransactionSerializer sr;
+    std::size_t in_errors{}, out_errros{}, mismatches{};
+
+    state.ResumeTiming();
+    for (std::size_t i{}; i < num_txs; ++i)
+    {
+      if(sr.Serialize(input[i]))
+      {
+	      cells[i] = sr.data();
+      }
+      else
+      {
+	      ++in_errors;
+      }
+    }
+    for (std::size_t i{}; i < num_txs; ++i)
+    {
+      if (!cells[i].empty())
+      {
+	  if (!sr.Deserialize(output[i]))
+	  {
+		  ++out_errors;
+	  }
+      }
+    }
+    // TODO (nobody): we've got a good randomized test to check that transactions match
+  }
+}
+
+BENCHMARK(TxSerialisation)->Ranges({{0, 1}, {1, 1000000}});
