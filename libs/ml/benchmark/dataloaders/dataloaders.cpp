95d2bbe6887451ef983a140af7fe608fd718d57b
jiri <jiri.vestfal@fetch.ai>
2020-03-05 14:57:03 +0000

    Merges

--- libs/ml/benchmark/dataloaders/dataloaders.cpp
+++ libs/ml/benchmark/dataloaders/dataloaders.cpp
@@ -84,6 +84,9 @@ static void TensorDataLoaderAddDataArguments(benchmark::internal::Benchmark *b)
   }
 }
 
+BENCHMARK_TEMPLATE(BM_TensorDataLoader_AddData, fetch::fixed_point::fp64_t)
+        ->Apply(TensorDataLoaderAddDataArguments)
+        ->Unit(::benchmark::kNanosecond);
 BENCHMARK_TEMPLATE(BM_TensorDataLoader_AddData, float)
     ->Apply(TensorDataLoaderAddDataArguments)
     ->Unit(::benchmark::kNanosecond);
@@ -91,9 +94,6 @@ BENCHMARK_TEMPLATE(BM_TensorDataLoader_AddData, double)
     ->Apply(TensorDataLoaderAddDataArguments)
     ->Unit(::benchmark::kNanosecond);
 BENCHMARK_TEMPLATE(BM_TensorDataLoader_AddData, fetch::fixed_point::fp32_t)
-    ->Apply(TensorDataLoaderAddDataArguments)
-    ->Unit(::benchmark::kNanosecond);
-BENCHMARK_TEMPLATE(BM_TensorDataLoader_AddData, fetch::fixed_point::fp64_t)
     ->Apply(TensorDataLoaderAddDataArguments)
     ->Unit(::benchmark::kNanosecond);
 BENCHMARK_TEMPLATE(BM_TensorDataLoader_AddData, fetch::fixed_point::fp128_t)
