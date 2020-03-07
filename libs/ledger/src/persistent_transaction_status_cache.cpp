7f8da47b030c79bbb52a00f19b7fe13edab21ce7
denis-trapeznikov <denis.trapeznikov@fetch.ai>
2020-03-07 02:21:36 +0000

    Current state.

--- libs/ledger/src/persistent_transaction_status_cache.cpp
+++ libs/ledger/src/persistent_transaction_status_cache.cpp
@@ -18,7 +18,7 @@
 
 #include "persistent_transaction_status_cache.hpp"
 
-#include "core/serialisers/main_serializer.hpp"
+#include "core/serialisers/main_serialiser.hpp"
 #include "logging/logging.hpp"
 
 namespace fetch {
